8280c7ac92655b8cc628d2ccd0a6dca6bd243e0e
geisbruch <gabrieleisbruch@gmail.com>
2014-12-29 10:45:34 -0300

    Update images

--- definitions/do_deploy.sh
+++ definitions/do_deploy.sh
@@ -2,9 +2,11 @@
 
 P=`dirname $0`
 
-IS_MAIN_REPO= $(echo $DRONE_BUILD_DIR | grep -e "github.com/mercadolibre/melidata-catalog$")
+IS_MAIN_REPO=$(echo $DRONE_BUILD_DIR | grep -e "github.com/mercadolibre/melidata-catalog$")
 if [[ $DRONE_BRANCH == "master" && $DRONE_BUILD_DIR != "" ]]; then
   cd $P
+  echo "Uploading path script: $P"
+  echo "Starting upload"
   ./gradlew uploadCatalog  
 else
   echo "This branch / fork doesn't deploy"
