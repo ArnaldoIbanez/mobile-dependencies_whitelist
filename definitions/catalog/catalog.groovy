0457d902fea89da64c3b53c84766d6a58c336cb5
apetalas <alexispetalas@gmail.com>
2015-01-02 13:00:11 -0300

    seller back, vip description with all info

--- definitions/catalog/catalog.groovy
+++ definitions/catalog/catalog.groovy
@@ -116,6 +116,7 @@ catalog {
 		"/vip/color_and_size"(platform:"/mobile") { }
 
 		"/vip/description"(platform:"/mobile") { }
+		"/vip/description/abort"(platform:"/mobile") { }
 
 		"/vip/back"(platform:"/mobile", type: TrackType.Event) { }
 
