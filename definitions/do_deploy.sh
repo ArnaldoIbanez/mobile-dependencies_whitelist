c594b49e00f0b25b4b03fff44421e63dfe32cef6
Jose Rozanec <jose.rozanec@mercadolibre.com>
2016-03-14 12:14:40 -0300

    Update do_deploy to get build context information.

--- definitions/do_deploy.sh
+++ definitions/do_deploy.sh
@@ -7,6 +7,8 @@ if [[ $DRONE_BRANCH == "master" && $DRONE_BUILD_DIR != "" ]]; then
   cd $P
   echo "Uploading path script: $P"
   echo "Starting upload"
+  echo $(pwd)
+  ls
   ./gradlew uploadCatalog  
 else
   echo "This branch / fork doesn't deploy"
