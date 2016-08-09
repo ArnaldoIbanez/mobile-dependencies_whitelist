08c2e04cdb1dd04f796ed2a37ab977c01fd7995f
Marcos Barreto <marcos.barreto@mercadolibre.com>
2016-08-09 11:42:50 -0300

    remove spaces

--- definitions/src/main/resources/catalog/marketplace.groovy
+++ definitions/src/main/resources/catalog/marketplace.groovy
@@ -1,20 +1,20 @@
-
-import com.ml.melidata.catalog.PropertyType
-import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
+import com.ml.melidata.catalog.PropertyType
+
+import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
 /**
-*
-* The events means actions that happens without launch a View,
-* as example of that we can consider Bookmark an item in a VIP page
-* Every event is an action, so the verbs available are:
-*
-* - back:  the event of back from a page, specially in mobile
-* - abort: the user abort the action (e.g: back pressed before api response)
-* - delete: when something is deleted
-* - apply: when a criteria is applied
-* - post: create a new entity
-*/
+ *
+ * The events means actions that happens without launch a View,
+ * as example of that we can consider Bookmark an item in a VIP page
+ * Every event is an action, so the verbs available are:
+ *
+ * - back:  the event of back from a page, specially in mobile
+ * - abort: the user abort the action (e.g: back pressed before api response)
+ * - delete: when something is deleted
+ * - apply: when a criteria is applied
+ * - post: create a new entity
+ */
 
 tracks {
     def categoryRegex = /(ROOT|[a-zA-Z]{1,3}[0-9]+)/
