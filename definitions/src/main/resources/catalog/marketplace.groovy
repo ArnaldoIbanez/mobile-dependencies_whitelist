d78758394b50e13ec0d417da7faabf8d5c0d2c38
Marcos Barreto <marcos.barreto@mercadolibre.com>
2016-08-10 18:01:17 -0300

    - change catalog for new notification center - use previous schema with modifications

--- definitions/src/main/resources/catalog/marketplace.groovy
+++ definitions/src/main/resources/catalog/marketplace.groovy
@@ -1554,18 +1554,33 @@ tracks {
 
     "/notification_center"(platform: "/mobile", type: TrackType.Event) {}
 
+    "/notification_center/abort"(platform: "/mobile", type: TrackType.Event) {}
+
+    "/notification_center/back"(platform: "/mobile", type: TrackType.Event) {}
+
+    "/notification_center/failure"(platform: "/mobile", type: TrackType.Event) {}
+
     /**
      * NOTIFICATIONS
      * disclaimer: when the action_type is set, the event_type should be always 'open'
      **/
     "/notification"(platform: "/mobile") {
-        news_id(required: false, description: "Identifier of the notification generated")
-        event_type(required: true, values: ["sent", "arrived", "received", "dismiss", "discarded", "open", "auto_dismiss", "shown", "swipe"], description: "Type of notification event")
-        action_type(required: false, values: ["deeplinking", "directions", "favorite", "reply", "ask", "postpone"])
+        event_type(required: true, 
+                values: ["sent", "arrived", "received", "dismiss", "discarded", "open", "auto_dismiss", "shown", 
+                         "swipe", "action_open", "twitter_refresh", "pull_to_refresh"], 
+                description: "Type of notification event")
+        action_type(required: false, 
+                values: ["deeplinking", "directions", "favorite", "reply", "ask", "postpone"])
         deeplink(required: false, description: "The link were the notification should navigate to, if applies")
-        status(required: false, values: ["read", "unread"], description: "The current notification status, used only when tracking from notification center.")
-        notification_style(required: false, description: "The notification style used when displaying the notification to the user.")
         context(required: false, values: ["notification", "notification_center"], description: "Current context of the notification")
+
+        //FOR NOTIFICATIONS - TRAY
+        news_id(required: false, description: "Identifier of the notification generated")
+        notification_style(required: false, description: "The notification style used when displaying the notification to the user.")
+
+        // FOR NOTIFICATION CENTER
+        newsgroup_id(required: false, description: "Identifier of the notification generated")
+        status(required: false, values: ["read", "unread"], description: "*Deprecated*: Just for old NotifCenter.")
     }
     //Tu producto está en camino
     "/notification/shipping_shipped"(platform: "/mobile") {
@@ -1701,155 +1716,6 @@ tracks {
     //Messages
     "/notification/messages_new"(platform: "/mobile") {
     }
-
-    /**
-     * NOTIFICATION CENTER
-     * disclaimer: when the action_type is set, the event_type should be always 'open'
-     **/
-    "/notification_center"(platform: "/mobile") {
-        newsgroup_id(required: false, description: "Identifier of the notification generated")
-        event_type(required: false, values: ["open", "action_open", "swipe"], description: "Type of notification event")
-        action_id(required: false, description: "The id of the action pressed, if applies")
-        deeplink(required: false, description: "The link were the notification should navigate to, if applies")
-    }
-
-    //Tu producto está en camino
-    "/notification_center/shipping_shipped"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order of the bought item which has been shipped")
-        shipment_id(required: true, type: PropertyType.Numeric)
-    }
-    //Retiro en sucursal
-    "/notification_center/shipping_agency_withdrawal"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-        agency_to_agency(required: false, type: PropertyType.Boolean, description: "Indicates if package was sent to an agency in the first place or was shipped there because the user wasnt found in his address")
-    }
-    //Devolución de costo de envío por demora
-    "/notification_center/shipping_delayed_bonus"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-    }
-    //Tienes que despachar (para el vendedor)
-    "/notification_center/shipping_pending"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-    }
-    //Devolución por no entrega, a su dirección de despacho (para el vendedor)
-    "/notification_center/shipping_returning_to_sender"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-    }
-    //Te demoraste en el handling time (para el vendedor)
-    "/notification_center/shipping_delayed_sender"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-    }
-    //Tu paquete está demorado (para el comprador)
-    "/notification_center/shipping_delayed_receiver"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-        delay_reason(required: true, type: PropertyType.String, description: "shipping_time or handling_time")
-    }
-    //Hubo un problema con tu paquete y te vamos a pagar (para el vendedor)
-    "/notification_center/shipping_not_delivered_sender"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-    }
-    //Hubo un problema con el envío (para el comprador)
-    "/notification_center/shipping_not_delivered_receiver"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the product that is available to withdrawal")
-        shipment_id(required: true, type: PropertyType.Numeric)
-    }
-
-    //Seller questions
-    "/notification_center/questions_new"(platform: "/mobile") {
-        question_id(required: true)
-    }
-    //Buyer questions
-    "/notification_center/questions_answered"(platform: "/mobile") {
-        question_id(required: true, type: PropertyType.Numeric)
-    }
-    //New Sale
-    "/notification_center/orders_new"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String)
-    }
-    //MKT Deals DEPRECADO
-    "/notification_center/deals_campaigns"(platform: "/mobile") {
-        deal_id(required: true, description: "Id of the deal related to the mkt notification sent.")
-    }
-
-    //MKT Deals
-    "/notification_center/campaigns_deals"(platform: "/mobile") {
-        campaign_id(required: true, description: "Id of the campaign related to the mkt notification sent.")
-    }
-
-    //Freemium SYI
-    "/notification_center/campaigns_syi_freemium"(platform: "/mobile") {
-        campaign_id(required: true, description: "Id of the campaign related to the SYI Freemium notification sent.")
-    }
-    //Tu cobro fué acreditado
-    "/notification_center/collections_approved"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String)
-    }
-
-    //Dropout de CHO
-    "/notification_center/purchase_pending"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-    }
-    //Loyalty
-    "/notification/reputation_buyer_in"(platform: "/mobile") {}
-
-    //Mediations
-    "/notification_center/mediations_complainant"(platform: "/mobile") {
-        order_id(required: true, type: PropertyType.String, description: "The order related to the claim")
-        claim_id(required: true, type: PropertyType.String)
-    }
-
-    //Moderation
-    "/notification_center/moderation_item_to_patch"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-    }
-
-    "/notification_center/moderation_item_forbidden"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-    }
-
-
-    "/notification_center/moderation_item_warning"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-    }
-
-
-    "/notification_center/moderation_message_banned"(platform: "/mobile") {
-    }
-
-    //Payments
-    "/notification_center/payments_pending_remainder"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-        order_id(required: true, type: PropertyType.Numeric)
-    }
-
-    "/notification_center/payments_approved"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-        order_id(required: true, type: PropertyType.Numeric)
-    }
-
-    "/notification_center/payments_rejected"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-        order_id(required: true, type: PropertyType.Numeric)
-    }
-
-    "/notification_center/orders_cancelled"(platform: "/mobile") {
-        item_id(required: true, type: PropertyType.String)
-    }
-
-    //Messages
-    "/notification_center/messages_new"(platform: "/mobile") {
-    }
-
-    "/notification_center/reputation_buyer_in"(platform: "/mobile") {}
-
-
 
     "/orders"(platform: "/", isAbstract: true) {}
 
