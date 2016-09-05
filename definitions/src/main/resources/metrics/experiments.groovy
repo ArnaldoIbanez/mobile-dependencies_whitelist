0902eae2633a959a02b96589e91498a97dee2e22
Nicolas Martin Ravasi <nicolas.ravasi@mercadolibre.com>
2016-09-05 13:00:29 -0300

    classi metrics

--- definitions/src/main/resources/metrics/experiments.groovy
+++ definitions/src/main/resources/metrics/experiments.groovy
@@ -216,7 +216,7 @@ metrics {
 	}
 
 
-    "seller_called"(description: "track vip call seller as success for classifieds in the new order experiment") {
+    "search/newFiltersAndSortWebMobile.seller_called"(description: "track vip call seller as success for classifieds in the new order experiment") {
 	    startWith {
             experiment("search/newFiltersAndSortWebMobile", "search/newFiltersAndSortWebMobile.classifieds")
         }
