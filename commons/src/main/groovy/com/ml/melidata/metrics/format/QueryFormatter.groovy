c8819ec44ccffae2a4a7241810da21f765eb9849
Jose Martin Rozanec <jose.rozanec@mercadolibre.com>
2016-09-21 17:27:58 -0300

    Update QueryFormatter

--- commons/src/main/groovy/com/ml/melidata/metrics/format/QueryFormatter.groovy
+++ commons/src/main/groovy/com/ml/melidata/metrics/format/QueryFormatter.groovy
@@ -8,7 +8,9 @@ class QueryFormatter {
 
 	def buildJson() {
 		//TODO: in a future we merge sql scripts to this json. Right now we edit just a single file, read it and print
-		println(new File(".").getAbsolutePath())
+		def files = new File("./src/main/resources/data-dependencies-catalog/sql/").listFiles()
+		files.each {file -> println(file.getAbsolutePath())}
+		
 		def jsontxt = new File('./src/main/resources/data-dependencies-catalog/jsonmelidata.json').getText('UTF-8')
 		new groovy.json.JsonSlurper().parseText(jsontxt)
 		return jsontxt
