be661aedae1db4c34035627c0dc78da2436ce511
Jose Martin Rozanec <jose.rozanec@mercadolibre.com>
2016-09-21 17:48:11 -0300

    Update QueryFormatter

--- commons/src/main/groovy/com/ml/melidata/metrics/format/QueryFormatter.groovy
+++ commons/src/main/groovy/com/ml/melidata/metrics/format/QueryFormatter.groovy
@@ -8,9 +8,25 @@ class QueryFormatter {
 
 	def buildJson() {
 		//TODO: in a future we merge sql scripts to this json. Right now we edit just a single file, read it and print
-		def files = new File("./src/main/resources/data-dependencies-catalog/sql/").listFiles()
-		files.each {file -> println(file.getAbsolutePath())}
-		
+		def basedirname = "src/main/resources/data-dependencies-catalog/sql/"
+		def files = [] as Queue
+		files.add(new File(basedirname))
+		def map = [:]
+		while(!files.isEmpty()){
+			def file = files.poll()
+			if(file.isDirectory()){
+				file.listFiles().each {
+					f -> files.add(f)
+				}
+			}else{
+				def abs = file.getAbsolutePath()
+				map[abs.split(basedirname)[1]]=file.text
+			}
+		}
+		map.entrySet().each {e ->
+			println(String.format("%s;%s", e.key, e.value))
+		}
+
 		def jsontxt = new File('./src/main/resources/data-dependencies-catalog/jsonmelidata.json').getText('UTF-8')
 		new groovy.json.JsonSlurper().parseText(jsontxt)
 		return jsontxt
