3a277eb00bbcf0174440d1a1d53ae7a839a7fb8e
Mauricio Jackson <mauricio.jackson@mercadolibre.com>
2016-10-17 15:30:41 -0300

    Add ds in ctr query

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/recommendations-ctr.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/recommendations-ctr.sql
@@ -1,28 +1,30 @@
 SELECT
-   t1.platform AS platform,
-   t1.site_id AS site_id,
-   t1.backend AS backend,
-   t1.client AS client,
-   t1.prints AS prints,
-   IF(t2.clicks IS NULL, 0, t2.clicks) AS clicks
+  t1.ds as ds,
+  t1.platform AS platform,
+    t1.site_id AS site_id,
+    t1.backend AS backend,
+    t1.client AS client,
+    t1.prints AS prints,
+    IF(t2.clicks IS NULL, 0, t2.clicks) AS clicks
 FROM
 (
-  SELECT
-       device.platform AS platform,
-       application.site_id,
-       get_json_object(recommendations,'$.algorithm') AS backend,
-       get_json_object(recommendations,'$.context') AS client,
-       COUNT(distinct(id)) AS prints
-   FROM tracks
-   LATERAL VIEW json_tuple(event_data,'recommendations') v1 AS recommendations
-   WHERE
+    SELECT
+       substr(ds, 1, 10) ds,
+          device.platform AS platform,
+        application.site_id,
+        get_json_object(recommendations,'$.algorithm') AS backend,
+        get_json_object(recommendations,'$.context') AS client,
+        COUNT(distinct(id)) AS prints
+    FROM tracks
+    LATERAL VIEW json_tuple(event_data,'recommendations') v1 AS recommendations
+    WHERE
         recommendations IS NOT NULL
-        AND CAST(get_json_object(
-             event_data,'$.recommendations.success_print') AS varchar(50))= 'true'
+        AND cast(get_json_object(recommendations,'$.success_print') as varchar(50)) = 'true'
         AND get_json_object(recommendations,'$.algorithm') IS NOT NULL
         AND get_json_object(recommendations,'$.context') IS NOT NULL
         AND ds >= '@param01 06' AND ds < '@param02 06'
     GROUP BY
+      substr(ds, 1, 10),
       device.platform,
         application.site_id,
         get_json_object(recommendations,'$.algorithm'),
@@ -31,11 +33,12 @@ FROM
 LEFT JOIN
 (
     SELECT
-        device.platform AS platform,
+      substr(ds, 1, 10) ds,
+      device.platform AS platform,
         application.site_id AS site_id,
         reco_backend AS backend,
         reco_client AS client,
-        COUNT(distinct usr.uid, item_id) AS clicks
+        COUNT(DISTINCT usr.uid, item_id) AS clicks
     FROM tracks
     LATERAL VIEW json_tuple(others['fragment'],'reco_client') v1 AS reco_client
     LATERAL VIEW json_tuple(others['fragment'],'reco_backend') v2 AS reco_backend
@@ -45,13 +48,15 @@ LEFT JOIN
         AND reco_client IS NOT NULL
         AND ds >= '@param01 06' AND ds < '@param02 06'
     GROUP BY
+      substr(ds, 1, 10),
         device.platform,
         application.site_id,
         reco_backend,
         reco_client
 ) t2
 ON
- t1.platform = t2.platform
- AND t1.site_id = t2.site_id
- AND t1.backend = t2.backend
- AND t1.client = t2.client
+  t1.ds = t2.ds
+  AND t1.platform = t2.platform
+  AND t1.site_id = t2.site_id
+  AND t1.backend = t2.backend
+  AND t1.client = t2.client
