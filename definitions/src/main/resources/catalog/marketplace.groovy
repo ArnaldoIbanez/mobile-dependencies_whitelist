a7b555d668670e7eaf996b40aa703acb1967232f
Nicolas <niguibru@users.noreply.github.com>
2016-10-26 15:32:43 -0300

    Add store type track

--- definitions/src/main/resources/catalog/marketplace.groovy
+++ definitions/src/main/resources/catalog/marketplace.groovy
@@ -316,6 +316,7 @@ tracks {
         local_pick_up(deprecated: true, required: false)
         promoted_items_clicked(required: false, descripcion: 'indicates whether clicked promoted items before reaching this vip')
         billboard_clicked_position(required:false, type: PropertyType.Numeric)
+        store_type(required: false, description: "Indicates store type")
     }
 
     "/vip"(platform: "/web") {
