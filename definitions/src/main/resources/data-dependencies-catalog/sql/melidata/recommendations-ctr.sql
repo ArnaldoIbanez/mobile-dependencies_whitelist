0267fe3c6b708f411e81c17d6d6ec038986a9c63
Mauricio Jackson <mauricio.jackson@mercadolibre.com>
2017-01-05 15:13:51 -0300

    fix ds recurring queries

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/recommendations-ctr.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/recommendations-ctr.sql
@@ -15,7 +15,7 @@ FROM(SELECT
          COUNT(distinct(id)) AS prints
 FROM(SELECT
         id,
-        substr(ds,1,10) AS ds,
+        '@param01' AS ds,
         platform_level(device.platform,2) AS platform,
         application.site_id AS site_id,
         IF(v2.algorithm IS NULL, v2.backend_id, v2.algorithm) AS backend,
@@ -39,7 +39,7 @@ GROUP BY a.ds,
 LEFT JOIN
 (
     SELECT
-        substr(ds,1,10) AS ds,
+        '@param01' AS ds,
         platform_level(device.platform,2) AS platform,
         application.site_id AS site_id,
         reco_backend AS backend,
