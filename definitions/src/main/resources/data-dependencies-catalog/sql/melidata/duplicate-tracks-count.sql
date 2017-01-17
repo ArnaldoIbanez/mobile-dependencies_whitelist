a68676be010641d2b1867537615603887c98c1a7
juanmlambre <juan.lambre@mercadolibre.com>
2017-01-17 11:09:15 -0300

    Fix duplicate_tracks_count query

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/duplicate-tracks-count.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/duplicate-tracks-count.sql
@@ -2,7 +2,7 @@ SELECT 'complete' AS source,
        substr(ds,1,10) AS ds,
        COUNT(id) AS total,
        COUNT(DISTINCT id) AS distinct_total
-FROM tracks_part
+FROM tracks
 WHERE ds >= '@param01'
 AND   ds < '@param02'
 GROUP BY substr(ds,1,10)
