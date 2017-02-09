478763b6d2c48f091cc6cc8e4c6cbf8266112b46
Guillermo Rodriguez <ext_grodrig@mercadolibre.com>
2017-02-09 16:03:24 -0300

    add suggested discount  json and sql files

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/suggested-discount-tracks.sql
@@ -0,0 +1,16 @@
+select substr(ds,1,10) as day,
+tracks.path as path,
+device.platform as platform,
+type as type,
+get_json_object(tracks.event_data,'$.item_id') as item_id,
+get_json_object(tracks.event_data,'$.selected_discount') as selected_discount,
+get_json_object(tracks.event_data,'$.discount_status') as discount_status,
+get_json_object(tracks.event_data,'$.action') as action,
+get_json_object(tracks.event_data,'$.onboarding_step') as onboarding_step,
+count(distinct usr.user_id) as distinct_user_count
+from tracks
+where ds >= '@param01'
+and ds <= '@param02'
+and tracks.path like '/myml/suggested_discounts%'
+group by substr(ds,1,10), device.platform, type, tracks.path, get_json_object(tracks.event_data,'$.event_data')
+order by substr(ds,1,10), device.platform, type, tracks.path, get_json_object(tracks.event_data,'$.event_data');
