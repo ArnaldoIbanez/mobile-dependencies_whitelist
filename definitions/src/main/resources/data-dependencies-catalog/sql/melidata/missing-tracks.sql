31046c058abee66fd98b11e3c9946b0af4dd6d24
Juan Manuel Lambre <juan.lambre@mercadolibre.com>
2017-02-10 17:11:38 -0300

    Add missing-tracks query

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/missing-tracks.sql
@@ -0,0 +1,26 @@
+WITH distinct_raw AS (
+  SELECT DISTINCT jest(data, 'id') AS id, ds
+  FROM raw
+  WHERE ds >= '@min_date' AND ds < '@max_date'  
+)
+
+SELECT raw_complete.ds AS ds,
+  (total_raw.total - raw_complete.processed) AS missing,
+  total_raw.total AS total
+FROM (
+  SELECT COUNT(distinct_raw.id) AS processed, distinct_raw.ds
+  FROM distinct_raw
+  INNER JOIN (
+    SELECT DISTINCT id, ds
+    FROM tracks
+    WHERE ds >= '@min_date' AND ds < '@max_date'
+  ) complete
+  ON distinct_raw.id = complete.id
+  GROUP BY distinct_raw.ds
+) raw_complete
+LEFT JOIN (
+  SELECT ds, count(1) AS total
+  FROM distinct_raw
+  GROUP BY ds
+) total_raw
+ON raw_complete.ds = total_raw.ds
