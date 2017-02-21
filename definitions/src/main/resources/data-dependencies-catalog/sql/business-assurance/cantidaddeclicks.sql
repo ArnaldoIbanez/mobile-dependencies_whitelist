ac1bbee7781f7a95354f439672fe3c24d1c937b0
Daniela Medina <daniela.medina@mercadolibre.com>
2017-02-21 19:12:34 -0300

    corremos nuevamente la historia

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/cantidaddeclicks.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/cantidaddeclicks.sql
@@ -6,8 +6,8 @@ get_json_object(others['fragment'], '$.banner_name') AS banner_name,
 get_json_object(others['fragment'], '$.sellerid') AS SellerID,
 count(1) as total
 from tracks
-where 	   ds >='@param01'
-and 	   ds <'@param02'
+where 	   ds >='2017-01-20'
+and 	   ds <'2017-02-10'
 and others['fragment'] like '%banner_name%'
 group by substr(ds,1,10), 
 application.site_id, 
