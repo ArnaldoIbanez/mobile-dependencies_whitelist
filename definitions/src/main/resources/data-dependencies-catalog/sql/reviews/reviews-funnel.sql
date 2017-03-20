6170e0203973e2cb56bef4245edd86ac63188064
JAVIER PEDRO MARTEGANI <jpedro@uywt09402.local>
2017-03-20 17:54:25 -0300

    se arreglan queries

--- definitions/src/main/resources/data-dependencies-catalog/sql/reviews/reviews-funnel.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/reviews/reviews-funnel.sql
@@ -3,14 +3,22 @@ select substr(ds,1,10) as fecha,
     case when (device.platform like '/email%') then '/email' 
         when (device.platform like '/web/desktop%') then '/web/desktop'
         when (device.platform like '/web/mobile%') then '/web/mobile'
+        when (device.platform like '/mobile/android%') then '/mobile/android'
+        when (device.platform like '/mobile/ios%') then '/mobile/ios'
         else '/undefined'
     end as platform,
-    count_if(path = '/reviews/email') as open_mail,
-    count_if(path = '/reviews/form' and json2txt(jet(event_data, 'flow')) = '"open"') as open_form,
-    count_if(path = '/reviews/congrats') as congrats
+    sum(case when (path = '/reviews/email') then 1 else 0 end) as open_mail,
+    sum(case when (path = '/reviews/form' and get_json_object(tracks.event_data, '$.flow') = 'open') then 1 else 0 end) as open_form,
+    sum(case when (path = '/reviews/congrats') then 1 else 0 end) as congrats
 from tracks
 where ds >= '@param01'
     and ds < '@param02'
     and application.site_id in ('MLA', 'MLB', 'MLC', 'MLM', 'MCO', 'MLV', 'MPE', 'MLU')
     and path in ('/reviews/email','/reviews/form','/reviews/congrats')
-group by 1, 2, 3
+group by substr(ds,1,10), application.site_id, (case when (device.platform like '/email%') then '/email' 
+        when (device.platform like '/web/desktop%') then '/web/desktop'
+        when (device.platform like '/web/mobile%') then '/web/mobile'
+        when (device.platform like '/mobile/android%') then '/mobile/android'
+        when (device.platform like '/mobile/ios%') then '/mobile/ios'
+        else '/undefined'
+    end)
