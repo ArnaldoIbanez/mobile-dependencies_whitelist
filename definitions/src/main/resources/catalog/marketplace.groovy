8b28a5042e0ca637b5a38b26f8ce698c7de2856b
Rodrigo Pintos <rodrigo.pintos@mercadolibre.com>
2017-03-23 08:50:20 -0300

    fix path

--- definitions/src/main/resources/catalog/marketplace.groovy
+++ definitions/src/main/resources/catalog/marketplace.groovy
@@ -1662,7 +1662,7 @@ tracks {
     "/devices_settings"(platform:"/mobile", isAbstract:true) {}
 
     //Bloqueo de notificaciones (Android > = API 19)
-    "/devices_settings/notification"(platform:"/mobile", type:TrackType.Event) {
+    "/devices_settings/notifications"(platform:"/mobile", type:TrackType.Event) {
         enable(required:true, type:PropertyType.Boolean)
         registration_id(required:true, type:PropertyType.String)
         device_id(required:true, type:PropertyType.String)
