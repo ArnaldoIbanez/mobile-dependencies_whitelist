e66b34c6911a1e4d8d14ef18abb914d94bff1031
Pablo Fernandez <pablo.fernandez@mercadolibre.com>
2017-05-26 12:33:32 -0300

    Remove redundant descriptions. Add description with options to checkout off scope

--- definitions/src/main/resources/catalog/checkoutOff.groovy
+++ definitions/src/main/resources/catalog/checkoutOff.groovy
@@ -7,18 +7,18 @@ tracks {
     // Chekout Flow
     "/checkout_off"(platform: "/", isAbstract: true){
         checkout_flow_id(required: true, description: "checkout flow id")
-        collector_id(required: true, description: "collector id")
-        collector_nickname(required: true, description: "collector nickname")
-        scope(required: false, description: "scope")
+        collector_id(required: true)
+        collector_nickname(required: true)
+        scope(required: false, description: "pool that generates the tracks: mla, mlb, mlm, rola, revamp")
     }
 
     "/checkout_off/init"(platform: "/", type: TrackType.View) {}
     "/checkout_off/congrats"(platform: "/", type: TrackType.View) {
-        payment_status(required: true, description: "payment status")
-        payment_id(required: true, description: "payment id")
-        payment_status_detail(required: true, description: "payment status detail")
-        payment_method_id(required: true, description: "payment method id")
-        payment_type_id(required: true, description: "payment type id")
-        is_express(required: true, type: PropertyType.Boolean, description: "is express")
+        payment_status(required: true)
+        payment_id(required: true)
+        payment_status_detail(required: true)
+        payment_method_id(required: true)
+        payment_type_id(required: true)
+        is_express(required: true, type: PropertyType.Boolean)
     }
 }
