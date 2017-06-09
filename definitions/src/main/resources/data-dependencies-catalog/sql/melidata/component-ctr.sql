9cdea9171d65ad5118ef0863f4a6d1f4b812b0f3
Franco Vitali <francovitali@gmail.com>
2017-06-09 09:57:54 -0300

    patched query to account fot 4 hour difference between data sources

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/component-ctr.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/component-ctr.sql
@@ -1,28 +1,28 @@
 SELECT 
-    t1.ds AS `ds`,
+    t1.dsx AS `ds`,
     t1.component AS `component`,
     prints,
     clicks
 
 FROM 
 
-(SELECT ds,
+(SELECT from_unixtime(unix_timestamp(ds, 'yyyy-MM-dd hh') - 14400, 'yyyy-MM-dd hh') AS `dsx`,
     `jt`.`id` AS `component`,
     COUNT(`jt`.`id`) AS `prints`
 FROM component_prints
 LATERAL VIEW json_tuple(`data`, '_event', '_id') jt AS `event`, `id`
-WHERE ds >= '@param01' AND ds < '@param02' 
+WHERE ds >= '@param03 20' AND ds < '@param04 20' 
     AND `jt`.`event` = 'print'
-GROUP BY ds, `jt`.`id`) AS t1
+GROUP BY from_unixtime(unix_timestamp(ds, 'yyyy-MM-dd hh') - 14400, 'yyyy-MM-dd hh'), `jt`.`id`) AS t1
 
-LEFT JOIN
+INNER JOIN
 
 (SELECT ds,
     regexp_extract(`jt`.`id`, '^(\/.*)\/.*$', 1) AS `component`,
     COUNT(`jt`.`id`) AS `clicks`
 FROM tracks
 LATERAL VIEW json_tuple(others['fragment'], '_id') jt AS `id`
-WHERE ds >= '@param01' AND ds < '@param02' 
+WHERE ds>= '@param01' AND ds < '@param02' 
     AND `type` = 'view'
     AND `path` = '/vip'
     AND `jt`.`id` IS NOT NULL
@@ -30,5 +30,5 @@ GROUP BY ds, regexp_extract(`jt`.`id`, '^(\/.*)\/.*$', 1)) AS t2
 
 ON 
 
-    t1.ds = t2.ds AND 
+    t1.dsx = t2.ds AND 
     t1.component = t2.component
