ee46f1572d0705a995180908c9041c7a6131d910
Pablo Lucadei <pablonazareno@gmail.com>
2017-06-12 16:21:07 -0300

    add push notifications presto job

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/notifications/notifications-push-count.sql
@@ -0,0 +1,16 @@
+SELECT
+    count(*) AS total,
+    current_date AS fecha,
+    split(path,'/')[3] AS notification_type,
+    jet(event_data,'event_type') AS event_type,
+    split(device.platform,'/')[3] AS platform,
+    device.os_version,
+    application.site_id,
+    application.business AS marketplace,
+    application.version AS app_version
+FROM tracks
+WHERE
+    path LIKE '/notification/%'
+    AND ds < cast(current_date AS varchar)
+    AND ds > cast((current_date - interval '1' day) AS varchar)
+GROUP BY 3,4,5,6,7,8,9
