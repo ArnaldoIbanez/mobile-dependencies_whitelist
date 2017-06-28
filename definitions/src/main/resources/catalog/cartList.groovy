89a5861b9636f57e28c5ed5fcee19cbc4ed6025a
mciruzzi <martin.ciruzzi@mercadolibre.com>
2017-06-28 13:57:48 -0300

    my cart changes

--- definitions/src/main/resources/catalog/cartList.groovy
+++ definitions/src/main/resources/catalog/cartList.groovy
@@ -7,9 +7,26 @@ import com.ml.melidata.TrackType
 
 tracks {
 
-"/cart"(platform: "/", isAbstract: true) {
+    propertyDefinitions {
+        item(required: true, description: "Item that is being taken in/out of carrito")
+            //id
+            //listing_type
+            //international_delivery_mode
+        currency_id(required:true, description: "Currency_id of the item that is taken in/out of carrito")
+        unit_price(required:true, description: "Unit price of the item added/changed/deleted")
+        quantity(required:true,  description: "Current quantity of the item at carrito")
+        quantity_change(required: false, decription: "Quantity that the item has been increased/decreased")
+    }
 
-	items(required: true, type:PropertyType.ArrayList, description: "Array of items in the cart with following data")
+    propertyGroups {
+        item_info(currency_id, item, unit_price, quantity,quantity_change)
+    }
+
+
+
+    "/cart"(platform: "/", isAbstract: true) {
+
+	items(required: false, type:PropertyType.ArrayList, description: "Array of items in the cart with following data")
     //currency_id
     //item
         //id
@@ -40,11 +57,17 @@ tracks {
 
 "/cart/my_cart"(platform: "/") {}
 
-"/cart/my_cart/save_for_later"(platform: "/", type: TrackType.Event) {}
+"/cart/my_cart/save_for_later"(platform: "/", type: TrackType.Event) {
+    item_info
+}
 
-"/cart/my_cart/delete_item"(platform: "/", type: TrackType.Event) {}
+"/cart/my_cart/delete_item"(platform: "/", type: TrackType.Event) {
+    item_info
+}
 
-"/cart/my_cart/change_quantity"(platform: "/", type: TrackType.Event) {}
+"/cart/my_cart/change_quantity"(platform: "/", type: TrackType.Event) {
+    item_info
+}
 
 "/cart/my_cart/select_address"(platform: "/", type: TrackType.Event) {}
 
@@ -58,9 +81,13 @@ tracks {
 
 "/cart/saved_for_later"(platform: "/") {}
 
-"/cart/saved_for_later/add_to_cart"(platform: "/", type: TrackType.Event) {}
+"/cart/saved_for_later/add_to_cart"(platform: "/", type: TrackType.Event) {
+    item_info
+}
 
-"/cart/saved_for_later/delete_item"(platform: "/", type: TrackType.Event) {}
+"/cart/saved_for_later/delete_item"(platform: "/", type: TrackType.Event) {
+    item_info
+}
 
 "/cart/change_address"(platform: "/") {}
 
