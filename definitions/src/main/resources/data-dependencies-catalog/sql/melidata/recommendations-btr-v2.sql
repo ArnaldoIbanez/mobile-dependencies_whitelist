ef0de3eacfb59e4479ecfc36ad2e40195481ca4e
Juan Manuel Lambre <juan.lambre@mercadolibre.com>
2017-07-04 16:57:39 -0300

    Add v2 queries for recommendations

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/recommendations-btr-v2.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/recommendations-btr-v2.sql
@@ -1,4 +1,45 @@
 SELECT
+ t1.ds AS ds,
+ t1.platform AS platform,
+ t1.site_id AS site_id,
+ t1.backend AS backend,
+ t1.client AS client,
+ t1.prints AS prints,
+ t2.orders AS orders,
+ t2.GMV AS GMV
+FROM(
+   SELECT
+         ds,
+         platform,
+         site_id,
+         backend,
+         client,
+         COUNT(distinct(id)) AS prints
+FROM(SELECT
+        id,
+        substr(ds,1,10) AS ds,
+        platform_level(device.platform,2) AS platform,
+        application.site_id AS site_id,
+        IF(v2.algorithm IS NULL, v2.backend_id, v2.algorithm) AS backend,
+        IF(v2.client IS NULL, v2.context, v2.client) AS client
+    FROM tracks
+    LATERAL VIEW json_tuple(event_data,'recommendations') v1 AS recommendations
+    LATERAL VIEW json_tuple(v1.recommendations,'track_info', 'hidden_by_client', 'backend_id', 'client', 'algorithm', 'context' ) v2 AS track_info, hidden_by_client, backend_id, client, algorithm, context
+    WHERE
+        v2.track_info IS NOT NULL
+        AND cast(get_json_object(v2.track_info,'$.has_recommendations') as varchar(50)) = 'true'
+        AND CAST(v2.hidden_by_client as varchar(50)) = 'false'
+        AND (v2.algorithm is not null or v2.backend_id is not null)
+        AND (v2.context is not null or v2.client is not null)
+        AND ds >= '@param02 02' AND ds < '@param03 02') a
+GROUP BY a.ds,
+         a.platform,
+         a.site_id,
+         a.backend,
+         a.client
+   )t1
+ LEFT JOIN(
+ SELECT
  orders.ds AS ds,
  recommendations.platform AS platform,
  recommendations.site_id AS site_id,
@@ -25,7 +66,7 @@ FROM(
    )recommendations
 INNER JOIN(
        	SELECT
-         	'@param02' AS ds,
+         substr(ds,1,10) AS ds,
          	usr.user_id AS user_id,
          	order_id,
          	CAST(get_json_object(get_json_object(get_json_object(event_data,'$.items[0]'),'$.item'),'$.id')AS varchar(50)) AS item_id,
@@ -48,4 +89,16 @@ GROUP BY
       recommendations.platform,
     	recommendations.site_id,
     	recommendations.client,
-    	recommendations.backend
+    	recommendations.backend) t2
+ON
+  t1.ds = t2.ds
+  AND t1.platform = t2.platform
+  AND t1.site_id = t2.site_id
+  AND t1.backend = t2.backend
+  AND t1.client = t2.client
+
+
+
+
+
+
