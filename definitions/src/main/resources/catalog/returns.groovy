dbd3bee4200d8b54573e0af651abc01da88d6766
alejoecheguia <alejo.echeguia@mercadolibre.com>
2017-07-07 09:49:52 -0300

    Correccion en el nombre del Track
    El correcto es /return/

--- definitions/src/main/resources/catalog/returns.groovy
+++ definitions/src/main/resources/catalog/returns.groovy
@@ -4,28 +4,28 @@ import com.ml.melidata.TrackType
 
 tracks {
 
-    "/returns"(platform: "/", isAbstract: true) {
+    "/return"(platform: "/", isAbstract: true) {
     }
 
-    "/returns/typification"(platform:"/", type: TrackType.View)  {
+    "/return/typification"(platform:"/", type: TrackType.View)  {
     }
 
-    "/returns/money_refund"(platform:"/", type: TrackType.View) {
+    "/return/money_refund"(platform:"/", type: TrackType.View) {
     }
 
-    "/returns/pick_up_option"(platform:"/", type: TrackType.View) {
+    "/return/pick_up_option"(platform:"/", type: TrackType.View) {
     }
 
-    "/returns/collection_step"(platform:"/", type: TrackType.View) {
+    "/return/collection_step"(platform:"/", type: TrackType.View) {
     }
 
-    "/returns/carrier_step"(platform:"/", type: TrackType.View) {
+    "/return/carrier_step"(platform:"/", type: TrackType.View) {
     }
 
-    "/returns/congrats"(platform:"/", type: TrackType.View) {
+    "/return/congrats"(platform:"/", type: TrackType.View) {
     }
 
-    "/returns/congrats/print_label"(platform:"/", type: TrackType.Event) {
+    "/return/congrats/print_label"(platform:"/", type: TrackType.Event) {
         print_url(required:true, type:PropertyType.String)
     }
 }
