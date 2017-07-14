64d12e24ced75e8e0941455f266382737b9b805a
alejoecheguia <alejo.echeguia@mercadolibre.com>
2017-07-14 12:46:20 -0300

    Updated Tracks for Returns
    Se agregaron parametros opcionales a los tracks existentes.
    Nuevo track remedy_label.

--- definitions/src/test/resources/returns.groovy
+++ definitions/src/test/resources/returns.groovy
@@ -11,22 +11,29 @@ trackTests {
       }
 
       "/return/money_refund"(platform:"/") {
+        typification = "Producto defectuoso"
       }
 
       "/return/pick_up_option"(platform:"/") {
+        money_refund = "Tarjeta Credito"
       }
 
-      "/return/collection_step"(platform:"/") {
+      "/return/collection"(platform:"/") {
       }
 
-      "/return/carrier_step"(platform:"/") {
+      "/return/carrier"(platform:"/") {
       }
 
       "/return/congrats"(platform:"/") {
+          pickup_option = "collection"
+          carrier_id = "12331123"
       }
 
       "/return/congrats/print_label"(platform:"/") {
           print_url = "https://www.mercadolibre.com"
+      }
+
+      "/return/congrats/remedy_label"(platform:"/") {
       }
     }
 }
