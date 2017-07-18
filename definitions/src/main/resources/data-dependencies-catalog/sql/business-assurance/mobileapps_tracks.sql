93ba3f2414c6f7c325ff16783b9735d45e1d55f5
daletake <fernando.gardien@mercadolibre.com>
2017-07-18 15:33:30 -0300

    arreglo query

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/mobileapps_tracks.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/mobileapps_tracks.sql
@@ -1,12 +1,9 @@
-select date_utc_4, site_id, path, app_version, business, platform, count (*) as count_
-from
-(
 select substr(ds,1,10) as date_utc_4,
        application.site_id as site_id,
        path as path,
        application.version as app_version,
        application.business as business,
-       device.platform as platform , count (*) as cant
+       device.platform as platform , count (*) as count_
 from tracks
 where type = 'view'
    AND (device.platform like '/mobile/android' or device.platform like '/mobile/ios')
