64336d30f785216bf4c537067a90aae046192b94
daletake <fernando.gardien@mercadolibre.com>
2017-07-18 14:11:20 -0300

    agregado de mobileapps_tracks.sql

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/mobileapps_tracks.sql
@@ -0,0 +1,15 @@
+select date_utc_4, site_id, path, app_version, business, platform, count (*) as count_
+from
+(
+select substr(ds,1,10) as date_utc_4,
+       application.site_id as site_id,
+       path as path,
+       application.version as app_version,
+       application.business as business,
+       device.platform as platform , count (*) as cant
+from tracks
+where type = 'view'
+   AND (device.platform like '/mobile/android' or device.platform like '/mobile/ios')
+   and ds >= '@param01'
+   and ds < '@param02'
+GROUP BY substr(ds,1,10), application.site_id, path, application.version, application.business, device.platform
