93770914857f791ae0c2a7167215c59e9fa18d5e
mciruzzi <martin.ciruzzi@mercadolibre.com>
2017-07-21 18:24:37 -0300

    fix

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/emails-open-rate_unique_opens.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/emails-open-rate_unique_opens.sql
@@ -1,38 +1,34 @@
-SELECT COALESCE(Sent.sent_date, Open.sent_date) AS sent_date, COALESCE(Sent.Template,Open.Template) as template, COALESCE(Sent.Path,'/email/generic') as path,
-COALESCE(Sent.Count_send,CAST(0 AS BIGINT)) as count_send, COALESCE(Open.Count_open,CAST(0 AS BIGINT)) as count_open
+SELECT Sent.sent_date, Sent.Template, Sent.Path,count (Sent.email_id) as count_send, sum(if(Open.email_id IS NOT NULL, 1, 0)) as count_open
 FROM (
-SELECT substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) AS sent_date,
+SELECT distinct substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) AS sent_date,
        get_json_object(tracks.event_data,'$.email_template') AS Template,
        get_json_object(tracks.event_data,'$.event_type') AS Event_Type,
-       tracks.path AS PATH,
-       COUNT( distinct usr.user_id ) AS Count_Send
+       get_json_object(tracks.event_data,'$.email_id') AS email_id,
+       tracks.path AS Path
 FROM tracks
-WHERE (ds >= '@param01'
-        AND ds < '@param02'
-        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) >= '@param01'
-        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) < '@param02'
+WHERE (ds >= '@param1'
+        AND ds < '@param2'
+        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) >= '@param1'
+        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) < '@param2'
         AND get_json_object(tracks.event_data,'$.event_type') = 'send'
         AND path like '/email%'
         )
-GROUP BY substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10),
-         get_json_object(tracks.event_data,'$.email_template'),
-         get_json_object(tracks.event_data,'$.event_type'),
-         path) AS Sent
-FULL OUTER JOIN
-(SELECT substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) AS sent_date,
+) AS Sent
+LEFT OUTER JOIN
+(SELECT distinct substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) AS sent_date,
        get_json_object(tracks.event_data,'$.email_template') AS Template,
        get_json_object(tracks.event_data,'$.event_type') AS Event_Type,
-       COUNT( distinct usr.user_id) AS Count_Open
+       get_json_object(tracks.event_data,'$.email_id') AS email_id
 FROM tracks
-WHERE    (ds >= '@param01'
-        AND ds < '@param03'
-        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) >= '@param01'
-        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) < '@param02'
+WHERE    (ds >= '@param1'
+        AND ds < '@param3'
+        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) >= '@param1'
+        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) < '@param2'
         AND get_json_object(tracks.event_data,'$.event_type') = 'open'
         AND path like '/email%'
-         )
-GROUP BY substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10),
-         get_json_object(tracks.event_data,'$.email_template'),
-         get_json_object(tracks.event_data,'$.event_type'))  AS Open
-ON Open.Template = Sent.template AND Sent.sent_date = Open.sent_date
-ORDER BY Template
+         ))  AS Open
+ON Sent.sent_date = Open.sent_date AND Sent.email_id=Open.email_id
+GROUP BY Sent.sent_date,
+         Sent.Template,
+         Sent.Path
+ORDER BY Sent.Template
