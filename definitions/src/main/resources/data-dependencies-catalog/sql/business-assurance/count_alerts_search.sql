623c2eee070ba4339ae426f480a9cf75aa1bf3ca
LBLANC <lucia.blanc@mercadolibre.com>
2017-07-24 12:10:22 -0300

    Count alerts search modified

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/count_alerts_search.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/count_alerts_search.sql
@@ -9,7 +9,7 @@ usr.user_id AS UserId,
 device.platform AS Platform,
 platform.http.http_referer AS Referer,
 platform.http.http_url as Url,
-jest(event_data, 'vertical') AS Vertical
+jest(event_data, 'category_id') AS Vertical
 FROM tracks
 WHERE ds >= '2017-04-21' AND ds <= '@param01'
 AND 
