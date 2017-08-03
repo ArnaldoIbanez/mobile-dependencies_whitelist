3e21fb0fc93790c9d599518baf3eb4a0582a5e88
Franco Vitali <francovitali@gmail.com>
2017-08-03 13:39:13 -0300

    changed click event filters

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/component-ctr.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/component-ctr.sql
@@ -11,7 +11,7 @@ FROM
     COUNT(`jt`.`id`) AS `prints`
 FROM component_prints
 LATERAL VIEW json_tuple(`data`, 'event_data') ed AS `event_data`
-LATERAL VIEW json_tuple(ed.`event_data`, '_event', '_id') jt AS `event`, `id`
+LATERAL VIEW json_tuple(ed.`event_data`, 'c_event', 'c_id') jt AS `event`, `id`
 WHERE ds >= '@param03 20' AND ds < '@param04 20' 
     AND `jt`.`event` = 'print'
 GROUP BY from_unixtime(unix_timestamp(ds, 'yyyy-MM-dd hh') - 14400, 'yyyy-MM-dd hh'), `jt`.`id`) AS t1
@@ -22,10 +22,10 @@ LEFT JOIN
     regexp_extract(`jt`.`id`, '^(\/.*)\/.*$', 1) AS `component`,
     COUNT(`jt`.`id`) AS `clicks`
 FROM tracks
-LATERAL VIEW json_tuple(others['fragment'], '_id') jt AS `id`
+LATERAL VIEW json_tuple(others['fragment'], 'c_event', 'c_id') jt AS `event`, `id`
 WHERE ds>= '@param01' AND ds < '@param02' 
     AND `type` = 'view'
-    AND `path` = '/vip'
+    AND `jt`.`event` = 'click'
     AND `jt`.`id` IS NOT NULL
 GROUP BY ds, regexp_extract(`jt`.`id`, '^(\/.*)\/.*$', 1)) AS t2
 
