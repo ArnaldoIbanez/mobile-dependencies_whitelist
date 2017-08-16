92dcb4140bfbd8a3800945da391b0b32dcc24c5a
Alejandro Rusi <alejandro.rusi@mercadolibre.com>
2017-08-16 15:24:18 -0300

    Split login/form definition into its platform

--- definitions/src/main/resources/catalog/authentication.groovy
+++ definitions/src/main/resources/catalog/authentication.groovy
@@ -29,6 +29,9 @@ tracks {
         has_error(type: PropertyType.Boolean, required: true)
         challenge(type: PropertyType.String, required: false)
         recaptcha(type: PropertyType.Boolean, required: false)
+    }
+
+    "/login/form"(platform: "/web") {
         tx(type: PropertyType.String, required: false)
         variant(type: PropertyType.String, required: false)
         operator_id(required: false)
