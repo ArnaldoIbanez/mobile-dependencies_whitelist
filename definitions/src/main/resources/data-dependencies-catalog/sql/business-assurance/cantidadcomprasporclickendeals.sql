f06d98b6f98cf47fcf3c590932416aaac69b16c9
Daniela Medina <daniela.medina@mercadolibre.com>
2017-08-24 17:21:51 -0300

    Dejamos croneado

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/cantidadcomprasporclickendeals.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/cantidadcomprasporclickendeals.sql
@@ -27,8 +27,8 @@ jest(others['fragment'],'V') AS deal_Position,
 jest(others['fragment'],'T') AS deal_Type,
 usr.uid
 from tracks
-where 	   ds >='2017-08-16'
-and 	   ds < '2017-08-23'
+where ds >= '@param01'
+and ds < '@param02'
 and others['fragment'] like '%DEAL_ID%'
 and jest(others['fragment'], 'DEAL_ID')!= ''
 group by  application.site_id, 
@@ -41,8 +41,8 @@ jest(others['fragment'],'V') ,
 jest(others['fragment'],'T') ,
 usr.uid
 			)deal ON (usr.uid = deal.uid and array_contains(SPLIT(jest(others['fragment'], 'DEAL_ID'),'"'), jest(event_data, 'items[0].item.deal_ids')) )
-where 	   ds >='2017-08-16'
-and 	   ds < '2017-08-23'
+where ds >= '@param01'
+and ds < '@param02'
 and path = '/orders/ordercreated'
 group by substr(ds,1,10),
 application.site_id,
