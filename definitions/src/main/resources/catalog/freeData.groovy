43d9741f3a170bbc30ea595f5962fcfd925b5be3
Valentín Holgado <valentin.holgado@mercadolibre.com>
2017-09-11 17:07:20 -0300

    More tracks

--- definitions/src/main/resources/catalog/freeData.groovy
+++ definitions/src/main/resources/catalog/freeData.groovy
@@ -1,12 +1,28 @@
-import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
+
+import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
 /**
  *  Tracks for the Free Data project where Mercado Libre pays for its users mobile data consumption.
  */
 tracks {
 
-    "/free_data"(platform: "/mobile", type: TrackType.Event) {
-        sponsored_data()
+    "/free_navigation"(platform:"/mobile", type:TrackType.Event) {
+    }
+
+    "/free_navigation/sponsored_data"(platform:"/mobile", type:TrackType.Event) {
+    }
+
+    "/free_navigation/mobile_data_not_sponsored"(platform:"/mobile", type:TrackType.Event) {
+    }
+
+    "/free_navigation/dialog_shown"(platform:"/mobile", type:TrackType.Event) {
+    }
+
+    "/free_navigation/snackbar_shown"(platform:"/mobile", type:TrackType.Event) {
+    }
+
+    "/free_navigation/dialog_dismissed"(platform:"/mobile", type:TrackType.Event) {
+        is_checkbox_checked(required:true)
     }
 }
