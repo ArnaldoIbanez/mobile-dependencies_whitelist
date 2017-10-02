3d1184dd41da7197d1f9b7d802ac602aa99e6f5e
Nacho Alessio <ignacio.alessio@mercadolibre.com>
2017-10-02 16:31:39 -0300

    Returns Analytics refactor

--- definitions/src/test/resources/returns.groovy
+++ definitions/src/test/resources/returns.groovy
@@ -8,42 +8,51 @@ trackTests {
     test("Returns tracks") {
 
       // STEP 01
-      "/return/typifications"(platform:"/")  {
+      "/return/typifications"(platform: "/")  {
+        typification = "broken_item"
       }
 
       // STEP 02
-      "/return/payments"(platform:"/") {
-        typification = "Producto defectuoso"
+      "/return/payments"(platform: "/") {
+        payment = "paymentId"
       }
 
       // STEP 03
-      "/return/shipping"(platform:"/") {
-        payment = "2863383085"
-      }
-
-      // STEP 04.a
-      "/return/pickup"(platform:"/") {
+      "/return/shipping"(platform: "/") {
         shipping = "pickup"
       }
 
+      // STEP 04.a
+      "/return/pickup"(platform: "/") {
+        pickup_time = "pickupTime"
+      }
+
       // STEP 04.b
-      "/return/delivery"(platform:"/") {
-        shipping = "carrier"
+      "/return/delivery"(platform: "/") {
+        carrier_id = "carrierId"
       }
 
       // STEP 05
-      "/return/congrats"(platform:"/") {
-          shipping = "carrier"
-          carrier_id = "12331123"
+      "/return/congrats"(platform: "/") {
+        typification = "broken_item"
+        payment = "paymentId"
+        shipping = "carrier"
+        pickup_time = "pickupTime"
+        carrier_id = "carrierId"
       }
 
       // STEP 05.cta.a
-      "/return/congrats/print_label"(platform:"/") {
-          print_url = "https://www.mercadolibre.com"
+      "/return/congrats/print_label"(platform: "/") {
+        print_url = "https://www.mercadolibre.com"
       }
 
       // STEP 05.cta.b
-      "/return/congrats/remedy_label"(platform:"/") {
+      "/return/congrats/remedy_label"(platform: "/") { }
+
+      // Modal
+      "/return/modal"(platform: "/") {
+        action = "modal_action"
+        id = "modal_id"
       }
     }
 }
