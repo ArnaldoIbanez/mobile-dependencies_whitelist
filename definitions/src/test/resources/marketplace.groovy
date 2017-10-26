ff179a8aaabcbae046471b37bb02250c4f8b2cc0
Kenneth Dan Weinberg Waks <kenneth.weinberg@mercadolibre.com>
2017-10-26 17:23:28 -0300

    Tests for referral program events

--- definitions/src/test/resources/marketplace.groovy
+++ definitions/src/test/resources/marketplace.groovy
@@ -4279,6 +4279,30 @@ trackTests {
     test("Referral Program tracks") {
         "/referral/promoter/main"(platform: "/", type: TrackType.View) {}
 
+        "/referral/promoter/main/social"(platform: "/", type: TrackType.Event) {
+            action = "share_link"
+            label = "email"
+        }
+
+        "/referral/promoter/main/social"(platform: "/", type: TrackType.Event) {
+            action = "share_link"
+            label = "facebook"
+        }
+
+        "/referral/promoter/main/social"(platform: "/", type: TrackType.Event) {
+            action = "share_link"
+            label = "twitter"
+        }
+
+        "/referral/promoter/main/social"(platform: "/", type: TrackType.Event) {
+            action = "share_link"
+            label = "whatsapp"
+        }
+
+        "/referral/promoter/main/social"(platform: "/", type: TrackType.Event) {
+            action = "copy_link"
+        }
+
         "/referral/promoter/error/self_invite"(platform: "/", type: TrackType.View) {}
 
         "/referral/promoter/error/activated_user"(platform: "/", type: TrackType.View) {}
