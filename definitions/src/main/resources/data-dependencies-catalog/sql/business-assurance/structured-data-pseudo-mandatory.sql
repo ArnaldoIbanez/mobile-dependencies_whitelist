6a79594908097669f1c589d968b182c473346001
Sebastián Schmukliar <sebastian.schmukliar@mercadolibre.com>
2017-11-02 17:57:33 -0300

    use jet

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/structured-data-pseudo-mandatory.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/structured-data-pseudo-mandatory.sql
@@ -1,12 +1,12 @@
 SELECT substr(ds,1,10) as ds,
        'LIST' AS source,
-       get_json_object(event_data,'domain_id') AS dominio,
-       get_json_object(event_data,'attribute_id') AS atributo,
-       get_json_object(event_data,'option_selected') AS opcion,
-       get_json_object(event_data,'value') AS valor,
-       get_json_object(event_data,'item_id') AS item,
+       jet(event_data,'domain_id') AS dominio,
+       jet(event_data,'attribute_id') AS atributo,
+       jet(event_data,'option_selected') AS opcion,
+       jet(event_data,'value') AS valor,
        usr.user_id AS seller,
-       get_json_object(event_data,'condition') AS condicion
+       jet(event_data,'item_id') AS item,
+       jet(event_data,'condition') AS condicion
 FROM tracks
 WHERE ds >= '@param01'
 AND   ds < '@param02'
@@ -14,13 +14,13 @@ AND   path = '/sell/list/modal_attributes/submit'
 UNION
 SELECT substr(ds,1,10) as ds,
        'UPDATE' AS source,
-       get_json_object(event_data,'domain_id') AS dominio,
-       get_json_object(event_data,'attribute_id') AS atributo,
-       get_json_object(event_data,'option_selected') AS opcion,
-       get_json_object(event_data,'value') AS valor,
-       get_json_object(event_data,'item_id') AS item,
+       jet(event_data,'domain_id') AS dominio,
+       jet(event_data,'attribute_id') AS atributo,
+       jet(event_data,'option_selected') AS opcion,
+       jet(event_data,'value') AS valor,
        usr.user_id AS seller,
-       get_json_object(event_data,'condition') AS condicion
+       jet(event_data,'item_id') AS item,
+       jet(event_data,'condition') AS condicion
 FROM tracks
 WHERE ds >= '@param01'
 AND   ds < '@param02'
