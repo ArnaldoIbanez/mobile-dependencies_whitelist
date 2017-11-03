4b178bf750fbdb7b48d14ef913087727bce0e283
Sebastián Schmukliar <sebastian.schmukliar@mercadolibre.com>
2017-11-03 10:29:32 -0300

    optimize query, remove union

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/structured-data-pseudo-mandatory.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/structured-data-pseudo-mandatory.sql
@@ -1,5 +1,7 @@
 SELECT substr(ds,1,10) as ds,
-       'LIST' AS source,
+       CASE WHEN path LIKE '%list%' THEN 'LIST'
+            WHEN path LIKE '%update%' THEN 'UPDATE'
+        END AS source,
        jet(event_data,'domain_id') AS dominio,
        jet(event_data,'attribute_id') AS atributo,
        jet(event_data,'option_selected') AS opcion,
@@ -10,19 +12,5 @@ SELECT substr(ds,1,10) as ds,
 FROM tracks
 WHERE ds >= '@param01'
 AND   ds < '@param02'
-AND   path = '/sell/list/modal_attributes/submit'
-UNION
-SELECT substr(ds,1,10) as ds,
-       'UPDATE' AS source,
-       jet(event_data,'domain_id') AS dominio,
-       jet(event_data,'attribute_id') AS atributo,
-       jet(event_data,'option_selected') AS opcion,
-       jet(event_data,'value') AS valor,
-       usr.user_id AS seller,
-       jet(event_data,'item_id') AS item,
-       jet(event_data,'condition') AS condicion
-FROM tracks
-WHERE ds >= '@param01'
-AND   ds < '@param02'
-AND   path = '/sell/update/modal_attributes/submit'
+AND   path IN ('/sell/list/modal_attributes/submit', '/sell/update/modal_attributes/submit')
 ORDER BY source, dominio, atributo
