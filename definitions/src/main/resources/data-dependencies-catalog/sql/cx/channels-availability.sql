f156d1ab1e37ff50165c8b443e15dbf94fbf8b25
Juan Andres Laura <juan.laura@mercadolibre.com>
2017-12-05 18:22:42 -0300

    Update query

--- definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability.sql
@@ -1,15 +1,15 @@
 select  
-      tj.ds as requested_datetime, 
+      from_unixtime(unix_timestamp(regexp_replace(jest(data, 'user_timestamp'), 'T',' ')), 'yyyy-MM-dd HH') as requested_datetime, 
       v2.site_id as site_id , 
-      v3.problem_type as problem_type, 
+      v3.problem_type as problem_type,
       v3.reason as reason, 
       v3.origin as origin, 
       v3.is_available as is_available, 
       v3.segment as segment, 
       v2.business as business, 
       v2.app_id as app_id,
-      v3.queue_id as queue_id, 
-      count(*) as quantity
+      v3.queue_id as queue_id,
+      count(*)
 from cx_help_channels tj
 LATERAL VIEW 
       json_tuple(tj.data,'application', 'event_data') v1 
@@ -21,11 +21,13 @@ LATERAL VIEW
       json_tuple(v1.event_data, 'problem_type', 'reason', 'origin', 'is_available', 'segment', 'queue_id') v3 
       as problem_type, reason, origin, is_available, segment, queue_id
 WHERE
-      tj.ds >= '@param01' AND tj.ds < '@param02'
-      and jest(data, 'path')='/availability/requested'
-      and jest(data, 'event_data.environment') = 'PRODUCTION'
+      tj.ds BETWEEN '@ds_from 02' AND '@ds_to 04' --desde DAY-1 A DAY-0
+      AND from_unixtime(unix_timestamp(regexp_replace(jest(data, 'user_timestamp'), 'T',' ')), 'dd-MM-yyyy HH') >= '@real_date_from 23'  -- DESDE DAY-2
+      AND from_unixtime(unix_timestamp(regexp_replace(jest(data, 'user_timestamp'), 'T',' ')), 'dd-MM-yyyy HH') < '@real_date_to 23' -- HASTA DAY-1
+      AND jest(data, 'path')='/availability/requested'
+      AND jest(data, 'event_data.environment') = 'PRODUCTION'
 GROUP BY
-      tj.ds, 
+      from_unixtime(unix_timestamp(regexp_replace(jest(data, 'user_timestamp'), 'T',' ')), 'yyyy-MM-dd HH'), 
       site_id,
       problem_type,
       reason,
@@ -35,4 +37,3 @@ GROUP BY
       business,
       app_id,
       queue_id
-
