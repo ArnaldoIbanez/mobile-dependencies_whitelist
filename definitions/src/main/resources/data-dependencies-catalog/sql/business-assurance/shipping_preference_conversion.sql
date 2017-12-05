d308e58b23f5fec527f4c2ff1c8cacda46c23865
LBLANC <lucia.blanc@mercadolibre.com>
2017-12-05 15:38:48 -0300

    Daily return-conversion and shipping_preference_conversion

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/shipping_preference_conversion.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/shipping_preference_conversion.sql
@@ -19,8 +19,8 @@ FROM
   WHERE
   path = '/vip'
   AND application.site_id IN ('MCO', 'MLA', 'MLB', 'MLC', 'MLM', 'MLU', 'MLV')
-  AND ds >= '2017-11-16'
-  AND ds < '2017-12-04'
+  AND ds >= '@param01'
+  AND ds < '@param02'
   AND jest(event_data, 'shipping_preference') IS NOT NULL
   GROUP BY jest(event_data, 'shipping_preference'), jest(event_data, 'item_id'), substr(ds,1,10), jest(event_data, 'free_shipping_benefit'), usr.uid, device.platform
 ) AS vips
@@ -33,8 +33,8 @@ INNER JOIN
   WHERE
   path = '/checkout/congrats'
   AND application.site_id IN ('MCO', 'MLA', 'MLB', 'MLC', 'MLM', 'MLU', 'MLV')
-  AND ds >= '2017-11-16'
-  AND ds < '2017-12-04'
+  AND ds >= '@param01'
+  AND ds < '@param02'
   GROUP BY jest(event_data, 'shipping[0].shipping_option.name'), substr(ds,1,10), jest(event_data, 'shipping[0].shipping_option.shipping_method_id'), jest(event_data, 'items[0].item.id'), usr.uid, usr.user_id, device.platform, application.site_id, jest(event_data,'items[0].item.category_path[0]'), jest(event_data,'items[0].item.category_path[1]')
 ) AS congrats
 ON vips.item_id = congrats.item_id and vips.fecha = congrats.fecha and vips.uid = congrats.uid and vips.platform = congrats.platform
