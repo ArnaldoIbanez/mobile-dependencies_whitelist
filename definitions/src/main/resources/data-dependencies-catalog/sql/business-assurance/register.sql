4450ac385adadf807417e69aad174228f54e26b7
Marina Rodriguez <mairodriguez@arcba014922.ml.com>
2017-12-20 15:28:48 -0300

    Historico de Registraciones

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/register.sql
@@ -0,0 +1,21 @@
+SELECT 
+substr (ds,1,10) as ds,
+type as Type, 
+jest(event_data, 'app') as App, 
+jest(event_data, 'source') as Source, 
+jest(event_data, 'item_id') as Item_ID,
+application.version as Version, 
+path as Path,
+application.site_id as Site, 
+application.business AS Business,
+usr.uid AS UserUid,
+usr.user_id AS UserId,
+device.platform AS Platform,
+device.device_name AS Platform_Device,
+device.device_type AS Platform_Device_Type,
+platform.http.http_referer AS Referer,
+platform.http.http_url as Url
+from tracks
+where path like '/register%'
+and ds >= '2017-10-01'
+and ds < '2017-11-01'
