2006aae3b2e7c4176a307e95e69d044d9e5530fa
Pablo Alejandro Torres <alejandro.torres@mercadolibre.com>
2017-12-29 14:45:24 -0300

    add mediations track metrics

---
+++ definitions/src/main/resources/catalog/mediations.groovy
@@ -0,0 +1,45 @@
+import com.ml.melidata.catalog.PropertyType
+import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
+import com.ml.melidata.TrackType
+
+tracks {
+    propertyDefinitions {
+        reason(required: false, type: PropertyType.String)
+    }
+
+    "/claims"(platform: "/", isAbstract: true) { }
+
+    /**
+     * Claim creation steps.
+     */
+    "/claims/create_claim"(platform: "/", isAbstract: true) { }
+    "/claims/create_claim/allow"(platform: "/", type: TrackType.View)  { }
+    "/claims/create_claim/denied"(platform: "/", type: TrackType.View) {
+        reason(description: 'Claim denied reason.')
+    }
+    "/claims/create_claim/creation"(platform: "/", type: TrackType.Event)  {
+        reason(required: true, description: 'The claim creation reason.', values: ['delivered', 'undelivered'])
+    }
+
+    /**
+     * Generic error views group by type.
+     */
+    "/claims/error"(platform: "/", type: TrackType.View) {
+        type(required: false, type: PropertyType.String)
+    }
+
+    /**
+     * Stale flow.
+     */
+    "/claims/stale"(platform: "/", type: TrackType.View) {
+        reason(description: 'The reason that the user gets a stale view.')
+        expected_resolution(required: false, description: 'The complaint expected resolution', values: ['refund', 'product'])
+    }
+
+    /**
+     * Claim detail views and actions.
+     */
+    "/claims/detail"(platform: "/", type: TrackType.View) {
+        status(required: false, type: PropertyType.String, description: 'The current claim status.')
+    }
+}
---
+++ definitions/src/test/resources/mediations.groovy
@@ -0,0 +1,37 @@
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+import com.ml.melidata.TrackType
+
+trackTests {
+    defaultBusiness = "mercadolibre"
+
+    test("Claim creation step") {
+        "/claims"(platform: "/") { }
+        "/claims/create_claim"(platform: "/") { }
+        "/claims/create_claim/allow"(platform: "/", type: TrackType.View)  { }
+        "/claims/create_claim/denied"(platform: "/", type: TrackType.View) {
+            reason = 'payment_not_found'
+        }
+        "/claims/create_claim/creation"(platform: "/", type: TrackType.Event)  {
+            reason = 'delivered'
+        }
+    }
+
+    test("Claim view error") {
+        "/claims/error"(platform: "/", type: TrackType.View) {
+            type = 'claim_not_found'
+        }
+    }
+
+    test("Stale flow") {
+        "/claims/stale"(platform: "/", type: TrackType.View) {
+            reason = 'shipment_on_time'
+            expected_resolution = 'refund'
+        }
+    }
+
+    test("Claim detail") {
+        "/claims/detail"(platform: "/", type: TrackType.View) {
+            status = 'claim_opened'
+        }
+    }
+}
