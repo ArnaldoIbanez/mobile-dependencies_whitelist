d857b1e95107498f8188618ef698b7580298d936
Mauro Reverter <revertermauro@gmail.com>
2018-01-22 12:22:30 -0300

    Optimized checkout off funnels data query

--- definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/checkout-off/checkout-off-funnels-all-platforms.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/checkout-off/checkout-off-funnels-all-platforms.sql
@@ -2,13 +2,13 @@ SELECT tracks.path as event_id, complete_flows.flow, device.platform, applicatio
 FROM tracks
 LEFT JOIN
 (
-  SELECT flow_id, array_join(array_agg(replace(path, '/checkout_off/v1')), ',', '') as flow
+  SELECT flow_id, array_join(array_sort(array_agg(replace(path, '/checkout_off'))), ',', '') as flow
     FROM (
       SELECT path, jest(event_data, 'checkout_flow_id') as flow_id, user_timestamp
       FROM tracks
       WHERE ds>='@param01' and
       ds<'@param02' and
-      path like '/checkout_off/v1%'
+      path like '/checkout_off/v1%' 
     )
   GROUP BY flow_id
 ) complete_flows
@@ -17,3 +17,5 @@ WHERE ds>='@param01' and
 ds<'@param02' and
 path like '/checkout_off/v1%'
 GROUP BY tracks.path, flow, device.platform, application.site_id, application.version, jest(event_data, 'collector_nickname'), jest(event_data, 'environment'), substr(ds, 1, 10)
+HAVING count() >= 20
+ORDER BY ocurrences DESC
