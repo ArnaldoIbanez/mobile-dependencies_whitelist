a41c679df1846d71addec3ec9cdd60af7e752c12
Mauro Reverter <revertermauro@gmail.com>
2018-01-24 18:33:09 -0300

    Fixed funnels query

--- definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/checkout-off/checkout-off-funnels-all-platforms.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/checkout-off/checkout-off-funnels-all-platforms.sql
@@ -1,21 +1,21 @@
-SELECT tracks.path as event_id, complete_flows.flow, device.platform, application.site_id, application.version, jest(event_data, 'collector_nickname') as collector, jest(event_data, 'environment') as environment, substr(ds, 1, 10) as ds_date, count() as ocurrences
+SELECT tracks.path as event_id, complete_flows.flow as flow, device.platform as platform, application.site_id as site_id, application.version as version, jest(event_data, 'collector_nickname') as collector, jest(event_data, 'environment') as environment, substr(ds, 1, 10) as ds_date, count(*) as ocurrences
 FROM tracks
 LEFT JOIN
 (
-  SELECT flow_id, array_join(array_sort(array_agg(replace(path, '/checkout_off'))), ',', '') as flow
-    FROM (
-      SELECT path, jest(event_data, 'checkout_flow_id') as flow_id, user_timestamp
-      FROM tracks
-      WHERE ds>='@param01' and
-      ds<'@param02' and
-      path like '/checkout_off/v1%' 
-    )
+  SELECT flow_id, concat_ws(',', sort_array(collect_list(regexp_replace(path, '/checkout_off/v1', '')))) as flow
+  FROM (
+    SELECT path, jest(event_data, 'checkout_flow_id') as flow_id, user_timestamp
+    FROM tracks
+    WHERE ds>='@param01' and
+    ds<'@param02' and
+    path like '/checkout_off/v1%' 
+  ) as all_tracks
   GROUP BY flow_id
-) complete_flows
+) as complete_flows
 ON jest(event_data, 'checkout_flow_id') = complete_flows.flow_id
 WHERE ds>='@param01' and
 ds<'@param02' and
 path like '/checkout_off/v1%'
-GROUP BY tracks.path, flow, device.platform, application.site_id, application.version, jest(event_data, 'collector_nickname'), jest(event_data, 'environment'), substr(ds, 1, 10)
-HAVING count() >= 20
+GROUP BY tracks.path, complete_flows.flow, device.platform, application.site_id, application.version, jest(event_data, 'collector_nickname'), jest(event_data, 'environment'), substr(ds, 1, 10)
+HAVING count(*) >= 20
 ORDER BY ocurrences DESC
