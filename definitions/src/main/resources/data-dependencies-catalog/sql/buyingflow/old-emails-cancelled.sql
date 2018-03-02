914e440ad151a5dd4453a0eec0dc8ce348cd45db
Emiliano Somoza <emiliano.somoza@mercadolibre.com>
2018-03-02 13:43:04 -0300

    Cambio función para obtener campos del json

--- definitions/src/main/resources/data-dependencies-catalog/sql/buyingflow/old-emails-cancelled.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/buyingflow/old-emails-cancelled.sql
@@ -1,15 +1,15 @@
 SELECT
   substr(ds,1,10) as ds,
   application.site_id as site,
-  json_extract_scalar(event_data,'$.email_template') as email_template,
+  jest(event_data,'email_template') as email_template,
   COUNT(1) as quantity
 FROM 
   tracks 
 WHERE 
-  ds >= '@param01' 
-	AND ds < '@param02'
+  ds >= '@param01'
+  AND ds < '@param02'
   AND path like '%/email/orders%'
-  AND json_extract_scalar(event_data,'$.event_type') = 'cancel_old_emails'  
+  AND jest(event_data,'event_type') = 'cancel_old_emails'  
 GROUP BY substr(ds,1,10),
   application.site_id,
-  json_extract_scalar(event_data,'$.email_template');
+  jest(event_data,'email_template');
