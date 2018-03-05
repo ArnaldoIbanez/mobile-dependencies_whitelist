c59ae282860cf6411c59c07738b55bec9c3d7e95
Carlos Rodrigo <carlos.rodrigo@mercadolibre.com>
2018-03-05 15:26:16 -0300

    Bundles Queries

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/recommendations/bundle-atc-metrics.sql
@@ -0,0 +1,12 @@
+SELECT  application.site_id as site_id,
+        SUM(CASE WHEN usr.user_id = NULL THEN 1 ELSE 0 END) unlogged_users_add_to_cart, 
+        COUNT(1) all_add_to_cart, 
+        AVG(SIZE(SPLIT(recommended_items,','))) as avg_items_added_to_cart 
+FROM TRACKS 
+LATERAL VIEW json_tuple(event_data, 'items') v as recommended_items 
+WHERE path = '/recommendations/add_to_cart' 
+AND JEST(event_data, 'client') = 'vip_combo' 
+AND JEST(event_data,'items') is not NULL 
+AND DS >= '2018-02-27' 
+AND DS < '@param2'
+GROUP BY application.site_id
