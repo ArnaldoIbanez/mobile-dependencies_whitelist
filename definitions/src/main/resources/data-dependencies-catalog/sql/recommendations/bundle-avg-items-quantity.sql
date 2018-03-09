dd97ae969b214d9d1fb00f9f8c6a6d20b7a1ba8e
Carlos Rodrigo <carlos.rodrigo@mercadolibre.com>
2018-03-09 01:09:17 -0300

    change cron time to overwrite the info

--- definitions/src/main/resources/data-dependencies-catalog/sql/recommendations/bundle-avg-items-quantity.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/recommendations/bundle-avg-items-quantity.sql
@@ -5,6 +5,6 @@ FROM tracks
 WHERE path = '/vip'
 AND jest(event_data, 'recommendations.client') = 'vip_combo'
 AND jest(event_data, 'recommendations.track_info.has_recommendations') = 'true'
-AND ds >= '@param01'
+AND ds >= '2018-01-27'
 AND ds < '@param02'
 GROUP BY application.site_id
