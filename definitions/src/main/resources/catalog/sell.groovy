8dff0e4793b9b2974e71ac681b87d330ff86a37e
Ricardo Orozco <32676409+rvoroz@users.noreply.github.com>
2018-03-14 14:25:11 -0300

    New Track For QR Code Printing Intention

--- definitions/src/main/resources/catalog/sell.groovy
+++ definitions/src/main/resources/catalog/sell.groovy
@@ -565,5 +565,12 @@ tracks {
         attribute_id(required: true, description: "Attribute id", type: PropertyType.String)
         option_selected(required: true, description: "Radio option selected", type: PropertyType.String)
     }
+    
+    "/sell/qr/print_intention"(platform: "/web", type: TrackType.View){
+        item_id(required: true)
+        qr_type(required: true)
+        listing_type_id(required: false)
+        vertical(required: false)
+    }
 
 }
