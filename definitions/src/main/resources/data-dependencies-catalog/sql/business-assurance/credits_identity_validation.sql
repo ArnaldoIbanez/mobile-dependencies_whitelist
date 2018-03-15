3293524f0858e7bcfc83f087a26f10083b123d08
Marina Rodriguez <mairodriguez@arcba014922.ml.com>
2018-03-15 09:55:07 -0300

    Correccion de perfomance. Metricas IV para Credits

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_identity_validation.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_identity_validation.sql
@@ -4,7 +4,7 @@ sum(if(S.PATH_1 IS NOT NULL, 1, 0)) as Start_Validation,
 sum(if(F.PATH_2 IS NOT NULL, 1, 0)) as Finish_Validation 
 FROM 
 (SELECT  usr.user_id as USER_1 , substr(ds,1,10)  as DS_1, path AS PATH_1 FROM TRACKS
-WHERE PATH LIKE '/identity_validation/start_validation'
+WHERE PATH = '/identity_validation/start_validation'
 and (ds >= '@param01'
 and ds < '@param02')
 and application.site_id = 'MLA'
@@ -13,7 +13,7 @@ and jest(event_data, 'flow') = 'credits') as S
 LEFT JOIN 
 
 (SELECT  usr.user_id as USER_2, substr(ds,1,10) as DS_2, path AS PATH_2 FROM TRACKS
-WHERE PATH LIKE '/identity_validation/finish_validation'
+WHERE PATH = '/identity_validation/finish_validation'
 and (ds >= '@param01'
         AND ds < '@param03')
 and application.site_id = 'MLA'
