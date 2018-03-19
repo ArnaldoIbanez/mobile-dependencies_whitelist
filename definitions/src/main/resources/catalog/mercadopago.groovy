0c5c9eac1950fa2c0ac2ff80ca99e712ecdc3c6e
José Ramirez <jarcodallo@gmail.com>
2018-03-19 10:55:29 -0300

    Cellphone Recharge Frontend path to underscore

--- definitions/src/main/resources/catalog/mercadopago.groovy
+++ definitions/src/main/resources/catalog/mercadopago.groovy
@@ -293,18 +293,18 @@ tracks {
     }
 
     // Cellphone Recharge Frontend
-    "/cellphone_recharge/suggested-phones"(platform: "/web", type: TrackType.View) {}
-    "/cellphone_recharge/second-password"(platform: "/web", type: TrackType.View) {}
-    "/cellphone_recharge/phone-income"(platform: "/web", type: TrackType.View) {}
+    "/cellphone_recharge/suggested_phones"(platform: "/web", type: TrackType.View) {}
+    "/cellphone_recharge/second_password"(platform: "/web", type: TrackType.View) {}
+    "/cellphone_recharge/phone_income"(platform: "/web", type: TrackType.View) {}
     "/cellphone_recharge/congrats"(platform: "/web", type: TrackType.View) {
         status (required:true, type: PropertyType.String, description: "Payment status (approved, pending, rejected)")
     }
     "/cellphone_recharge/companies"(platform: "/web", type: TrackType.View) {}
     "/cellphone_recharge/checkout"(platform: "/web/desktop", type: TrackType.View) {}
     "/cellphone_recharge/amounts"(platform: "/web", type: TrackType.View) {}
-    "/cellphone_recharge/amount-recommended"(platform: "/web", type: TrackType.View) {}
-    "/cellphone_recharge/alias-income"(platform: "/web/mobile", type: TrackType.View) {}
-    "/cellphone_recharge/account-money"(platform: "/web", type: TrackType.View) {}
+    "/cellphone_recharge/amount_recommended"(platform: "/web", type: TrackType.View) {}
+    "/cellphone_recharge/alias_income"(platform: "/web/mobile", type: TrackType.View) {}
+    "/cellphone_recharge/account_money"(platform: "/web", type: TrackType.View) {}
 
     "/scheduled_recharge"(platform: "/mobile", isAbstract: true) {
         flow (required:true, type: PropertyType.String, description: "Use case that has been executed")
