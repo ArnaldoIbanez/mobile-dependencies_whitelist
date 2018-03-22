02b192c8d09286f021cdd45b947e125f992ac200
Jose Ojeda <jose.ojeda@mercadolibre.com>
2018-03-22 18:00:57 -0300

    add json and query for credits consumer journey open rate

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_consumer_journey.sql
@@ -0,0 +1,48 @@
+SELECT substr(ds,1,10) as fecha, 
+		jest(event_data, 'email_id') as email_id, 
+		path, 
+		jest(event_data, 'email_template') as template, 
+		jest(event_data, 'event_type') as event_type, 
+		jest(event_data, 'track_data.milestone') as milestone, 
+		jest(event_data, 'track_data.bucket') as bucket, 
+		substr(jest(event_data, 'sent_date'), 1, 10) sent_date 
+FROM tracks
+	where type = 'event' 
+	and path = '/email/generic' 
+	and ds >= '@param01' 
+	and jest(event_data, 'email_template') IN ('@param02')
+	and jest(event_data, 'event_type') = 'send'
+	and application.site_id IN ('MLA')
+
+UNION
+
+SELECT substr(ds,1,10) as fecha, 
+		jest(event_data, 'email_id') as email_id, 
+		path, 
+		jest(event_data, 'email_template') as template, 
+		jest(event_data, 'event_type') as event_type, 
+		milestone, 
+		bucket, 
+		substr(jest(event_data, 'sent_date'), 1, 10) sent_date  
+FROM tracks 
+	LEFT JOIN 
+		(SELECT jest(event_data, 'email_id') as email_id, 
+				jest(event_data, 'track_data.milestone') as milestone, 
+				jest(event_data, 'track_data.bucket') as bucket 
+		FROM tracks
+		WHERE 
+				type = 'event' 
+				and path = '/email/generic' 
+				and ds >= '@param1' 
+				and jest(event_data, 'email_template') IN ('@param2') 
+				and jest(event_data, 'event_type') = 'send' 
+				and application.site_id IN ('MLA')) TRACKDATA 
+	ON jest(event_data, 'email_id') = TRACKDATA.email_id
+WHERE 
+		type = 'event' 
+		and path = '/email/generic' 
+		and ds >= '@param1' 
+		and jest(event_data, 'email_template') IN ('@param2') 
+		and jest(event_data, 'event_type') = 'open' 
+		and application.site_id IN ('MLA')
+
