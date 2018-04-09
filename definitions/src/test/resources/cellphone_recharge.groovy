ddfda374a086afbf42c6b55da60b028c487ec228
Ezequiel <ezebongiovi@gmail.com>
2018-04-09 15:27:45 -0300

    Added test for each business

--- definitions/src/test/resources/cellphone_recharge.groovy
+++ definitions/src/test/resources/cellphone_recharge.groovy
@@ -2,9 +2,14 @@ import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 import com.ml.melidata.TrackType;
 
 trackTests {
-    defaultBusiness = "Single Player"
+    defaultBusiness = "mercadopago"
 
     test("Cellphone recharge") {
+        "/cellphone_recharge"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
         "/cellphone_recharge/recents"(platform: "/mobile") {
             flow = "/cellphone_recharge"
             from = "/deep_link"
@@ -35,7 +40,56 @@ trackTests {
             from = "/deep_link"
         }
 
-        "/cellphone_recharge/manual_amount"(platform: "/mobile") {
+        "/cellphone_recharge/carrier_disabled"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/terms_and_conditions"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+    }
+
+    defaultBusiness = "mercadolibre"
+
+    test("Cellphone recharge") {
+        "/cellphone_recharge"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/recents"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/set_telephone_number"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/contacts"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/carriers"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/recommended"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/packages"(platform: "/mobile") {
+            flow = "/cellphone_recharge"
+            from = "/deep_link"
+        }
+
+        "/cellphone_recharge/carrier_disabled"(platform: "/mobile") {
             flow = "/cellphone_recharge"
             from = "/deep_link"
         }
