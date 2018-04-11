1b88a307cf3e4beaaf4b280b3f3ae462a3dd61b8
gmaceira <guido.maceira@mercadolibre.com>
2018-04-11 11:28:54 -0300

    Updated query

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/mobile-mydata-bu-users-by-platform.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/mobile-mydata-bu-users-by-platform.sql
@@ -1,7 +1,9 @@
-SELECT count(DISTINCT usr) AS total,
-       substr(ds,1,10),
-       device.platform AS platform
+SELECT count(DISTINCT usr) as total ,
+       substr(ds,1,10) as ds,
+       device.platform as platform
 FROM tracks
 WHERE path = '/myml/company_profile'
   AND ds >= '@param01'
   AND ds <= '@param02'
+group by substr(ds,1,10), device.platform
+order by substr(ds,1,10)
