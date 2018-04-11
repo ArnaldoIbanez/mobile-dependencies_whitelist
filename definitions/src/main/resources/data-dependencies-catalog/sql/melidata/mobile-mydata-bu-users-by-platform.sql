7cf941bf624f5f539f4e966a7497fc1a65d58840
gmaceira <guido.maceira@mercadolibre.com>
2018-04-11 13:50:38 -0300

    + Site Country JSON

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/mobile-mydata-bu-users-by-platform.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/mobile-mydata-bu-users-by-platform.sql
@@ -1,9 +1,10 @@
 SELECT count(DISTINCT usr) as total ,
        substr(ds,1,10) as ds,
+       application.site_id as country,
        device.platform as platform
 FROM tracks
 WHERE path = '/myml/company_profile'
-  AND ds >= '@param01'
-  AND ds <= '@param02'
-group by substr(ds,1,10), device.platform
+  AND ds >= '2018-04-10'
+  AND ds <= '2018-04-12'
+group by substr(ds,1,10), device.platform, application.site_id
 order by substr(ds,1,10)
