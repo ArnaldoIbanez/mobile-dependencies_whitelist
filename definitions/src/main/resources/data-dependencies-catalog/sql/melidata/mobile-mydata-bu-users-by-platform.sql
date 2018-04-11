8dd58eee783ac04c18288cc0ee6f038c47dbeb1e
gmaceira <guido.maceira@mercadolibre.com>
2018-04-11 13:51:31 -0300

    Fixed param

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/mobile-mydata-bu-users-by-platform.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/mobile-mydata-bu-users-by-platform.sql
@@ -5,6 +5,6 @@ SELECT count(DISTINCT usr) as total ,
 FROM tracks
 WHERE path = '/myml/company_profile'
   AND ds >= '@param01'
-  AND ds <= '@param012'
+  AND ds <= '@param02'
 group by substr(ds,1,10), device.platform, application.site_id
 order by substr(ds,1,10)
