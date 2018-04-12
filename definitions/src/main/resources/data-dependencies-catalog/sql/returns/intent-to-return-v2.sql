339463bc465b1f4365e29f6e2d3aa22fd0be5965
Martin Maldonado <martin.maldonado@mercadolibre.com>
2018-04-12 17:39:12 -0300

    Added more contextual info

--- definitions/src/main/resources/data-dependencies-catalog/sql/returns/intent-to-return-v2.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/returns/intent-to-return-v2.sql
@@ -4,13 +4,21 @@ termino_en_claims,
 otro_problema,
 platform,
 site,
-user_id
+user_id,
+loyalty_level,
+is_cart_order,
+item_category,
+item_category_l1
 FROM (SELECT substr(ds,1,10) AS fecha,
 	COUNT (DISTINCT CASE WHEN (path IN ('/return/external', '/return/external/claims') AND type = 'view') THEN usr.user_id ELSE NULL END) AS termino_en_claims,
 	COUNT (DISTINCT CASE WHEN (path = '/return/typifications' AND (jest(event_data, 'typification') IN ('go_to_claim', 'gtc_other_problem') OR jest(event_data, 'dictionary_data.typification') IN ('go_to_claim', 'gtc_other_problem'))) THEN usr.user_id ELSE NULL END) AS otro_problema,
 	device.platform as platform,
 	application.site_id as site,
-	usr.user_id as user_id
+	usr.user_id as user_id,
+	jest(event_data, 'loyalty_level') as loyalty_level,
+ 	jest(event_data, 'cart_order') as is_cart_order,
+ 	jest(event_data, 'item_category') as item_category,
+ 	jest(event_data, 'item_category_l1') as item_category_l1
 FROM tracks 
 WHERE ds >= '@param01'
 	AND ds < '@param02' 
@@ -20,6 +28,7 @@ WHERE ds >= '@param01'
 	AND usr.user_nick not like 'BUYER%'
 	AND application.site_id IN ('MLM','MLB', 'MLA')
 	AND path IN ('/return/external', '/return/external/claims', '/return/typifications')
+	AND usr.user_id IS NOT NULL
 GROUP BY substr(ds,1,10),
 	device.platform,
 	application.site_id,
