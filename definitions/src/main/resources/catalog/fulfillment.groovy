c45a51ab06781a7aeb6be6ed027e9d54c57df372
Max Draeger <maximiliano.draeger@mercadolibre.com.ar>
2018-04-17 12:24:44 -0300

    Changes based on PR comments and Observations

--- definitions/src/main/resources/catalog/fulfillment.groovy
+++ definitions/src/main/resources/catalog/fulfillment.groovy
@@ -14,13 +14,14 @@ tracks {
     "/fbm/landing"(platform: "/", type: TrackType.View) {}
     "/fbm/landing/optin"(platform: "/", type: TrackType.View) {}
     "/fbm/landing/congrats"(platform: "/", type: TrackType.View) {}
-    
+
     //Contact me Button
     "/fbm/landing/contact_me"(platform: "/", type: TrackType.Event) {
         location(
             required: true,
             type: PropertyType.String,
-            description: "Location of the CTA Button."
+            description: "Location of the CTA Button.",
+            values: ["top", "bottom"]
         )
     }
 
@@ -29,7 +30,8 @@ tracks {
         faq(
             required: true,
             type: PropertyType.String,
-            description: "Which FAQ has been opened."
+            description: "Which FAQ has been opened.",
+            values: ["opt_in", "dc_location", "send_my_inventory", "send_inventory_from_supplier", "fees", "manage_inventory"]
         )
     }
 
