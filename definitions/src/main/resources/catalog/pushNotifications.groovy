ab60c8a5f5c12a63db62a8f628c011f06e27662b
Miguel Lattuada <miguellattuada@outlook.com>
2018-04-17 11:35:57 -0300

    PR review changes

--- definitions/src/main/resources/catalog/pushNotifications.groovy
+++ definitions/src/main/resources/catalog/pushNotifications.groovy
@@ -4,10 +4,11 @@ import com.ml.melidata.TrackType
 
 tracks {
 
-    "/push_checkout_recovery"(platform: "/web/mobile", type: TrackType.View) {
+    "/checkout_recovery"(platform: "/web/mobile", type: TrackType.View) {
+        item_id(required: true, type: PropertyType.String, description: "main item id")
         item_status(required: true, type: PropertyType.String, description: "main item status (no-stock, inative, etc)")
     }
-    "/push_checkout_recovery/error"(platform: "/web/mobile", type: TrackType.View) {}
-    "/push_checkout_recovery/notfound"(platform: "/web/mobile", type: TrackType.View) {}
+    "/checkout_recovery/error"(platform: "/web/mobile", type: TrackType.View) {}
+    "/checkout_recovery/notfound"(platform: "/web/mobile", type: TrackType.View) {}
 
 }
