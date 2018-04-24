75531b87645147d9ac19830dfc63f4d98ad97ea1
Martin Dos Santos <martin.dossantos@mercadolibre.com>
2018-04-24 16:10:33 -0300

    Added meaningful names to paths

--- definitions/src/main/resources/catalog/fiscaldata.groovy
+++ definitions/src/main/resources/catalog/fiscaldata.groovy
@@ -2,19 +2,23 @@ import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
 
 tracks {
-    "/registration_company"(platform: "/", isAbstract: true) {}
-    "/registration_company/form"(platform: "/", type: TrackType.View) {}
-    "/registration_company/congrats"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data"(platform: "/", isAbstract: true) {}
+    "/fiscal_data/companies"(platform: "/", isAbstract: true) {}
+    "/fiscal_data/companies/registration"(platform: "/", isAbstract: true) {}
+    "/fiscal_data/companies/registration/form"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/registration/congrats"(platform: "/", type: TrackType.View) {}
 
-    "/flow_ddff"(platform: "/", isAbstract: true) {}
-    "/flow_ddff/tax_payer"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/iva"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/load_iva"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/iibb"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/load_iibb"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/province_iibb"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/address"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/summary"(platform: "/", type: TrackType.View) {}
-    "/flow_ddff/congrats"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data"(platform: "/", isAbstract: true) {}
+    "/fiscal_data/companies"(platform: "/", isAbstract: true) {}
+    "/fiscal_data/companies/fiscal_data_flow"(platform: "/", isAbstract: true) {}
+    "/fiscal_data/companies/fiscal_data_flow/tax_payer"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/iva"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/load_iva"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/iibb"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/load_iibb"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/province_iibb"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/address"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/summary"(platform: "/", type: TrackType.View) {}
+    "/fiscal_data/companies/fiscal_data_flow/congrats"(platform: "/", type: TrackType.View) {}
 
 }
--- definitions/src/test/resources/fiscaldata.groovy
+++ definitions/src/test/resources/fiscaldata.groovy
@@ -6,37 +6,45 @@ trackTests {
     defaultBusiness = "mercadolibre"
 
     // Company registration
-    test("registration_company") {
-        "/registration_company"(platform: "/") {}
-        "/registration_company/form"(platform: "/", type: TrackType.View) {}
-        "/registration_company/congrats"(platform: "/", type: TrackType.View) {}
+    test("/fiscal_data/companies/registration") {
+        "/fiscal_data"(platform: "/") {}
+        "/fiscal_data/companies"(platform: "/") {}
+        "/fiscal_data/companies/registration"(platform: "/") {}
+        "/fiscal_data/companies/registration/form"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/registration/congrats"(platform: "/", type: TrackType.View) {}
 
-        "/registration_company"(platform: "/", business:"mercadopago") {}
-        "/registration_company/form"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/registration_company/congrats"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data"(platform: "/", business:"mercadopago") {}
+        "/fiscal_data/companies"(platform: "/", business:"mercadopago") {}
+        "/fiscal_data/companies/registration"(platform: "/", business:"mercadopago") {}
+        "/fiscal_data/companies/registration/form"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/registration/congrats"(platform: "/", business:"mercadopago", type: TrackType.View) {}
     }
-    // DDFF = Datos fiscales
-    test("/flow_ddff"){
-        "/flow_ddff"(platform: "/") {}
-        "/flow_ddff/tax_payer"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/iva"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/load_iva"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/iibb"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/load_iibb"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/province_iibb"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/address"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/summary"(platform: "/", type: TrackType.View) {}
-        "/flow_ddff/congrats"(platform: "/", type: TrackType.View) {}
+    // Fiscal data flow
+    test("/fiscal_data/companies/fiscal_data_flow"){
+        "/fiscal_data/"(platform: "/") {}
+        "/fiscal_data/companies"(platform: "/") {}
+        "/fiscal_data/companies/fiscal_data_flow"(platform: "/") {}
+        "/fiscal_data/companies/fiscal_data_flow/tax_payer"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/iva"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/load_iva"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/iibb"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/load_iibb"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/province_iibb"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/address"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/summary"(platform: "/", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/congrats"(platform: "/", type: TrackType.View) {}
 
-        "/flow_ddff"(platform: "/", business:"mercadopago") {}
-        "/flow_ddff/tax_payer"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/iva"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/load_iva"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/iibb"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/load_iibb"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/province_iibb"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/address"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/summary"(platform: "/", business:"mercadopago", type: TrackType.View) {}
-        "/flow_ddff/congrats"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data"(platform: "/", business:"mercadopago") {}
+        "/fiscal_data/companies"(platform: "/", business:"mercadopago") {}
+        "/fiscal_data/companies/fiscal_data_flow"(platform: "/", business:"mercadopago") {}
+        "/fiscal_data/companies/fiscal_data_flow/tax_payer"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/iva"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/load_iva"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/iibb"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/load_iibb"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/province_iibb"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/address"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/summary"(platform: "/", business:"mercadopago", type: TrackType.View) {}
+        "/fiscal_data/companies/fiscal_data_flow/congrats"(platform: "/", business:"mercadopago", type: TrackType.View) {}
     }
 }
