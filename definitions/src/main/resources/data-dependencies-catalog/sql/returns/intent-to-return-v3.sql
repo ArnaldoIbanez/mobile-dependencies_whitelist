69b563d8859c0cb1d104374fb065b7d1c50d52e3
Martin Maldonado <martin.maldonado@mercadolibre.com>
2018-04-26 10:00:53 -0300

    Updated to run in Hive and removed authorized field

--- definitions/src/main/resources/data-dependencies-catalog/sql/returns/intent-to-return-v3.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/returns/intent-to-return-v3.sql
@@ -4,7 +4,6 @@ SELECT * from (
       CASE WHEN (substr(device.user_agent, 1, 13) = 'MercadoLibre-') THEN IF((substr(device.user_agent, 14, 3) = 'iOS'), '/mobile/ios', '/mobile/android') ELSE device.platform END AS platform,
       application.site_id as site_id,
       usr.user_id as user_id,
-      jest(event_data, 'returns_authorized') as authorized,
       jest(event_data, 'returns_loyalty_level') as loyalty_level,
       jest(event_data, 'returns_cart_order') as is_cart_order,
       jest(event_data, 'returns_item_category') as item_category,
