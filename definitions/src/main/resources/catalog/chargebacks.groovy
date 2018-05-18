07f6dd9e0d7d77d2d2ade29018db13f1cfea12eb
facevedo-meli <federico.acevedo@mercadolibre.com>
2018-05-18 11:36:14 -0300

    adding chargebacks tracks

---
+++ definitions/src/main/resources/catalog/chargebacks.groovy
@@ -0,0 +1,128 @@
+import com.ml.melidata.catalog.PropertyType
+import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
+import com.ml.melidata.TrackType
+
+
+tracks {
+
+    "/chargebacks/payer/list"(platform: "/", parentPropertiesInherited: false, type: TrackType.View) {}
+
+    "/chargebacks/payer/viewdetail"(platform: "/", type: TrackType.Event) {
+        category_id(required: true, type: PropertyType.String)
+        item_id(required: true, type: PropertyType.String)
+        case_id(required: true, type: PropertyType.String)
+        payment_id(required: true, type: PropertyType.String)
+        payer_id(required: true, type: PropertyType.Numeric)
+    }
+
+    "/chargebacks/payer/viewdetail_buy"(platform: "/", type: TrackType.Event) {
+        category_id(required: true, type: PropertyType.String)
+        item_id(required: true, type: PropertyType.String)
+        case_id(required: true, type: PropertyType.String)
+        payment_id(required: true, type: PropertyType.String)
+        payer_id(required: true, type: PropertyType.Numeric)
+    }
+
+    "/chargebacks/payer/select-remedy"(platform: "/", type: TrackType.View) {
+        category_id: (required: false, type: PropertyType.String, description: "Category id")
+        item_id: (required: false, type: PropertyType.String, description: "Item id")
+        case_id: (required: false, type: PropertyType.String, description: "CBK case id")
+        payment_id: (required: false, type: PropertyType.String, description: "Payment id")
+        used_disable: (required: false, type: PropertyType.Boolean, description: "Category id")
+    }
+
+    "/chargebacks/payer/attach"(platform: "/", type: TrackType.Event) {
+        category_id(required: true, type: PropertyType.String)
+        item_id(required: true, type: PropertyType.String)
+        case_id(required: true, type: PropertyType.String)
+        payment_id(required: true, type: PropertyType.String)
+        payer_id(required: true, type: PropertyType.Numeric)
+        start_conversation(required: false, type: PropertyType.Boolean)
+        select_reason(required: true, type: PropertyType.String)
+        flow(required: true, type: PropertyType.String)
+    }
+
+    "/chargebacks/payer/send_msj"(platform: "/", type: TrackType.Event) {
+        category_id(required: true, type: PropertyType.String)
+        item_id(required: true, type: PropertyType.String)
+        case_id(required: true, type: PropertyType.String)
+        payment_id(required: true, type: PropertyType.String)
+        payer_id(required: true, type: PropertyType.Numeric)
+        start_conversation(required: false, type: PropertyType.Boolean)
+        select_reason(required: false, type: PropertyType.Boolean)
+        attached_doc(required: false, type: PropertyType.Boolean)
+        flow(required: true, type: PropertyType.String)
+    }
+
+    "/chargebacks/payer/remedy"(platform: "/", type: TrackType.View) {
+        category_id: (required: false, type: PropertyType.String, description: "Category id")
+        item_id: (required: false, type: PropertyType.String, description: "Item id")
+        case_id: (required: false, type: PropertyType.String, description: "CBK case id")
+        payment_id: (required: false, type: PropertyType.String, description: "Payment id")
+        used_disable: (required: false, type: PropertyType.Boolean, description: "Category id")
+        problem_id: (required: false, type: PropertyType.String, description: "Problem selected by payer")
+    }
+
+    "/chargebacks/payer/change_reason"(platform: "/", type: TrackType.Event) {
+        category_id(required: true, type: PropertyType.String)
+        item_id(required: true, type: PropertyType.String)
+        case_id(required: true, type: PropertyType.String)
+        payment_id(required: true, type: PropertyType.String)
+        problem_id(required: true, type: PropertyType.String)
+    }
+
+    "/chargebacks/payer/attach_main"(platform: "/", type: TrackType.Event) {
+        category_id(required: true, type: PropertyType.String)
+        item_id(required: true, type: PropertyType.String)
+        case_id(required: true, type: PropertyType.String)
+        payment_id(required: true, type: PropertyType.String)
+        problem_id(required: true, type: PropertyType.String)
+    }
+
+    "/chargebacks/payer/send"(platform: "/", type: TrackType.Event) {
+            category_id(required: true, type: PropertyType.String)
+            item_id(required: true, type: PropertyType.String)
+            case_id(required: true, type: PropertyType.String)
+            payment_id(required: true, type: PropertyType.String)
+            payer_id(required: true, type: PropertyType.Numeric)
+            problem_id(required: true, type: PropertyType.String)
+    }
+
+    "/chargebacks/payer/pay"(platform: "/", type: TrackType.Event) {
+            category_id(required: true, type: PropertyType.String)
+            item_id(required: true, type: PropertyType.String)
+            case_id(required: true, type: PropertyType.String)
+            payment_id(required: true, type: PropertyType.String)
+            payer_id(required: true, type: PropertyType.Numeric)
+            problem_id(required: true, type: PropertyType.String)
+    }
+
+    "/chargebacks/payer/confirm"(platform: "/", type: TrackType.Event) {
+            category_id(required: true, type: PropertyType.String)
+            item_id(required: true, type: PropertyType.String)
+            case_id(required: true, type: PropertyType.String)
+            payment_id(required: true, type: PropertyType.String)
+            payer_id(required: true, type: PropertyType.Numeric)
+            problem_id(required: true, type: PropertyType.String)
+    }
+
+    "/chargebacks/payer/resolve"(platform: "/", type: TrackType.View) {
+        category_id: (required: false, type: PropertyType.String, description: "Category id")
+        item_id: (required: false, type: PropertyType.String, description: "Item id")
+        case_id: (required: false, type: PropertyType.String, description: "CBK case id")
+        payment_id: (required: false, type: PropertyType.String, description: "Payment id")
+        payment_status: (required: false, type: PropertyType.String, description: "Payment status")
+        used_disable: (required: false, type: PropertyType.Boolean, description: "Category id")
+        problem_id: (required: false, type: PropertyType.String, description: "Problem selected by payer")
+    }
+
+    "/chargebacks/payer/pay-again"(platform: "/", type: TrackType.Event) {
+            category_id(required: true, type: PropertyType.String)
+            item_id(required: true, type: PropertyType.String)
+            case_id(required: true, type: PropertyType.String)
+            payment_id(required: true, type: PropertyType.String)
+            payer_id(required: true, type: PropertyType.Numeric)
+            problem_id(required: true, type: PropertyType.String)
+            status_repayment(required: true, type: PropertyType.String)
+    }
+}
---
+++ definitions/src/test/resources/chargebacks.groovy
@@ -0,0 +1,132 @@
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+import com.ml.melidata.TrackType;
+
+trackTests {
+    defaultBusiness = "mercadopago"
+
+    test("Chargebacks payer frontend tracks") {
+
+        "/chargebacks/payer/list"(platform: "/", ptype: TrackType.View) {}
+
+        "/chargebacks/payer/viewdetail"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payer_id = 306162056
+            payment_id = "3536747103"
+        }
+
+        "/chargebacks/payer/viewdetail_buy"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payer_id = 306162056
+            payment_id = "3536747103"
+        }
+
+        "/chargebacks/payer/select-remedy"(platform: "/", type: TrackType.View) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payer_id = 306162056
+            payment_id = "3536747103"
+            used_disable = false
+        }
+
+
+        "/chargebacks/payer/attach"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payer_id = 306162056
+            payment_id = "3536747103"
+            select_reason = false
+            start_conversation = false
+            flow = "CBK"
+        }
+
+        "/chargebacks/payer/send_msj"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payer_id = 306162056
+            payment_id = "3536747103"
+            select_reason = false
+            start_conversation = false
+            attached_doc = true
+            flow = "CBK"
+        }
+
+        "/chargebacks/payer/remedy"(platform: "/", type: TrackType.View) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            used_disable = false
+            problem_id = "DUPLICATED_PAYMENT"
+        }
+
+        "/chargebacks/payer/change_reason"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            problem_id = "DUPLICATED_PAYMENT"
+        }
+
+        "/chargebacks/payer/attach_main"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            problem_id = "DUPLICATED_PAYMENT"
+            payer_id = 306162056
+        }
+
+        "/chargebacks/payer/send"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            problem_id = "DUPLICATED_PAYMENT"
+            payer_id = 306162056
+        }
+
+        "/chargebacks/payer/pay"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            problem_id = "DUPLICATED_PAYMENT"
+            payer_id = 306162056
+        }
+
+        "/chargebacks/payer/confirm"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            problem_id = "DUPLICATED_PAYMENT"
+            payer_id = 306162056
+        }
+
+        "/chargebacks/payer/resolve"(platform: "/", type: TrackType.View) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            problem_id = "DUPLICATED_PAYMENT"
+            used_disable = false
+            payment_status = "CANCELLED"
+        }
+
+        "/chargebacks/payer/pay-again"(platform: "/", type: TrackType.Event) {
+            case_id = "987987987004"
+            category_id = "MLA407408"
+            item_id = "MLA673024191"
+            payment_id = "3536747103"
+            problem_id = "DUPLICATED_PAYMENT"
+            payer_id = 306162056
+            payment_status = "CANCELLED"
+        }
+}
