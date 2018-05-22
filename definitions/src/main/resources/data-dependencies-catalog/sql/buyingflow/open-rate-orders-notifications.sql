7d3d695b63e9459d69632b1a5c6a7671f52b9652
Emiliano <emiliano.somoza@mercadolibre.com>
2018-05-22 18:29:19 -0300

    Cambios en query

--- definitions/src/main/resources/data-dependencies-catalog/sql/buyingflow/open-rate-orders-notifications.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/buyingflow/open-rate-orders-notifications.sql
@@ -1,15 +1,15 @@
 SELECT
-  Sent.sent_date, 
-  Sent.site_id,
-  Sent.email_id,
+  Sent.sent_date as sent_date, 
+  Sent.site_id as site_id,
+  Sent.email_id as email_id,
   Sent.payment_method_id as payment_method_id,
   Sent.payment_type as payment_type,
   Sent.payment_status as payment_status,
   Sent.status as status,
-  count (Sent.email_id) as count_send, 
-  sum(if(Open.email_id IS NOT NULL, 1, 0)) as count_open,
-  if(count (Sent.email_id) <> 0,
-  sum(if(Open.email_id IS NOT NULL, 1, 0)) * 100 / count (Sent.email_id),
+  count (Sent.order_id) as count_send, 
+  sum(if(Open.order_id IS NOT NULL, 1, 0)) as count_open,
+  if(count (Sent.order_id) <> 0,
+  sum(if(Open.order_id IS NOT NULL, 1, 0)) * 100 / count (Sent.order_id),
   0) as percentage
 FROM (
   SELECT 
@@ -23,7 +23,7 @@ FROM (
     jest(event_data,'order_status') as status
   FROM tracks
   WHERE 
-    ds >= '@param01' 
+    ds >= '@param02' 
     AND ds < '@param02'
     AND jest(event_data,'event_type') = 'send'
     AND path = '/email/orders'
@@ -37,7 +37,7 @@ LEFT OUTER JOIN
     jest(event_data,'email_template') as email_id
   FROM tracks
   WHERE   
-    ds >= '@param01'
+    ds >= '@param01' 
     AND ds < '@param03'
     AND jest(event_data,'event_type') = 'open'
     AND path = '/email/orders' ) AS Open
