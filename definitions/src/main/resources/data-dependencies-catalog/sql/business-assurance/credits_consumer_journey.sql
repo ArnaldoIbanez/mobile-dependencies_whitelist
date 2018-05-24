ae5cf7ce226748ba78975c369672bcdc24ea86f1
Gabriel Neil <gabriel.neil@mercadolibre.com>
2018-05-24 11:06:26 -0300

    no message

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_consumer_journey.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_consumer_journey.sql
@@ -1,47 +1,34 @@
-SELECT jest(event_data, 'email_id') as email_id,
-		substr(ds,1,10) as fecha, 
-		path,
-		jest(event_data, 'email_template') as template,
-		jest(event_data, 'event_type') as event_type,
-		jest(event_data, 'track_data.milestone') as milestone,
-		jest(event_data, 'track_data.bucket') as bucket,
-		substr(jest(event_data, 'sent_date'), 1, 10) sent_date
+SELECT
+  distinct jest(event_data, 'email_id')             as  email_id,
+  substr(ds, 1, 10)                        as  fecha,
+  path,
+  jest(event_data, 'email_template')       as  template,
+  jest(event_data, 'event_type')           as  event_type,
+  jest(event_data, 'track_data.milestone') as  milestone,
+  jest(event_data, 'track_data.bucket')    as  bucket,
+  substr(jest(event_data, 'sent_date'), 1, 10) sent_date
 FROM tracks
-	where type = 'event' 
-	and path = '/email/generic' 
-	and (ds >= '@param01' and ds <= '@param02')
-	and jest(event_data, 'email_template') = 'CONS_COLLECTION'
-	and jest(event_data, 'event_type') = 'send'
-	and application.site_id = 'MLA'
-
+where type = 'event'
+      and path = '/email/generic'
+      and (ds >= '@param01' and ds < '@param02')
+      and jest(event_data, 'email_template') = 'CONS_COLLECTION'
+      and jest(event_data, 'event_type') = 'send'
+      and application.site_id = 'MLA'
 UNION
-
-SELECT distinct jest(event_data, 'email_id') as email_id, 
-		substr(ds,1,10) as fecha, 
-		path, 
-		jest(event_data, 'email_template') as template, 
-		jest(event_data, 'event_type') as event_type, 
-		milestone, 
-		bucket, 
-		substr(jest(event_data, 'sent_date'), 1, 10) sent_date  
-FROM tracks 
-	LEFT JOIN 
-		(SELECT jest(event_data, 'email_id') as email_id, 
-				jest(event_data, 'track_data.milestone') as milestone, 
-				jest(event_data, 'track_data.bucket') as bucket 
-		FROM tracks
-		WHERE 
-				type = 'event' 
-				and path = '/email/generic' 
-				and (ds >= '@param01' and ds <= '@param02')
-				and jest(event_data, 'email_template') = 'CONS_COLLECTION'
-				and jest(event_data, 'event_type') = 'send' 
-				and application.site_id = 'MLA' ) TRACKDATA 
-	ON jest(event_data, 'email_id') = TRACKDATA.email_id
-WHERE 
-		type = 'event' 
-		and path = '/email/generic' 
-		and (ds >= '@param01' and ds <= '@param02')
-		and jest(event_data, 'email_template') = 'CONS_COLLECTION'
-		and jest(event_data, 'event_type') = 'open' 
-		and application.site_id = 'MLA'
+SELECT distinct
+  jest(event_data, 'email_id')             as  email_id,
+  substr(ds, 1, 10)                        as  fecha,
+  path,
+  jest(event_data, 'email_template')       as  template,
+  jest(event_data, 'event_type')           as  event_type,
+  jest(event_data, 'track_data.milestone') as  milestone,
+  jest(event_data, 'track_data.bucket')    as  bucket,
+  substr(jest(event_data, 'sent_date'), 1, 10) sent_date
+FROM tracks
+WHERE
+  type = 'event'
+  and path = '/email/generic'
+  and (ds >= '@param01' and ds < '@param02')
+  and jest(event_data, 'email_template') = 'CONS_COLLECTION'
+  and jest(event_data, 'event_type') = 'open'
+  and application.site_id = 'MLA'
