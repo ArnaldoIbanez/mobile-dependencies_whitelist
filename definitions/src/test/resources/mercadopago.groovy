3d95cb84d22a1d361cd739284897c480bb76eb4d
alecaceresprado <alejandro.caceres@mercadolibre.com>
2018-05-28 15:41:43 -0300

    path in QR test

--- definitions/src/test/resources/mercadopago.groovy
+++ definitions/src/test/resources/mercadopago.groovy
@@ -709,11 +709,10 @@ trackTests {
         "/catalog/discount"(platform: "/mobile", type: TrackType.View) {}
     }
 
-    test("Qr Landing Buyers") {
-        "qr/landing/buyers"(platform: "/", type: TrackType.View){}
-    }
-
     test("QR") {
+
+        "qr/landing/buyers"(platform: "/", type: TrackType.View){}
+
         "/qr_code/qr_reader"(platform: "/mobile") {
             flow = "/qr_code"
             from = "/deep_link"
