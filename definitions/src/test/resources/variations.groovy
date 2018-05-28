5452e2213afd8dbba0544e169c4e05c367503434
Manuel Kenar <manuel.kenar@mercadolibre.com>
2018-05-28 14:50:28 -0300

    fix all test

--- definitions/src/test/resources/variations.groovy
+++ definitions/src/test/resources/variations.groovy
@@ -18,6 +18,7 @@ trackTests {
 
         def model2 = {
             context = "test-context2"
+            close_index = 1
             items = [
                         [
                             label : "talle",
@@ -27,31 +28,11 @@ trackTests {
             ]
         }
 
-
-        def defaultCheckoutTrackInformation = {
-            purchase_id = 1000000016961120
-            purchase_status = "payment_in_process"
-            shipping_type = "ENVIO"
-            payments = [
-                    [
-                            id : 3753658455,
-                            status : "in_process",
-                            date_created : "2018-05-22T18:24:50Z",
-                            status_detail : "pending_review_manual",
-                            date_approved : "0001-01-01T00:00:00Z",
-                            method_id : "hipercard",
-                            type : "credit_card",
-                            date_last_modified : "2018-05-22T18:24:50Z",
-                    ]
-            ]
-            buy_equals_pay = true
-        }
-
         "/variations/edit"(platform:"/", {
 			model1()
         })
 
-        "/variations/cancel"(platform:"/web", {
+        "/variations/cancel"(platform:"/web/desktop", {
         	model2()
         })
 
