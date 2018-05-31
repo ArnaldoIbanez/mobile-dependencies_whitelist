56feeae8e76eed46854fa131fb269978e5972ec7
Nicolas Vespoli <nicolas.vespoli@mercadolibre.com>
2018-05-31 11:01:45 -0300

    Fix de query

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/custom_attribute_color_list.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/custom_attribute_color_list.sql
@@ -3,7 +3,7 @@ substr(ds,1,10) as fecha,
 GET_JSON_OBJECT(tracks.event_data, '$.category_domain') as category_domain,
 GET_JSON_OBJECT(tracks.event_data, '$.category_id') as category_id,
 GET_JSON_OBJECT(tracks.event_data, '$.session_id') as session_id,
-GET_JSON_OBJECT(tracks.event_data, '$.is_custom_name') as is_custom_name,
+GET_JSON_OBJECT(tracks.event_data, '$.is_custom_name') as is_custom_name
 FROM tracks
 WHERE tracks.path LIKE '%sell/update/color_selection/custom_color'
 AND ds >= '@param01' AND ds < '@param02'
