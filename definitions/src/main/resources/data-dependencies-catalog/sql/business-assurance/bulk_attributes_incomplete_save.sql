381001b915a8163b4ad3f2db079fc568a9a52e46
Nicolas Vespoli <nicolas.vespoli@mercadolibre.com>
2018-06-01 14:36:19 -0300

    JSON y query para bulk_attributes_incomplete_save

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/bulk_attributes_incomplete_save.sql
@@ -0,0 +1,11 @@
+SELECT
+substr(ds,1,10) as fecha,
+GET_JSON_OBJECT(tracks.event_data, '$.campaignId') as campaignId,
+GET_JSON_OBJECT(tracks.event_data, '$.totalItems') as totalItems,
+GET_JSON_OBJECT(tracks.event_data, '$.updatedItems') as updatedItems,
+GET_JSON_OBJECT(tracks.event_data, '$.badItems') as badItems
+FROM tracks
+WHERE tracks.path LIKE '%bulk_attributes/incomplete/save'
+AND ds >= '@param01' AND ds < '@param02'
+AND application.business='mercadolibre'
+AND type='event'
