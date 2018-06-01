94f23591e1f1c0b97f613f42b57db971ab0875ed
Max Draeger <maximiliano.draeger@mercadolibre.com.ar>
2018-06-01 18:07:44 -0300

    Query para Extracto de la Conversión de la Landing de Opt-In de FBM

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/fbm/opt_in_landing.sql
@@ -0,0 +1,64 @@
+SELECT landing.tracking_date,
+        landing.site_id,
+        COUNT(DISTINCT landing.uid) as visits_quantity,
+        COUNT(DISTINCT optin.uid) as optin_quantity,
+        COUNT(DISTINCT congrats.uid) as congrats_quantity,
+        COUNT(DISTINCT cancel_optin.uid) as cancel_optin_quantity,
+        CASE WHEN COUNT(DISTINCT landing.uid) > 0 THEN COUNT(DISTINCT optin.uid) * 100 / CAST(COUNT(DISTINCT landing.uid) AS DOUBLE) ELSE 0 END AS conversion_optin_landing,
+        CASE WHEN COUNT(DISTINCT landing.uid) > 0 THEN COUNT(DISTINCT congrats.uid) * 100 / CAST(COUNT(DISTINCT landing.uid) AS DOUBLE) ELSE 0 END AS conversion_congrats_landing,
+        CASE WHEN COUNT(DISTINCT landing.uid) > 0 THEN COUNT(DISTINCT cancel_optin.uid) * 100 / CAST(COUNT(DISTINCT landing.uid) AS DOUBLE) ELSE 0 END AS conversion_cancel_landing,
+        CASE WHEN COUNT(DISTINCT optin.uid) > 0 THEN COUNT(DISTINCT congrats.uid) * 100 / CAST(COUNT(DISTINCT optin.uid) AS DOUBLE) ELSE 0 END AS conversion_congrats_optin,
+        CASE WHEN COUNT(DISTINCT optin.uid) > 0 THEN COUNT(DISTINCT cancel_optin.uid) * 100 / CAST(COUNT(DISTINCT optin.uid) AS DOUBLE) ELSE 0 END AS conversion_cancel_optin
+FROM
+    (
+        SELECT SUBSTR(DS,1,10) AS tracking_date, application.site_id as site_id, usr.uid as uid
+        FROM tracks
+        WHERE ds >= '@param01' AND ds < '@param02'
+          AND path = '/fbm/landing'
+          AND application.site_id = 'MLM'
+          AND usr.user_nick <> 'MEXICO-SELLER'
+          AND NOT regexp_like(usr.user_nick,'^(TEST|TETE|TT)[0-9]*')
+    ) as landing
+ LEFT JOIN
+    (
+        SELECT SUBSTR(DS,1,10) AS tracking_date, application.site_id as site_id, usr.uid as uid
+        FROM tracks
+        WHERE ds >= '@param01' AND ds < '@param02'
+          AND path = '/fbm/landing/optin'
+          AND application.site_id = 'MLM'
+          AND usr.user_nick <> 'MEXICO-SELLER'
+          AND NOT regexp_like(usr.user_nick,'^(TEST|TETE|TT)[0-9]*')
+    ) as optin
+    ON landing.uid = optin.uid
+    AND landing.site_id = optin.site_id
+    AND landing.tracking_date = optin.tracking_date
+ LEFT JOIN
+    (
+        SELECT SUBSTR(DS,1,10) AS tracking_date, application.site_id as site_id, usr.uid as uid
+        FROM tracks
+        WHERE ds >= '@param01' AND ds < '@param02'
+          AND path = '/fbm/landing/congrats'
+          AND application.site_id = 'MLM'
+          AND usr.user_nick <> 'MEXICO-SELLER'
+          AND NOT regexp_like(usr.user_nick,'^(TEST|TETE|TT)[0-9]*')
+    ) as congrats
+    ON optin.uid = congrats.uid
+    AND optin.site_id = congrats.site_id
+    AND optin.tracking_date = congrats.tracking_date
+ LEFT JOIN
+    (
+        SELECT SUBSTR(DS,1,10) AS tracking_date, application.site_id as site_id, usr.uid as uid
+        FROM tracks
+        WHERE ds >= '@param01' AND ds < '@param02'
+          AND path = '/fbm/landing/cancel_opt_in'
+          AND application.site_id = 'MLM'
+          AND usr.user_nick <> 'MEXICO-SELLER'
+          AND NOT regexp_like(usr.user_nick,'^(TEST|TETE|TT)[0-9]*')
+    ) as cancel_optin
+    ON optin.uid = cancel_optin.uid
+    AND optin.site_id = cancel_optin.site_id
+    AND optin.tracking_date = cancel_optin.tracking_date
+
+GROUP BY landing.tracking_date, landing.site_id
+
+
