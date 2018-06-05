1f90a61186bdebd751a12aa5086cfc646cda2205
Nicolas Vespoli <nicolas.vespoli@mercadolibre.com>
2018-06-05 12:56:31 -0300

    Fix de query

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/bulk_attributes_incomplete_save.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/bulk_attributes_incomplete_save.sql
@@ -1,11 +1,14 @@
 SELECT
 substr(ds,1,10) as fecha,
 GET_JSON_OBJECT(tracks.event_data, '$.campaignId') as campaignId,
-GET_JSON_OBJECT(tracks.event_data, '$.totalItems') as totalItems,
-GET_JSON_OBJECT(tracks.event_data, '$.updatedItems') as updatedItems,
+SUM(GET_JSON_OBJECT(tracks.event_data, '$.totalItems')) as totalItems,
+SUM(GET_JSON_OBJECT(tracks.event_data, '$.updatedItems')) as updatedItems,
+COUNT(1) as registrosTotales
 GET_JSON_OBJECT(tracks.event_data, '$.badItems') as badItems
 FROM tracks
 WHERE tracks.path = '/bulk_attributes/incomplete/save'
 AND ds >= '@param01' AND ds < '@param02'
+AND GET_JSON_OBJECT(event_data, '$.badItems') IN ('true', 'false')
+GROUP BY GET_JSON_OBJECT(event_data, '$.campaignId'), GET_JSON_OBJECT(event_data, '$.badItems'), substr(ds, 1, 10)
 AND application.business='mercadolibre'
 AND type='event'
