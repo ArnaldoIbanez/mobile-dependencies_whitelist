ab62c95303e1bf2eb64b68eb9f7ea13b4678b186
epereyra <emmanuel.pereyra@mercadolibre.com>
2018-06-11 17:53:32 -0300

    fix congrats, error with values

--- definitions/src/main/resources/catalog/cards_prepaid.groovy
+++ definitions/src/main/resources/catalog/cards_prepaid.groovy
@@ -64,7 +64,7 @@ tracks {
     "/prepaid/acquisition/need_fund"(platform: "/", type: TrackType.View) {}
     "/prepaid/acquisition/congrats"(platform: "/", type: TrackType.View) {
         congrats_type(
-            required: true, 
+            required: false, 
             type: PropertyType.String, 
             values: ["bapropagos", "pagofacil", "rapipago", "cobroexpress", "cargavirtual", "redlink", "maestro", "debcabal", "bolbradesco"],
             description: "Types of congrats pages by payment method ID."
@@ -72,7 +72,7 @@ tracks {
     }
     "/prepaid/acquisition/error"(platform: "/", type: TrackType.View) {
         error_type(
-            required: true, 
+            required: false, 
             type: PropertyType.String, 
             values: ["main_error", "have_a_card", "juridical_error", "personal_error", "deceased_error", "underage_error", "denied_error"],
             description: "Types of error pages in acquisition flow."
@@ -87,7 +87,7 @@ tracks {
     "/prepaid/activation/congrats"(platform: "/", type: TrackType.View) {}
     "/prepaid/activation/error"(platform: "/", type: TrackType.View) {
         error_type(
-            required: true, 
+            required: false, 
             type: PropertyType.String, 
             values: ["main_error", "not_found"],
             description: "Types of error pages in activitation flow."
