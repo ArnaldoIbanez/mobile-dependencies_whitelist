5f91f9b494534c65a28c476d9daae0c2d253d5c2
ZeusMode <cintra.wesley@gmail.com>
2018-06-13 12:31:42 -0300

    fix date range

--- definitions/src/main/resources/data-dependencies-catalog/sql/openplatform/requests_api_daily.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/openplatform/requests_api_daily.sql
@@ -1,1 +1,1 @@
-select request_date, count(1) AS total_requests from openplatform.requests_api_daily WHERE request_date >= '@param01' AND request_date <= '@param02' GROUP BY request_date;
+select request_date, count(1) AS total_requests from openplatform.requests_api_daily WHERE request_date >= '@param01' AND request_date < '@param02' GROUP BY request_date;
