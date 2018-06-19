f732ccd3ebe865a937f8386a79172966951750ce
Martin <mciruzzi@users.noreply.github.com>
2018-06-19 16:49:27 -0300

    fix tests

--- definitions/src/test/resources/classifieds_landings.groovy
+++ definitions/src/test/resources/classifieds_landings.groovy
@@ -9,17 +9,19 @@ trackTests {
         category_id = "MLA123"
         vertical = "services"
     }
-
-    "/classifieds/hirable/sellers"(platform: "/", type: TrackType.View) {
-        defaultTrackInformation()
-    }
-    "/classifieds/hirable/buyers"(platform: "/", type: TrackType.View) {
-        defaultTrackInformation()
-    }
-    "/classifieds/contract/sellers"(platform: "/", type: TrackType.View) {
-        defaultTrackInformation()
-    }
-    "/classifieds/contract/buyers"(platform: "/", type: TrackType.View) {
-        defaultTrackInformation()
+    
+    test("Classifieds landing_services tracks test") {
+        "/classifieds/hirable/sellers"(platform: "/", type: TrackType.View) {
+            defaultTrackInformation()
+        }
+        "/classifieds/hirable/buyers"(platform: "/", type: TrackType.View) {
+            defaultTrackInformation()
+        }
+        "/classifieds/contract/sellers"(platform: "/", type: TrackType.View) {
+            defaultTrackInformation()
+        }
+        "/classifieds/contract/buyers"(platform: "/", type: TrackType.View) {
+            defaultTrackInformation()
+        }
     }
 }
