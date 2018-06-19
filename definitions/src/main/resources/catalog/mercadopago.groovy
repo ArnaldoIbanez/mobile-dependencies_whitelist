10b886fc6c5141edd3c5212ec5c7bb473660b7ec
Martin <mciruzzi@users.noreply.github.com>
2018-06-19 16:00:15 -0300

    trigger drone

--- definitions/src/main/resources/catalog/mercadopago.groovy
+++ definitions/src/main/resources/catalog/mercadopago.groovy
@@ -3,6 +3,7 @@ import com.ml.melidata.catalog.PropertyType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
 
+
 /**
 *
 * The events means actions that happens without launch a View,
