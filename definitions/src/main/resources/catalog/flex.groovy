e0ddc95f6ce20143e2ea563c4a33b8f52ccf1958
Sebastian Bravo <sebastian.bravo@mercadolibre.com>
2018-06-25 18:32:44 -0300

    Login tracks added

--- definitions/src/main/resources/catalog/flex.groovy
+++ definitions/src/main/resources/catalog/flex.groovy
@@ -3,6 +3,11 @@ import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
 
 tracks {
+
+    propertyDefinitions {
+        packs_info(required: true, type: PropertyType.ArrayList, description: "Array of packages to deliver")
+    }
+
     "/flex"(platform: "/", isAbstract: true) {}
 
     "/flex/landing"(platform: "/", type: TrackType.View) {}
@@ -13,5 +18,26 @@ tracks {
 
     "/flex/optout"(platform: "/", type: TrackType.Event) {
         view(required: true, type: PropertyType.String, description: "specific view the app returned")
+    }
+
+    "/flex/login/name"(platform: "/mobile", type: TrackType.View) {
+    }
+
+    "/flex/package/start"(platform: "/mobile", type: TrackType.View) {
+    }
+
+    "/flex/package/add"(platform: "/mobile", type: TrackType.View) {
+    }
+
+    "/flex/package/add/qr_detected"(platform: "/mobile", type: TrackType.Event) {
+        status(required: true, type: PropertyType.String,  values: ["ok", "already_registered", "invalid_format"],
+            description: "specific view the app returned")
+    }
+
+    "/flex/package/add/start_delivery"(platform: "/mobile", type: TrackType.Event) {
+        packs_info
+    }
+
+    "/flex/package/add/view_list"(platform: "/mobile", type: TrackType.Event) {
     }
 }
