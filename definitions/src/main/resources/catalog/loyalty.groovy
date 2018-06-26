3dc926dd10a3b54b54028d797e4619686c0922db
Jaime Vidal <jaime.vidal@mercadolibre.com>
2018-06-26 09:28:47 -0300

    remove origin requirement loyalty

--- definitions/src/main/resources/catalog/loyalty.groovy
+++ definitions/src/main/resources/catalog/loyalty.groovy
@@ -85,7 +85,7 @@ tracks {
 
     "/loyalty/buylevel/checkout"(platform: "/",type: TrackType.Event){
         action(required: true, values: ["started","success","success_orange","error"], description: "'started' when the CHO starts, success/success_orange/error is when the CHO finish")
-        origin(required: true, values: ["landing", "mail","vip"])
+        origin(required: false, values: ["landing", "mail","vip"])
         item_id(required: false, description: "If flow starts from vip")
     }
 
