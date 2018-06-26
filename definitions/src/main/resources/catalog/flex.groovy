6fe1d6ed973d05e6cf9dc84d8066cbd40cbe86b7
Sebastian Bravo <sebastian.bravo@mercadolibre.com>
2018-06-26 10:05:18 -0300

    Tests platform fixes and some tests

--- definitions/src/main/resources/catalog/flex.groovy
+++ definitions/src/main/resources/catalog/flex.groovy
@@ -27,49 +27,49 @@ tracks {
         view(required: true, type: PropertyType.String, description: "specific view the app returned")
     }
 
-    "/flex/login/name"(platform: "/mobile", type: TrackType.View) {
+    "/flex/login/name"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/package/start"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/start"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/package/add"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/add"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/package/add/qr_detected"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add/qr_detected"(platform: "/", type: TrackType.Event) {
         status(required: true, type: PropertyType.String,  values: ["ok", "already_registered", "invalid_format"],
             description: "The feedback of the scan for an specific QR.")
         location
     }
 
-    "/flex/package/add/start_delivery"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add/start_delivery"(platform: "/", type: TrackType.Event) {
         packsAndLocation
     }
 
-    "/flex/package/add/view_list"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add/view_list"(platform: "/", type: TrackType.Event) {
         location
     }
 
-    "/flex/package/add/delete_package"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add/delete_package"(platform: "/", type: TrackType.Event) {
         packsAndLocation
     }
 
-    "/flex/package/add/close_list"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add/close_list"(platform: "/", type: TrackType.Event) {
         location
     }
 
-    "/flex/package/list/loading"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/list/loading"(platform: "/", type: TrackType.View) {
         packsAndLocation
     }
 
-    "/flex/package/list/map_too_many_packages"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/list/map_too_many_packages"(platform: "/", type: TrackType.View) {
         packsAndLocation
     }
 
-    "/flex/package/list"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/list"(platform: "/", type: TrackType.View) {
         delivered_packages(required: true, type: PropertyType.String,
             description: "The number of packages with delivered status")
         packsAndLocation
@@ -77,75 +77,75 @@ tracks {
             description: "This field declares the status of the delivery, depending on each pack status")
     }
 
-    "/flex/package/start_trip"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/start_trip"(platform: "/", type: TrackType.Event) {
         packsAndLocation
         context(required: true, type: PropertyType.String,  values: ["list", "detail"],
             description: "Indicates whether start trip action was executed in the list or detail of packages")
     }
 
-    "/flex/package/see_on_google_maps"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/see_on_google_maps"(platform: "/", type: TrackType.Event) {
         packsAndLocation
         context(required: true, type: PropertyType.String,  values: ["list", "detail", "map_missing_addresses"],
             description: "Indicates whether start trip action was executed in the list or detail of packages")
     }
 
-    "/flex/package/add_more_packages"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/add_more_packages"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/package/add_more_packages/qr_detected"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add_more_packages/qr_detected"(platform: "/", type: TrackType.Event) {
         location
         status(required: true, type: PropertyType.String,  values: ["ok", "already_registered", "invalid_format"],
             description: "The feedback of the scan for an specific QR.")
     }
 
-    "/flex/package/add_more_packages/add_packages"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add_more_packages/add_packages"(platform: "/", type: TrackType.Event) {
         packsAndLocation
     }
 
-    "/flex/package/add_more_packages/view_list"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add_more_packages/view_list"(platform: "/", type: TrackType.Event) {
         location
     }
 
-    "/flex/package/add_more_packages/delete_package"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add_more_packages/delete_package"(platform: "/", type: TrackType.Event) {
         packsAndLocation
     }
 
-    "/flex/package/add_more_packages/close_list"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/add_more_packages/close_list"(platform: "/", type: TrackType.Event) {
         location
     }
 
-    "/flex/package/list/map_missing_addresses"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/list/map_missing_addresses"(platform: "/", type: TrackType.View) {
         packsAndLocation
     }
 
-    "/flex/package/detail"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/detail"(platform: "/", type: TrackType.View) {
         location
         pack_status(required: true, type: PropertyType.String,  values: ["delivered", "pending", "not_delivered"],
             description: "The current status of the package")
     }
 
-    "/flex/package/retry_delivery"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/retry_delivery"(platform: "/", type: TrackType.Event) {
         packsAndLocation
         context(required: true, type: PropertyType.String,  values: ["list", "detail"],
             description: "Indicates whether retry action was executed in the list or detail of packages")
     }
 
-    "/flex/package/list/end_trip"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/list/end_trip"(platform: "/", type: TrackType.Event) {
         packsAndLocation
     }
 
-    "/flex/package/detail/call_buyer"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/detail/call_buyer"(platform: "/", type: TrackType.Event) {
         packsAndLocation
         buyer_info(required: false, description: "The receiver data")
     }
 
-    "/flex/package/detail/call_seller"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/package/detail/call_seller"(platform: "/", type: TrackType.Event) {
         packsAndLocation
         seller_info(required: true, description: "The seller data")
     }
 
-    "/flex/package/detail/receipt"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/detail/receipt"(platform: "/", type: TrackType.View) {
         packsAndLocation
         receiver_info(required: true, description: "The receiver data")
         //Name
@@ -153,65 +153,65 @@ tracks {
         //Doc number
     }
 
-    "/flex/package/detail/receipt_other_person"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/detail/receipt_other_person"(platform: "/", type: TrackType.View) {
         packsAndLocation
     }
 
-    "/flex/package/finish_delivery"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/finish_delivery"(platform: "/", type: TrackType.View) {
         packsAndLocation
         pack_status(required: true, type: PropertyType.String,  values: ["delivered", "not_delivered"],
             description: "The status of the congrats page.")
     }
 
-    "/flex/package/start/ask_geolocation"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/start/ask_geolocation"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/package/start/ask_camera"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/start/ask_camera"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/package/list/cant_delete"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/list/cant_delete"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/package/scan/cant_add"(platform: "/mobile", type: TrackType.View) {
+    "/flex/package/scan/cant_add"(platform: "/", type: TrackType.View) {
         location
         invalid_labels(required: true, type: PropertyType.String, description: "The labels that could not be added")
     }
 
-    "/flex/error/session_expired"(platform: "/mobile", type: TrackType.View) {
+    "/flex/error/session_expired"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/error"(platform: "/mobile", type: TrackType.View) {
+    "/flex/error"(platform: "/", type: TrackType.View) {
         location
         error_type(required: true, type: PropertyType.String,  values: ["generic", "connection"],
             description: "Specifies what kind of error happened.")
     }
 
-    "/flex/error/snackbar"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/error/snackbar"(platform: "/", type: TrackType.Event) {
         location
         error_type(required: true, type: PropertyType.String,  values: ["generic", "delivery_failed",
                                                                         "not_delivered_failed", "connection"],
             description: "Specifies what kind of error happened.")
     }
 
-    "/flex/onboarding/package/add"(platform: "/mobile", type: TrackType.View) {
+    "/flex/onboarding/package/add"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/onboarding/got_it"(platform: "/mobile", type: TrackType.Event) {
+    "/flex/onboarding/got_it"(platform: "/", type: TrackType.Event) {
         location
         context(required: true, type: PropertyType.String,  values: ["add", "start_trip", "confirm_delivery"],
             description: "Indicates in which onboarding the action was executed.")
     }
 
-    "/flex/onboarding/package/start_trip"(platform: "/mobile", type: TrackType.View) {
+    "/flex/onboarding/package/start_trip"(platform: "/", type: TrackType.View) {
         location
     }
 
-    "/flex/onboarding/package/confirm_delivery"(platform: "/mobile", type: TrackType.View) {
+    "/flex/onboarding/package/confirm_delivery"(platform: "/", type: TrackType.View) {
         location
     }
 }
--- definitions/src/test/resources/flex.groovy
+++ definitions/src/test/resources/flex.groovy
@@ -5,6 +5,71 @@ trackTests {
 
     defaultBusiness = "mercadoenvios"
 
+    def defaultLocation =
+        {
+            latitude = "-36.34443"
+            longitude = "-35.34332"
+        }
+
+    def defaultPacksInfo =
+        {
+            packs_info = [
+                {
+                    shipping_id = "27596585622"
+                    origin_info = {
+                        addresses_info = "Guevara 533"
+                    }
+                    destintation_info = {
+                        addresses_info = "Suipacha 108"
+                        latitude = "-36.434321"
+                        longitude = "-35.56065"
+                    }
+                    status = "shipped"
+                },
+                {
+                    shipping_id = "342994423"
+                    origin_info = {
+                        addresses_info = "San martin 3223"
+                    }
+                    destintation_info = {
+                        addresses_info = "Uruguay 756"
+                    }
+                    status = "not_delivered"
+                },
+                {
+                    shipping_id = "645292393"
+                    origin_info = {
+                        addresses_info = "Navarro 3232"
+                    }
+                    destintation_info = {
+                        addresses_info = "Libertador 250"
+                        latitude = "-36.312234"
+                        longitude = "-33.645333"
+                    }
+                    status = "pending"
+                }
+            ]
+        }
+
+    def defaultSinglePack =
+        {
+            packs_info = [
+                {
+                    shipping_id = "27596585622"
+                    origin_info = {
+                        addresses_info = "Guevara 533"
+                    }
+                    destintation_info = {
+                        addresses_info = "Suipacha 108"
+                        latitude = "-36.434321"
+                        longitude = "-35.56065"
+                    }
+                    status = "shipped"
+                }
+            ]
+        }
+
+
     test("flex optin track returned success view"){
 
         def defaultTrackInformation = {
@@ -31,5 +96,179 @@ trackTests {
 
     test("flex landing view"){
         "/flex/landing"(platform:"/", type: TrackType.View) {}
+    }
+
+    //FLEX APP TESTS
+
+    test("Login view track success"){
+
+        "/flex/login/name"(platform:"/", type: TrackType.View) {
+            defaultLocation()
+        }
+    }
+
+    test("Camera tutorial view track success"){
+
+        "/flex/package/add"(platform:"/", type: TrackType.View) {
+            defaultLocation()
+        }
+    }
+
+    test("Scanner view track success"){
+
+        "/flex/package/add"(platform:"/", type: TrackType.View) {
+        }
+    }
+
+    test("QR detected track success"){
+
+        "/flex/package/add/qr_detected"(platform:"/", type: TrackType.Event) {
+            status = "ok"
+        }
+    }
+
+    test("Start delivery event track success"){
+
+        "/flex/package/add/start_delivery"(platform:"/", type: TrackType.Event) {
+            defaultPacksInfo
+            defaultLocation
+        }
+    }
+
+    test("Scanner list opened event track success"){
+
+        "/flex/package/add/view_list"(platform:"/", type: TrackType.Event) {
+        }
+    }
+
+    test("Scanner list delete package event track success"){
+
+        "/flex/package/add/delete_package"(platform:"/", type: TrackType.Event) {
+        }
+    }
+
+    test("Scanner list closed event track success"){
+
+        "/flex/package/add/close_list"(platform:"/", type: TrackType.Event) {
+            defaultLocation
+        }
+    }
+
+    test("Loading view track success"){
+
+        "/flex/package/list/loading"(platform:"/", type: TrackType.View) {
+            defaultPacksInfo
+            defaultLocation
+        }
+    }
+
+    test("Too many packages modal page success"){
+
+        "/flex/package/list/map_too_many_packages"(platform:"/", type: TrackType.View) {
+            defaultPacksInfo
+            defaultLocation
+        }
+    }
+
+    test("List page success"){
+
+        "/flex/package/list"(platform:"/", type: TrackType.View) {
+            delivered_packages = "2-10"
+            defaultPacksInfo
+            defaultLocation
+        }
+    }
+
+    test("Start trip event on list success"){
+
+        "/flex/package/start_trip"(platform:"/", type: TrackType.Event) {
+            defaultPacksInfo
+            context = "list"
+        }
+    }
+
+    test("See on google maps event on detail success"){
+
+        "/flex/package/see_on_google_maps"(platform:"/", type: TrackType.Event) {
+            defaultPacksInfo
+            defaultLocation
+            context = "detail"
+        }
+    }
+
+    test("Add more package to delivery page success"){
+
+        "/flex/package/add_more_packages"(platform:"/", type: TrackType.View) {
+            defaultLocation
+        }
+    }
+
+    test("QR detected but already registered event in Scanner edition success"){
+
+        "/flex/package/add_more_packages/qr_detected"(platform:"/", type: TrackType.Event) {
+            defaultLocation
+            status = "already_registered"
+        }
+    }
+
+    test("See on google maps event on detail success"){
+
+        "/flex/package/add_more_packages/add_packages"(platform:"/", type: TrackType.Event) {
+            defaultLocation
+            defaultPacksInfo
+        }
+    }
+
+    test("Open packages list event in add more packages view success"){
+
+        "/flex/package/add_more_packages/view_list"(platform:"/", type: TrackType.Event) {
+            defaultLocation
+        }
+    }
+
+    test("Delete package event in list in add more packages view success"){
+
+        "/flex/package/add_more_packages/delete_package"(platform:"/", type: TrackType.Event) {
+            defaultSinglePack
+        }
+    }
+
+    test("Close packages list event in add more packages view success"){
+
+        "/flex/package/add_more_packages/close_list"(platform:"/", type: TrackType.Event) {
+        }
+    }
+
+    test("Packages with low accuracy modal view success"){
+
+        "/flex/package/list/map_missing_addresses"(platform:"/", type: TrackType.View) {
+            defaultLocation
+            defaultPacksInfo
+        }
+    }
+
+    test("Detail view with status pending success"){
+
+        "/flex/package/detail"(platform:"/", type: TrackType.View) {
+            defaultLocation
+            pack_status = "pending"
+        }
+    }
+
+    test("Retry delivery event in detail success"){
+
+        "/flex/package/retry_delivery"(platform:"/", type: TrackType.Event) {
+            defaultSinglePack
+            defaultLocation
+            context = "detail"
+        }
+    }
+
+    test("End trip action event success"){
+
+        "/flex/package/list/end_trip"(platform:"/", type: TrackType.Event) {
+            defaultPacksInfo
+            defaultLocation
+        }
     }
 }
