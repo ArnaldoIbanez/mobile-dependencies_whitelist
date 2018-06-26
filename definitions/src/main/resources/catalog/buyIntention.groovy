cce4363e2a082f9ef2009ca9659aab637cd81f09
Martin <mciruzzi@users.noreply.github.com>
2018-06-26 12:20:50 -0300

    Update buyIntention.groovy

--- definitions/src/main/resources/catalog/buyIntention.groovy
+++ definitions/src/main/resources/catalog/buyIntention.groovy
@@ -43,7 +43,7 @@ tracks {
     //unit_price
 
     total_amount(required: true, description: "totalAmount")
-    context(required:true, description: "Indicates from where the buy_intention was initiated. Where did the user pressed de 'COMPRAR' button", values: ["vip", "saved_for_later", "cart", "cart_item", "UNKNOWN"])
+    context(required:true, description: "Indicates from where the buy_intention was initiated. Where did the user pressed de 'COMPRAR' button", values: ["vip", "saved_for_later", "cart", "cart_item", "item_add", "UNKNOWN"])
     checkout_flow(required: true, type: PropertyType.String, values: ["contract", "reservation", "subscription", "direct"])
 }
 
