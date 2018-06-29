726caa1469f5bc1749ac97ea05cbad19307e75d0
Santiago Mater <santiago.mater@mercadolibre.com>
2018-06-29 17:55:42 -0300

    Add waiting events tracks

--- definitions/src/main/resources/catalog/instore.groovy
+++ definitions/src/main/resources/catalog/instore.groovy
@@ -1,4 +1,5 @@
 import com.ml.melidata.TrackType
+import com.ml.melidata.catalog.PropertyType
 
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
@@ -18,15 +19,10 @@ tracks {
     "/instore/map"(platform: "/mobile", type: TrackType.View) {}
     "/instore/scan_qr/ftu"(platform: "/mobile", type: TrackType.View) {}
 
-    "/instore/waiting"(platform: "/mobile", isAbstract: true) {}
+    "/instore/waiting"(platform: "/mobile", type: TrackType.View) {}
 
-    "/instore/waiting/gas_jockey"(platform: "/mobile", type: TrackType.View) {}
-    "/instore/waiting/gas_pump"(platform: "/mobile", type: TrackType.View) {}
-    "/instore/waiting/gas_clock"(platform: "/mobile", type: TrackType.View) {}
-    "/instore/waiting/gastronomy_cashier"(platform: "/mobile", type: TrackType.View) {}
-    "/instore/waiting/gastronomy_order"(platform: "/mobile", type: TrackType.View) {}
-    "/instore/waiting/generic_cashier"(platform: "/mobile", type: TrackType.View) {}
-    "/instore/waiting/generic_ticket"(platform: "/mobile", type: TrackType.View) {}
-
+    "/instore/waiting"(platform: "/mobile", type: TrackType.Event) {
+        view (required:true, description: "View type", type: PropertyType.String)
+    }
 
 }
