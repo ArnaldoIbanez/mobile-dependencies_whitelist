6ccba6c0e562a6d74fb6810804eb0767e58df62a
Santiago Mater <santiago.mater@mercadolibre.com>
2018-06-29 10:54:56 -0300

    Add abstract declaration for waiting view

--- definitions/src/main/resources/catalog/instore.groovy
+++ definitions/src/main/resources/catalog/instore.groovy
@@ -15,7 +15,11 @@ tracks {
     "/instore/generic_error"(platform: "/mobile", type: TrackType.View) {
         additional_info (required:false, description: "Extra info")
     }
+    "/instore/map"(platform: "/mobile", type: TrackType.View) {}
     "/instore/scan_qr/ftu"(platform: "/mobile", type: TrackType.View) {}
+
+    "/instore/waiting"(platform: "/mobile", isAbstract: true) {}
+
     "/instore/waiting/gas_jockey"(platform: "/mobile", type: TrackType.View) {}
     "/instore/waiting/gas_pump"(platform: "/mobile", type: TrackType.View) {}
     "/instore/waiting/gas_clock"(platform: "/mobile", type: TrackType.View) {}
@@ -23,6 +27,6 @@ tracks {
     "/instore/waiting/gastronomy_order"(platform: "/mobile", type: TrackType.View) {}
     "/instore/waiting/generic_cashier"(platform: "/mobile", type: TrackType.View) {}
     "/instore/waiting/generic_ticket"(platform: "/mobile", type: TrackType.View) {}
-    "/instore/map"(platform: "/mobile", type: TrackType.View) {}
+
 
 }
