45aef59e31bf000bbb5eaab9c3c4f839a639b9e5
Juan Andres Laura <juan.laura@mercadolibre.com>
2018-07-23 10:48:37 -0300

    refactor query

--- definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability-click.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability-click.sql
@@ -1,1 +1,1 @@
-select * from bi.app_hivetab_channels_availability_and_clicks where requested_datetime_day between '2018-03-01' and '2018-07-21'
+select * from bi.app_hivetab_channels_availability_and_clicks where requested_datetime_day between '2018-03-01' and '2018-03-31'
