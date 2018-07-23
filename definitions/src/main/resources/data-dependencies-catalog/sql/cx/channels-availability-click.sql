9eb65603b6422d2389516a2250f8cd4a19a634da
Juan Andres Laura <juan.laura@mercadolibre.com>
2018-07-23 10:51:36 -0300

    put parameters '

--- definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability-click.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability-click.sql
@@ -1,1 +1,1 @@
-select * from bi.app_hivetab_channels_availability_and_clicks where requested_datetime_day between @param01 and @param02
+select * from bi.app_hivetab_channels_availability_and_clicks where requested_datetime_day between '@param01' and '@param02'
