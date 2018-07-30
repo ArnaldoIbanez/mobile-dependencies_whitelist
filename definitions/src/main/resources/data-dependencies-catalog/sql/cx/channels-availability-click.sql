ddfff9ecd769fd7355c281cadc522e98c71d86f2
Juan Andres Laura <juan.laura@mercadolibre.com>
2018-07-30 17:06:03 -0300

    add path filtering

--- definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability-click.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/cx/channels-availability-click.sql
@@ -98,6 +98,7 @@ SELECT
         AND tj.ds < '@param02 05'
         AND from_unixtime(unix_timestamp(regexp_replace(v1.user_timestamp, 'T', ' ')), 'yyyy-MM-dd HH') >= '@param03 23'
         AND from_unixtime(unix_timestamp(regexp_replace(v1.user_timestamp, 'T', ' ')), 'yyyy-MM-dd HH') < '@param04 23'        AND v1.path = '/availability/requested'
+        AND v1.path = '/availability/requested'
         AND v3.environment = 'PRODUCTION'
         AND v2.app_id = 'CHAT') chattable
    FULL OUTER JOIN
