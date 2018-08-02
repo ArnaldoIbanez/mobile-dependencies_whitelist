ae885955bf3b09d6ac1650003d03f2a269b161f2
Pablo Diaz <pablo.diazuboe@mercadolibre.com>
2018-08-02 16:39:38 -0300

    Add tests

--- definitions/src/test/resources/wms.groovy
+++ definitions/src/test/resources/wms.groovy
@@ -1,13 +1,11 @@
 package src.test.resources
 
-import com.ml.melidata.TrackType
-
 import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 
 trackTests {
 
 	defaultBusiness = "mercadoenvios"
-	
+
     def trackData = {
         warehouse_id = "BRWT01"
         email = "email@mercadolibre.com.ar"
@@ -15,6 +13,18 @@ trackTests {
         last_name = "Some last name"
         error_type = "SERVER"
         feedback_type = "WARNING"
+    }
+
+    def trackPutAwayData = {
+        warehouse_id = "BRWT01"
+        email = "email@mercadolibre.com.ar"
+        first_name = "Some first name"
+        last_name = "Some last name"
+        error_type = "SERVER"
+        feedback_type = "WARNING"
+        put_away_id = "1234"
+        check_in_id = "4567"
+        cart = "CA-0-001-000-00-00"
     }
 
     test("WMS App") {
@@ -30,8 +40,17 @@ trackTests {
         	trackData()
         }
 		"/wms/put_away"(platform: "/mobile/android") {
-			trackData()
+			trackPutAwayData()
 		}
+		"/wms/put_away/scan_cart"(platform: "/mobile/android") {
+            trackPutAwayData()
+        }
+        "/wms/put_away/scan_inventory"(platform: "/mobile/android") {
+            trackPutAwayData()
+        }
+        "/wms/put_away/item"(platform: "/mobile/android") {
+            trackPutAwayData()
+        }
 		"/wms/inbound_audit"(platform: "/mobile/android") {
 			trackData()
 		}
@@ -51,5 +70,15 @@ trackTests {
 			trackData()
 		}
 
+        //Events
+        "/wms/put_away/scan_cart/start_put_away"(platform: "/mobile/android") {
+            trackPutAwayData()
+        }
+        "/wms/put_away/scan_inventory/cancel_put_away"(platform: "/mobile/android") {
+            trackPutAwayData()
+        }
+        "/wms/put_away/scan_inventory/end_put_away"(platform: "/mobile/android") {
+            trackPutAwayData()
+        }
     }
 }
