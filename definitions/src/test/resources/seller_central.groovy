ae1be9214cacd78355ccddddd9726ce4a0ad2f50
Ezequiel Perez <ezequiel.perez@mercadolibre.com>
2018-08-07 17:25:29 -0300

    Agregados primeros path para realizar pruebas de seller central

---
+++ definitions/src/test/resources/seller_central.groovy
@@ -0,0 +1,27 @@
+    
+import com.ml.melidata.TrackType
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+
+trackTests {
+
+    defaultBusiness = "mercadolibre"
+    
+    //------------------------------------------------------------------------------------------------------------------------------------------------------
+    // TRACKS Seller central
+    //------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    test("Test base path") {
+        "/seller_central"(platform: "/", type: TrackType.View) {}
+        "/seller_central/listings"(platform: "/", type: TrackType.View) {}
+    }
+
+    test("seller central listing main page") {
+        "/seller_central/listings/list"(platform: "/", type: TrackType.View) {}
+    }
+
+
+    test("seller central listing filters") {
+        "/seller_central/listings/filters"(platform: "/", type: TrackType.Event) {}
+    }
+
+}
