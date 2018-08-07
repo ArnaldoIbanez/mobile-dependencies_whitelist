02d05b167f0cec8c61f56d79d3379246b17384ea
Unknown <valentinorfila@gmail.com>
2018-08-07 16:42:53 -0300

    Adaptar query CHO OFF

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_consumer_journey_checkout.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_consumer_journey_checkout.sql
@@ -2,14 +2,14 @@ WITH
   t1 AS (
     SELECT
       usr.user_id               AS user_id,
-      substr(ds, 1, 10)         AS date_sent,
+      SUBSTR(ds, 1, 10)         AS date_sent,
       application.site_id       AS site_id,
       device.platform           AS platform,
-      element_at(split_to_map(substr(platform.http.http_referer, position('utm_source=' IN platform.http.http_referer), length(platform.http.http_referer)), '&','='), 'utm_source')    AS utm_source,
-      element_at(split_to_map(substr(platform.http.http_referer, position('utm_source=' IN platform.http.http_referer), length(platform.http.http_referer)), '&','='), 'utm_content')   AS utm_content,
-      element_at(split_to_map(substr(platform.http.http_referer, position('utm_source=' IN platform.http.http_referer), length(platform.http.http_referer)), '&','='), 'utm_medium')    AS utm_medium
+      STR_TO_MAP(substring(platform.http.http_referer, LOCATE('utm_source=', platform.http.http_referer), LENGTH(platform.http.http_referer)), '&','=')["utm_source"] AS utm_source,
+      STR_TO_MAP(substring(platform.http.http_referer, LOCATE('utm_content=', platform.http.http_referer), LENGTH(platform.http.http_referer)), '&','=')["utm_content"] AS utm_content,
+      STR_TO_MAP(substring(platform.http.http_referer, LOCATE('utm_medium=', platform.http.http_referer), LENGTH(platform.http.http_referer)), '&','=')["utm_medium"]  AS utm_medium
     FROM tracks
-    WHERE (ds >= '@param01' and ds < '@param02')
+    WHERE (ds >= '2018-04-04' AND ds < '2018-04-05')
       AND path = '/landing/promotions'
       AND platform.http.http_referer LIKE '%Journey_Cobranza%'
     GROUP BY
@@ -17,12 +17,12 @@ WITH
       substr(ds, 1, 10),
       application.site_id,
       device.platform,
-      element_at(split_to_map(substr(platform.http.http_referer, position('utm_source=' IN platform.http.http_referer), length(platform.http.http_referer)), '&','='), 'utm_source'),
-      element_at(split_to_map(substr(platform.http.http_referer, position('utm_source=' IN platform.http.http_referer), length(platform.http.http_referer)), '&','='), 'utm_content'),
-      element_at(split_to_map(substr(platform.http.http_referer, position('utm_source=' IN platform.http.http_referer), length(platform.http.http_referer)), '&','='), 'utm_medium')
+      STR_TO_MAP(substring(platform.http.http_referer, LOCATE('utm_source=', platform.http.http_referer), LENGTH(platform.http.http_referer)), '&','=')["utm_source"],
+      STR_TO_MAP(substring(platform.http.http_referer, LOCATE('utm_content=', platform.http.http_referer), LENGTH(platform.http.http_referer)), '&','=')["utm_content"],
+      STR_TO_MAP(substring(platform.http.http_referer, LOCATE('utm_medium=', platform.http.http_referer), LENGTH(platform.http.http_referer)), '&','=')["utm_medium"]
   )
 SELECT
-  t1.site_id, t1.platform, t1.utm_source AS reference, count(*) AS total, t1.date_sent
+  t1.site_id AS site_id, t1.platform AS platform, t1.utm_source AS reference, COUNT(*) AS total, t1.date_sent as date_sent
 FROM
   t1
 GROUP BY
