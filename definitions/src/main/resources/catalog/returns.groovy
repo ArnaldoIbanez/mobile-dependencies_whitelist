d452aadbbd440de6f3f818f1496c3e63866f508c
marcosjuarez <marcos.juarez@mercadolibre.com>
2018-08-17 11:15:46 -0300

    Re-struct returns tracks

--- definitions/src/main/resources/catalog/returns.groovy
+++ definitions/src/main/resources/catalog/returns.groovy
@@ -5,56 +5,49 @@ import com.ml.melidata.TrackType
 tracks {
 
     "/return"(platform: "/", isAbstract: true) {
-      category_id(required: false, type: PropertyType.String, description: 'category identifier')
-      officialstore(required: false, type: PropertyType.String, description: 'official store identifier')
-      item_id(required: false, type: PropertyType.String, description: 'item identifier')
-      action(required: false, type: PropertyType.String, description: 'action of the event')
-      loyalty_level(required: false, type: PropertyType.Numeric, description: 'buyer level loyalty')
-      date_delivered(required: false, type: PropertyType.String, description: 'date of order shipment')
-      cart_order(required: false, type: PropertyType.Boolean, description: 'order created by cart')
-      item_category(required: false, type: PropertyType.String, description: 'category of item')
-      item_category_l1(required: false, type: PropertyType.String, description: 'main category of item')
-      refund_account_money(required: false, type: PropertyType.Boolean, description: 'refound money in the buyers account')
-      ref(required: false, type: PropertyType.String, description: 'reference of the beginning of the flow')
+      loyalty_level(required: true, type: PropertyType.String, description: 'buyer level loyalty')
+      order_id(required: true, type: PropertyType.String, description: 'order identifier')
       buyer_scoring(required: false, type: PropertyType.String, description: 'buyer reputation level')
       seller_scoring(required: false, type: PropertyType.String, description: 'seller reputation level')
-      showed_payment_methods(required: false, type: PropertyType.String, description: 'origin of the payment method')
-      order_id(required: false, type: PropertyType.Numeric, description: 'order identifier')
-      category_path(required: false, type: PropertyType.ArrayList, description: 'list of categories')
-      order_delayed_by_seller(required: false, type: PropertyType.Boolean, description: 'order delayed by seller')
-      deferred_payment(required: false, type: PropertyType.String, description: 'when will the buyer be refunded', values: ["shipped", "delivered"])
+      category_id(required: true, type: PropertyType.String, description: 'category identifier')
+      category_id_l1(required: true, type: PropertyType.String, description: 'main category of item')
     }
 
     // STEP 01
-    "/return/typifications"(platform: "/", type: TrackType.View) { }
-    "/return/typifications"(platform: "/", type: TrackType.Event) {
-      typification(required: false, type: PropertyType.String, description: 'reason why the product is returned')
+    "/return/typifications"(platform: "/", type: TrackType.View) {
+      ref(required: true, type: PropertyType.String, description: 'reference of the beginning of the flow')
+    }
+    "/return/typifications/selection"(platform: "/", type: TrackType.Event) {
+      typification(required: true, type: PropertyType.String, description: 'reason why the product is returned')
     }
 
     // STEP 02
-    "/return/conditions"(platform: "/", type: TrackType.View) { }
-    "/return/conditions"(platform: "/", type: TrackType.Event) {
-        action(required: false, values:['selection'], type: PropertyType.String, description: 'action of the event')
-        selection(required: false, values:['accepted', 'rejected'], type: PropertyType.String, description: 'selected option button')
+    "/return/conditions"(platform: "/", type: TrackType.View) {
+      ref(required: true, type: PropertyType.String, description: 'reference of the beginning of the flow')
+    }
+    "/return/conditions/selection"(platform: "/", type: TrackType.Event) {
+      selection(required: true, values:['accepted', 'rejected'], type: PropertyType.String, description: 'selected option button')
     }
 
     // STEP 03
-    "/return/payments"(platform: "/", type: TrackType.View) { }
-    "/return/payments"(platform: "/", type: TrackType.Event) {
-      payment(required: false, type: PropertyType.String, description: 'origin of payment')
-      continue_button(required: false, type: PropertyType.Boolean, description: 'origin from the continue button')
+    "/return/payments"(platform: "/", type: TrackType.View) {
+      ref(required: true, type: PropertyType.String, description: 'reference of the beginning of the flow')
+      showed_payment_methods(required: true, type: PropertyType.String, description: 'origin of the payment method')
+    }
+    "/return/payments/selection"(platform: "/", type: TrackType.Event) {
+      payment(required: true, type: PropertyType.String, description: 'origin of payment')
     }
 
     // STEP 04
     "/return/shipping"(platform: "/", type: TrackType.View) { }
-    "/return/shipping"(platform: "/", type: TrackType.Event) {
-      shipping(required: false, type: PropertyType.String, description: 'shipping status')
+    "/return/shipping/selection"(platform: "/", type: TrackType.Event) {
+      shipment_type(required: true, values:['delivery', 'pickup'], type: PropertyType.String, description: 'selected shipping type')
     }
 
     // STEP 05.a
     "/return/pickup"(platform: "/", type: TrackType.View) { }
-    "/return/pickup"(platform: "/", type: TrackType.Event) {
-      pickup_time(required: false, type: PropertyType.String, description: 'pickup time product to seller')
+    "/return/pickup/selection"(platform: "/", type: TrackType.Event) {
+      pickup_time(required: true, type: PropertyType.String, description: 'pickup time product to seller')
     }
 
     // STEP 05.b
@@ -62,43 +55,51 @@ tracks {
 
     // STEP 05.c
     "/return/review"(platform: "/", type: TrackType.View) { }
-    "/return/review"(platform: "/", type: TrackType.Event) {
-      continue_button(required: false, type: PropertyType.Boolean, description: 'origin from the continue button')
-      type(required: false, type: PropertyType.String, description: 'generate return label')
+    "/return/review/selection"(platform: "/", type: TrackType.Event) {
+      continue_button(required: true, type: PropertyType.Boolean, description: 'origin from the continue button')
+      is_shipping_code(required: true, type: PropertyType.Boolean, description: 'generate return label')
     }
 
     // STEP 06
     "/return/congrats"(platform: "/", type: TrackType.View) {
-      typification(required: false, type: PropertyType.String, description: 'reason why the product is returned')
-      payment(required: false, type: PropertyType.String, description: 'origin of payment')
-      shipping(required: false, type: PropertyType.String, description: 'shipping status')
+      typification(required: true, type: PropertyType.String, description: 'reason why the product is returned')
+      payment(required: true, type: PropertyType.String, description: 'origin of payment')
+      shipment_type(required: true, type: PropertyType.String, description: 'shipment type')
       pickup_time(required: false, type: PropertyType.String, description: 'pickup time product to seller')
+      showed_payment_methods(required: true, type: PropertyType.String, description: 'origin of the payment method')
+      refund_account_money(required: true, type: PropertyType.Boolean, description: 'refound money in the buyers account')
+      cart_order(required: true, type: PropertyType.Boolean, description: 'order created by cart')
+      payment_refund_at(required: true, type: PropertyType.String, description: 'when will the buyer be refunded', values:['shipped', 'delivered_3'])
     }
 
-    // STEP 06.cta.a
-    "/return/congrats/print_label"(platform: "/", type: TrackType.Event) {
-        print_url(required: false, type: PropertyType.String, description: 'Address to print return label')
+    "/return/congrats/selection"(platform: "/", type: TrackType.Event) {
+      selection(required: true, type: PropertyType.String, description: 'selected option button')
     }
 
-    // STEP 06.cta.b
-    "/return/congrats/remedy_label"(platform: "/", type: TrackType.Event) { }
-
-    "/return/modal"(platform: "/", type: TrackType.Event) {
-      action(required: false, type: PropertyType.String, description: 'event that launched the modal')
-      id(required: false, type: PropertyType.String, description: 'item identifier')
-      modal_data(required: false, type: PropertyType.Map, description: 'internal data of the modal')
+    // Bloqueo Scoring High
+    "/return/congrats_error"(platform: "/", type: TrackType.View) {
+      typification(required: true, type: PropertyType.String, description: 'reason why the product is returned')
+      payment(required: true, type: PropertyType.String, description: 'origin of payment')
+      shipment_type(required: true, type: PropertyType.String, description: 'shipment type')
+      pickup_time(required: false, type: PropertyType.String, description: 'pickup time product to seller')
+      showed_payment_methods(required: true, type: PropertyType.String, description: 'origin of the payment method')
+      cart_order(required: true, type: PropertyType.Boolean, description: 'order created by cart')
+      payment_refund_at(required: true, type: PropertyType.String, description: 'when will the buyer be refunded', values:['shipped', 'delivered_3'])
     }
 
+    "/return/congrats_error/selection"(platform: "/", type: TrackType.Event) {
+      selection(required: true, type: PropertyType.String, description: 'selected option button')
+    }
+
+    /*
     "/return/external"(platform: "/", type: TrackType.View) {
-        id(required: false, type: PropertyType.String, description: 'name of the page to redirect')
-        data(required: false, type: PropertyType.Map, description: 'context information to send')
-    }
-    "/return/external"(platform: "/", type: TrackType.Event) {
         id(required: false, type: PropertyType.String, description: 'name of the page to redirect')
         data(required: false, type: PropertyType.Map, description: 'context information to send')
     }
 
     "/return/error"(platform: "/", type: TrackType.View) {
+      //R E V I S A R
       previous_step(required: false, type: PropertyType.String, description: 'step before the error')
     }
+    */
 }
--- definitions/src/test/resources/returns.groovy
+++ definitions/src/test/resources/returns.groovy
@@ -8,146 +8,214 @@ trackTests {
     test("Returns tracks") {
 
       // STEP 01
-      "/return/typifications"(platform: "/")  {
-        typification = "broken_item"
-        action = "selection"
-        loyalty_level = 1
-        ref = "mediations_init"
+      "/return/typifications"(platform: "/", type: TrackType.View)  {
+        loyalty_level = "5"
+        order_id = "1763279429"
         buyer_scoring = "low"
         seller_scoring = "low"
-        date_delivered = "2018-05-21"
-        cart_order = true
-        item_category = "MLB48550"
-        item_category_l1 = "MLB1051"
-        refund_account_money=false
-        order_id=1920434799
-        category_path=["MLA1648", "MLA2141", "MLA1676", "MLA1683", "MLA402171", "MLA402176"]
-        order_delayed_by_seller = false
-        deferred_payment= "shipped"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        ref = "mediations_init"
+      }
+
+      "/return/typifications/selection"(platform: "/", type: TrackType.Event)  {
+        loyalty_level = "5"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        ref = "mediations_init"
+        typification = "broken_item"
       }
 
       // STEP 02
-      "/return/conditions"(platform: "/") {
-        action = "selection"
+      "/return/conditions"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        ref = "mediations_init"
+      }
+
+      "/return/conditions/selection"(platform: "/", type: TrackType.Event) {
+        loyalty_level = "3"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        ref = "mediations_init"
         selection = "accepted"
-        order_delayed_by_seller = false
-        deferred_payment= "shipped"
       }
 
       // STEP 03
-      "/return/payments"(platform: "/") {
-        payment = "paymentId"
-        action = "selection"
-        loyalty_level = 1
-        ref = "mediations_init"
+      "/return/payments"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
         buyer_scoring = "low"
         seller_scoring = "low"
-        date_delivered = "2018-05-21"
-        cart_order = true
-        item_category = "MLB48550"
-        item_category_l1 = "MLB1051"
-        refund_account_money=false
-        order_id=1920434799
-        showed_payment_methods = "account_money"
-        continue_button = true
-        category_path=["MLA1648", "MLA2141", "MLA1676", "MLA1683", "MLA402171", "MLA402176"]
-        order_delayed_by_seller = false
-        deferred_payment= "shipped"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        ref = "mediations_init"
+        showed_payment_methods = "credit_card"
+      }
+
+      "/return/payments/selection"(platform: "/", type: TrackType.Event) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        ref = "mediations_init"
+        showed_payment_methods = "credit_card"
+        payment = "732582934"
       }
 
       // STEP 04
-      "/return/shipping"(platform: "/") {
-        shipping = "pickup"
-        action = "selection"
-        loyalty_level = 1
-        ref = "mediations_init"
+      "/return/shipping"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
         buyer_scoring = "low"
         seller_scoring = "low"
-        date_delivered = "2018-05-21"
-        cart_order = true
-        item_category = "MLB48550"
-        item_category_l1 = "MLB1051"
-        refund_account_money=false
-        category_path=["MLA1648", "MLA2141", "MLA1676", "MLA1683", "MLA402171", "MLA402176"]
-        order_delayed_by_seller = false
-        deferred_payment= "shipped"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+      }
+
+      "/return/shipping/selection"(platform: "/", type: TrackType.Event) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        shipment_type = "pickup"
       }
 
       // STEP 05.a
-      "/return/pickup"(platform: "/") {
-        pickup_time = "pickupTime"
-        action = "selection"
-        loyalty_level = 1
-        ref = "mediations_init"
+      "/return/pickup"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
         buyer_scoring = "low"
         seller_scoring = "low"
-        date_delivered = "2018-05-21"
-        cart_order = true
-        item_category = "MLB48550"
-        item_category_l1 = "MLB1051"
-        refund_account_money=false
-        category_path=["MLA1648", "MLA2141", "MLA1676", "MLA1683", "MLA402171", "MLA402176"]
-        order_delayed_by_seller = false
-        deferred_payment= "shipped"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+      }
+
+      "/return/pickup/selection"(platform: "/", type: TrackType.Event) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        pickup_time = "2018-01-31 18:00.000"
       }
 
       // STEP 05.b
-      "/return/delivery"(platform: "/") { }
+      "/return/delivery"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+      }
 
       // STEP 05.c
-      "/return/review"(platform: "/") {
-        cart_order = false
-        date_delivered = "2018-05-30"
-        item_category = "MLA1915"
-        item_category_l1 = "MLA1051"
-        loyalty_level = 6
-        order_id = 1722099605
-        ref = "myml-listings_primary"
-        refund_account_money = false
-        showed_payment_methods = "account_money"
+       "/return/review"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
         seller_scoring = "low"
-        category_path=["MLA1648", "MLA2141", "MLA1676", "MLA1683", "MLA402171", "MLA402176"]
-        order_delayed_by_seller = false
-        deferred_payment= "shipped"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+      }
+
+      "/return/review/selection"(platform: "/", type: TrackType.Event) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        continue_button = true
+        is_shipping_code = true
       }
 
       // STEP 06
-      "/return/congrats"(platform: "/") {
-        item_id = "item_id"
-        category_id = "category_id"
-        officialstore = "officialstore"
-        typification = "broken_item"
-        payment = "paymentId"
-        shipping = "carrier"
-        pickup_time = "pickupTime"
-        loyalty_level = 1
-        ref = "mediations_init"
+      "/return/congrats"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
         buyer_scoring = "low"
         seller_scoring = "low"
-        date_delivered = "2018-05-21"
-        cart_order = true
-        item_category = "MLB48550"
-        item_category_l1 = "MLB1051"
-        refund_account_money=false
-        category_path=["MLA1648", "MLA2141", "MLA1676", "MLA1683", "MLA402171", "MLA402176"]
-        order_delayed_by_seller = false
-        deferred_payment= "shipped"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        typification = "broken_item"
+        payment = "732582934"
+        shipment_type = "delivery"
+        pickup_time = ""
+        showed_payment_methods = "credit_card"
+        refund_account_money = true
+        cart_order = false
+        payment_refund_at = "delivered_3"
       }
 
-      // STEP 06.cta.a
-      "/return/congrats/print_label"(platform: "/") {
-        print_url = "https://www.mercadolibre.com"
+      "/return/congrats/selection"(platform: "/", type: TrackType.Event) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        typification = "broken_item"
+        payment = "732582934"
+        shipment_type = "delivery"
+        pickup_time = ""
+        showed_payment_methods = "credit_card"
+        refund_account_money = true
+        cart_order = false
+        payment_refund_at = "shipped"
+        selection = "to_myml"
       }
 
-      // STEP 06.cta.b
-      "/return/congrats/remedy_label"(platform: "/") { }
-
-      // Modal
-      "/return/modal"(platform: "/") {
-        action = "modal_action"
-        id = "modal_id"
+      "/return/congrats_error"(platform: "/", type: TrackType.View) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        typification = "broken_item"
+        payment = "732582934"
+        shipment_type = "delivery"
+        pickup_time = ""
+        showed_payment_methods = "credit_card"
+        cart_order = false
+        payment_refund_at = "shipped"
       }
 
+      "/return/congrats_error/selection"(platform: "/", type: TrackType.Event) {
+        loyalty_level = "4"
+        order_id = "1763279429"
+        buyer_scoring = "low"
+        seller_scoring = "low"
+        category_id = "MLB48550"
+        category_id_l1 = "MLB48551"
+        typification = "broken_item"
+        payment = "732582934"
+        shipment_type = "delivery"
+        pickup_time = ""
+        showed_payment_methods = "credit_card"
+        cart_order = false
+        payment_refund_at = "shipped"
+        selection = "to_myml"
+      }
+
+      /*
       // Redirect to Claims
       "/return/external"(platform: "/") {
         id = "claims"
@@ -164,5 +232,6 @@ trackTests {
       "/return/error"(platform: "/") {
         previous_step = "delivery"
       }
+      */
     }
 }
