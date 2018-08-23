6747e6d685d658bb6cfc9223481d2b83fd298fdf
Oleh Burkhay <oleh.burkhay@mercadolibre.com>
2018-08-23 09:47:34 -0300

    Add definitions for zip finder tracks

---
+++ definitions/src/test/resources/zip_finder.groovy
@@ -0,0 +1,31 @@
+import com.ml.melidata.TrackType
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+
+trackTests {
+    defaultBusiness = "mercadolibre"
+
+    test("Test base path") {
+        "/zip_finder"(platform: "/", type: TrackType.View) {}
+    }
+
+    test("zip finder search page") {
+        "/zip_finder/search"(platform: "/", type: TrackType.View) {}
+    }
+
+    test("zip finder final step") {
+        "/zip_finder/finish"(platform: "/", type: TrackType.View) {}
+    }
+
+    test("zip finder listing page") {
+        "/zip_finder/list"(platform: "/", type: TrackType.View) {}
+    }
+
+    test("zip finder interaction with zipcode") {
+        "/zip_finder/list/select_zip_code"(platform: "/", type: TrackType.Event) {
+            action = "use"
+        }
+        "/zip_finder/list/select_zip_code"(platform: "/", type: TrackType.Event) {
+            action = "copy"
+        }
+    }
+}
