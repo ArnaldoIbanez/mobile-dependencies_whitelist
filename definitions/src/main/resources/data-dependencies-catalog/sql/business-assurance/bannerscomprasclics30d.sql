637fd2c5a1ee81cf7f5b82dd286803f2d1939e38
Ezequiel Boehler <ezequiel.boehler@mercadolibre.com>
2018-08-28 14:25:44 -0300

    Actualice el SQL

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/bannerscomprasclics30d.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/bannerscomprasclics30d.sql
@@ -1,7 +1,7 @@
 select 
 substr(user_timestamp,1,10) as orderDate,
 substr(banner.last_click_date,1,10) as lastClickDate,
-date_diff('day', CAST(substr(user_timestamp,1,10) as date), CAST(substr(banner.last_click_date,1,10) as date)) as day_difference,
+CAST(datediff(CAST(substr(banner.last_click_date,1,10) as date), CAST(substr(user_timestamp,1,10) as date)) as integer) as day_difference,
 application.site_id as Site,
 device.platform AS Plataforma,
 usr.uid as uid,
@@ -18,28 +18,26 @@ sum(CAST(jest(event_data,'total_amount_usd') AS DOUBLE)) as sum_dol_amount,
 count(1) as purchases_total
 from tracks orders
 INNER JOIN (
-Select Site, Plataforma, size, banner_name, SellerId, uid, max(bds) as last_click_date
-From (
-			select user_timestamp as bds, 
-			application.site_id as Site,
-			device.platform AS Plataforma,
-			jest(others['fragment'], 'size') AS size,
-			jest(others['fragment'], 'banner_name') AS banner_name,
-			jest(others['fragment'], 'sellerid') AS SellerID,
-			usr.uid as uid
-			from tracks
-			where ds >= '@param3'
-			and ds < '@param2'
-			and others['fragment'] like '%banner_name%'
-		  )
-Group by 	Site, Plataforma, size, banner_name, SellerId, uid
-			) banner ON (jest(event_data, 'seller[0].id') = banner.SellerId and usr.uid = banner.uid)
-where ds >= '@param1'
-and   ds <  '@param2'
+  	select 
+  	application.site_id as Site,
+  	device.platform AS Plataforma,
+  	jest(others['fragment'], 'size') AS size,
+  	jest(others['fragment'], 'banner_name') AS banner_name,
+  	jest(others['fragment'], 'sellerid') AS SellerID,
+  	usr.uid as uid,
+  	max(user_timestamp) as last_click_date
+  	from tracks
+  	where ds >= '2018-08-26'
+  	and ds < '2018-08-28'
+  	and others['fragment'] like '%banner_name%'
+  	group by application.site_id, device.platform, jest(others['fragment'], 'size'), jest(others['fragment'], 'banner_name'), jest(others['fragment'], 'sellerid'), usr.uid
+  ) banner ON (jest(event_data, 'seller[0].id') = banner.SellerID and usr.uid = banner.uid)
+where ds >= '2018-08-27'
+and   ds <  '2018-08-28'
 and path = '/orders/ordercreated'
 group by substr(user_timestamp,1,10),
 substr(banner.last_click_date,1,10),
-date_diff('day', CAST(substr(user_timestamp,1,10) as date), CAST(substr(banner.last_click_date,1,10) as date)),
+CAST(date_diff('day', CAST(substr(user_timestamp,1,10) as date), CAST(substr(banner.last_click_date,1,10) as date)) as integer),
 application.site_id,
 device.platform, 
 usr.uid,
