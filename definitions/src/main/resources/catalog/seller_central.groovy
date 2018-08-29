74713c96c41350f87e2096ac6658b7956d13f1ac
Ezequiel Perez <ezequiel.perez@mercadolibre.com>
2018-08-29 17:14:42 -0300

    Agregados track

--- definitions/src/main/resources/catalog/seller_central.groovy
+++ definitions/src/main/resources/catalog/seller_central.groovy
@@ -3,11 +3,59 @@ import com.ml.melidata.TrackType
 import com.ml.melidata.catalog.PropertyType
 
 tracks {
-    "/seller_central"(platform: "/", type: TrackType.View) {}
-    "/seller_central/listings"(platform: "/", type: TrackType.View) {}
-    "/seller_central/listings/list"(platform: "/", type: TrackType.View) {}
-    "/seller_central/listings/filters"(platform: "/", type: TrackType.Event) {}
+    "/seller_central"(platform: "/", isAbstract: true) {}
+    "/seller_central/listings"(platform: "/", isAbstract: true) {}
+    "/seller_central/listings/list"(platform: "/", type: TrackType.View) {
+
+    }
+
+    "/seller_central/listings/onboarding"(platform: "/", type: TrackType.Event) {
+        action(required: true, type: PropertyType.String, description: "Id of the action ")
+        page(required: false, type: PropertyType.Numeric, description: "Page number")
+
+    }
+
+    "/seller_central/listings/communication"(platform: "/", type: TrackType.Event) {
+        action(required: false, type: PropertyType.String, description: "Id of the action ")
+        id(required: true, type: PropertyType.String, description: "Id of the communication, news or task")
+    }
+
+    "/seller_central/listings/editor"(platform: "/", type: TrackType.Event) {}
+
+    "/seller_central/listings/filters"(platform: "/", isAbstract: true) {
+        action(required: true, type: PropertyType.String, description: "Id of the action ")
+    }
+
     "/seller_central/listings/action"(platform: "/", type: TrackType.Event) {
         action_id(required: true, type: PropertyType.String, description: "Action id")
     }
+
+    "/seller_central/listings/preferences"(platform: "/", type: TrackType.Event) {
+        id(required: true, type: PropertyType.String, description: "Preference id")
+    }
+
+
+    "/seller_central/bulk"(platform: "/", isAbstract: true) {}
+    "/seller_central/bulk/list"(platform: "/", type: TrackType.View) {}
+
+    "/seller_central/bulk/onboarding"(platform: "/", type: TrackType.Event) {
+        action(required: true, type: PropertyType.String, description: "Id of the action ")
+    }
+
+    "/seller_central/bulk/domain"(platform: "/", type: TrackType.Event) {
+        id(required: true, type: PropertyType.String, description: "Id of the domaing group")
+    }
+
+    "/seller_central/bulk/offline"(platform: "/", type: TrackType.Event) {
+        action(required: true, type: PropertyType.String, description: "Id of the action ")
+    }
+
+    "/seller_central/bulk/filters"(platform: "/", type: TrackType.Event) {
+        action(required: true, type: PropertyType.String, description: "Id of the action ")
+
+    }
+    "/seller_central/bulk/search"(platform: "/", type: TrackType.Event) {}
+
+    "/seller_central/bulk/undo_changes"(platform: "/", type: TrackType.Event) {}
+
 }
