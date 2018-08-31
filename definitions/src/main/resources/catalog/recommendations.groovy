b226299fa9f7cd57dcaa93655017571e24be2925
Pablo Carminatti <pablo.carminatti@mercadolibre.com>
2018-08-31 14:32:13 -0300

    fix descriptions

--- definitions/src/main/resources/catalog/recommendations.groovy
+++ definitions/src/main/resources/catalog/recommendations.groovy
@@ -10,11 +10,11 @@ import com.ml.melidata.TrackType
 tracks {
 
     propertyDefinitions {
-        backend_id(required: true, type: PropertyType.String, description: "A unique string that identifies the backend used to create the recommendation")
-        client(required: true, type: PropertyType.String, description: "A unique string that identifies the spot")
-        direct(required: true, type: PropertyType.Boolean, description: "Item ids that are being added")
+        backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
+        client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
+        direct(required: true, type: PropertyType.Boolean, description: "Boolean flag to indicate if we have a direct CHO A2c")
         items(required: true, type: PropertyType.ArrayList, description: "Item ids that are being added")
-        recommendation_id(required: true, type: PropertyType.String, description: "A unique string that identifies the recommendation from were the item comes from")
+        recommendation_id(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
         shipping_benefit(required: false, type: PropertyType.String, values: ["none", "free", "save"], description: "Recommendations shipping benefit, indicates if shipping is free, has savings or does not have any benefit")
     }
 
