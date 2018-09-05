ec1bb13433a32e0f26e58d09717f756dad8cffba
Gabriel Neil <gabriel.neil@mercadolibre.com>
2018-09-05 16:31:20 -0300

    new consumer journey mobile data source

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_consumer_journey_mobile.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/credits_consumer_journey_mobile.sql
@@ -1,17 +1,32 @@
-select
-  substr(ds, 1, 10)              as date_sent,
-  path,
-  jest(event_data, 'event_type') as event_type,
-  application.site_id            as site_id,
-  usr.user_id                    as user_id,
-  device.platform                as platform,
-  device.device_name             as device_name
-from tracks
-where
-  ((type = 'event'
-    and (device.platform = '/mobile/android' or device.platform = '/mobile/ios' or device.platform = '/web/mobile')
-    and (path like '/notification/credits_consumer%' or path like '/notification/credits-consumer%' or
-         path like '/notification_center/credits_consumer%' or path like '/notification_center/credits-consumer%')
-    and (jest(event_data, 'event_type') = 'shown'
-         or jest(event_data, 'event_type') = 'open')))
-  and (ds >= '@param01' and ds < '@param02')
+SELECT
+ t1.site_id AS site_id, 
+ t1.platform AS platform, 
+ t1.path AS path, 
+ t1.event_type as event_type,
+ COUNT(*) AS total, 
+ t1.date_sent as date_sent
+FROM
+ (
+   SELECT
+     application.site_id       AS site_id,
+     usr.user_id               AS user_id,
+     device.platform           AS platform,
+     path,
+     SUBSTR(ds, 1, 10)         AS date_sent,
+     jest(event_data, 'event_type') as event_type
+   FROM tracks
+   WHERE (ds >= '@param01' and ds < '@param02')
+     AND (path like '/notification_center/credits-consumer%' or path like '/notification/credits_consumer%')
+     AND (jest(event_data, 'event_type') = 'shown' or jest(event_data, 'event_type') = 'open')
+   GROUP BY
+     usr.user_id,
+     SUBSTR(ds, 1, 10),
+     application.site_id,
+     device.platform,
+     path,
+     jest(event_data, 'event_type')
+ ) AS t1
+GROUP BY
+ t1.site_id, t1.date_sent, t1.platform, t1.path, t1.event_type
+ORDER BY
+ t1.site_id, t1.date_sent, t1.platform, t1.path, t1.event_type
