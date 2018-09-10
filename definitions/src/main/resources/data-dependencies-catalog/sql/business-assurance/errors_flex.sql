e9bdd3ddecc28d1f240531c821c408caf245b6b4
Mathias Miranda <mathias.miranda@mercadolibre.com>
2018-09-10 17:04:23 -0300

    se agrego atributo partition en json errores flex

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/errors_flex.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/errors_flex.sql
@@ -1,5 +1,4 @@
 select 
-  substr(ds,1,10) as fecha,
   path,
   application.site_id as site,
   usr.user_id as usuario,
@@ -8,7 +7,8 @@ select
     if (path = '/flex/error/session_expired','Error_Session_Expired', 
         if (path = '/flex/package/list/map_too_many_packages', 'Error_TooMany_Package',
             if (path = '/flex/error/snackbar', 'Error_SnackBar','Otro')))) as tipo_error,
-  jest(event_data, 'error_type') as error_type
+  jest(event_data, 'error_type') as error_type,
+  substr(ds,1,10) as ds
 from tracks 
 where  ds>='@param01' AND ds<'@param02'
   and application.business='mercadoenvios'
