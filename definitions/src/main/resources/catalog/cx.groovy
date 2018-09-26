818e527452ef916b1241f576290653c83f32cd7f
Martin Stancanelli <mastanca14@gmail.com>
2018-09-26 16:24:42 -0300

    Add portal and support tracks

--- definitions/src/main/resources/catalog/cx.groovy
+++ definitions/src/main/resources/catalog/cx.groovy
@@ -15,4 +15,52 @@ tracks {
     "/cx/contact_types" (platform: "/mobile", isAbstract: true){}
     "/cx/contact_types/click_on_contact_form" (platform: "/mobile", type: TrackType.Event){}
 
+    propertyDefinitions {
+        portal_contact(required: false, type: PropertyType.String,
+                description: "Indicates if the current hub has channels enabled and why it hasn't")
+        portal_title(required: false, type: PropertyType.String,
+                description: "Indicates the title shown to the user on the url of the current page")
+        portal_source_id(required: false, type: PropertyType.Numeric,
+                description: "Indicates the source ID for the current page")
+        portal_problem_id(required: false, type: PropertyType.Numeric,
+                description: "Indicates the problem ID for the current page (only in widget)")
+        portal_has_channels_configured(required: false, type: PropertyType.Boolean,
+                description: "Indicates if the current content has any channels configured")
+    }
+
+    propertyGroups {
+        portal_default(portal_contact, portal_title, portal_source_id, portal_problem_id, portal_has_channels_configured)
+    }
+
+    "/portal"(platform: "/", isAbstract:  true) {}
+
+    "/portal/faq"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+    "/portal/hub"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+    "/portal/form"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+    "/portal/folder"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+    "/portal/create_case"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+
+    "/support"(platform: "/", isAbstract:  true) {}
+    "/support/widget"(platform: "/", isAbstract:  true) {}
+
+    "/support/widget/faq"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+    "/support/widget/problem"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+    "/support/widget/form"(platform: "/", type: TrackType.View) {
+        portal_default
+    }
+
 }
--- definitions/src/test/resources/cx.groovy
+++ definitions/src/test/resources/cx.groovy
@@ -14,4 +14,45 @@ trackTests {
         "/cx/click_on_suggestion" (platform: "/mobile", type: TrackType.Event){}
         "/cx/contact_types/click_on_contact_form" (platform: "/mobile", type: TrackType.Event) {}
     }
+
+    def defaultPortalInfo = {
+        portal_title = "/home"
+        portal_source_id = 123
+        portal_has_channels_configured = true
+        portal_contact = "{form: \"true\", c2c: \"api_timeout\", chat: \"api_timeout\"}"
+    }
+
+    test("Portal") {
+        "/portal/faq"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+        }
+        "/portal/hub"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+        }
+        "/portal/form"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+        }
+        "/portal/folder"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+        }
+        "/portal/create_case"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+        }
+    }
+
+
+    test("Support Widget") {
+        "/support/widget/faq"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+            portal_problem_id = 456
+        }
+        "/support/widget/problem"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+            portal_problem_id = 456
+        }
+        "/support/widget/form"(platform: "/", type: TrackType.View) {
+            defaultPortalInfo()
+            portal_problem_id = 456
+        }
+    }
 }
