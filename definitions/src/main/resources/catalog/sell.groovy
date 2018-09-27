0f56177c7d19d8d2f41dc4966df8df0ea4015501
Sebastián Schmukliar <sschmukliar@ar0fvfwx04dhv2g.ml.com>
2018-09-27 12:20:47 -0300

    add category_path

--- definitions/src/main/resources/catalog/sell.groovy
+++ definitions/src/main/resources/catalog/sell.groovy
@@ -242,6 +242,7 @@ tracks {
         category_domain(required: false, description: "Category Domain", type: PropertyType.String)
         category_id(required: true, description: "Category Id", type: PropertyType.String)
         is_custom_name(required: true, description: "True:The user changed the color´s name", type: PropertyType.Boolean)
+        category_path(required: true, description: "Category path", type: PropertyType.ArrayList)
     }
     "/sell/list/phone_suggestion"(platform:"/mobile", type:TrackType.View){}
     "/sell/list/condition"(platform: "/", type: TrackType.View) {}
@@ -409,6 +410,7 @@ tracks {
         category_domain(required: false, description: "Category Domain", type: PropertyType.String)
         category_id(required: true, description: "Category Id", type: PropertyType.String)
         is_custom_name(required: true, description: "True:The user changed the color´s name", type: PropertyType.Boolean)
+        category_path(required: true, description: "Category path", type: PropertyType.ArrayList)
     }
     "/sell/update/condition"(platform: "/", type: TrackType.View) {}
     "/sell/update/condition_review"(platform: "/", type: TrackType.View) {}
