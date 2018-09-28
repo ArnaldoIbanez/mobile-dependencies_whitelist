6722379c8fa230e106bbb92f27ca7b787a5000d7
nsoria <natalia.soria@mercadolibre.com>
2018-09-28 15:56:01 -0300

    Create cart-events-a2c.sql

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/cart-events-a2c.sql
@@ -0,0 +1,34 @@
+SELECT application.site_id,
+       substr(ds, 1 , 10) AS date,
+       device.platform AS device,
+       path,
+       COUNT(DISTINCT(usr.user_id)) AS cant
+FROM tracks
+WHERE ds >= '@param01'
+  AND ds < '@param02'
+  AND path IN
+    ('/vip/add_to_cart',
+     '/vip/cart/add_to_cart',
+     '/vip/combo/add_to_cart',
+     '/recommendations/add_to_cart',
+     '/vip/sellers_carousel/add_to_cart',
+     '/bookmarks/add_to_cart',
+     '/cart/my_cart/delete_item',
+     'cart/my_cart/delete_item',
+     '/cart/change_list',
+     '/cart/my_cart/save_for_later',
+     'cart/my_cart/save_for_later',
+     '/cart/save_for_later',
+     '/cart/saved_for_later/add_to_cart',
+     'cart/saved_for_later/add_to_cart',
+     '/cart/add_to_cart',
+     '/cart/my_cart/change_quantity',
+     'cart/my_cart/change_quantity',
+     '/change_quantity/cart',
+     '/add_to_cart',
+     '/cart/my_cart/free_shipping_lost_warning',
+     '/cart/my_cart/free_shipping_lost_warning/dismiss',
+     '/cart/my_cart/free_shipping_lost_warning/confirm',
+     '/cart/saved_for_later/cant_add_to_cart')
+GROUP BY 1,2,3,4
+ORDER BY 1,2,3,4
