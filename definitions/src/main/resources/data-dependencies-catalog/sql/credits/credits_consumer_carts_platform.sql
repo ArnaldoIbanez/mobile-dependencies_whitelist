a5e228bba174374e7e8495a64d301b42f4b54e9a
lucas.pecchio <lucas.pecchio@mercadolibre.com>
2018-10-05 17:26:27 -0300

    fixed date column

--- definitions/src/main/resources/data-dependencies-catalog/sql/credits/credits_consumer_carts_platform.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/credits/credits_consumer_carts_platform.sql
@@ -2,7 +2,7 @@ SELECT count(*) AS Count,
 path AS Path,
 device.platform AS Platform,
 application.site_id AS Site,
-substring(ds, 1, 10) AS Date
+substring(ds, 1, 10) AS Fecha
 FROM tracks
 WHERE path IN ('/checkout/congrats','/cart/checkout/congrats') AND jest(event_data, 'payments[0].payment_method') = 'consumer_credits'
       AND (ds >= '@param01' AND ds < '@param02')
