8f4fbb367440c19a91df2efd3ed5d6cb2690fd80
nacho2487 <ignacio.olaizola@mercadolibre.com>
2018-10-11 17:43:43 -0300

    update paths

--- definitions/src/main/resources/catalog/current_location.groovy
+++ definitions/src/main/resources/catalog/current_location.groovy
@@ -8,30 +8,8 @@ tracks {
     "/current_location/navigation/pick"(platform: "/", type: TrackType.Event) {}
     "/current_location/navigation/close"(platform: "/", type: TrackType.Event) {}
     "/current_location/hub"(platform: "/", type: TrackType.View) {}
-    "/current_location/hub/pick"(platform: "/", type: TrackType.Event) {
-        action(
-            required: true,
-            type: PropertyType.String,
-            description: "The action that triggers selected address",
-            values: ["address", "zipcode"]
-        )
-    }
-    "/current_location/hub/pick/error"(platform: "/", type: TrackType.Event) {
-        action(
-            required: true,
-            type: PropertyType.String,
-            description: "The action that triggers selected address",
-            values: ["address", "zipcode"]
-        )
-        text(
-            required: true,
-            type: PropertyType.String,
-            description: "Error text",
-        )
-    }
+    "/current_location/hub/select_address"(platform: "/", type: TrackType.Event) {}
+    "/current_location/hub/add_zipcode"(platform: "/", type: TrackType.Event) {}
     "/current_location/hub/create_address"(platform: "/", type: TrackType.Event) {}
-    "/current_location/hub/zipcode_finder"(platform: "/", type: TrackType.Event) {}
-    "/current_location/hub/error"(platform: "/", type: TrackType.View) {}
-    "/current_location/hub/notfound"(platform: "/", type: TrackType.View) {}
-
+    "/current_location/hub/find_zipcode"(platform: "/", type: TrackType.Event) {}
 }
--- definitions/src/test/resources/current_location.groovy
+++ definitions/src/test/resources/current_location.groovy
@@ -16,35 +16,19 @@ trackTests {
         "/current_location/navigation/close"(platform: "/", type: TrackType.Event) {}
     }
 
-    test("current location hub pick error") {
-        "/current_location/hub/pick/error"(platform: "/", type: TrackType.Event) {
-            action = "address"
-            text = "Ocurrio un error"
-        }
-    }
-
     test("current location hub create address") {
         "/current_location/hub/create_address"(platform: "/", type: TrackType.Event) {}
     }
 
     test("current location hub zipcode finder") {
-        "/current_location/hub/zipcode_finder"(platform: "/", type: TrackType.Event) {}
+        "/current_location/hub/find_zipcode"(platform: "/", type: TrackType.Event) {}
     }
 
     test("current location hub address selected") {
-        "/current_location/hub/pick"(platform: "/", type: TrackType.Event) {
-            action = "address"
-        }
-        "/current_location/hub/pick"(platform: "/", type: TrackType.Event) {
-            action = "zipcode"
-        }
+        "/current_location/hub/select_address"(platform: "/", type: TrackType.Event) {}
     }
 
-    test("current location hub error view") {
-        "/current_location/hub/error"(platform: "/", type: TrackType.View) {}
-    }
-
-    test("current location hub not found view") {
-        "/current_location/hub/notfound"(platform: "/", type: TrackType.View) {}
+    test("current location hub added zipcode") {
+        "/current_location/hub/add_zipcode"(platform: "/", type: TrackType.Event) {}
     }
 }
