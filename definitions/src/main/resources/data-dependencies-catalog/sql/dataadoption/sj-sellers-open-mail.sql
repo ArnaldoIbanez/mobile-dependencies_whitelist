606b32f92fb8acf439b95e89bbdeae6c24c2f076
Lucas Barenchi Gileno <lucas.barenchi@fing.edu.uy>
2018-10-12 12:43:39 -0300

    Change variables

--- definitions/src/main/resources/data-dependencies-catalog/sql/dataadoption/sj-sellers-open-mail.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/dataadoption/sj-sellers-open-mail.sql
@@ -1,10 +1,10 @@
-SELECT ds as open_date, tracks.usr.user_id as seller_id, get_json_object(tracks.event_data,'$.email_template') as Template,
-          substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) as sent_date
+SELECT substr(ds,1,10) as Open_date, tracks.usr.user_id as Seller_id, get_json_object(tracks.event_data,'$.email_template') as Template,
+          substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) as Sent_date
 FROM tracks
 WHERE (ds >= '@param01'
         AND ds < '@param02'
         AND path = '/email/generic'
         AND get_json_object(tracks.event_data,'$.email_template') = 'TEMP_LOW_1_SJ'
-        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) >= '@param03'
         AND get_json_object(tracks.event_data,'$.event_type') = 'open'
+        AND substr(get_json_object(tracks.event_data,'$.sent_date') ,1,10) >= '@param03'
         )
