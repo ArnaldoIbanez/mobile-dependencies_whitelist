b0d8904d560d234eea2b3b8b1a5666028cf72f74
Martín Varela <martin.varela@mercadolibre.com>
2018-10-23 17:17:08 -0300

    Add keys to supermarket tracks (#1692)
    * add supermarket
    
    * update keys

--- definitions/src/main/resources/catalog/supermarket.groovy
+++ definitions/src/main/resources/catalog/supermarket.groovy
@@ -1,9 +1,18 @@
+import com.ml.melidata.catalog.PropertyType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
 
 tracks {
     // Supermarket
     "/supermarket"(platform: "/", isAbstract: true) {}
-    "/supermarket/header"(platform: "/", type: TrackType.Event) {}
-    "/supermarket/discovery"(platform: "/", type: TrackType.Event) {}
+    "/supermarket/header"(platform: "/", type: TrackType.Event) {
+        client_id(type: PropertyType.String)
+        selected_department(type: PropertyType.String)
+        departments(type: PropertyType.ArrayList)
+    }
+    "/supermarket/discovery"(platform: "/", type: TrackType.Event) {
+        client_id(type: PropertyType.String)
+        selected_department(type: PropertyType.String)
+        departments(type: PropertyType.ArrayList)
+    }
 }
--- definitions/src/test/resources/supermarket.groovy
+++ definitions/src/test/resources/supermarket.groovy
@@ -1,10 +1,19 @@
+import com.ml.melidata.catalog.PropertyType
 import com.ml.melidata.TrackType
 import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 
 trackTests {
     defaultBusiness = "mercadolibre"
     test("supermaket") {
-        "/supermarket/header"(platform: "/", type: TrackType.Event) {}
-        "/supermarket/discovery"(platform: "/", type: TrackType.Event) {}
+        "/supermarket/header"(platform: "/", type: TrackType.Event) {
+            client_id="test"
+            selected_department="test"
+            departments=["department"]
+        }
+        "/supermarket/discovery"(platform: "/", type: TrackType.Event) {
+            client_id="test"
+            selected_department="test"
+            departments=["department"]
+        }
     }
 }
