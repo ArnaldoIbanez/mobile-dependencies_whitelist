14a0e868160c98dab9592f8eb8b40c800c2c0641
Martín Varela <martin.varela@mercadolibre.com>
2018-10-23 15:56:04 -0300

    update keys

--- definitions/src/test/resources/supermarket.groovy
+++ definitions/src/test/resources/supermarket.groovy
@@ -4,7 +4,15 @@ import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 trackTests {
     defaultBusiness = "mercadolibre"
     test("supermaket") {
-        "/supermarket/header"(platform: "/", type: TrackType.Event) {}
-        "/supermarket/discovery"(platform: "/", type: TrackType.Event) {}
+        "/supermarket/header"(platform: "/", type: TrackType.Event) {
+            client_id="test"
+            selected_department="test"
+            departments=["department"]
+        }
+        "/supermarket/discovery"(platform: "/", type: TrackType.Event) {
+            client_id="test"
+            selected_department="test"
+            departments=["department"]
+        }
     }
 }
