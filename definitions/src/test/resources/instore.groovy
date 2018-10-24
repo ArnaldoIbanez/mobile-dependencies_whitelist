2cb5c2c375178a729fe58aac32f03b0eb5fbf53f
Santiago Mater <santiago.mater@mercadolibre.com>
2018-10-24 09:39:08 -0300

    Add new view tracks for instore (#1683)
    Update view tracks for instore

--- definitions/src/test/resources/instore.groovy
+++ definitions/src/test/resources/instore.groovy
@@ -11,25 +11,8 @@ trackTests {
     //------------------------------------------------------------------------------------------------------------------------------------------------------
 
     test("Instore - views") {
-        "/instore/scan_qr"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/manual_price"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/scan_qr/ftu"(platform: "/mobile", type: TrackType.View) {}
 
-        "/instore/waiting"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/gas_jockey"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/gas_jockey"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/gas_pump"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/gas_clock"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/gastronomy_cashier"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/gastronomy_order"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/generic_cashier"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/waiting/generic_ticket"(platform: "/mobile", type: TrackType.View) {}
-
-        "/instore/required_action/data_required"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/required_action/data_required/dni"(platform: "/mobile", type: TrackType.View) {}
-        "/instore/required_action/bluetooth_on"(platform: "/mobile", type: TrackType.View) {}
-
-        "/instore/map"(platform: "/mobile", type: TrackType.View) {}
+        // Error
         "/instore/generic_error"(platform: "/mobile", type: TrackType.View) {
             additional_info = {message="Escanear código QR"
                 icon="error"
@@ -42,5 +25,46 @@ trackTests {
                              type="link"
                          }]}
         }
+
+        // Scan QR
+        "/instore/scan_qr"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/qr_first_time_use"(platform: "/mobile", type: TrackType.View) {}
+
+        // Permissions
+        "/instore/camera_permission"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/permission/location"(platform: "/mobile", type: TrackType.View) {}
+
+        // Amount
+        "/instore/amount/calculator"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/amount/price_list"(platform: "/mobile", type: TrackType.View) {}
+
+        // Shell
+        "/instore/gas_station_shell"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/shell_first_time_use"(platform: "/mobile", type: TrackType.View) {}
+
+        // Vending
+        "/instore/vending/select_product"(platform: "/mobile", type: TrackType.View) {}
+
+        // Waiting
+        "/instore/waiting"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/waiting/gas_jockey"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/waiting/gas_pump"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/waiting/gas_clock"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/waiting/gastronomy_cashier"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/waiting/gastronomy_order"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/waiting/generic_cashier"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/waiting/generic_ticket"(platform: "/mobile", type: TrackType.View) {}
+
+        // Required action
+        "/instore/required_action/data_required"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/required_action/data_required/dni"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/required_action/bluetooth_permission"(platform: "/mobile", type: TrackType.View) {}
+
+        // Deprecated
+        "/instore/map"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/required_action/bluetooth_on"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/map"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/scan_qr/ftu"(platform: "/mobile", type: TrackType.View) {}
+        "/instore/manual_price"(platform: "/mobile", type: TrackType.View) {}
     }
 }
