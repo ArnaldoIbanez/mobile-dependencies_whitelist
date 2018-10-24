7eee6e84a952d788f5aa5c3377bcc4bd647de022
Martín Varela <martin.varela@mercadolibre.com>
2018-10-24 17:02:06 -0300

    Change type tracks for supermarket (#1696)
    * add supermarket
    
    * update keys
    
    * change type

--- definitions/src/test/resources/supermarket.groovy
+++ definitions/src/test/resources/supermarket.groovy
@@ -5,12 +5,12 @@ import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 trackTests {
     defaultBusiness = "mercadolibre"
     test("supermaket") {
-        "/supermarket/header"(platform: "/", type: TrackType.Event) {
+        "/supermarket/header"(platform: "/", type: TrackType.View) {
             client_id="test"
             selected_department="test"
             departments=["department"]
         }
-        "/supermarket/discovery"(platform: "/", type: TrackType.Event) {
+        "/supermarket/discovery"(platform: "/", type: TrackType.View) {
             client_id="test"
             selected_department="test"
             departments=["department"]
