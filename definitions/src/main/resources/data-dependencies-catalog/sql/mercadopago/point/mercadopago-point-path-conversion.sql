8a3c4b100de54771b63e73b43aa8f09331bfc792
Emiliano D. González <emiliano.gonzalez@mercadolibre.com>
2018-11-02 16:37:41 -0300

    Point path conversion fix.

--- definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/point/mercadopago-point-path-conversion.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/point/mercadopago-point-path-conversion.sql
@@ -6,9 +6,9 @@ SELECT path AS track,
        substr(ds,1,10) AS ds
 FROM tracks
 WHERE ds >= '@param01' AND ds < '@param02'
-AND.  usr.user_id IS NOT NULL
-AND.  device.platform IN ('/mobile/android')
-AND.  application.business = 'mercadopago'
-AND.  path LIKE '/point_payment/%'
-AND.  jest(event_data,'flow_id') IS NOT NULL
+AND   usr.user_id IS NOT NULL
+AND   device.platform IN ('/mobile/android')
+AND   application.business = 'mercadopago'
+AND   path LIKE '/point_payment/%'
+AND   jest(event_data,'flow_id') IS NOT NULL
 GROUP BY path, application.version, substr(ds,1,10)
