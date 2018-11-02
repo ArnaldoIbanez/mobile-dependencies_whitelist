5b90224b3a22b7ace20136a2c550af134678b595
Emiliano D. González <emiliano.gonzalez@mercadolibre.com>
2018-11-02 15:51:51 -0300

    Point path conversion. Fixed sql for partitions.

--- definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/point/mercadopago-point-path-conversion.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/mercadopago/point/mercadopago-point-path-conversion.sql
@@ -1,8 +1,9 @@
-SELECT path AS track,
+SELECT path AS path,
        application.version AS version,
        count(*) AS count_tracks,
-       count(DISTINCT usr) AS count_unique_users,
-       count(DISTINCT jest(event_data,'flow_id')) AS count_flow_id
+       count(DISTINCT usr) AS count_usr,
+       count(DISTINCT jest(event_data,'flow_id')) AS count_flow_id,
+       substr(ds,1,10) AS ds
 FROM tracks
 WHERE ds >= '@param01' AND ds < '@param02'
 AND.  usr.user_id IS NOT NULL
@@ -10,4 +11,4 @@ AND.  device.platform IN ('/mobile/android')
 AND.  application.business = 'mercadopago'
 AND.  path LIKE '/point_payment/%'
 AND.  jest(event_data,'flow_id') IS NOT NULL
-GROUP BY path, application.version
+GROUP BY path, application.version, substr(ds,1,10)
