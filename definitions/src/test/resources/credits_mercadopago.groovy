15683d6b7be84290db4333e2fcb89e89ac583b80
Gonzalo E. Rodriguez Isleño <gonzalo.rodriguez@mercadolibre.com>
2018-11-02 15:45:40 -0300

    Trigger tests

--- definitions/src/test/resources/credits_mercadopago.groovy
+++ definitions/src/test/resources/credits_mercadopago.groovy
@@ -19,7 +19,7 @@ trackTests {
         "/credits/merchant/enrollment/feedback"(platform: "/web/desktop") {}
         "/credits/merchant/enrollment/feedback/interested"(platform: "/web/desktop") {}
         "/credits/merchant/enrollment/feedback/not_interested"(platform: "/web/desktop") {}
-        "/credits/merchant/enrollment/feedback/success"(platform: "/web/desktop") {}
+        "/credits/merchant/enrollment/feedback/success"(platform: "/web/desktop")  {}
         "/credits/merchant/enrollment/feedback/error"(platform: "/web/desktop") {}
         "/credits/merchant/enrollment/error/accept"(platform: "/web/desktop") {}
 
