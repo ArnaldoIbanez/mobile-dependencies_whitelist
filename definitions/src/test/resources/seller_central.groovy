33303996a98f974ee15926be691960f3f49cdd78
Ezequiel Perez <ezequiel.perez@mercadolibre.com>
2018-11-06 10:03:51 -0300

    .

--- definitions/src/test/resources/seller_central.groovy
+++ definitions/src/test/resources/seller_central.groovy
@@ -1,11 +1,11 @@
-
+    
 import com.ml.melidata.TrackType
 import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 
 trackTests {
 
     defaultBusiness = "mercadolibre"
-
+    
     //------------------------------------------------------------------------------------------------------------------------------------------------------
     // TRACKS Seller central Listing
     //------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -93,15 +93,16 @@ trackTests {
         }
         "/seller_central/bulk/onboarding"(platform: "/", type: TrackType.Event) {
             action = "dismiss"
-            page = 2
         }
         "/seller_central/bulk/onboarding"(platform: "/", type: TrackType.Event) {
             action = "rollback"
         }
     }
 
-    test("seller central bulk technical specification") {
-        "/seller_central/bulk/technical_specification"(platform: "/", type: TrackType.Event) {}
+    test("seller central bulk view") {
+        "/seller_central/bulk/domain"(platform: "/", type: TrackType.Event) {
+            id = "technical_specification"
+        }
     }
 
     test("seller central bulk filters and search") {
@@ -222,35 +223,4 @@ trackTests {
         }
     }
 
-    //------------------------------------------------------------------------------------------------------------------------------------------------------
-    // TRACKS Seller central Structured Data
-    //------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    test("structured data hint available"){
-        "/seller_central/technical_specifications/hints/available"(platform: "/", type: TrackType.Event){
-            type = "actionable"
-            attribute = "BRAND"
-            category_domain= "MLA-CAMERAS"
-            item_id= "MLA682118081"
-        }
-    }
-
-    test("structured data hint showed"){
-        "/seller_central/technical_specifications/hints/showed"(platform: "/", type: TrackType.Event){
-            type = "info"
-            attribute = "BRAND"
-            category_domain= "MLA-CAMERAS"
-            item_id= "MLA682118081"
-        }
-    }
-
-    test("structured data hint showed"){
-        "/seller_central/technical_specifications/hints/completed"(platform: "/", type: TrackType.Event){
-            type = "actionable"
-            user_action = "click"
-            attribute = "BRAND"
-            category_domain= "MLA-CAMERAS"
-            item_id= "MLA682118081"
-        }
-    }
 }
