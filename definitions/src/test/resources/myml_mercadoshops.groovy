48d60bc13e72427104b7755eaf11f124e8c55061
Artiom Kolesnikov <artiom.kolesnikov@mercadolibre.com>
2018-11-12 11:15:47 -0300

    Add tests for new mercadoshops admin's tracks

---
+++ definitions/src/test/resources/myml_mercadoshops.groovy
@@ -0,0 +1,44 @@
+
+import com.ml.melidata.TrackType
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+
+trackTests {
+
+    defaultBusiness = "mercadolibre"
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------
+    // TRACKS MYML Mercadoshops Admin
+    //------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    test("Mercadoshops Admin") {
+        "/myml/mercadoshops/admin"(platform: "/", type: TrackType.View) {
+            custId = 158689680
+            shopDomain = "www.test-shop.mercadoshops.com.ar"
+            shopName = "Test Shop"
+        }
+    }
+
+    test("Mercadoshops Admin Sidebars") {
+        "/myml/mercadoshops/admin/sidebar"(platform: "/", type: TrackType.Event) {
+            sidebarName = "afip"
+        }
+    }
+
+    test("Mercadoshops Admin Sidebars") {
+        "/myml/mercadoshops/admin/sidebar/afip"(platform: "/", type: TrackType.Event) {}
+        "/myml/mercadoshops/admin/sidebar/banner"(platform: "/", type: TrackType.Event) {
+            imageId = "784348-MLA28624404159_112018"
+            alignment = "center"
+        }
+        "/myml/mercadoshops/admin/sidebar/colors"(platform: "/", type: TrackType.Event) {}
+        "/myml/mercadoshops/admin/sidebar/logo"(platform: "/", type: TrackType.Event) {
+            imageId = "784348-MLA28624404159_112018"
+        }
+        "/myml/mercadoshops/admin/sidebar/shop_name"(platform: "/", type: TrackType.Event) {
+            shopName = "Test Shop"
+        }
+        "/myml/mercadoshops/admin/sidebar/subdomain"(platform: "/", type: TrackType.Event) {
+            subdomain = "test-shop"
+        }
+    }
+}
