e04db09c4c0887ea7778153d456d033bcef3eae5
Artiom Kolesnikov <artiom.kolesnikov@mercadolibre.com>
2018-11-13 18:10:38 -0300

    Use lower case for attribute's names

--- definitions/src/main/resources/catalog/myml_mercadoshops.groovy
+++ definitions/src/main/resources/catalog/myml_mercadoshops.groovy
@@ -4,13 +4,12 @@ import com.ml.melidata.TrackType
 
 tracks {
     propertyDefinitions {
-        custId(required: false, type: PropertyType.Numeric)
-        shopDomain(required: false, type: PropertyType.String)
-        shopName(required: false, type: PropertyType.String)
+        shop_domain(required: false, type: PropertyType.String)
+        shop_name(required: false, type: PropertyType.String)
     }
 
     propertyGroups {
-        mshopsGroup(custId, shopDomain, shopName)
+        mshopsGroup(shop_domain, shop_name)
     }
 
     "/myml/mercadoshops"(platform: "/", isAbstract: true) {
@@ -20,7 +19,7 @@ tracks {
     "/myml/mercadoshops/admin"(platform: "/", type: TrackType.View){}
 
     "/myml/mercadoshops/admin/sidebar"(platform: "/", type: TrackType.Event){
-        sidebarName(
+        sidebar_name(
                 required: false,
                 type: PropertyType.String,
                 values: ['AFIP', 'BANNER', 'COLORS', 'LOGO', 'SHOP_NAME', 'SLIDER', 'SUBDOMAIN']
@@ -30,26 +29,24 @@ tracks {
     "/myml/mercadoshops/admin/sidebar/afip"(platform: "/", type: TrackType.Event){}
 
     "/myml/mercadoshops/admin/sidebar/banner"(platform: "/", type: TrackType.Event){
-        imageId(required: false, type: PropertyType.String)
+        image_id(required: false, type: PropertyType.String)
         alignment(required: false, type: PropertyType.String, values:['top', 'center', 'bottom'])
     }
 
     "/myml/mercadoshops/admin/sidebar/colors"(platform: "/", type: TrackType.Event){
-        presetName(required: false, type: PropertyType.String, description: "Selected preset name")
+        preset_name(required: false, type: PropertyType.String, description: "Selected preset name")
         colors(required: false, type: PropertyType.ArrayList, description: "Array of colors in hex notation")
     }
 
     "/myml/mercadoshops/admin/sidebar/logo"(platform: "/", type: TrackType.Event){
-        imageId(required: false, type: PropertyType.String)
+        image_id(required: false, type: PropertyType.String)
     }
 
-    "/myml/mercadoshops/admin/sidebar/shop_name"(platform: "/", type: TrackType.Event){
-        shopName(required: false, type: PropertyType.String)
-    }
+    "/myml/mercadoshops/admin/sidebar/shop_name"(platform: "/", type: TrackType.Event){}
 
     "/myml/mercadoshops/admin/sidebar/slider"(platform: "/", type: TrackType.Event){
-        imageList(required: false, type: PropertyType.ArrayList, description: "Array of image ids")
-        sliderSize(required: false, type: PropertyType.String, values: ['small', 'medium', 'large'])
+        image_list(required: false, type: PropertyType.ArrayList, description: "Array of image ids")
+        slider_size(required: false, type: PropertyType.String, values: ['small', 'medium', 'large'])
     }
 
     "/myml/mercadoshops/admin/sidebar/subdomain"(platform: "/", type: TrackType.Event){
--- definitions/src/test/resources/myml_mercadoshops.groovy
+++ definitions/src/test/resources/myml_mercadoshops.groovy
@@ -12,37 +12,36 @@ trackTests {
 
     test("Mercadoshops Admin") {
         "/myml/mercadoshops/admin"(platform: "/", type: TrackType.View) {
-            custId = 158689680
-            shopDomain = "www.test-shop.mercadoshops.com.ar"
-            shopName = "Test Shop"
+            shop_domain = "www.test-shop.mercadoshops.com.ar"
+            shop_name = "Test Shop"
         }
     }
 
     test("Mercadoshops Admin Sidebars") {
         "/myml/mercadoshops/admin/sidebar"(platform: "/", type: TrackType.Event) {
-            sidebarName = "SHOP_NAME"
+            sidebar_name = "SHOP_NAME"
         }
     }
 
     test("Mercadoshops Admin Sidebars Configuration Events") {
         "/myml/mercadoshops/admin/sidebar/afip"(platform: "/", type: TrackType.Event) {}
         "/myml/mercadoshops/admin/sidebar/banner"(platform: "/", type: TrackType.Event) {
-            imageId = "784348-MLA28624404159_112018"
+            image_id = "784348-MLA28624404159_112018"
             alignment = "center"
         }
         "/myml/mercadoshops/admin/sidebar/colors"(platform: "/", type: TrackType.Event) {
-            presetName = "dummy preset"
+            preset_name = "dummy preset"
             colors = ["#000", "#fff"]
         }
         "/myml/mercadoshops/admin/sidebar/logo"(platform: "/", type: TrackType.Event) {
-            imageId = "784348-MLA28624404159_112018"
+            image_id = "784348-MLA28624404159_112018"
         }
         "/myml/mercadoshops/admin/sidebar/shop_name"(platform: "/", type: TrackType.Event) {
-            shopName = "Test Shop"
+            shop_name = "Test Shop"
         }
         "/myml/mercadoshops/admin/sidebar/slider"(platform: "/", type: TrackType.Event) {
-            imageList = ["784348-MLA28624404159_112018", "784348-MLA28624404159_112019", "784348-MLA28624404159_112020"]
-            sliderSize = 'medium'
+            image_list = ["784348-MLA28624404159_112018", "784348-MLA28624404159_112019", "784348-MLA28624404159_112020"]
+            slider_size = 'medium'
         }
         "/myml/mercadoshops/admin/sidebar/subdomain"(platform: "/", type: TrackType.Event) {
             subdomain = "test-shop"
