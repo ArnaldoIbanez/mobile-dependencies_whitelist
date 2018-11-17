6f52cd9a7c482774b2834fc90d4b591a2db92948
Emmanuel Contreras Pereyra <emmanuel.pereyra@mercadolibre.com>
2018-11-17 12:59:50 -0300

    nuevo catalogo para el equipo de cards, cobranded MLM

--- definitions/src/main/resources/catalog/cards_cobranded.groovy
+++ definitions/src/main/resources/catalog/cards_cobranded.groovy
@@ -37,4 +37,43 @@ tracks {
     "/cobranded/acquisition/master_info"(platform: "/", type: TrackType.View){}
     "/cobranded/acquisition/master_rejected"(platform: "/", type: TrackType.View){}
     "/cobranded/acquisition/ready_applicant"(platform: "/", type: TrackType.View){}
+
+
+    // Acquisition
+    // MLM => https://www.mercadopago.com.mx/tarjeta-credito
+    // General Path
+    "/tarjeta_credito"(platform: "/", isAbstract: true) {}
+    "/tarjeta_credito/error"(platform: "/", type: TrackType.View){}
+    
+    // Landing
+    "/tarjeta_credito/landing/"(platform: "/", isAbstract: true) {}
+    "/tarjeta_credito/landing/card_request"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/landing/pending_card"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/landing/approved_card"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/landing/rejected_card"(platform: "/", type: TrackType.View){}
+
+    // Acquisition
+    "/tarjeta_credito/acquisition/"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/welcome"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/personal_info"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/cards"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/last_digits"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/welcome_address"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/address"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/additional_info"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/welcome_phone"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/phone"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/review"(platform: "/", type: TrackType.View){}
+    
+    "/tarjeta_credito/acquisition/congrats"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/congrats/approved"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/congrats/pending"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/congrats/rejected"(platform: "/", type: TrackType.View){}
+    
+    "/tarjeta_credito/acquisition/error"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/error/has_card"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/error/corporation"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/error/operator"(platform: "/", type: TrackType.View){}
+    "/tarjeta_credito/acquisition/error/without_credit_card"(platform: "/", type: TrackType.View){}
+    
 }
--- definitions/src/test/resources/cards_cobranded.groovy
+++ definitions/src/test/resources/cards_cobranded.groovy
@@ -46,4 +46,76 @@ trackTests {
         "/cobranded/acquisition/master_rejected"(platform: "/", type: TrackType.View){}
         "/cobranded/acquisition/ready_applicant"(platform: "/", type: TrackType.View){}
     }
+
+    test("cards tarjeta-credito landing") {
+
+        // View desktop
+        "/tarjeta_credito/landing/card_request"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/landing/pending_card"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/landing/approved_card"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/landing/rejected_card"(platform: "/web/desktop", type: TrackType.View){}
+
+        // View mobile
+        "/tarjeta_credito/landing/card_request"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/landing/pending_card"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/landing/approved_card"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/landing/rejected_card"(platform: "/web/mobile", type: TrackType.View){}
+
+    }
+
+    test("cards tarjeta-credito acquisition") {
+
+        // View desktop
+        "/tarjeta_credito/acquisition/"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/welcome"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/personal_info"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/cards"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/last_digits"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/welcome_address"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/address"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/additional_info"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/welcome_phone"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/phone"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/review"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats/approved"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats/pending"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats/rejected"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/has_card"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/corporation"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/operator"(platform: "/web/desktop", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/without_credit_card"(platform: "/web/desktop", type: TrackType.View){}
+
+        // View mobile
+        "/tarjeta_credito/acquisition/"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/welcome"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/personal_info"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/cards"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/last_digits"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/welcome_address"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/address"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/additional_info"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/welcome_phone"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/phone"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/review"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats/approved"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats/pending"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/congrats/rejected"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/has_card"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/corporation"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/operator"(platform: "/web/mobile", type: TrackType.View){}
+        "/tarjeta_credito/acquisition/error/without_credit_card"(platform: "/web/mobile", type: TrackType.View){}
+    }
+
+    test("cards tarjeta-credito error") {
+
+        // View desktop
+        "/tarjeta_credito/error"(platform: "/web/desktop", type: TrackType.View){}
+
+        // View mobile
+        "/tarjeta_credito/error"(platform: "/web/mobile", type: TrackType.View){}
+    }
 }
