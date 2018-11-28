dd81c578669a2484f5060dde988a1e23476fe7e5
fernando keuroglian <fkeuroglian@uy0fvfwq02whv2g.ml.com>
2018-11-28 15:39:19 -0300

    cambios query new_relic_javascripterror

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/new_relic_javascripterror.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/new_relic_javascripterror.sql
@@ -7,13 +7,13 @@ from_unixtime(CAST(substr(jest(data,'timestamp'),1,10) as BIGINT), 'yyyy-MM-dd H
 ,deviceType
 ,errorClass
 ,requestUri
+,transactionName
 ,userAgentName
 ,userAgentOS
 ,userAgentVersion
-,transactionName
 ,from_unixtime(CAST(substr(jest(data,'timestamp'),1,10) as BIGINT), 'yyyy-MM-dd') as Fecha
 from ba.new_relic_data
-lateral view json_tuple(data, 'eventType','applicationId','appName','deviceType','errorClass','errorMessage','requestUri','transactionName','userAgentName','userAgentOS','userAgentVersion') t1 
+lateral view json_tuple(data, 'eventType','applicationId','appName','deviceType','errorClass','requestUri','transactionName','userAgentName','userAgentOS','userAgentVersion') t1 
 as eventType,applicationId,appName,deviceType,errorClass,requestUri,transactionName,userAgentName,userAgentOS,userAgentVersion
 where substr(ds,1,10) >= '@param01' AND substr(ds,1,10)< '@param02' 
 and jest(data,'eventType')='JavaScriptError'
