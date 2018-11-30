7f82eb00e518d82fcf6d1675562c03f623522f77
Emanuel Lopez <emanuel.lopez@mercadolibre.com>
2018-11-30 10:50:44 -0300

    change query

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/technical_specifications_hints.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/technical_specifications_hints.sql
@@ -1,21 +1,39 @@
 SELECT 
-tracks.path AS path, 
-jest(tracks.event_data, 'user_action') action,   
-jest(tracks.event_data, 'session_id') session, 
-jest(tracks.event_data, 'item_id') item, 
-jest(tracks.event_data, 'category_domain') domain, 
-jest(tracks.event_data, 'attribute') attribute, 
-jest(tracks.event_data, 'type') hintType, 
-CASE   
-WHEN tracks.path LIKE '/seller_central/bulk/technical_specifications/hints/%' THEN 'bulk'  
-WHEN tracks.path LIKE '/seller_central/modify/technical_specifications/hints/%' THEN 'modify'  
-END AS source,
-application.site_id AS site,
-usr.user_id AS user,
-substr(ds,1,10) AS date
-FROM tracks 
-WHERE tracks.path LIKE '/seller_central/%/technical_specifications/hints/%' 
-AND ds >= '@from'
-AND ds < '@to'
-AND type = 'event'
-AND application.business = 'mercadolibre';
+    tracks.path AS path, 
+    jest(tracks.event_data, 'user_action') action,   
+    jest(tracks.event_data, 'session_id') session, 
+    jest(tracks.event_data, 'item_id') item, 
+    jest(tracks.event_data, 'category_domain') domain, 
+    jest(tracks.event_data, 'attribute') attribute, 
+    jest(tracks.event_data, 'type') hintType, 
+    CASE   
+        WHEN tracks.path LIKE '/seller_central/bulk/technical_specifications/hints/%' THEN 'bulk'  
+        WHEN tracks.path LIKE '/seller_central/modify/technical_specifications/hints/%' THEN 'modify'  
+        END AS source,
+    application.site_id AS site,
+    usr.user_id AS user,
+    substr(ds,1,10) AS date
+FROM tracks
+WHERE jest(tracks.event_data, 'item_id') IN
+      (SELECT DISTINCT
+          jest(tracks.event_data, 'item_id')
+      FROM tracks
+      WHERE tracks.path LIKE '/seller_central/%/technical_specifications/hints/available' 
+          AND ds >= '@from'
+          AND ds < '@to'
+          AND type = 'event'
+          AND application.business = 'mercadolibre')
+    AND jest(tracks.event_data, 'session_id') IN
+        (SELECT DISTINCT
+            jest(tracks.event_data, 'session_id')
+        FROM tracks
+        WHERE tracks.path LIKE '/seller_central/%/technical_specifications/hints/available' 
+            AND ds >= '@from'
+            AND ds < '@to'
+            AND type = 'event'
+            AND application.business = 'mercadolibre')
+    AND tracks.path LIKE '/seller_central/%/technical_specifications/hints/%' 
+    AND ds >= '@from'
+    AND ds < '@to'
+    AND type = 'event'
+    AND application.business = 'mercadolibre'
