24cb70b5ea98e2dd82c69d3d7ac4f25a9976f22e
IvanRufino2471 <ivan.rufino@mercadolibre.com>
2018-12-03 09:47:48 -0300

    do through setBase dir

---
+++ definitions/src/main/groovy/com/melidata/definitions/tests.groovy
@@ -0,0 +1,24 @@
+import com.melidata.definitions.JsonUploaderValidator
+import com.melidata.definitions.outs.StdOut
+import com.melidata.definitions.TestRunner
+import com.ml.melidata.catalog.parsers.dsl.CatalogDsl
+
+static void main(String[] args) {
+
+    println( "Preparing....")
+    def status = true
+
+    args.eachWithIndex { catalogName, index ->
+        catalogName = args[index]
+        System.setProperty("CATALOG_NAME", catalogName)
+        CatalogDsl.setBaseDir("src/main/resources/catalog/" + catalogName + "/")
+
+        status = status && TestRunner.run(catalogName, new StdOut())
+    }
+
+    status = status && JsonUploaderValidator.validate()
+
+    //If any test fails main program will exit with status=1
+    if(!status)
+        System.exit(1)
+}
