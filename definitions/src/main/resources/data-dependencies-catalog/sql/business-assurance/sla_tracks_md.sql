e79c40b21ae68220ccc7e9d11dc477c69d7994a4
Alejandro Carlos Maciel <amaciel@ar0fvfwx054hv2g.ml.com>
2018-12-04 15:42:56 -0300

    Se modifica la consulta SQL

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/sla_tracks_md.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/sla_tracks_md.sql
@@ -1,4 +1,4 @@
-SELECT fecha,
+SELECT datos.fecha                                                                                         AS fecha,
        IF(iniciatives.iniciative IS NULL, 'SIN DATOS EN EL MAPEO PARA EL SECTION', iniciatives.iniciative) AS iniciativa, 
        datos.section_path                                                                                  AS section,
        datos.platform                                                                                      AS platform,
@@ -10,59 +10,78 @@ SELECT fecha,
        SUM(datos.invalid_tracks)                                                                           AS invalidTracks 
 FROM (
       (
-       SELECT *
+       SELECT fecha, section_path, platform, version, type, path, total_tracks, valid_tracks, invalid_tracks
        FROM (
-             SELECT substr(ds, 1, 10)                                           AS fecha,
-                    regexp_extract(path,'^(\/.*?)(\/|$)',1)                     AS section_path, 
-                    device.platform                                             AS platform, 
-                    IF(device.platform like '/web%', null, application.version) AS version,
-                    type                                                        AS type,
-                    path                                                        AS path,
-                    COUNT(1)                                                    AS total_tracks, 
-                    SUM(IF(catalog_data.is_valid = TRUE,1,0))                   AS valid_tracks,
-                    SUM(IF(catalog_data.is_valid = FALSE,1,0))                  AS invalid_tracks 
-             FROM tracks
-             LEFT JOIN
-             (
-              SELECT DISTINCT business, platform, version
-              FROM (
-                    SELECT business, platform, version, percentage, rank() OVER (PARTITION BY business, platform ORDER BY version DESC) AS rnk
+             SELECT subQueryTracks.fechaTracks         AS fecha,
+                    subQueryTracks.section_path        AS section_path,
+                    subQueryTracks.platformTracks      AS platform,
+                    subQueryTracks.versionTracks       AS version,
+                    subQueryTracks.typeTracks          AS type,
+                    subQueryTracks.pathTracks          AS path,
+                    SUM(subQueryTracks.total_tracks)   AS total_tracks,
+                    SUM(subQueryTracks.valid_tracks)   AS valid_tracks,
+                    SUM(subQueryTracks.invalid_tracks) AS invalid_tracks
+             FROM (
+                   SELECT substr(ds, 1, 10)                                           AS fechaTracks,
+                          regexp_extract(path,'^(\/.*?)(\/|$)',1)                     AS section_path,
+                          application.business                                        AS businessTracks,  
+                          device.platform                                             AS platformTracks, 
+                          IF(device.platform like '/web%', null, application.version) AS versionTracks,
+                          type                                                        AS typeTracks,
+                          path                                                        AS pathTracks,
+                          COUNT(1)                                                    AS total_tracks, 
+                          SUM(IF(catalog_data.is_valid = TRUE,1,0))                   AS valid_tracks,
+                          SUM(IF(catalog_data.is_valid = FALSE,1,0))                  AS invalid_tracks 
+                   FROM tracks
+                   WHERE ds >= '2018-12-03 00:00:00'
+                     AND ds  < '2018-12-04 00:00:00'
+                     AND NOT is_bot(device.user_agent)
+                     AND (not (application.server_poolname rlike 'checkout-on.+') OR application.server_poolname is null)
+                   GROUP BY substr(ds, 1, 10), 
+                            regexp_extract(path,'^(\/.*?)(\/|$)',1), 
+                            application.business, 
+                            device.platform, 
+                            IF(device.platform like '/web%', NULL, application.version),
+                            type,
+                            path
+                   ) subQueryTracks  
+                   LEFT JOIN
+                   (
+                    SELECT DISTINCT business, platform, version
                     FROM (
-                          SELECT business, platform, version, percentage
+                          SELECT business, platform, version, percentage, rank() OVER (PARTITION BY business, platform ORDER BY version DESC) AS rnk
                           FROM (
-                                SELECT application.business                                                                         AS business,
-                                       device.platform                                                                              AS platform,
-                                       application.version                                                                          As version,
-                                       (count(*) * 100.0) / sum(count(*)) OVER (PARTITION BY application.business, device.platform) AS percentage
-                                FROM tracks
-                                WHERE ds >= '@param01'
-                                  AND ds  < '@param02'
-                                  AND NOT is_bot(device.user_agent)
-                                  AND (
-                                          (application.business = 'mercadolibre'  AND device.platform = '/mobile/android')                                       
-                                       OR (application.business = 'mercadolibre'  AND device.platform = '/mobile/ios')                                           
-                                       OR (application.business = 'mercadopago'   AND device.platform = '/mobile/android')                                       
-                                       OR (application.business = 'mercadopago'   AND device.platform = '/mobile/ios')                                           
-                                       OR (application.business = 'mercadoenvios' AND device.platform = '/mobile/android' AND NOT application.app_id = 'WMS_APP')
-                                      )
-                                GROUP BY application.business, device.platform, application.version 
-                               ) subQueryVersions1
-                          WHERE percentage >= 20
-                         ) subQueryVersions2
-                   ) subQueryVersions3
-              WHERE rnk = 1
-             ) versions ON versions.business = application.business AND versions.platform = device.platform AND versions.version = application.version
-             WHERE ds >= '@param01'
-               AND ds  < '@param02'
-               AND NOT is_bot(device.user_agent)
-               AND ((device.platform LIKE '/web%' AND (not (application.server_poolname rlike 'checkout-on.+') OR application.server_poolname is null))
-                 OR (versions.version IS NOT NULL AND application.version = versions.version))
-             GROUP BY substr(ds, 1, 10), regexp_extract(path,'^(\/.*?)(\/|$)',1), device.platform, if(device.platform like '/web%', null, application.version), type, path
+                                SELECT business, platform, version, percentage
+                                FROM (
+                                      SELECT application.business                                                                         AS business,
+                                             device.platform                                                                              AS platform,
+                                             application.version                                                                          As version,
+                                             (count(*) * 100.0) / sum(count(*)) OVER (PARTITION BY application.business, device.platform) AS percentage
+                                      FROM tracks
+                                      WHERE ds >= '2018-12-03 00:00:00'
+                                        AND ds  < '2018-12-04 00:00:00'
+                                        AND NOT is_bot(device.user_agent)
+                                        AND (
+                                                (application.business = 'mercadolibre'  AND device.platform = '/mobile/android')                                       
+                                             OR (application.business = 'mercadolibre'  AND device.platform = '/mobile/ios')                                           
+                                             OR (application.business = 'mercadopago'   AND device.platform = '/mobile/android')                                       
+                                             OR (application.business = 'mercadopago'   AND device.platform = '/mobile/ios')                                           
+                                             OR (application.business = 'mercadoenvios' AND device.platform = '/mobile/android' AND NOT application.app_id = 'WMS_APP')
+                                            )
+                                      GROUP BY application.business, device.platform, application.version 
+                                     ) subQueryVersions1
+                                WHERE percentage >= 20
+                               ) subQueryVersions2
+                         ) subQueryVersions3
+                    WHERE rnk = 1
+                   ) versions ON versions.business = subQueryTracks.businessTracks AND versions.platform = subQueryTracks.platformTracks AND versions.version  = subQueryTracks.versionTracks 
+             WHERE (subQueryTracks.platformTracks like '/web%'  OR versions.version IS NOT NULL)
+             GROUP BY subQueryTracks.fechaTracks, subQueryTracks.section_path, subQueryTracks.platformTracks, subQueryTracks.versionTracks, subQueryTracks.typeTracks, subQueryTracks.pathTracks
             ) subQueryDatos1
       ) datos
       LEFT JOIN (SELECT jest(data,'Iniciativa') AS iniciative, jest(data,'Section') AS section
                  FROM external_data 
                  WHERE usr = 'nalaimo' 
                    AND tb = 'map_project_initiative') iniciatives ON datos.section_path = iniciatives.section
-     )
-GROUP BY fecha, iniciatives.iniciative, datos.section_path, datos.platform, datos.version, datos.type, datos.path     
+     ) 
+GROUP BY datos.fecha, IF(iniciatives.iniciative IS NULL, 'SIN DATOS EN EL MAPEO PARA EL SECTION', iniciatives.iniciative), datos.section_path, datos.platform, datos.version, datos.type, datos.path
