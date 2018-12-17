06826215ed51c8275e33b988296c9a7c57ddecf5
MARCOS POZZI <marcos.pozzi@mercadolibre.com>
2018-12-17 10:23:35 -0300

    catalogo con whitelist de path editable

--- definitions/src/main/groovy/com/melidata/definitions/manager/WhitelistPathUploader.groovy
+++ definitions/src/main/groovy/com/melidata/definitions/manager/WhitelistPathUploader.groovy
@@ -34,9 +34,6 @@ class WhitelistPathUploader {
         def json =  gson.toJson(gson.fromJson(pathContents, Map))
 
 
-        //println("Checking if there were updates")
-        //def previousVersion = s3Controller.getObject( FILE_NAME ).getObjectContent()
-
         String versionedName = "versions/path_" + new Date().format("yyyMMddHHmmss") + ".json"
 
         println("JSON loaded")
