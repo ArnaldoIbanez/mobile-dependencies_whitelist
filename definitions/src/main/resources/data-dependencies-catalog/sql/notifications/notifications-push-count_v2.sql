e0ab53cd4369d1fef50a493428a7755c8bb620ce
pballan <pballan@dc.uba.ar>
2019-02-06 15:51:39 -0300

    typo in substring

--- definitions/src/main/resources/data-dependencies-catalog/sql/notifications/notifications-push-count_v2.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/notifications/notifications-push-count_v2.sql
@@ -1,4 +1,4 @@
-select count(*) as total, t.event_type as event_type, t.marketplace as marketplace, substr(t.fecha,1,10) AS ds from 
+select count(*) as total, t.event_type as event_type, t.marketplace as marketplace, t.fecha AS ds from 
   (SELECT application.business as marketplace, SUBSTRING (ds, 1, 10) AS fecha, jest(event_data, 'event_type') as event_type
   FROM tracks
   WHERE ds >= '@param01'
