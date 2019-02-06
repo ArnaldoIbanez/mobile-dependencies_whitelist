9fc8015004e19eaa5d7c3e4858b32bbd4e3aa3f5
pballan <pballan@dc.uba.ar>
2019-02-06 13:11:17 -0300

    moved column

--- definitions/src/main/resources/data-dependencies-catalog/sql/notifications/notifications-push-count_v2.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/notifications/notifications-push-count_v2.sql
@@ -1,4 +1,4 @@
-select count(*) as total, substr(t.fecha,1,10) AS ds, t.event_type as event_type, t.marketplace as marketplace from 
+select substr(t.fecha,1,10) AS ds, count(*) as total, t.event_type as event_type, t.marketplace as marketplace from 
   (SELECT application.business as marketplace, SUBSTRING (ds, 1, 10) AS fecha, jest(event_data, 'event_type') as event_type
   FROM tracks
   WHERE ds >= '@param01'
