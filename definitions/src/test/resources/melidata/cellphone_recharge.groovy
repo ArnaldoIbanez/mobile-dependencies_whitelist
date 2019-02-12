c9c87d4136b4d1affc60f6bda9989743dc0e1edc
Federico Ventre <federico.ventre@mercadolibre.com>
2019-02-12 16:59:55 -0300

    fix test

--- definitions/src/test/resources/melidata/cellphone_recharge.groovy
+++ definitions/src/test/resources/melidata/cellphone_recharge.groovy
@@ -8,10 +8,11 @@ trackTests {
 
     test("Cellphone recharge") {
         "/cellphone_recharge"(platform: "/mobile") {
-                    }
+            
+        }
 
         "/cellphone_recharge/recents"(platform: "/mobile") {
-           
+            
         }
 
         "/cellphone_recharge/set_telephone_number"(platform: "/mobile") {
@@ -39,20 +40,20 @@ trackTests {
         }
 
         "/cellphone_recharge/terms_and_conditions"(platform: "/mobile") {
-          
+           
         }
 
         "/cellphone_recharge/px_payment_method_search"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_discount_summary"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_card_vault"(platform: "/mobile") {
             
         }
         "/cellphone_recharge/px_card_number"(platform: "/mobile") {
-          
+            
         }
         "/cellphone_recharge/px_card_holder_name"(platform: "/mobile") {
             
@@ -64,55 +65,58 @@ trackTests {
             
         }
         "/cellphone_recharge/px_identification_number"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_card_issuers"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_card_installments"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_review_and_confirm"(platform: "/mobile") {
             
         }
         "/cellphone_recharge/px_result"(platform: "/mobile") {
-          
+            result_status = "rejected"
         }
 
-        "/cellphone_recharge/push_handler"(platform: "/mobile") {
+
+"/cellphone_recharge/push_handler"(platform: "/mobile") {
             
         }
 
         "/cellphone_recharge/pay"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/deals"(platform: "/mobile") {
             
         }
         "/cellphone_recharge/deals/terms"(platform: "/mobile") {
-         
+            
         }
         "/cellphone_recharge/payment_methods"(platform: "/mobile") {
             
         }
         "/cellphone_recharge/other_payment_methods"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/cards"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/add_card"(platform: "/mobile") {
             
         }
         "/cellphone_recharge/issuers"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/result"(platform: "/mobile") {
-          
+           
+            result_status = "rejected"
+            status_detail = "call_for_auth"
         }
 
         "/cellphone_recharge/confirm"(platform: "/web"){
-            
+
         }
 
         "/cellphone_recharge/suggested_phones"(platform: "/web", type: TrackType.View) {
@@ -125,12 +129,16 @@ trackTests {
 
         }
         "/cellphone_recharge/congrats"(platform: "/web", type: TrackType.View) {
+
+            status = "approved"
         }
         "/cellphone_recharge/congrats"(platform: "/web", type: TrackType.View) {
 
+            status = "pending"
         }
         "/cellphone_recharge/congrats"(platform: "/web", type: TrackType.View) {
 
+            status = "rejected"
         }
         "/cellphone_recharge/companies"(platform: "/web", type: TrackType.View) {
 
@@ -157,33 +165,34 @@ trackTests {
 
         }
 
+
     }
 
-defaultBusiness = "mercadolibre"
+    defaultBusiness = "mercadolibre"
 
     test("Cellphone recharge") {
         "/cellphone_recharge"(platform: "/mobile") {
-           
+            
         }
 
         "/cellphone_recharge/recents"(platform: "/mobile") {
-
+            
         }
 
         "/cellphone_recharge/set_telephone_number"(platform: "/mobile") {
-
+            
         }
 
         "/cellphone_recharge/contacts"(platform: "/mobile") {
-
+          
         }
 
         "/cellphone_recharge/carriers"(platform: "/mobile") {
-
+            
         }
 
         "/cellphone_recharge/recommended"(platform: "/mobile") {
-
+            
         }
 
         "/cellphone_recharge/packages"(platform: "/mobile") {
@@ -191,11 +200,11 @@ defaultBusiness = "mercadolibre"
         }
 
         "/cellphone_recharge/carrier_disabled"(platform: "/mobile") {
-           
+            
         }
 
         "/cellphone_recharge/terms_and_conditions"(platform: "/mobile") {
-           
+            
         }
 
         "/cellphone_recharge/px_payment_method_search"(platform: "/mobile") {
@@ -205,33 +214,35 @@ defaultBusiness = "mercadolibre"
             
         }
         "/cellphone_recharge/px_card_vault"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_card_number"(platform: "/mobile") {
-         
+            
         }
         "/cellphone_recharge/px_card_holder_name"(platform: "/mobile") {
             
         }
         "/cellphone_recharge/px_card_expiry_date"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_card_security_code"(platform: "/mobile") {
             
         }
         "/cellphone_recharge/px_identification_number"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_card_issuers"(platform: "/mobile") {
-        
+            
         }
         "/cellphone_recharge/px_card_installments"(platform: "/mobile") {
-                    }
+            
+        }
         "/cellphone_recharge/px_review_and_confirm"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/px_result"(platform: "/mobile") {
-          
+           
+            result_status = "rejected"
         }
 
         "/cellphone_recharge/push_handler"(platform: "/mobile") {
@@ -239,31 +250,33 @@ defaultBusiness = "mercadolibre"
         }
 
         "/cellphone_recharge/pay"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/deals"(platform: "/mobile") {
-         
+            
         }
         "/cellphone_recharge/deals/terms"(platform: "/mobile") {
-          
+            
         }
         "/cellphone_recharge/payment_methods"(platform: "/mobile") {
-        
+            
         }
         "/cellphone_recharge/other_payment_methods"(platform: "/mobile") {
-        
+            
         }
         "/cellphone_recharge/cards"(platform: "/mobile") {
-           
+            
         }
         "/cellphone_recharge/add_card"(platform: "/mobile") {
-          
+            
         }
         "/cellphone_recharge/issuers"(platform: "/mobile") {
-          
+            
         }
         "/cellphone_recharge/result"(platform: "/mobile") {
-      
+           
+            result_status = "rejected"
+            status_detail = "call_for_auth"
         }
 
         "/cellphone_recharge/confirm"(platform: "/web"){
@@ -271,7 +284,7 @@ defaultBusiness = "mercadolibre"
         }
 
         "/cellphone_recharge/suggested_phones"(platform: "/web", type: TrackType.View) {
-
+            
         }
         "/cellphone_recharge/second_password"(platform: "/web", type: TrackType.View) {
 
@@ -281,12 +294,15 @@ defaultBusiness = "mercadolibre"
         }
         "/cellphone_recharge/congrats"(platform: "/web", type: TrackType.View) {
 
+            status = "approved"
         }
         "/cellphone_recharge/congrats"(platform: "/web", type: TrackType.View) {
 
+            status = "pending"
         }
         "/cellphone_recharge/congrats"(platform: "/web", type: TrackType.View) {
 
+            status = "rejected"
         }
         "/cellphone_recharge/companies"(platform: "/web", type: TrackType.View) {
 
@@ -313,7 +329,11 @@ defaultBusiness = "mercadolibre"
 
         }
 
+
+
+
+
+
+
     }
-
-
 }
