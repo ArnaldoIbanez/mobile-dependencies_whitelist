ad2b03db5851bb18127a5c4b5ecede80ae7b68e9
Nicolas <nmouteda@gmail.com>
2019-02-20 15:02:57 -0300

    Update query to avoid using two hive.groupby prop
    Update query to avoid using 	set hive.groupby.orderby.position.alias=true;

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/element-ctr.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/element-ctr.sql
@@ -1,5 +1,3 @@
-set hive.groupby.orderby.position.alias=true;
-
 SELECT
     prints.component AS `component`,
     prints.platform AS `device_type`,
@@ -8,8 +6,8 @@ SELECT
     prints.campaign AS `campaign`,
     prints.brand_name AS `brand_name`,
     prints.category_id AS `category_id`,
-    prints_count AS `prints`,
-    clicks_count AS `clicks`,
+    prints_count as `prints`,
+    clicks_count as `clicks`,
     prints.dsx AS `ds`
 
 FROM
@@ -23,7 +21,6 @@ FROM
     COALESCE(`jt`.`brand_name`, `jt`.`legacy_brand_name`) AS `brand_name`,
     COALESCE(`jt`.`category_id`, `jt`.`legacy_category_id`) AS `category_id`,
     COUNT(`jt`.`id`) AS `prints_count`
-    
 FROM component_prints
 LATERAL VIEW json_tuple(`data`, 'event_data') ed AS `event_data`
 LATERAL VIEW json_tuple(ed.`event_data`, 'c_event', 'c_id', 'c_element_order', 'c_campaign', 'c_brand_name', 'brand_name', 'c_category_id', 'category_id') jt AS `event`, `id`, `element_order`, `campaign`, `brand_name`, `legacy_brand_name`, `category_id`, `legacy_category_id`
@@ -31,12 +28,11 @@ LATERAL VIEW json_tuple(`data`, 'device') dev AS `device`
 LATERAL VIEW json_tuple(dev.`device`, 'platform') jt2 AS `platform`
 LATERAL VIEW json_tuple(`data`, 'application') app AS `application`
 LATERAL VIEW json_tuple(app.`application`, 'site_id') jt3 AS `site_id`
-
 WHERE ds >= '@param03 20' AND ds < '@param04 20'
     AND `jt`.`event` = 'print'
     AND `jt`.`id` IS NOT NULL
     AND (`jt`.`id` RLIKE '.*(?<=\/element)$' OR `jt`.`id` RLIKE '.*(?<=\/item)$')
-GROUP BY 1, 2, 3, 4, 5, 6, 7, 8) AS prints
+GROUP BY from_unixtime(unix_timestamp(ds, 'yyyy-MM-dd HH') + 14400, 'yyyy-MM-dd'), `jt2`.`platform`,`jt3`.`site_id`, `jt`.`id`, `jt`.`element_order`, `jt`.`campaign`, COALESCE(`jt`.`brand_name`, `jt`.`legacy_brand_name`), COALESCE(`jt`.`category_id`, `jt`.`legacy_category_id`)) AS prints
 
 LEFT JOIN
 
@@ -50,13 +46,13 @@ LEFT JOIN
     `jt`.`category_id` AS `category_id`,
     COUNT(`jt`.`id`) AS `clicks_count`
 FROM tracks
-LATERAL VIEW json_tuple(others['fragment'], 'c_id', 'c_element_order', 'c_campaign', 'c_brand_name', 'c_category_id') jt AS `id`, `element_order`, `campaign`, `brand_name`, `category_id`
+LATERAL VIEW json_tuple(others['fragment'], 'c_event', 'c_id', 'c_element_order', 'c_campaign', 'c_brand_name', 'c_category_id') jt AS `event`, `id`, `element_order`, `campaign`, `brand_name`, `category_id`
 WHERE ds >= '@param01' AND ds < '@param02'
     AND `type` = 'view'
     AND `path` <> '/recommendations'
     AND `jt`.`id` IS NOT NULL
     AND others['intersection_observer_supported'] = 'true'
-GROUP BY 1, 2, 3, 4, 5, 6, 7, 8) AS clicks
+GROUP BY SUBSTR(tracks.ds, 1, 10), device.platform, application.site_id, `jt`.`id`, `jt`.`element_order`, `jt`.`campaign`, `jt`.`brand_name`, `jt`.`category_id`) AS clicks
 
 ON
 
