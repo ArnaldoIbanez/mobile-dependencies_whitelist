b1f5cf3cd9e93bed537c49aeff6d138ed3327914
IvanRufino2471 <ivan.rufino@mercadolibre.com>
2019-02-22 00:27:49 -0300

    rename of queries

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/uid_sanitized_desktop.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/uid_sanitized_desktop.sql
@@ -2,6 +2,7 @@ SELECT
 path,
 application.business AS business,
 COUNT(DISTINCT usr.uid) AS uids_quantity,
+(SELECT COUNT(DISTINCT usr.uid) FROM tracks WHERE ds >= '@param01' AND ds < '@param02' AND device.platform IN ('/web/desktop', '/web/mobile')) AS uids_total,
 substr(ds, 1, 10) AS ds
 FROM tracks
 WHERE ds >= '@param01'
