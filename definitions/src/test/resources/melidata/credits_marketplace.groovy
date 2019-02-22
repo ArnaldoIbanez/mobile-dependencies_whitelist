7d7eb4c4a66a38d5e4e0c7edc9f351888d011e1a
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-02-22 14:44:42 -0300

    no message

--- definitions/src/test/resources/melidata/credits_marketplace.groovy
+++ definitions/src/test/resources/melidata/credits_marketplace.groovy
@@ -226,7 +226,7 @@ trackTests {
         }
 
         "/credits/consumer/myml/summary/payment_intention"(platform: "/web/desktop", type: TrackType.Event) {
-            status = "daily_charge_period"
+            loans_status = "daily_charge_period"
             place = "right_section_message"
         }
         /******************************************
