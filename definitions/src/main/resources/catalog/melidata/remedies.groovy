cf237d19f4b38f56dfea1d2f923597db52b332af
Emiliano Hernandez <emhernandez@uy0c02vg1dghv2h.ml.com>
2019-02-25 10:52:48 -0300

    fix pr

--- definitions/src/main/resources/catalog/melidata/remedies.groovy
+++ definitions/src/main/resources/catalog/melidata/remedies.groovy
@@ -9,6 +9,8 @@ tracks {
 
     "/remedies"(platform: "/", isAbstract: true) {}
 
+    "/remedies/activity"(platform: "/", isAbstract: true) {}
+
     "/remedies/executed_action"(platform: "/mobile", type: TrackType.Event) {
         executed_action(type: PropertyType.String, required: true, description: "Action executed from component actions")
         deeplink_params(type: PropertyType.String, required: false, description: "The deeplink params with which the flow was invoked")
