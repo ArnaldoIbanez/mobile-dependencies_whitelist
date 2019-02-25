5bef4237f4e67c7e87f46231f13d53f99d62c22c
augustocollerone <collerone.augusto@gmail.com>
2019-02-25 14:41:53 -0300

    PX - CVV and T&C tracks fixed

--- definitions/src/main/resources/catalog/melidata/px.groovy
+++ definitions/src/main/resources/catalog/melidata/px.groovy
@@ -47,18 +47,18 @@ tracks {
     "/px_checkout/payments/select_method/prepaid_card"(platform: "/mobile", isAbstract: true){}
 
     "/px_checkout/payments/select_method/credit_card/cvv"(platform: "/mobile", parentPropertiesInherited: false, type: TrackType.View) {
-        payment_method_id(required: true, type: PropertyType.String, description: "Payment method id")
-        card_id(required: true, type: PropertyType.String , description: "Card id")
+        payment_method_id(required: false, type: PropertyType.String, description: "Payment method id")
+        card_id(required: false, type: PropertyType.String , description: "Card id")
         externalData
     }
     "/px_checkout/payments/select_method/debit_card/cvv"(platform: "/mobile", parentPropertiesInherited: false, type: TrackType.View) {
-        payment_method_id(required: true, type: PropertyType.String, description: "Payment method id")
-        card_id(required: true, type: PropertyType.String , description: "Card id")
+        payment_method_id(required: false, type: PropertyType.String, description: "Payment method id")
+        card_id(required: false, type: PropertyType.String , description: "Card id")
         externalData
     }
     "/px_checkout/payments/select_method/prepaid_card/cvv"(platform: "/mobile", parentPropertiesInherited: false, type: TrackType.View) {
-        payment_method_id(required: true, type: PropertyType.String, description: "Payment method id")
-        card_id(required: true, type: PropertyType.String , description: "Card id")
+        payment_method_id(required: false, type: PropertyType.String, description: "Payment method id")
+        card_id(required: false, type: PropertyType.String , description: "Card id")
         externalData
     }
 
@@ -143,7 +143,7 @@ tracks {
 
     // Terms and conditions view
     "/px_checkout/payments/terms_and_conditions"(platform: "/mobile", parentPropertiesInherited: false, type: TrackType.View) {
-        url(required: true, type: PropertyType.String, description: "Web view url")
+        url(required: false, type: PropertyType.String, description: "Web view url")
         externalData
     }
 
