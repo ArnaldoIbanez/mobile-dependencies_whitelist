c02db595e4f091497a89022cbc56c23229d73f91
IvanRufino2471 <ivan.rufino@mercadolibre.com>
2019-02-27 19:39:24 -0300

    fix query

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/uid_sanity_check_navigation_v2.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/uid_sanity_check_navigation_v2.sql
@@ -1,17 +1,17 @@
-SELECT business, platform, AVG(average_uids_quantity) AS average_uids_quantity, SUM(paths_quantity) AS paths_quantity, ds FROM
-(SELECT user_id, user_second_id, business, platform, AVG(uids_quantity) AS average_uids_quantity, COUNT(*) AS paths_quantity, ds FROM
+SELECT business, platform, AVG(average_uids_quantity) AS average_uids_quantity, SUM(paths_quantity) AS paths_quantity, ds FROM (
+SELECT user_id, user_second_id, business, platform, AVG(uids_quantity) AS average_uids_quantity, COUNT(*) AS paths_quantity, ds FROM
 (SELECT usr.user_id, (CASE WHEN device.platform IN ('/web/mobile','/web/desktop') THEN device.user_agent
             WHEN device.platform IN ('/mobile/android', '/mobile/ios') THEN device.device_id
             END) AS user_second_id
 , path, application.business, device.platform, COUNT(DISTINCT usr.uid) AS uids_quantity, substr(ds,1,10) AS ds
 FROM tracks
-WHERE ds >= '2019-02-25'
-AND ds < '2019-02-26'
+WHERE ds >= '@param01'
+AND ds < '@param02'
 AND device.platform IN ('/web/desktop', '/web/mobile', '/mobile/android', '/mobile/ios')
 AND usr.uid IS NOT NULL
 AND usr.user_id IS NOT NULL
 AND usr.user_id NOT IN ('-1', '0', 'UNKNOWN', 'WMS_USER_ID')
-AND ' ' NOT IN business
+AND ' ' NOT IN (application.business)
   AND (CASE WHEN device.platform IN ('/web/mobile','/web/desktop') THEN is_bot(device.user_agent) = false AND device.user_agent IS NOT NULL
             WHEN device.platform IN ('/mobile/android', '/mobile/ios') THEN device.device_id IS NOT NULL
             END)
