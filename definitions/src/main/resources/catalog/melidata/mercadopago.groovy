6e8539d1d5c8366ecb13911142397b2e9cecaf2c
Jose Larreal <jose.larreal@mercadolibre.com>
2019-03-01 13:51:41 -0300

    cambios de required y tests

--- definitions/src/main/resources/catalog/melidata/mercadopago.groovy
+++ definitions/src/main/resources/catalog/melidata/mercadopago.groovy
@@ -179,13 +179,15 @@ tracks {
         flow (required: false, type: PropertyType.String, description: "Flow")
         error_msg (required:false, type: PropertyType.String, description: "Error shown to seller")
     }
+
+    "/pos_mobile"(platform: "/mobile", type: TrackType.Event) {}
     "/pos_mobile/friction"(platform: "/mobile", type: TrackType.Event) {
         flow_id(required: false, type: PropertyType.String, description: "Flow id.")
         id(required: true, type: PropertyType.String, description: "Flow id.")
         context(required: true, type: PropertyType.String, description: "Context friction")
-        style(required: false, type: PropertyType.String, description: "Style showed, window, dialog, toast.. ")
-        message(required: false, type: PropertyType.String, description: "Message showed ")
-        attributable_to(required: false, type: PropertyType.String, description: "User, Phone, Device, network or card")
+        style(required: true, type: PropertyType.String, description: "Style showed, window, dialog, toast.. ")
+        message(required: true, type: PropertyType.String, description: "Message showed ")
+        attributable_to(required: true, type: PropertyType.String, description: "User, Phone, Device, network or card")
         extra_info(required: false, type: PropertyType.String, description: "Extra info")
     }
     //TODO: The flow_origin field must be changed to mandatory, when all the productive versions send this information
