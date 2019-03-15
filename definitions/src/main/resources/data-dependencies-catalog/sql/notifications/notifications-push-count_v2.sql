f789cb9df8e88aeb1a844e1ae981780f8c4dbcb0
pballan <pballan@dc.uba.ar>
2019-03-15 17:04:24 -0300

    is this the real fix?

--- definitions/src/main/resources/data-dependencies-catalog/sql/notifications/notifications-push-count_v2.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/notifications/notifications-push-count_v2.sql
@@ -1,6 +1,6 @@
-SELECT summary.business AS marketplace, summary.event AS event, CONCAT(summary.business, '-', summary.fecha, '-', summary.event) AS event_id, sum(summary.total) AS total, summary.fecha AS ds
+SELECT summary.business AS marketplace, summary.event AS event, summary.total AS total, summary.fecha AS ds
 FROM 
-(SELECT application.business AS business, '@send_date' AS fecha, jest(event_data, 'event_type') AS event, 1 AS total
+(SELECT application.business AS business, '@send_date' AS fecha, jest(event_data, 'event_type') AS event, sum(1) AS total
   FROM tracks
   WHERE ds >= '@send_date' 
   AND ds < '@one_day_after_send_date'
@@ -88,5 +88,3 @@ FROM
   AND arrives.event_id IS null
   GROUP BY application.business, '@send_date', 'track_without_arrived'
 ) summary
-GROUP BY summary.business, summary.fecha, summary.event, CONCAT(summary.business, '-', summary.fecha, '-', summary.event)
-ORDER BY 1,2,3,4
