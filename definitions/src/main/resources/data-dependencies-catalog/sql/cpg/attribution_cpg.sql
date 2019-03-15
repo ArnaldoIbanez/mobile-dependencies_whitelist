42867a12baa85c582d7b4c924cf43ee09da4477b
Agustin Romano <romanoagu@gmail.com>
2019-03-15 12:16:09 -0300

    Se cambia la query para que recorra una sola vez la tabla tracks

--- definitions/src/main/resources/data-dependencies-catalog/sql/cpg/attribution_cpg.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/cpg/attribution_cpg.sql
@@ -13,24 +13,9 @@ from melilake.bt_bids a11
           FROM
             tracks 
           WHERE
-            ds >= @param01 
-            AND ds < @param02
-            AND path like '/search'
-            AND jest(event_data, 'filters.deal') = 'MLM525'
-          group by 
-            substr(ds,1,10),
-            usr.user_id,
-            device.platform
-      union all
-          SELECT usr.user_id as user_id,
-            device.platform as platform,
-            substr(ds,1,10) AS track_date
-          FROM
-            tracks 
-          WHERE
             ds >= @param01
             AND ds < @param02
-            AND path like '/home/supermarket'
+            AND ( (path like '/home/supermarket') OR  (path like '/search' AND jest(event_data, 'filters.deal') = 'MLM525') )
           group by 
             substr(ds,1,10),
             usr.user_id,
