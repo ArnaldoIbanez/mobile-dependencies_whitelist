f3448afb5df8b4f30e08a705bf4cc2bf5ae09d73
AgusBMeli <agustin.blanco@mercadolibre.com>
2019-03-28 13:55:37 -0300

    vip-trackings-convertions: dejo solo para web/mobile y desktop

--- definitions/src/main/resources/data-dependencies-catalog/sql/vip/vip-trackings-convertion.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/vip/vip-trackings-convertion.sql
@@ -12,5 +12,5 @@ and application.site_id in ('MLA', 'MLB', 'MLC', 'MLM', 'MCO', 'MLV', 'MPE', 'ML
 and jest(event_data, 'vertical') = 'core'
 and (((regexp_like(application.version, '(9\.2[8-9][0-9]*\.)|(9\.[3-9][0-9]\.)|(9\.[0-9]{3,}\.)|([1-9][0-9]{1,}\.[0-9]+\.)'))
   and device.platform = '/mobile/android') or ((regexp_like(application.version, '(10\.4[1-9]\.)|(10\.[4-9][2-9]\.)|(10\.[5-9][0-9]\.)|(10\.[0-9]{3,}\.)|([0-9]{3,}\.[0-9]+\.)'))
-  and device.platform = '/mobile/ios') or (device.platform LIKE '/web%'))
+  and device.platform = '/mobile/ios') or (device.platform LIKE '/web/desktop') or (device.platform LIKE '/web/mobile'))
 GROUP BY application.site_id, device.platform, substr(ds, 1, 10);
