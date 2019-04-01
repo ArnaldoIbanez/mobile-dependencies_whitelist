3d8793e488fd5c33a1ff58aea806246c5c7a2544
Nicolas Vespoli <nicolas.vespoli@mercadolibre.com>
2019-04-01 14:44:53 -0300

    Fix de query y json warranty_items

--- definitions/src/main/resources/data-dependencies-catalog/sql/seller_central/warranty/warranty_items.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/seller_central/warranty/warranty_items.sql
@@ -5,13 +5,14 @@ f.domainId as domainId,
 (CASE WHEN f.clientId IN (1570, 1572, 1573, 1574, 1575, 1576, 1577, 1578, 1754, 6002, 6009, 6010, 7092, 1505, 6808261514773724, 3376461333454861, 1449998327610753,
 5689318823430110, 4060709329251625, 1165691498204664, 3942444344910321, 4954751883138633)
 THEN 'SYI' ELSE 'OTROS' END) as client,
-f.itemCondition as itemCondition, COUNT(*) as totalItems,
+f.itemCondition as itemCondition,
+COUNT(*) as totalItems,
 SUM(CASE WHEN f.warrantyText is null AND f.saleTermValueId is null THEN 1 ELSE 0 END) as SinGarantia,
 SUM(CASE WHEN f.warrantyText is not null AND f.saleTermValueId is null THEN 1 ELSE 0 END) as Desestructurada,
 SUM(CASE WHEN f.saleTermValueId = 2230279 AND f.saleTermValueName is not null THEN 1 ELSE 0 END) as EstructuradaFabrica,
 SUM(CASE WHEN f.saleTermValueId = 2230280 AND f.saleTermValueName is not null THEN 1 ELSE 0 END) as EstructuradaVendedor,
 SUM(CASE WHEN f.saleTermValueId = 6150835 AND f.saleTermValueName is null THEN 1 ELSE 0 END) as EstructuradaSinGarantia,
-CAST(itemDateCreated as string) as ds
+substr(itemDateCreated, 1, 10) as ds
 FROM
 
 (SELECT c.siteId, c.itemId, c.domainId, c.itemCondition, c.warrantyText, c.clientId, c.itemDateCreated, d.saleTermValueId, d.saleTermValueName
@@ -38,4 +39,4 @@ ON c.siteId = d.siteId AND c.itemId = d.itemId) f
 WHERE CAST(itemDateCreated as string) >= '@param01'
 AND CAST(itemDateCreated as string) < '@param02'
 
-GROUP BY cast(itemDateCreated as string), siteId, domainId, clientId, itemCondition
+GROUP BY substr(itemDateCreated, 1, 10), siteId, domainId, clientId, itemCondition
