89af26efc0f3664180f19a6c8308fd5f0bcffcaf
AgusBMeli <agustin.blanco@mercadolibre.com>
2019-04-11 10:13:59 -0300

    vip-trackings-convertion-v2: cambio posible palabra reservada

--- definitions/src/main/resources/data-dependencies-catalog/sql/vip/vip-trackings-convertion-v2.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/vip/vip-trackings-convertion-v2.sql
@@ -1,8 +1,8 @@
-with vips as (
+WITH vipsView as (
 select application.site_id as sit, device.platform as dev, count(*) as tvip, substr(ds,1,10) as dateS
 from tracks t
-WHERE ds >= '@param01'
-AND   ds < '@param02'
+WHERE ds >= '2019-04-07'
+AND   ds < '2019-04-08'
 AND   application.business = 'mercadolibre'
 AND   application.site_id in ('MLA','MLM','MLB','MPE','MCO','MLC','MLU','MLV')
 and path in ('/vip')
@@ -13,8 +13,8 @@ group by application.site_id, device.platform, substr(ds,1,10)),
 buyI as (
 select application.site_id as sit, device.platform as dev, count(*) as tvip, substr(ds,1,10) as dateS
 from tracks t
-WHERE ds >= '@param01'
-AND   ds < '@param02'
+WHERE ds >= '2019-04-07'
+AND   ds < '2019-04-08'
 AND   application.business = 'mercadolibre'
 AND   application.site_id in ('MLA','MLM','MLB','MPE','MCO','MLC','MLU','MLV')
 and path in ('/vip/buy_action')
@@ -24,8 +24,8 @@ group by application.site_id, device.platform, substr(ds,1,10)),
 cartI as (
 select application.site_id as sit, device.platform as dev, count(*) as tvip, substr(ds,1,10) as dateS
 from tracks t
-WHERE ds >= '@param01'
-AND   ds < '@param02'
+WHERE ds >= '2019-04-07'
+AND   ds < '2019-04-08'
 AND   application.business = 'mercadolibre'
 AND   application.site_id in ('MLA','MLM','MLB','MPE','MCO','MLC','MLU','MLV')
 and path in ('/vip/add_cart_action')
@@ -35,8 +35,8 @@ group by application.site_id, device.platform, substr(ds,1,10)),
 gratsCommon as (
 select application.site_id as sit, device.platform as dev, count(*) as tvip, substr(ds,1,10) as dateS
 from tracks t
-WHERE ds >= '@param01'
-AND   ds < '@param02'
+WHERE ds >= '2019-04-07'
+AND   ds < '2019-04-08'
 AND   application.business = 'mercadolibre'
 AND   application.site_id in ('MLA','MLM','MLB','MPE','MCO','MLC','MLU','MLV')
 and path in ('/checkout/congrats')
@@ -46,8 +46,8 @@ group by application.site_id, device.platform, substr(ds,1,10)),
 gratsCart as (
 select application.site_id as sit, device.platform as dev, count(*) as tvip, substr(ds,1,10) as dateS
 from tracks t
-WHERE ds >= '@param01'
-AND   ds < '@param02'
+WHERE ds >= '2019-04-07'
+AND   ds < '2019-04-08'
 AND   application.business = 'mercadolibre'
 AND   application.site_id in ('MLA','MLM','MLB','MPE','MCO','MLC','MLU','MLV')
 and path in ('/cart/checkout/congrats')
@@ -55,7 +55,7 @@ and ((application.version rlike '(9\.29[0-9]*\.)|(9\.[3-9][0-9]\.)|(9\.[0-9]{3,}
 and type = 'view'
 group by application.site_id, device.platform, substr(ds,1,10))
 select v.sit as site, v.dev as device, v.tvip as total_vips, bi.tvip as amount_buy, ci.tvip as amount_cart, gCo.tvip as amount_buy_real, gCa.tvip as amount_cart_real, v.dateS as ds
-from vips v
+from vipsView v
 join buyI bi on bi.sit = v.sit and bi.dev = v.dev
 left join cartI ci on ci.sit = v.sit and ci.dev = v.dev
 join gratsCommon gCo on gCo.sit = v.sit and gCo.dev = v.dev
