ba85e687f71975c62bad6982397c394a9fd418c7
Nicolas Rudolph <nicolas.rudolph@mercadolibre.com>
2019-04-23 17:43:19 -0300

    melilake orders vs ordercreated added

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/melilake-orders-vs-ordercreated.sql
@@ -0,0 +1,13 @@
+select site, coalesce( path, 'no_track'), count(1) as counter, date_created
+from (
+  select o.ord_order_id as id , o.ord_created_dt as date_created, o.sit_site_id as site, t.path as path
+  from melilake.bt_odr_purchase_orders o
+  left join (
+    select distinct get_json_object(event_data, '$.order_id') as order_id, path
+      from default.tracks where ds >= '@param1'
+        and ds < '@param2'
+        and path='/orders/ordercreated'
+  ) t on (cast(o.ord_order_id as string) = t.order_id)
+  where substring(from_unixtime(unix_timestamp(o.ord_created_dt, 'MMM dd, yyyy')),1,10)='@param3' and o.is_test = 'false'
+) a
+group by a.date_created, site, a.path
