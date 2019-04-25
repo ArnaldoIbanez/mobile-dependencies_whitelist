09094faf2346fb8170104f08b3e8f50c82f0208f
matiasromar <matias.romar@mercadolibre.com>
2019-04-25 16:48:32 -0300

    removed discount_id

--- definitions/src/main/resources/catalog/melidata/px.groovy
+++ definitions/src/main/resources/catalog/melidata/px.groovy
@@ -193,21 +193,18 @@ tracks {
     "/px_checkout/result/success"(platform: "/mobile", type: TrackType.View) {
         preference_amount(required: true, type: PropertyType.Numeric, description: "Total amount")
         currency_id(required: true, type: PropertyType.String, description: "Currency id")
-        discount_id(required: false, type: PropertyType.String, description: "Discount id")
         discount_coupon_amount(required: false, type: PropertyType.Numeric, description: "Discount coupon amount")
         has_split_payment(required: true, type: PropertyType.Boolean, description: "Pay with split payment")
     }
     "/px_checkout/result/further_action_needed"(platform: "/mobile", type: TrackType.View) {
         preference_amount(required: true, type: PropertyType.Numeric, description: "Total amount")
         currency_id(required: true, type: PropertyType.String, description: "Currency id")
-        discount_id(required: false, type: PropertyType.String, description: "Discount id")
         discount_coupon_amount(required: false, type: PropertyType.Numeric, description: "Discount coupon amount")
         has_split_payment(required: true, type: PropertyType.Boolean, description: "Pay with split payment")
     }
     "/px_checkout/result/error"(platform: "/mobile", type: TrackType.View) {
         preference_amount(required: true, type: PropertyType.Numeric, description: "Total amount")
         currency_id(required: true, type: PropertyType.String, description: "Currency id")
-        discount_id(required: false, type: PropertyType.String, description: "Discount id")
         discount_coupon_amount(required: false, type: PropertyType.Numeric, description: "Discount coupon amount")
         has_split_payment(required: true, type: PropertyType.Boolean, description: "Pay with split payment")
     }
