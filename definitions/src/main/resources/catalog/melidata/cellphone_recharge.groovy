8e167185ff142bf0afb3d6a2b42e01f51a14070c
Peter <pedro.wav@gmail.com>
2019-04-26 15:55:35 -0300

    EVENTS VALIDATION AND CONTACTS ERROR

--- definitions/src/main/resources/catalog/melidata/cellphone_recharge.groovy
+++ definitions/src/main/resources/catalog/melidata/cellphone_recharge.groovy
@@ -62,7 +62,48 @@ tracks {
     "/cellphone_recharge/error"(platform: "/web", type: TrackType.View) {}
     "/cellphone_recharge/not-found"(platform: "/web", type: TrackType.View) {}
 
+    "/set_telephone_number_blacklist"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
     "/set_telephone_number_invalid_country_code"(platform: "/mobile", type: TrackType.Event) {
         category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_missing_area_code"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_invalid_area_code"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_missing_phone_number"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_invalid_phone_number"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_short_phone_number"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_long_phone_number"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_invalid_sum_up"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/set_telephone_number_unknown"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
+    }
+    "/contacts_invalid"(platform: "/mobile", type: TrackType.Event) {
+        category(required: true, type: PropertyType.String, description: "Event Category")
+        label(required: false, type: PropertyType.String, description: "Optional Description")
     }
 }
--- definitions/src/test/resources/melidata/cellphone_recharge.groovy
+++ definitions/src/test/resources/melidata/cellphone_recharge.groovy
@@ -164,8 +164,38 @@ trackTests {
         "/cellphone_recharge/not-found"(platform: "/web", type: TrackType.View) {
 
         }
+        "/set_telephone_number_blacklist"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
         "/set_telephone_number_invalid_country_code"(platform: "/mobile") {
             category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_missing_area_code"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_invalid_area_code"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_missing_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_invalid_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_short_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_long_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_invalid_sum_up"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_unknown"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/contacts_invalid"(platform: "/mobile") {
+            category = "CONTACTS_ERROR"
         }
 
     }
@@ -331,14 +361,39 @@ trackTests {
 
         }
 
+
+        "/set_telephone_number_blacklist"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
         "/set_telephone_number_invalid_country_code"(platform: "/mobile") {
             category = "VALIDATOR_ERROR"
         }
-
-
-
-
-
-
+        "/set_telephone_number_missing_area_code"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_invalid_area_code"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_missing_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_invalid_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_short_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_long_phone_number"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_invalid_sum_up"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/set_telephone_number_unknown"(platform: "/mobile") {
+            category = "VALIDATOR_ERROR"
+        }
+        "/contacts_invalid"(platform: "/mobile") {
+            category = "CONTACTS_ERROR"
+        }
     }
 }
