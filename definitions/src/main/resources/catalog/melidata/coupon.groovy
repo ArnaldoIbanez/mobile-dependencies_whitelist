82b6b0f3bf27994947f51e47b4d74d7f2b4da995
Juan Joel Cerviño <cerviniojoel@gmail.com>
2019-04-29 15:19:03 -0300

    - fix pr

--- definitions/src/main/resources/catalog/melidata/coupon.groovy
+++ definitions/src/main/resources/catalog/melidata/coupon.groovy
@@ -11,47 +11,41 @@ tracks {
      */
 
     // General Path
-    "/coupon"(platform: "/", isAbstract: true) {}
+    "/coupon"(platform: "/", isAbstract: true) {
+        code(required: false, type: PropertyType.String, description: "Indicates the coupon code")
+    }
 
     // MOBILE
 
 
     // Coupon Input  Screen
-    "/coupon/input"(platform: "/mobile", type: TrackType.View) {
-        code(required: false, type: PropertyType.String, description: "Indicates the coupon code")
-    }
+    "/coupon/input"(platform: "/mobile", type: TrackType.View, parentPropertiesInherited: false) {}
 
     // Coupon Input  Events
-    "/coupon/input/continue"(platform: "/mobile", type: TrackType.Event) {
-        code(required: true, type: PropertyType.String, description: "Indicates the coupon code")
-    }
+    "/coupon/input/continue"(platform: "/mobile", type: TrackType.Event) {}
 
     // Coupon Success Screen
     "/coupon/success"(platform: "/mobile", type: TrackType.View) {
         flow(required: false, type: PropertyType.String, description: "Indicates the flow of screen")
         entity_id(required: false, type: PropertyType.Numeric, description: "Indicates the entity id")
         entity_type(required: true, type: PropertyType.String, description: "Which success type are we tracking")
-        code(required: true, type: PropertyType.String, description: "Indicates the coupon code")
     }
 
     // Coupon Success Events
     "/coupon/success/button"(platform: "/mobile", type: TrackType.Event) {
         entity_type(required: true, type: PropertyType.String, description: "Which success type are we tracking")
-        code(required: true, type: PropertyType.String, description: "Indicates the coupon code")
         type(required: true, type: PropertyType.String, description: "Which action type are we tracking (ex: primary / secondary / tyc)", values: ["primary", "secondary", "tyc"])
         action(required: true, type: PropertyType.String, description: "Which action are we tracking (ex: retry / back_to_root / use_coupon / tyc)", values: ["retry", "back_to_root", "use_coupon", "tyc"])
     }
 
     // Coupon Error Screen
     "/coupon/error"(platform: "/mobile", type: TrackType.View) {
-        code(required: true, type: PropertyType.String, description: "Indicates the coupon code")
         entity_type(required: true, type: PropertyType.String, description: "Which error type are we tracking")
     }
 
     // Coupon Error Events
     "/coupon/error/button"(platform: "/mobile", type: TrackType.Event) {
         entity_type(required: true, type: PropertyType.String, description: "Which success type are we tracking")
-        code(required: true, type: PropertyType.String, description: "Indicates the coupon code")
         type(required: true, type: PropertyType.String, description: "Which action type are we tracking (ex: primary / secondary / tyc)", values: ["primary", "secondary", "tyc"])
         action(required: true, type: PropertyType.String, description: "Which action are we tracking (ex: retry / back_to_root / use_coupon / tyc)", values: ["retry", "back_to_root", "use_coupon", "tyc"])
     }
