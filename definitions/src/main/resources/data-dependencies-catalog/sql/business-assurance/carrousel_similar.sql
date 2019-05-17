f62e5415e11b0b0db27ac1a2abd7c3adc03d214e
Fede Carballo <federico.carballo@mercadolibre.com>
2019-05-17 17:20:22 -0300

    Delete with

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/carrousel_similar.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/carrousel_similar.sql
@@ -1,25 +1,13 @@
-with prints as (
- SELECT  substr(ds,1,10) ds,
-          application.site_id site_id,
-          device.platform platform,
-          jest(event_data,'recommendations.client') client, 
-          jest(event_data, 'recommendations.backend_id') backend_id,
-          SUM(CASE WHEN jest(event_data, 'recommendations.track_info.has_recommendations') = 'true' THEN 1 ELSE 0 END) vips_with_bundle,
-          count(1) cnt
-  FROM tracks
-  WHERE path = '/recommendations'
-    AND ds >= DATE_FORMAT('@param01' - INTERVAL '5' DAY,'YYYY-MM-dd')
-    AND ds < DATE_FORMAT('@param01','YYYY-MM-dd')
-    AND type = 'view'
-    AND application.site_id IN ('MLA')
-    AND application.business = 'mercadolibre'
-    AND NOT is_bot(device.user_agent)
-    AND jest(event_data, 'recommendations.client') = 'vip'
-    AND device.platform in ('/web/desktop','/web/mobile')
-    AND jest(event_data, 'recommendations.backend_id') = 'tagging-searchsimilar_fashion'
-  GROUP BY  substr(ds,1,10), jest(event_data, 'recommendations.client'), jest(event_data, 'recommendations.backend_id'), device.platform, application.site_id
-),
-clicks AS (
+SELECT
+  p.ds AS ds,
+  p.platform AS platform,
+  p.backend_id AS backend_id,
+  p.cnt AS total_vips,
+  p.vips_with_bundle AS vips_with_reco,
+  c.total_clics AS total_clicks,
+  REGEXP_REPLACE (CAST ((p.vips_with_bundle / CAST(p.cnt AS DOUBLE)) * 100 AS STRING), '\\.', ',') AS coverage,
+  REGEXP_REPLACE (CAST (((c.total_clics / CAST(p.vips_with_bundle AS DOUBLE)) * 100) AS STRING), '\\.', ',') AS ctr
+FROM (
   SELECT
     device.platform AS platform,
     application.site_id AS site_id,
@@ -44,18 +32,28 @@ clicks AS (
     jest(others['fragment'], 'reco_client'),
     jest(others['fragment'], 'reco_backend'),
     substr(t.ds, 1, 10)
-)
-SELECT
-  p.ds AS ds,
-  p.platform AS platform,
-  p.backend_id AS backend_id,
-  p.cnt AS total_vips,
-  p.vips_with_bundle AS vips_with_reco,
-  c.total_clics AS total_clicks,
-  REGEXP_REPLACE (CAST ((p.vips_with_bundle / CAST(p.cnt AS DOUBLE)) * 100 AS STRING), '\\.', ',') AS coverage,
-  REGEXP_REPLACE (CAST (((c.total_clics / CAST(p.vips_with_bundle AS DOUBLE)) * 100) AS STRING), '\\.', ',') AS ctr
-FROM clicks c 
-  JOIN prints p 
+  ) c 
+  JOIN (
+    SELECT  substr(ds,1,10) ds,
+      application.site_id site_id,
+      device.platform platform,
+      jest(event_data,'recommendations.client') client, 
+      jest(event_data, 'recommendations.backend_id') backend_id,
+      SUM(CASE WHEN jest(event_data, 'recommendations.track_info.has_recommendations') = 'true' THEN 1 ELSE 0 END) vips_with_bundle,
+      count(1) cnt
+    FROM tracks
+    WHERE path = '/recommendations'
+      AND ds >= DATE_FORMAT('@param01' - INTERVAL '5' DAY,'YYYY-MM-dd')
+      AND ds < DATE_FORMAT('@param01','YYYY-MM-dd')
+      AND type = 'view'
+      AND application.site_id IN ('MLA')
+      AND application.business = 'mercadolibre'
+      AND NOT is_bot(device.user_agent)
+      AND jest(event_data, 'recommendations.client') = 'vip'
+      AND device.platform in ('/web/desktop','/web/mobile')
+      AND jest(event_data, 'recommendations.backend_id') = 'tagging-searchsimilar_fashion'
+    GROUP BY  substr(ds,1,10), jest(event_data, 'recommendations.client'), jest(event_data, 'recommendations.backend_id'), device.platform, application.site_id
+  ) p 
   ON (
     p.backend_id = c.backend_id 
       AND p.platform = c.platform
