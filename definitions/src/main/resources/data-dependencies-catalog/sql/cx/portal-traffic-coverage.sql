8a1893c64646849e94a9658c3ae0f6a4b6b2f07c
Unknown <occhiuzzi.santiago@gmail.com>
2019-05-24 11:42:28 -0300

    Apagado de faqs en métrica de cobertura

--- definitions/src/main/resources/data-dependencies-catalog/sql/cx/portal-traffic-coverage.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/cx/portal-traffic-coverage.sql
@@ -15,7 +15,11 @@ LATERAL VIEW json_tuple(t.event_data, 'portal_source_id', 'portal_content_id', '
 where 
     ds >= '@param01 23' and
     ds < '@param02 23' and 
-    (path LIKE '/support/widget/faq%' OR path LIKE '/support/widget/folder%' OR path LIKE '/portal/faq%')
+    (
+        path LIKE '/support/widget/faq%' OR
+        -- path LIKE '/support/widget/folder%' OR -- Esto quedará apagado que se envíe el content type, para asegurar retrocompatibilidad
+        path LIKE '/portal/faq%'
+    )
 GROUP BY
     v2.portal_source_id, 
     v2.portal_content_id, 
