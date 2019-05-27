7bad6efa3eac397d18e9c05fb85548b26e2f90fa
lgrill <linette.grill@mercadolibre.com>
2019-05-27 11:43:39 -0300

    *

--- definitions/src/test/resources/melidata/search.groovy
+++ definitions/src/test/resources/melidata/search.groovy
@@ -224,7 +224,7 @@ trackTests {
         "/search/filters"(platform: "/mobile", defaultSearchInformation)
         "/search/filters_carousel"(platform: "/web", type: TrackType.Event){
             defaultWebTrack()
-            name = "19"
+            name = "shoes"
             position = 4
         }
         "/search/back"(platform: "/mobile", defaultSearchInformation)
