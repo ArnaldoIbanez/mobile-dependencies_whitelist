4581f05ad326dd39a2423277ee31eae92e293c7a
lgrill <linette.grill@mercadolibre.com>
2019-05-27 11:37:23 -0300

    *

--- definitions/src/test/resources/melidata/search.groovy
+++ definitions/src/test/resources/melidata/search.groovy
@@ -224,7 +224,7 @@ trackTests {
         "/search/filters"(platform: "/mobile", defaultSearchInformation)
         "/search/filters_carousel"(platform: "/web", type: TrackType.Event){
             defaultWebTrack()
-            name = "9997262-AMLA_7262_2"
+            name = "19"
             position = 4
         }
         "/search/back"(platform: "/mobile", defaultSearchInformation)
