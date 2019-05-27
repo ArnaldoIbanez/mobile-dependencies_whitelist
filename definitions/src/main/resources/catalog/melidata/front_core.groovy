2af669d896365c94886a5b64f9e2471f87fa9891
Julián Lima <julian.lima@mercadolibre.com>
2019-05-27 15:16:02 -0300

    change paths in home

--- definitions/src/main/resources/catalog/melidata/front_core.groovy
+++ definitions/src/main/resources/catalog/melidata/front_core.groovy
@@ -12,96 +12,61 @@ tracks {
     *       HOME TRACKS      *
     *************************/
 
-    def headerTitle = objectSchemaDefinitions {
-        title(required: true, type: PropertyType.String)
+    def item_value_definition = objectSchemaDefinitions {
+        id(required: true, type: PropertyType.String)
+        ordinal(required: true, type: PropertyType.Numeric)
+        type(required: true, type: PropertyType.String)
     }
 
-    "/front_core" (platform: "/mobile", isAbstract: true) {}
+    "/wallet/home" (platform: "/mobile", isAbstract: true) {}
 
     // Views
-    "/front_core/home" (platform: "/mobile", type: TrackType.View) {}
+    "/wallet/home" (platform: "/mobile", type: TrackType.View) {}
 
     // Events
-    "/front_core/home/pull" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/pull" (platform: "/mobile", type: TrackType.Event) {
         backend_tracking_data(required: true, type: PropertyType.Map)
      }
 
-    "/front_core/home/show" (platform: "/mobile", type: TrackType.Event) {
-        header(required: true, type: PropertyType.Map(headerTitle))
-        items(required: true, type: PropertyType.Map)
+    "/wallet/home/show" (platform: "/mobile", type: TrackType.Event) {
+        header(required: true, type: PropertyType.String, description: "Contains the header string")
+        items(required: true, type: PropertyType.ArrayList(PropertyType.Map(item_value_definition)), description: "Contains the sections payload")
         backend_tracking_data(required: true, type: PropertyType.Map)
     }
 
-    "/front_core/home/banking" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/show/banking" (platform: "/mobile", parentPropertiesInherited: false, type: TrackType.Event) {
         type(required: true, type: PropertyType.String)
-        ordinal(required: true, type: PropertyType.Numeric)
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position in the list of sections")
     }
 
-    "/front_core/home/main_actions" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/show/main_actions" (platform: "/mobile", parentPropertiesInherited: false, type: TrackType.Event) {
         type(required: true, type: PropertyType.String)
-        ordinal(required: true, type: PropertyType.Numeric)
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position in the list of sections")
     }
 
-    "/front_core/home/secondary_actions" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/show/secondary_actions" (platform: "/mobile", parentPropertiesInherited: false, type: TrackType.Event) {
         type(required: true, type: PropertyType.String)
-        ordinal(required: true, type: PropertyType.Numeric)
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position in the list of sections")
     }
 
-    "/front_core/home/benefits" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/show/benefits" (platform: "/mobile", parentPropertiesInherited: false, type: TrackType.Event) {
         type(required: true, type: PropertyType.String)
-        ordinal(required: true, type: PropertyType.Numeric)
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position in the list of sections")
     }
 
-    "/front_core/home/cross_selling" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/show/cross_selling" (platform: "/mobile", parentPropertiesInherited: false, type: TrackType.Event) {
         type(required: true, type: PropertyType.String)
-        ordinal(required: true, type: PropertyType.Numeric)
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position in the list of sections")
     }
 
-    "/front_core/home/activities" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/show/activities" (platform: "/mobile", parentPropertiesInherited: false, type: TrackType.Event) {
         type(required: true, type: PropertyType.String)
-        ordinal(required: true, type: PropertyType.Numeric)
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position in the list of sections")
     }
 
-    "/front_core/home/qr_map" (platform: "/mobile", type: TrackType.Event) {
+    "/wallet/home/show/qr_map" (platform: "/mobile", parentPropertiesInherited: false, type: TrackType.Event) {
         type(required: true, type: PropertyType.String)
-        ordinal(required: true, type: PropertyType.Numeric)
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position in the list of sections")
     }
 
-    /********************************
-    *     ACTIVITY MOBILE TRACKS    *
-    *********************************/
-
- 
-    "/front_core/activities" (platform: "/mobile", isAbstract: true) {}
-
-    // MP Activities List
-    "/front_core/activities/list" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/list/remove_filter" (platform: "/mobile", type: TrackType.Event) {}
-
-    // MP Activities Filters
-    "/front_core/activities/filters" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/filters/options" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/filters/apply" (platform: "/mobile", type: TrackType.Event) {}
-
-    // MP Activities Opertion Detail
-    "/front_core/activities/detail" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/detail/add_note" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/detail/list" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/detail/user_info" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/detail/shipping" (platform: "/mobile", type: TrackType.View) {}
-    "/front_core/activities/detail/web_view" (platform: "/mobile", type: TrackType.View) {
-        url(required: true, type: PropertyType.String, description: "The url that will load the webview")
-    }
-
-    // MP Activities Realtime Activities
-    "/front_core/activities/real_time" (platform: "/mobile", isAbstract: true) {}
-
-    "/front_core/activities/real_time/push" (platform: "/mobile", type: TrackType.Event) {
-        activity_id(required: true, type: PropertyType.String, description: "id of the activity")
-        date_created(required: true, type: PropertyType.String, description: "the creation date of the activity")
-    }
-
-    "/front_core/activities/real_time/total" (platform: "/mobile", type: TrackType.Event) {
-        total(required: true, type: PropertyType.Numeric, description: "Total of activities sended")
-    }
 }
--- definitions/src/test/resources/melidata/front_core.groovy
+++ definitions/src/test/resources/melidata/front_core.groovy
@@ -18,125 +18,68 @@ trackTests {
     *************************/
 
     test("Mercadopago Home") {
-        "/front_core/home" (platform: "/mobile", type: TrackType.View) {}
+        "/wallet/home" (platform: "/mobile", type: TrackType.View) {}
     }
 
     test("Mercadopago Home Pull") {
-        "/front_core/home/pull" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/pull" (platform: "/mobile", type: TrackType.Event) {
             backend_tracking_data = [ title: "name" ]
         }
     }
 
     test("Mercadopago Home Show") {
-        "/front_core/home/show" (platform: "/mobile", type: TrackType.Event) {
-            header = [ title : "name" ]
+        "/wallet/home/show" (platform: "/mobile", type: TrackType.Event) {
+            header = "name"
             items = [
-                banking: [
+                [
+                    id: "banking",
                     ordinal: 1,
                     type: "BANKING"
+                ],
+                [
+                    id: "cross_selling",
+                    ordinal: 2,
+                    type: "CARROUSEL"
                 ]
             ]
-            backend_tracking_data = [ title: "name" ]
+            backend_tracking_data = [ partial: false ]
         }
     }
     
     test("Mercadopago Home Show by Id") {
-        "/front_core/home/banking" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/show/banking" (platform: "/mobile", type: TrackType.Event) {
             type = "BANKING"
             ordinal = 1
         }
 
-        "/front_core/home/main_actions" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/show/main_actions" (platform: "/mobile", type: TrackType.Event) {
             type = "MAIN_ACTIONS"
             ordinal = 2
         }
 
-        "/front_core/home/secondary_actions" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/show/secondary_actions" (platform: "/mobile", type: TrackType.Event) {
             type = "SECONDARY_ACTIONS"
             ordinal = 3
         }
 
-        "/front_core/home/benefits" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/show/benefits" (platform: "/mobile", type: TrackType.Event) {
             type = "VERTICAL_LIST"
             ordinal = 4
         }
 
-        "/front_core/home/cross_selling" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/show/cross_selling" (platform: "/mobile", type: TrackType.Event) {
             type = "CAROUSEL"
             ordinal = 5
         }
 
-        "/front_core/home/activities" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/show/activities" (platform: "/mobile", type: TrackType.Event) {
             type = "ACTIVITIES"
             ordinal = 6
         }
 
-        "/front_core/home/qr_map" (platform: "/mobile", type: TrackType.Event) {
+        "/wallet/home/show/qr_map" (platform: "/mobile", type: TrackType.Event) {
             type = "PROMOTION"
             ordinal = 7
-        }
-    }
-
-    /************************************
-    *    ACTIVITY MOBILE TEST TRACKS    *
-    ************************************/
-
-    test("Mercadopago Activities List Mobile") {
-        "/front_core/activities/list" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Activity Apply Filters Mobile") {
-        "/front_core/activities/filters/apply" (platform: "/mobile", type: TrackType.Event) {}
-    }
-
-    test("Mercadopago Activity Filters List Mobile") {
-        "/front_core/activities/filters" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Activity Filters Options List Mobile") {
-        "/front_core/activities/filters/options" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Activity Remove Filters Mobile") {
-        "/front_core/activities/list/remove_filter" (platform: "/mobile", type: TrackType.Event) {}
-    }
-
-    test("Mercadopago Operation Detail List Mobile") {
-        "/front_core/activities/detail" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Operation Detail List Mobile") {
-        "/front_core/activities/detail/list" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Operation Detail Add Note List Mobile") {
-        "/front_core/activities/detail/add_note" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Operation Detail Buyer Information Mobile") {
-        "/front_core/activities/detail/user_info" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Operation Detail Shipping Mobile") {
-        "/front_core/activities/detail/shipping" (platform: "/mobile", type: TrackType.View) {}
-    }
-
-    test("Mercadopago Operation Detail Web View Mobile") {
-        "/front_core/activities/detail/web_view" (platform: "/mobile", type: TrackType.View) {
-            url = "https://mercadolibre.com"
-        }
-    }
-
-    test("Mercadopago Operation Detail Send Realtimes Activities Mobile") {
-        "/front_core/activities/real_time/push" (platform: "/mobile", type: TrackType.Event) {
-            activity_id = 'merch-123124'
-            date_created = '12/11/19'
-        }
-    }
-
-    test("Mercadopago Operation Detail Number Of Realtimes Activities Mobile") {
-        "/front_core/activities/real_time/total" (platform: "/mobile", type: TrackType.Event) {
-            total = 12
         }
     }
 }
