73bb9dceb0e2e28f304f563a09f4e837ad265940
Nicolas Vespoli <nicolas.vespoli@mercadolibre.com>
2019-05-31 16:42:54 -0300

    Change property group and fix tests

--- definitions/src/test/resources/melidata/seller_central.groovy
+++ definitions/src/test/resources/melidata/seller_central.groovy
@@ -470,7 +470,6 @@ trackTests {
 
     test("seller central track to measure health changes"){
         "/seller_central/modify/success"(platform: "/web", type: TrackType.Event){
-            item_type = "default"
             item_id = "MLA12345"
             session_id = "123-update-abc123"
             goals_achieved = ["mercado_envios"]
@@ -513,7 +512,6 @@ trackTests {
 
     test("structured data hint available"){
         "/seller_central/modify/technical_specifications/hints/available"(platform: "/", type: TrackType.Event){
-            item_type = "default"
             type = "actionable"
             attribute = "BRAND"
             category_domain= "MLA-CAMERAS"
@@ -525,7 +523,6 @@ trackTests {
 
     test("structured data hint showed"){
         "/seller_central/modify/technical_specifications/hints/showed"(platform: "/", type: TrackType.Event){
-            item_type = "default"
             type = "info"
             attribute = "BRAND"
             category_domain= "MLA-CAMERAS"
@@ -537,7 +534,6 @@ trackTests {
 
     test("structured data hint completed"){
         "/seller_central/modify/technical_specifications/hints/completed"(platform: "/", type: TrackType.Event){
-            item_type = "default"
             type = "actionable"
             user_action = "click"
             attribute = "BRAND"
@@ -585,7 +581,6 @@ trackTests {
 
     test("structured data multivalue"){
         "/seller_central/modify/technical_specifications/multivalue"(platform: "/", type: TrackType.Event){
-            item_type = "default"
             quantity = 3
             previous_quantity = 0
             category_domain = "MLA-SHORTS"
@@ -598,7 +593,6 @@ trackTests {
 
     test("structured data suggested values"){
         "/seller_central/modify/technical_specifications/suggested"(platform: "/", type: TrackType.Event){
-            item_type = "default"
             type = "dynamic"
             category_domain = "MLA-CAMERAS"
             item_id= "MLA682118081"
@@ -610,7 +604,6 @@ trackTests {
 
     test("structured data change category"){
         "/seller_central/modify/update_change_category"(platform: "/", type: TrackType.Event){
-            item_type = "default"
             category_domain = "MLA-CAMERAS"
             item_id= "MLA682118081"
             session_id = "123-update-abc123"
