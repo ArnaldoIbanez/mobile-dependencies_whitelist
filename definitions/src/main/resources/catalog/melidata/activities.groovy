72000fb9a895a005452ccbd3a60a0f31a24ae3a5
Julián Lima <julian.lima@mercadolibre.com>
2019-06-10 10:24:54 -0300

    modify activities tracks

--- definitions/src/main/resources/catalog/melidata/activities.groovy
+++ definitions/src/main/resources/catalog/melidata/activities.groovy
@@ -40,24 +40,33 @@ tracks {
     *     ACTIVITY MOBILE TRACKS    *
     *********************************/
 
+    def filter_definition = objectSchemaDefinitions {
+        id(required: true, type: PropertyType.String, description: "The id of the Filter")
+        ordinal(required: true, type: PropertyType.Numeric, description: "The position of the filter")
+    }
 
     // MP Activities List
     "/listing/activities" (platform: "/mobile", type: TrackType.View) {}
-    "/listing/activities/remove_filter" (platform: "/mobile", type: TrackType.Event) {}
+    "/listing/activities/pull" (platform: "/mobile", type: TrackType.Event) {
+        has_changes(required: true, type: PropertyType.Boolean, description: "If changed the list of activities") 
+    }
 
     // MP Activities Filters
     "/listing/activities/filters" (platform: "/mobile", type: TrackType.View) {}
     "/listing/activities/filters/options" (platform: "/mobile", type: TrackType.View) {}
-    "/listing/activities/filters/apply" (platform: "/mobile", type: TrackType.Event) {}
+    "/listing/activities/filters/apply" (platform: "/mobile", type: TrackType.Event) {
+        items(required: true, type: PropertyType.ArrayList(PropertyType.Map(filter_definition)), description: "The filters applied")
+    }
 
     // MP Activities Opertion Detail
     "/listing/activities/detail" (platform: "/mobile", type: TrackType.View) {}
     "/listing/activities/detail/add_note" (platform: "/mobile", type: TrackType.View) {}
+    "/listing/activities/detail/add_note/added" (platform: "/mobile", type: TrackType.Event) {}
     "/listing/activities/detail/list" (platform: "/mobile", type: TrackType.View) {}
     "/listing/activities/detail/user_info" (platform: "/mobile", type: TrackType.View) {}
     "/listing/activities/detail/shipping" (platform: "/mobile", type: TrackType.View) {}
     "/listing/activities/detail/web_view" (platform: "/mobile", type: TrackType.View) {
-        url(required: true, PropertyType.String, description: "The url that will load the webview")
+        url(required: true, type: PropertyType.String, description: "The url that will load the webview")
     }
     
 }
