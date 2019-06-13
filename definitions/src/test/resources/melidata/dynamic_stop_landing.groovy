ec62d107bfd5acc5213cd09928098f6aec9cabc2
Ariel Fernandez <ariel.fernandez@mercadolibre.com>
2019-06-13 16:22:41 -0300

    add path for stop landing payers growth

---
+++ definitions/src/test/resources/melidata/dynamic_stop_landing.groovy
@@ -0,0 +1,86 @@
+package src.test.resources.melidata
+
+import com.ml.melidata.TrackType
+
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+
+	//------------------------------------------------------------------------------------------------------------------------------------------------------
+	// TRACKS Dynamic stop landing
+	//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+trackTests {
+
+	defaultBusiness = "mercadolibre"
+
+	test("Dynamic Stop Landing track ML") {
+		"/stop_landing"(platform: '/web'){
+			campaign = 'copa_america'
+		}
+		"/stop_landing"(platform: '/web'){
+			campaign = 'sube'
+			extra_params = 'booster_id=1234'
+		}
+		"/stop_landing"(platform: '/web'){
+			campaign = 'coupons'
+			extra_params = 'booster_id=1234&media=whatsapp'
+		}
+		"/stop_landing/redirect"(platform: '/web'){
+			campaign = 'sube'
+			osName = 'android'
+			extra_params = 'booster_id=1234&media=whatsapp'
+		}
+		"/stop_landing/redirect"(platform: '/web'){
+			campaign = 'copa_america'
+			osName = 'ios'
+			extra_params = 'booster_id=1234'
+		}
+		"/stop_landing/redirect"(platform: '/web'){
+			campaign = 'copa_america'
+			osName = 'ios'
+		}
+		"/stop_landing/button"(platform: '/web'){
+			campaign = 'copa_america'
+		}
+		"/stop_landing/button"(platform: '/web'){
+			campaign = 'copa_america'
+			extra_params = 'booster_id=1234&media=whatsapp'
+		}
+	}
+
+	defaultBusiness = "mercadopago"
+
+	test("Dynamic Stop Landing track MP") {
+		"/stop_landing"(platform: '/web'){
+			campaign = 'copa_america'
+		}
+		"/stop_landing"(platform: '/web'){
+			campaign = 'sube'
+			extra_params = 'booster_id=1234'
+		}
+		"/stop_landing"(platform: '/web'){
+			campaign = 'coupons'
+			extra_params = 'booster_id=1234&media=whatsapp'
+		}
+		"/stop_landing/redirect"(platform: '/web'){
+			campaign = 'sube'
+			osName = 'android'
+			extra_params = 'booster_id=1234&media=whatsapp'
+		}
+		"/stop_landing/redirect"(platform: '/web'){
+			campaign = 'copa_america'
+			osName = 'ios'
+			extra_params = 'booster_id=1234'
+		}
+		"/stop_landing/redirect"(platform: '/web'){
+			campaign = 'copa_america'
+			osName = 'ios'
+		}
+		"/stop_landing/button"(platform: '/web'){
+			campaign = 'copa_america'
+		}
+		"/stop_landing/button"(platform: '/web'){
+			campaign = 'copa_america'
+			extra_params = 'booster_id=1234&media=whatsapp'
+		}
+	}
+}
