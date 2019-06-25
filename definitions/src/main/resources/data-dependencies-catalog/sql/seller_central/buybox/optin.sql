cf7abc4289b78cd339efb3e28bc076c294954bde
Nicolas Vespoli <nicolas.vespoli@mercadolibre.com>
2019-06-25 14:26:10 -0300

    Add new json a query

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/seller_central/buybox/optin.sql
@@ -0,0 +1,30 @@
+SELECT tracks.path as path,
+application.site_id as siteId,
+jest(event_data,'category_domain') as domainId,
+jest(event_data,'original_catalog_product_id') as fatherProductId,
+jest(event_data,'selected_catalog_product_id') as childProductId,
+jest(event_data,'task_id') as taskId,
+jest(event_data,'to') as oldValue,
+jest(event_data,'from') as newValue,
+jest(event_data,'invalid_product_cause') as invalidCause,
+jest(event_data,'has_variations') as hasVariations,
+jest(event_data,'has_variations_already_opt_in') as hasVariationsOptIn,
+jest(event_data,'seller_profile') as sellerProfile,
+COUNT(DISTINCT path, session_id) as total
+substr(ds,1,10) as ds,
+FROM tracks
+WHERE tracks.path LIKE '/seller_central/catalog/optin%'
+AND ds >= '@param01' AND ds < '@param02'
+AND application.business='mercadolibre'
+GROUP BY path,
+siteId,
+jest(event_data,'category_domain'),
+jest(event_data,'original_catalog_product_id'),
+jest(event_data,'selected_catalog_product_id'),
+jest(event_data,'task_id'),
+jest(event_data,'to'),
+jest(event_data,'from'),
+jest(event_data,'invalid_product_cause'),
+jest(event_data,'has_variations'),
+jest(event_data,'has_variations_already_opt_in'),
+jest(event_data,'seller_profile')
