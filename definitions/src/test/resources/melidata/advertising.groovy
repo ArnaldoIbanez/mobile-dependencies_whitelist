9be3e36d4e2827fb03180742dd2b105c795d4e93
Nicolas Rodriguez <nicolas.nrodriguez@mercadolibre.com>
2019-06-25 15:18:41 -0300

    advertising upselling test

--- definitions/src/test/resources/melidata/advertising.groovy
+++ definitions/src/test/resources/melidata/advertising.groovy
@@ -112,4 +112,36 @@ trackTests {
             free_trial_ad = false
         }
     }
+
+    test("Advertising upselling") {
+        "/advertising/pads2/manager/upselling/modal/show"(platform: "/web", type: TrackType.Event) {
+            budget_old = "500"
+            budget_new = "1000"
+        }
+
+        "/advertising/pads2/manager/upselling/modal/go"(platform: "/web", type: TrackType.Event) {
+            budget_old = "500"
+            budget_new = "1000"
+        }
+
+        "/advertising/pads2/manager/upselling/modal/close"(platform: "/web", type: TrackType.Event) {
+            budget_old = "500"
+            budget_new = "1000"
+        }
+
+        "/advertising/pads2/manager/upselling/banner/show"(platform: "/web", type: TrackType.Event) {
+            budget_old = "500"
+            budget_new = "1000"
+        }
+
+        "/advertising/pads2/manager/upselling/banner/go"(platform: "/web", type: TrackType.Event) {
+            budget_old = "500"
+            budget_new = "1000"
+        }
+
+        "/advertising/pads2/manager/upselling/banner/close"(platform: "/web", type: TrackType.Event) {
+            budget_old = "500"
+            budget_new = "1000"
+        }
+    }
 }
