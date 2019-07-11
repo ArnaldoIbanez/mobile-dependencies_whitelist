907716a7e3eb204ae60e72f0063209ac99240df0
Romina Cortazzo <romina.cortazzo@mercadolibre.com>
2019-07-11 11:44:25 -0300

    - A "/checkout/payment/curp" se cambia el platform por el generico ya que se habilitó el formulario también para mobile. - Se agrega el path /checkout/payment/curp/not_my_curp de momento sólo para mobile para trackear cuando los usuarios indican que el curp generado no es el suyo e intentan editarlo.

--- definitions/src/test/resources/melidata/checkout.groovy
+++ definitions/src/test/resources/melidata/checkout.groovy
@@ -972,6 +972,8 @@ trackTests {
             payment_method = "telecomm"
             agencies = 10
         }
+         // Step Curp Credits MLM
+        "/checkout/payment/curp/not_my_curp"(platform: "/mobile", type: TrackType.Event) {}
     }
 
     test("checkout congrats"){
@@ -2255,7 +2257,7 @@ trackTests {
         }
 
          // Step Curp Credits MLM
-        "/checkout/payment/curp"(platform:"/web", dataSet)
+        "/checkout/payment/curp"(platform:"/", dataSet)
     }
 
     test("Checkout recovery with recos - push notification flow") {
