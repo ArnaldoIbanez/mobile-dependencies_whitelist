75c13a7651d409d7999d34af3e3fe07c685e9c0f
yuriiichung <yuri.chung@mercadolibre.com>
2019-07-26 10:54:28 -0300

    add catalog_listing vip tracking in web

--- definitions/src/test/resources/melidata/vip.groovy
+++ definitions/src/test/resources/melidata/vip.groovy
@@ -418,6 +418,7 @@ trackTests {
             seller_id = 131662738
             power_seller_status = "platinum"
             deal_ids = ["MLA100"]
+            catalog_listing = false
         }
 
         "/vip"(platform:"/web", dataSet)
