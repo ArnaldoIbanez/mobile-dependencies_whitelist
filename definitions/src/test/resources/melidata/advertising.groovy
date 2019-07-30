e2d583fd51a499b6b0815b21f00e368750aacd4f
mariam <mariam.sleiman@mercadolibre.com>
2019-07-30 17:44:34 -0300

    fix test

--- definitions/src/test/resources/melidata/advertising.groovy
+++ definitions/src/test/resources/melidata/advertising.groovy
@@ -158,7 +158,7 @@ trackTests {
     }
 
     test("Landing congrats") {
-        "/advertising/pads2/manager/upselling/modal/show"(platform: "/web", type: TrackType.View) {
+        "/advertising/landings/pads2/mail/congrats"(platform: "/web", type: TrackType.View) {
             user_id = "2222222"
             items_id = "MLA1,MLA2,MLA3,MLA4,MLA5"
         }
