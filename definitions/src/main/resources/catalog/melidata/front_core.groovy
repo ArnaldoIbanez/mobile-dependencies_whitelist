fe8363277cd5cdca8bd504b8b3cafe2d56eb5b8b
MATIAS TESIO <matias.tesio@mercadolibre.com>
2019-07-30 11:24:02 -0300

    drone

--- definitions/src/main/resources/catalog/melidata/front_core.groovy
+++ definitions/src/main/resources/catalog/melidata/front_core.groovy
@@ -269,4 +269,5 @@ tracks {
     "/wallet_home/notification/tap" (platform: "/mobile", type: TrackType.Event) {
         badge_count(required: true, type: PropertyType.Numeric, description: "The amount of notifications that's been showed in the notification label icon")
     }
+    
 }
