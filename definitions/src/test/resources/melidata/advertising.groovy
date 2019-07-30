e743d506ff922536b1afe518cd3a8e3bc983efb2
mariam <mariam.sleiman@mercadolibre.com>
2019-07-30 16:23:00 -0300

    congrats

--- definitions/src/test/resources/melidata/advertising.groovy
+++ definitions/src/test/resources/melidata/advertising.groovy
@@ -156,4 +156,11 @@ trackTests {
             budget_new = "1000"
         }
     }
+
+    test("Landing congrats") {
+        "/advertising/pads2/manager/upselling/modal/show"(platform: "/web", type: TrackType.View) {
+            user_id = "2222222"
+            items_id = "MLA1,MLA2,MLA3,MLA4,MLA5"
+        }
+    }
 }
