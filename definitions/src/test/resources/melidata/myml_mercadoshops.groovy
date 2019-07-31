e9efaa03e65c213ef358aad5c53faefbce715076
Jairo Gonzalez <jairo.gonzalez@mercadolibre.com>
2019-07-31 13:54:36 -0300

    Fix error

--- definitions/src/test/resources/melidata/myml_mercadoshops.groovy
+++ definitions/src/test/resources/melidata/myml_mercadoshops.groovy
@@ -315,23 +315,23 @@ trackTests {
     }
 
     test("Mercadoshops Merketing Tools events") {
-        "/mercado_shops/marketing/google-analytics/save"(platform: "/", type: TrackType.event) {
+        "/mercado_shops/marketing/google-analytics/save"(platform: "/", type: TrackType.Event) {
             shop_id = 158689680
             shop_domain = "www.test-shop.mercadoshops.com.ar"
             shop_name = "Test Shop"
             success = true
         }
-        "/mercado_shops/marketing/google-analytics/open-faqs"(platform: "/", type: TrackType.event) {
+        "/mercado_shops/marketing/google-analytics/open-faqs"(platform: "/", type: TrackType.Event) {
             shop_id = 158689680
             shop_domain = "www.test-shop.mercadoshops.com.ar"
             shop_name = "Test Shop"
         }
-        "/mercado_shops/marketing/google-analytics/cancel"(platform: "/", type: TrackType.event) {
+        "/mercado_shops/marketing/google-analytics/cancel"(platform: "/", type: TrackType.Event) {
             shop_id = 158689680
             shop_domain = "www.test-shop.mercadoshops.com.ar"
             shop_name = "Test Shop"
         }
-        "/mercado_shops/marketing/google-analytics/context-help"(platform: "/", type: TrackType.event) {
+        "/mercado_shops/marketing/google-analytics/context-help"(platform: "/", type: TrackType.Event) {
             shop_id = 158689680
             shop_domain = "www.test-shop.mercadoshops.com.ar"
             shop_name = "Test Shop"
