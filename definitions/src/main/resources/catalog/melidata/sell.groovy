45edb79316651abb80a53f338f570b778e41347d
Matías M. Méndez <matimendez88@gmail.com>
2019-08-06 11:31:34 -0300

    pr fixes

--- definitions/src/main/resources/catalog/melidata/sell.groovy
+++ definitions/src/main/resources/catalog/melidata/sell.groovy
@@ -11,10 +11,23 @@ tracks {
         seller_profile(required: true, type: PropertyType.String, description: "Type of seller")
         seller_segment(required: true, type: PropertyType.String, description: "Seller segment by GMV")
         session_id(required: true, type: PropertyType.String, description: "Id for user session")
+        category_prediction(required: true, description: "Category prediction path result", type: PropertyType.ArrayList)
+        category_prediction_score(required: true, description: "Category prediction scores result", type: PropertyType.ArrayList)
+        category_prediction_zordon(required: true, description: "Category prediction path result from zordon", type: PropertyType.ArrayList)
+        category_prediction_zordon_score(required: true, description: "Category prediction scores result from zordon", type: PropertyType.ArrayList)
+        predictor_chosen(required: true, description: "Which predictor we used to predict category", values:["ZORDON", "DEFAULT"], type: PropertyType.String)
+        title_predicted(required: true, description: "Title used to predict category", type: PropertyType.String)
+        attributes_values_predicted(required: false, description: "Attributes values that we predict by matchers", type: PropertyType.ArrayList)
+        attributes_ids_predicted(required: false, description: "Attributes ids that we predict by matchers", type: PropertyType.ArrayList)
+        attributes_selected_in_tree(required: false, description: "Attributes values selected by user", type: PropertyType.ArrayList)
     }
 
     propertyGroups {
         sellGroup(category_id, category_path, seller_profile, seller_segment, session_id)
+        predictionData(
+            category_prediction, category_prediction_score, category_prediction_zordon, category_prediction_zordon_score, predictor_chosen,
+            title_predicted, attributes_values_predicted, attributes_ids_predicted, attributes_selected_in_tree
+        )
     }
 
     // Sell
@@ -767,19 +780,18 @@ tracks {
     "/sell/congrats"(platform: "/web", type: TrackType.View) {
         sellGroup
         item_id(required: false, type: PropertyType.String)
-        category_prediction(required: false, description: "Category prediction path result", type: PropertyType.ArrayList)
-        category_prediction_score(required: false, description: "Category prediction scores result", type: PropertyType.ArrayList)
-        category_prediction_zordon(required: false, description: "Category prediction path result from zordon", type: PropertyType.ArrayList)
-        category_prediction_zordon_score(required: false, description: "Category prediction scores result from zordon", type: PropertyType.ArrayList)
-        predictor_chosen(required: false, description: "Which predictor we used to predict category: ZORDON/DEFAULT/etc...", type: PropertyType.String)
-        attributes_values_predicted(required: false, description: "Attributes values that we predict by matchers", type: PropertyType.ArrayList)
-        attributes_ids_predicted(required: false, description: "Attributes ids that we predict by matchers", type: PropertyType.ArrayList)
-        attributes_selected_in_tree(required: false, description: "Attributes values selected by user", type: PropertyType.ArrayList)
-        title_predicted(required: false, description: "Title used to predict category", type: PropertyType.String)
+        predictionData
     }
-    "/sell/congrats/show"(platform: "/web", type: TrackType.Event) {}
+
+    "/sell/congrats/show"(platform: "/web", parentPropertiesInherited: false, type: TrackType.Event) {
+        sellGroup
+        item_id(required: false, type: PropertyType.String)
+    }
     "/sell/congrats/listing_types"(platform: "/web", isAbstract: true) {}
-    "/sell/congrats/listing_types/show"(platform: "/web", type: TrackType.Event) {}
+    "/sell/congrats/listing_types/show"(platform: "/web", parentPropertiesInherited: false, type: TrackType.Event) {
+        sellGroup
+        item_id(required: false, type: PropertyType.String)
+    }
 
 
     "/sell/sip"(platform: "/web", type: TrackType.View) {
