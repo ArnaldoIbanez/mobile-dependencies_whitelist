96eb430c5d427c4664806cb93601c72ef618b874
mauricio gigena <mauricio.gigena@mercadolibre.com>
2019-08-09 13:24:19 -0300

    fixing test

--- definitions/src/test/resources/melidata/credits_marketplace.groovy
+++ definitions/src/test/resources/melidata/credits_marketplace.groovy
@@ -253,7 +253,7 @@ trackTests {
             paid_installments = 2
         }
 
-         "/credits/consumer/administrator_v2/installment_selection/payment_intention"(platform: "/web", type: TrackType.Event) {
+         "/credits/consumer/administrator_v2/installment_selection/secondary_payment_intention"(platform: "/web", type: TrackType.Event) {
             total_installments = 3
         }
 
