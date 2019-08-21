5bf209764b53bc161eae7da3cca7699b08ff7957
Ariel Fernandez <ariel.fernandez@mercadolibre.com>
2019-08-21 15:25:56 -0300

    fix property type

--- definitions/src/main/resources/catalog/melidata/mkt_performance_landings.groovy
+++ definitions/src/main/resources/catalog/melidata/mkt_performance_landings.groovy
@@ -9,24 +9,22 @@ tracks {
     */
     // Web
     "/mkt_performance_landing"(platform: "/", isAbstract: true) {}
-
     // Recharge Landing
     "/mkt_performance_landing/recharge"(platform: "/web", type: TrackType.View) {}
     "/mkt_performance_landing/recharge/sms"(platform: "/web", type: TrackType.Event) {}
     "/mkt_performance_landing/recharge/copy-clipboard"(platform: "/web", type: TrackType.Event) {
-        position (require: true, type: PropertyType.string, values: ["coupon", "steps"], description: "Indicates position of copy button")
+        position (require: true, type: PropertyType.String, values: ["coupon", "steps"], description: "Indicates position of copy button")
     }
     "/mkt_performance_landing/recharge/button"(platform: "/", type: TrackType.Event) {
-        position (require: true, type: PropertyType.string, values: ["hero", "fixed", "footer"], description: "Indicates position of button in landing")
+        position (require: true, type: PropertyType.String, values: ["hero", "fixed", "footer"], description: "Indicates position of button in landing")
     }
-
     // Transport Landing
     "/mkt_performance_landing/transport"(platform: "/web", type: TrackType.View) {}
     "/mkt_performance_landing/transport/sms"(platform: "/web", type: TrackType.Event) {}
-    "/mkt_performance_landing/transport/copy-clipboard"(platform: "/web", type: TrackType.Event) { 
-        position (require: true, type: PropertyType.string, values: ["coupon", "steps"], description: "Indicates position of copy button")
+    "/mkt_performance_landing/transport/copy-clipboard"(platform: "/web", type: TrackType.Event) {
+        position (require: true, type: PropertyType.String, values: ["coupon", "steps"], description: "Indicates position of copy button")
     }
     "/mkt_performance_landing/transport/button"(platform: "/web", type: TrackType.Event) {
-        position (require: true, type: PropertyType.string, values: ["hero", "fixed", "footer"], description: "Indicates position of button in landing")
+        position (require: true, type: PropertyType.String, values: ["hero", "fixed", "footer"], description: "Indicates position of button in landing")
     }
 }
