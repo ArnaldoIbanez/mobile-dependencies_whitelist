a7aa219718f0e2cc167866fda3b53365484c3c10
Andrés García <andres.ggarcia@mercadolibre.com>
2019-08-21 12:01:10 -0300

    Updated track definition

--- definitions/src/main/resources/catalog/melidata/mercadopago.groovy
+++ definitions/src/main/resources/catalog/melidata/mercadopago.groovy
@@ -113,6 +113,18 @@ tracks {
       installments (type: PropertyType.Numeric, required: false, description: "Selected installments")
     }
 
+    "/point/buyingflow/error"(platform: "/", type: TrackType.View) {
+      flow_id (type: PropertyType.String, required: true, description: "Flow ID")
+      type (type: PropertyType.String, required: true, description: "Error type")
+    }
+
+    "/point/buyingflow/paymentRejected"(platform: "/", type: TrackType.View) {
+      groupCheckoutProperties
+      selected_payment_method_id (type: PropertyType.String, required: true, description: "Selected payment method ID")
+      selected_payment_method_type (type: PropertyType.String, required: false, description: "Selected payment method type, ex: credit card")
+      installments (type: PropertyType.Numeric, required: false, description: "Selected installments")
+    }
+
     "/point/buyingflow/paymentReview/confirmPurchase"(platform: "/", type: TrackType.Event) {
       groupCheckoutProperties
       selected_payment_method_id (type: PropertyType.String, required: true, description: "Selected payment method ID")
--- definitions/src/test/resources/melidata/mercadopago.groovy
+++ definitions/src/test/resources/melidata/mercadopago.groovy
@@ -575,6 +575,21 @@ trackTests {
           installments = 6
         }
 
+        "/point/buyingflow/paymentRejected"(platform: "/", type: TrackType.View) {
+          flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
+          product = "11"
+          currency = "ARS"
+          price = 299
+          is_guest = true
+          selected_payment_method_id = "tarshop"
+          selected_payment_method_type = "credit_card"
+          installments = 6
+        }
+        "/point/buyingflow/error"(platform: "/", type: TrackType.View) {
+          flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
+          type = "error: Session ended"
+        }
+
         "/point/buyingflow/paymentReview/confirmPurchase"(platform: "/", type: TrackType.Event) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
           product = "11"
