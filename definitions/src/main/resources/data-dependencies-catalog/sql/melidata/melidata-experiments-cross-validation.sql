c9b0f7e0f02892b889c72cdb0a94b7ab457f9d9e
pdbenitez91 <patricio.benitez@mercadolibre.com>
2019-09-18 18:26:13 -0300

    fix field type

--- definitions/src/main/resources/data-dependencies-catalog/sql/melidata/melidata-experiments-cross-validation.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/melidata/melidata-experiments-cross-validation.sql
@@ -8,7 +8,7 @@ group by experiment_name, platform, variant_id, substr(ds,1,10)
 
 union all
 
-select 'BUCKETIZED_DATA' AS SOURCE, experiment_name, platform, cast(variant_id as varchar) variant_id, sum(valueb) as sessions, sum(valuea) as congrats, substr(ds,1,10) AS ds
+select 'BUCKETIZED_DATA' AS SOURCE, experiment_name, platform, cast(variant_id as string) variant_id, sum(valueb) as sessions, sum(valuea) as congrats, substr(ds,1,10) AS ds
 from melidata.experiments_by_uid
 WHERE ds >= '@param01'
 AND ds < '@param02'
