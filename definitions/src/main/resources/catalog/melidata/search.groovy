ff8d365bdf3a236526e165d3cd7628ac57d892ac
florenciaMELI <florencia.fdiaz@mercadolibre.com>
2019-09-25 17:31:29 -0300

    Agrego definición de item para tack de SEO

--- definitions/src/main/resources/catalog/melidata/search.groovy
+++ definitions/src/main/resources/catalog/melidata/search.groovy
@@ -84,7 +84,7 @@ tracks {
         carousel_filters(required: true, description: 'carousel filter ids shown in search', PropertyType.ArrayList)
         pdp_tracking_info(required: true, description: 'pdp products info', PropertyType.Map(PropertyType.String, PropertyType.String))
         is_in_seo_whitelist(required: false, description: 'is request in seo whitelist', PropertyType.Boolean)
-        seo(required: false, description: 'seo tracking info')
+        seo(required: false, description: 'seo tracking info', type: PropertyType.Map(seo_item_definition))
     }
 
     "/search"(platform: "/mobile") {
