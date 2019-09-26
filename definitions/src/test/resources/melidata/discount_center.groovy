624f3eb72edbc6d2c71aa740f5ec4ed43ff8f5a3
Tomi De Lucca <tomas.delucca@mercadolibre.com>
2019-09-26 13:48:28 -0300

    Discount center tracks

---
+++ definitions/src/test/resources/melidata/discount_center.groovy
@@ -0,0 +1,54 @@
+package src.test.resources.melidata
+
+import com.ml.melidata.TrackType
+
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+
+trackTests {
+
+    defaultBusiness = "mercadopago"
+
+    // LIST
+
+    test("Mercado Pago discount center payers list") {
+        "/discount_center/payers/list" (platform: "/mobile", type: TrackType.View) {}
+    }
+
+    test("Mercado Pago discount center payers list show") {
+        "/discount_center/payers/list/show" (platform: "/mobile", type: TrackType.Event) {
+            items= [
+                [ tracking_id: "1004194" ]
+            ]
+        }
+    }
+
+    // DETAIL
+
+    test("Mercado Pago discount center payers detail") {
+        "/discount_center/payers/detail" (platform: "/mobile", type: TrackType.View) {
+            tracking_id= "1004194"
+        }
+    }
+
+    test("Mercado Pago discount center payers detail share") {
+        "/discount_center/payers/detail/share" (platform: "/mobile", type: TrackType.Event) {
+            tracking_id= "1004194"
+        }
+    }
+
+    // REQUEST LOCATION
+
+    test("Mercado Pago discount center payers request location") {
+        "/discount_center/payers/request_location" (platform: "/mobile", type: TrackType.View) {}
+    }
+
+    test("Mercado Pago discount center payers request location event") {
+        "/discount_center/payers/request_location/result" (platform: "/mobile", type: TrackType.Event) {
+            result= "enabled"
+        }
+    }
+
+    test("Mercado Pago discount center payers request location back") {
+        "/discount_center/payers/request_location/back" (platform: "/mobile", type: TrackType.Event) {}
+    }
+}
