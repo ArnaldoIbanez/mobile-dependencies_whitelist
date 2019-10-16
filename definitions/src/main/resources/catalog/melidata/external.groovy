f68a6c96bab21905d922721814676a1bab189bfe
Franco Cuevas <franco.cuevas@mercadolibre.com>
2019-10-16 15:02:51 -0300

    Added property type

--- definitions/src/main/resources/catalog/melidata/external.groovy
+++ definitions/src/main/resources/catalog/melidata/external.groovy
@@ -1,17 +1,16 @@
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
+import com.ml.melidata.catalog.PropertyType
 import com.ml.melidata.TrackType
 
 
 tracks {
   	// External tracks
-    "/external"(platform: "/", isAbstract: true){}
-
-    "/external/home"(platform: "/", type: TrackType.View){
-        domain(required: true, description: "current path")
+    "/external"(platform: "/", isAbstract: true){
+    	domain(required: true, description: "domain of current view", type: PropertyType.String)
     }
 
-    "/external/checkout"(platform: "/", type: TrackType.View){
-        domain(required: true, description: "current path")
-    }
+    "/external/home"(platform: "/", type: TrackType.View){}
+
+    "/external/checkout"(platform: "/", type: TrackType.View){}
 
 }
