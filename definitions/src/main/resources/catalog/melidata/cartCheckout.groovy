04d52b579b80e9a0ce65054ef305a474265eaa7c
lugarbarini <lucia.garbarini@mercadolibre.com>
2019-10-17 18:03:14 -0300

    fix cart checkout tracks

--- definitions/src/main/resources/catalog/melidata/cartCheckout.groovy
+++ definitions/src/main/resources/catalog/melidata/cartCheckout.groovy
@@ -116,7 +116,9 @@ tracks {
 
     "/cart/checkout/items_not_available"(platform: "/", type: TrackType.View) {}
 
-    "/cart/checkout/error"(platform: "/", type: TrackType.View) {}
+    "/cart/checkout/error"(platform: "/", type: TrackType.View) {
+        error(required: false, type: PropertyType.String, description: "Error that was shown to the user if known")
+    }
 
     "/cart/checkout/payment"(platform: "/", isAbstract: true) {}
     "/cart/checkout/shipping"(platform:"/", isAbstract: true) {}
@@ -188,7 +190,9 @@ tracks {
 
     "/cart/checkout/review/edit_shipping"(platform: "/", type: TrackType.View) {}
 
-    "/cart/checkout/review/edit_payment_method"(platform: "/", type: TrackType.View) {}
+    "/cart/checkout/review/edit_payment_method"(platform: "/", type: TrackType.View) {
+        coupon(required: false, type: PropertyType.Boolean, description: "If the user redeemed a coupon code")
+    }
 
     "/cart/checkout/congrats"(platform: "/", type: TrackType.View) {
         congrats_seq(serverSide: true) // Lo completa Melidata automaticamente
@@ -215,6 +219,10 @@ tracks {
 
     "/cart/checkout/finish/invalid_sec_code"(platform: "/", type: TrackType.View, isAbstract: true) {}
     "/cart/checkout/finish/invalid_sec_code/input_code"(platform: "/", type: TrackType.View) {}
+    "/cart/checkout/finish/invalid_sec_code/input_code/confirm_purchase"(platform: "/mobile", type: TrackType.Event, parentPropertiesInherited: false) {
+        session_id(required: false, type: PropertyType.String, description: "Session in which the checkout is being held")
+        status(required: false, type: PropertyType.String, description: "The result of the purchase")
+    }
 
     "/cart/checkout/finish/call_for_auth"(platform: "/", type: TrackType.View, isAbstract: true) {}
     "/cart/checkout/finish/call_for_auth/instructions"(platform: "/", type: TrackType.View) {}
@@ -231,6 +239,8 @@ tracks {
         combination_2mp(required: false, description: "2MP switch state")
         total_amount(required: false, description: "totalAmount")
     }
+
+    "/cart/checkout/loading/back"(platform: "/mobile", type: TrackType.Event) {}
 
 // Addresses
 // Page
@@ -279,6 +289,11 @@ tracks {
         label(required: true, type: PropertyType.String, description: "If the address has an error on the phone")
         items(required: true, type: PropertyType.ArrayList, description: "Array of items in the cart with following data")
         recovery_flow(required: true, description: "Is recovery CHO flow")
+    }
+    "/cart/checkout/shipping/input_address#submit"(platform: "/mobile", type: TrackType.Event, parentPropertiesInherited: false) {
+        session_id(required: true, type: PropertyType.String, description: "Session in which the checkout is being held")
+        success(required: true, type: PropertyType.Boolean, description: "Success or failure getting card config")
+
     }
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -356,7 +371,7 @@ tracks {
         session_id(required: false, type: PropertyType.String, description: "Session in which the checkout is being held")
     }
 
-    "/cart/checkout/shipping/select_address"(platform: "/mobile", type: TrackType.View) {}
+    "/cart/checkout/shipping/select_address"(platform: "/mobile", type: TrackType.View, parentPropertiesInherited:false) {}
 
     "/cart/checkout/shipping/select_option_detail"(platform: "/mobile", type: TrackType.View) {}
 
@@ -416,7 +431,7 @@ tracks {
 
     "/cart/checkout/review"(platform: "/mobile", type: TrackType.View, isAbstract: true) {}
 
-    "/cart/checkout/review/edit_unique_installment"(platform: "/mobile", type: TrackType.View) {}
+    "/cart/checkout/review/edit_unique_installment"(platform: "/mobile", type: TrackType.View, parentPropertiesInherited: false) {}
 
     "/cart/checkout/review/edit_first_installment"(platform: "/mobile", type: TrackType.View) {}
 
@@ -451,7 +466,7 @@ tracks {
     }
 
 // Discount coupons
-    "/cart/checkout/payment/add_coupon"(platform: "/mobile", type: TrackType.View) {}
+    "/cart/checkout/payment/add_coupon"(platform: "/mobile", type: TrackType.View, parentPropertiesInherited: false) {}
     "/cart/checkout/payment/coupon_ok"(platform: "/mobile", type: TrackType.View) {
         coupon(required: true, type: PropertyType.String)
     }
@@ -485,6 +500,10 @@ tracks {
 
 // ESC: Enter the Sec Code to generate an Encrypted Security Code
     "/cart/checkout/payment/encrypted_security_code_add"(platform: "/mobile", type: TrackType.View) {}
+    "/cart/checkout/payment/encrypted_security_code_add/confirm_purchase"(platform: "/mobile", type: TrackType.Event, parentPropertiesInherited: false) {
+        session_id(required: false, type: PropertyType.String, description: "Session in which the checkout is being held")
+        status(required: false, type: PropertyType.String, description: "The result of the purchase")
+    }
 
 //Billing info
     "/cart/checkout/billing"(platform: "/", isAbstract: true) {}
@@ -493,6 +512,8 @@ tracks {
     "/cart/checkout/review/edit_billing_info"(platform: "/mobile", type: TrackType.Event, parentPropertiesInherited: false) {
         session_id(required: false, type: PropertyType.String, description: "Session in which the checkout is being held")
     }
+    "/cart/checkout/billing/physical_person/back"(platform: "/mobile", type: TrackType.Event) {}
+    "/cart/checkout/billing/legal_person/back"(platform: "/mobile", type: TrackType.Event) {}
 
 // 2MP Inconsistencias
     "/cart/checkout/review/discard_payment_combination"(platform: "/mobile") {}
@@ -508,6 +529,7 @@ tracks {
         session_id(required: false, type: PropertyType.String, description: "Session in which the checkout is being held")
         status(required: false, type: PropertyType.String, description: "The result of the purchase")
     }
+    "/cart/checkout/review/confirm_purchase/abort"(platform: "/mobile", type: TrackType.Event) {}
 
 //Payment form input tack events
 
