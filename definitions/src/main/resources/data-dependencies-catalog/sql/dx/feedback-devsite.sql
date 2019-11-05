e3a29ee535ff1decf3d728fa5731ad2d4a3ae7f4
Juani <juan.veltri@mercadolibre.com>
2019-11-05 15:23:45 -0300

    query para devsite - encuesta de satisfaccion de contenidos

---
+++ definitions/src/main/resources/data-dependencies-catalog/sql/dx/feedback-devsite.sql
@@ -0,0 +1,20 @@
+SELECT 
+TempTable.device.platform as device.platform,
+TempTable.feedback_value as feedback_value,
+TempTable.url as url,
+TempTable.site as site,
+TempTable.lang as lang,
+TempTable.date_created as date_created
+FROM (
+        SELECT SUBSTRING(ds,1,10) as date_created,
+        device.platform,
+        jest(event_data, 'like') as feedback_value,
+        platform.http.http_url as url,
+        jest(event_data, 'site') as site,
+        jest(event_data, 'lang') as lang
+        FROM tracks
+        WHERE ds >= '@param01'
+        AND   ds < '@param02'
+        AND path LIKE '/devsite/feedback'
+        GROUP BY 1,2,3,4,5,6
+) as TempTable
