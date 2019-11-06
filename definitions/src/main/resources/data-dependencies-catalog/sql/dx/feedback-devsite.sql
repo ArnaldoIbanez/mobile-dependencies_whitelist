99f4d29a1522e525c13c6911eb58121e49ceefe4
Juani <juan.veltri@mercadolibre.com>
2019-11-06 17:17:02 -0300

    fix query

--- definitions/src/main/resources/data-dependencies-catalog/sql/dx/feedback-devsite.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/dx/feedback-devsite.sql
@@ -1,20 +1,18 @@
-SELECT 
-TempTable.device.platform as platform,
-TempTable.feedback_value as feedback_value,
-TempTable.url as url,
-TempTable.site as site,
-TempTable.lang as lang,
-TempTable.date_created as date_created
-FROM (
-        SELECT SUBSTRING(ds,1,10) as date_created,
-        device.platform,
+SELECT COUNT(*) ocurrencias,
+        device.platform as platform,
         jest(event_data, 'like') as feedback_value,
         platform.http.http_url as url,
         jest(event_data, 'site') as site,
-        jest(event_data, 'lang') as lang
+        jest(event_data, 'lang') as lang,
+        SUBSTRING(ds,1,10) as date_created
         FROM tracks
         WHERE ds >= '@param01'
         AND   ds < '@param02'
-        AND path LIKE '/devsite/feedback'
-        GROUP BY 1,2,3,4,5,6
-) as TempTable
+        AND path = '/devsite/feedback'
+        GROUP BY
+        device.platform, 
+        jest(event_data, 'like'), 
+        platform.http.http_url, 
+        jest(event_data, 'site'), 
+        jest(event_data, 'lang'),
+        SUBSTRING(ds,1,10)
