f7ac22e2a58c846eaa2033cb0be0466a6e63f0e8
pdbenitez91 <patricio.benitez@mercadolibre.com>
2019-11-08 12:33:29 -0300

    test field in track

--- definitions/src/main/resources/catalog/component/test_tracking.groovy
+++ definitions/src/main/resources/catalog/component/test_tracking.groovy
@@ -1,16 +1,19 @@
 package src.main.resources.catalog.component
 
 import com.ml.melidata.TrackType
+import com.ml.melidata.catalog.PropertyType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
 tracks {
 
     "/"(platform: "/") {}
 
-    "/component"(platform: "/", type: TrackType.Event) {}
+    "/component"(platform: "/") {}
 
     "/wallet_home"(platform: "/" , isAbstract: true) {}
 
-    "/wallet_home/print"(platform: "/", type: TrackType.Event) {}
+    "/wallet_home/print"(platform: "/mobile") {
+        id(type: PropertyType.Numeric, required: false)
+    }
 
 }
