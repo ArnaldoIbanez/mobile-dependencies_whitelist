b76c4d66d66a22c6903e7e3ae55080b525bedaad
Daniel Perez Olivares <daniel.operez@mercadolibre.cl>
2019-11-18 12:42:54 -0300

    profile seller intention track added

--- definitions/src/main/resources/catalog/melidata/vip.groovy
+++ definitions/src/main/resources/catalog/melidata/vip.groovy
@@ -166,6 +166,14 @@ tracks {
         whatsapp_installed(required: false, type: PropertyType.Boolean, description: "determines if whatsapp is installed on the device")
     }
 
+    "/vip/profile_intention"(platform: "/", type: TrackType.Event) {
+        seller_id(required: true, type: PropertyType.Numeric, description: "Seller ID")
+        buyer_id(required: false, type: PropertyType.Numeric, description: "Buyer id")
+        buying_mode(required: false, type: PropertyType.String, description: "Indicates if it's an auction, buy_it_now or classified")
+        category_id(required: true, type: PropertyType.String, description: "Item's category id")
+        item_id(required: true, type: PropertyType.String, description: "Item id")
+    }
+
     "/vip/abort"(platform: "/mobile", type: TrackType.Event) {}
     "/vip/price_comparison"(platform: "/", type: TrackType.Event) {
         item_id(required: true, type: PropertyType.String, description: "Item ID")
