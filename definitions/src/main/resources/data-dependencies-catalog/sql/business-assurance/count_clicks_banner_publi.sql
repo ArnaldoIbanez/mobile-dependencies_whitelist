374d39a3400768f1aa9e68d6bd9d5ca7018ad10f
janijaimes <ext_jjaimes@mercadolibre.com>
2019-11-20 15:08:06 -0300

    Update count_clicks_banner_publi.sql

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/count_clicks_banner_publi.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/count_clicks_banner_publi.sql
@@ -1,11 +1,12 @@
-SELECT substr(ds,1,10) as ds,
+SELECT 
     device.platform AS Plataforma,
     jest(platform.fragment, 'sellerid') AS SellerID,
     jest(COALESCE(platform.fragment, others['fragment']), 'DEAL_ID') AS	Deals,
     jest(platform.fragment, 'banner_name') as banner,
     jest(platform.fragment, 'c_id') as component,
     jest(platform.fragment, 'c_campaign') as c_campaign,
-    count(1) as Clicks
+    count(1) as Clicks,
+    substr(ds,1,10) as ds                                        
 FROM tracks
 WHERE ds >= '@param01' AND ds < '@param02' 
     AND `type` = 'view'
