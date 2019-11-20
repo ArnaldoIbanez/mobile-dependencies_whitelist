2f5a23eb03211049d2fb3d2fd21a1072ad7aa3db
marianamorelli7 <marianamorelli7@gmail.com>
2019-11-20 16:07:58 -0300

    Se agrega una view que no estaba

--- definitions/src/main/resources/catalog/melidata/checkout.groovy
+++ definitions/src/main/resources/catalog/melidata/checkout.groovy
@@ -1113,6 +1113,7 @@ tracks {
     "/checkout/loading/error"(platform: "/", type: TrackType.View) {}
     "/checkout/shipping/edit_address"(platform:"/", type: TrackType.Event) {}
     "/checkout/shipping/"(platform:"/", type: TrackType.View) {}
+    "/checkout/shipping/input_zipcode"(platform:"/", type: TrackType.View) {}
     "/checkout/shipping/input_zipcode/i_dont_know_my_cp"(platform:"/", type: TrackType.Event) {}
 
     // Addresses
