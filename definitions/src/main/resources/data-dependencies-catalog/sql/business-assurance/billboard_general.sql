02f057fefeb8bb424f6e2dc164dc369ee0a128e9
María Virginia Navas Faría <mariavirginia.navas@mercadolibre.cl>
2019-11-21 14:57:38 -0300

    Partition added for billboards process

--- definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/billboard_general.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/business-assurance/billboard_general.sql
@@ -1,12 +1,12 @@
-SELECT 
-      substr(ds,1,10) as ds,
+SELECT
       application.site_id as site,
       application.business AS business,
       substr(jest (event_data, 'category_id'),4,8) as category_id,
       device.platform as plataforma,
       path as path,
       jest (event_data, 'is_new_billboard') as is_new_billboard,
-      IF (path = '/search/billboard', jest (event_data, 'position_shown'), jest (event_data, 'action')) AS detail
+      IF (path = '/search/billboard', jest (event_data, 'position_shown'), jest (event_data, 'action')) AS detail,
+      substr(ds,1,10) as ds
 FROM tracks
 WHERE ds >= '@param01' AND ds < '@param02'
       AND (path = '/search/billboard' OR path = '/search/billboard/resize')
