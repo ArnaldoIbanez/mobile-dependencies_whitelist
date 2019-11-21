349559483c1af09633b832dc7a8e08e38f7528a4
nacho2487 <ignacio.olaizola@mercadolibre.com>
2019-11-21 17:18:04 -0300

    added event data widget type

--- definitions/src/main/resources/catalog/melidata/incentivate_login.groovy
+++ definitions/src/main/resources/catalog/melidata/incentivate_login.groovy
@@ -6,6 +6,7 @@ tracks {
     "/navigation"(platform: "/", isAbstract: true) {}
     "/navigation/login"(platform: "/", isAbstract: true) {
         app_id(required: true, type: PropertyType.String,  description: "Refering app")
+        widget_type(required: true, type: PropertyType.String, values: ["modal", "banner", "tooltip"],  description: "Type of widget")
     }
     "/navigation/login/show"(platform: "/", type: TrackType.View) {}
     "/navigation/login/close"(platform: "/", type: TrackType.Event) {}
--- definitions/src/test/resources/melidata/incentivate_login.groovy
+++ definitions/src/test/resources/melidata/incentivate_login.groovy
@@ -8,30 +8,49 @@ trackTests {
     test("navigation incentivate login show track") {
         "/navigation/login/show"(platform: "/", type: TrackType.View) {
             app_id="web-vip"
+            widget_type="modal"
         }
     }
 
     test("navigation incentivate login close track") {
         "/navigation/login/close"(platform: "/", type: TrackType.Event) {
             app_id="web-vip"
+            widget_type="modal"
         }
     }
 
     test("navigation incentivate login autoclose track") {
         "/navigation/login/autoclose"(platform: "/", type: TrackType.Event) {
             app_id="web-vip"
+            widget_type="modal"
         }
     }
 
     test("navigation incentivate login for login link track") {
         "/navigation/login/login-link"(platform: "/", type: TrackType.Event) {
             app_id="web-vip"
+            widget_type="modal"
         }
     }
 
     test("navigation incentivate login for register link track") {
         "/navigation/login/register-link"(platform: "/", type: TrackType.Event) {
             app_id="web-vip"
+            widget_type="modal"
+        }
+    }
+
+    test("navigation incentivate login show track with widget type tooltip") {
+        "/navigation/login/show"(platform: "/", type: TrackType.View) {
+            app_id="web-vip"
+            widget_type="tooltip"
+        }
+    }
+
+    test("navigation incentivate login show track with widget type banner") {
+        "/navigation/login/show"(platform: "/", type: TrackType.View) {
+            app_id="web-vip"
+            widget_type="banner"
         }
     }
 }
