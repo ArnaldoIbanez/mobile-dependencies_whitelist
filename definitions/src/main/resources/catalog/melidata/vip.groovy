7ae4f660fdd4275f74646819bd094085dce1ec59
Vladimir Pomsztein <vladimir.pomsztein@mercadolibre.com>
2019-11-25 11:48:54 -0300

    Update required

--- definitions/src/main/resources/catalog/melidata/vip.groovy
+++ definitions/src/main/resources/catalog/melidata/vip.groovy
@@ -86,7 +86,7 @@ tracks {
         return_available(required: false, type: PropertyType.Boolean,
                 description: "Indicates if the user has free return for the item")
         has_variations(required: false, type: PropertyType.Boolean, description: "Indicates if the item has variations")
-        stock_type(required: true, type: PropertyType.String, inheritable: false, values: ["normal", "deferred"], description: "Indicates the type of stock for the product (normal = immediate stock, deferred = within x days)")
+        stock_type(required: false, type: PropertyType.String, inheritable: false, values: ["normal", "deferred"], description: "Indicates the type of stock for the product (normal = immediate stock, deferred = within x days)")
         stock_deferred_time(required: false, type: PropertyType.Numeric, inheritable: false, description: "Amount of days when the product will have available stock. Will only be used when stock_type = deferred")
         
         // CART
