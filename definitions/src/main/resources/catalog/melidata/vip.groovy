8f14f602a0086ad78d1d657b1bceb2ab298b5795
Bautista Querejeta <bautista.querejeta@mercadolibre.com>
2019-11-26 13:45:00 -0300

    item attributes

--- definitions/src/main/resources/catalog/melidata/vip.groovy
+++ definitions/src/main/resources/catalog/melidata/vip.groovy
@@ -147,8 +147,8 @@ tracks {
     
         quantity_models(required: false, type: PropertyType.Numeric, description: "Quantity models real estate developments")
 
-        // PROMOTIONS
-        promotion(required: false, type: PropertyType.String, description: "determines if item is promoted")
+        // ITEM_ATTRIBUTES
+        item_attributes(required: false, type: PropertyType.String, description: "")
 
     }
 
