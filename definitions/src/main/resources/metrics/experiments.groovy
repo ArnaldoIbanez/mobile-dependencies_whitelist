f9d02df25251ac34e47d07c51ede62591afe597f
Luigi Frassato <luigi.frassato@mercadolibre.com>
2019-12-05 12:01:09 -0300

    remove old metrics

--- definitions/src/main/resources/metrics/experiments.groovy
+++ definitions/src/main/resources/metrics/experiments.groovy
@@ -48,44 +48,6 @@ metrics {
 		}
 	}
 
-	// TODO REMOVE WHEN THIS EXPERIMENT IS OVER
-	"orders.InCarrouselCategories"(description: "extend experiment /search/brandCarrousel", parametricName: false, compute_order: true) {
-		startWith {
-			experiment("search/officialStoresCarousel")
-		}
-
-		countsOn {
-			condition {
-				path("/orders/ordercreated")
-				or(
-					like('event_data.items.item.category_path', '.*MLM(6585|5607|120666|182735|81531|4651|8574|2827|158119|1271|180982|1676|168281|1285|187814|158842|1010|158828|1386|5723|1578|1712|8378),.*'),
-					like('event_data.items.item.category_path', '.*MLB(23332|181294|1248|1002|181294|1676|191839|1286|264721|1580|1386|21168|1456),.*'),
-					like('event_data.items.item.category_path', '.*MLA(109027|1002|398582|6839|1248|1676|1042|1285|409558|1575|409810|1618|1386|6750|18353|3959),.*')
-				)
-			}
-		}
-	}
-
-	"orders.officialStores.InCarrouselCategories"(description: "extend experiment /search/brandCarrousel", parametricName: false, compute_order: true) {
-		startWith {
-			experiment("search/officialStoresCarousel")
-		}
-
-		countsOn {
-			condition {
-				path("/orders/ordercreated")
-				and(
-					empty("event_data.items.item.official_store_id", false),
-					or(
-						like('event_data.items.item.category_path', '.*MLM(6585|5607|120666|182735|81531|4651|8574|2827|158119|1271|180982|1676|168281|1285|187814|158842|1010|158828|1386|5723|1578|1712|8378),.*'),
-						like('event_data.items.item.category_path', '.*MLB(23332|181294|1248|1002|181294|1676|191839|1286|264721|1580|1386|21168|1456),.*'),
-						like('event_data.items.item.category_path', '.*MLA(109027|1002|398582|6839|1248|1676|1042|1285|409558|1575|409810|1618|1386|6750|18353|3959),.*')
-					)
-				)	
-			}
-		}
-	}
-
 	"relist_upgrade"(description: "An Item was relisted in a higher listing type than its parent") {
 		startWith {
 			experiment("sell/full_relist_single_item")
