d3aa27b121b6a6f9d85ccbde95e5a862d07f644b
Luigi Frassato <luigi.frassato@mercadolibre.com>
2019-12-05 11:21:30 -0300

    remove unused deals metrics

--- definitions/src/main/resources/metrics/banner_metrics.groovy
+++ definitions/src/main/resources/metrics/banner_metrics.groovy
@@ -33,26 +33,6 @@ metrics {
                 }
         }
 
-        "deal_banner_shown"(description: "Search deal banner shown", acceptMultiVariantSession: true, deprecation_date:"2019/12/10") {
-                startWith {
-                        condition {
-                                path("/search")
-
-                                and(
-                                        notEquals("event_data.banner.deal_id", ""),
-                                        notEquals("event_data.banner.exhibitors_id", "")
-                                )
-                        }
-
-                        set_property("campaign_id", "event_data.banner.deal_id")
-                        set_property("exhibitors_id", "event_data.banner.exhibitors_id")
-
-                        openBy {
-                                "event_data.banner.deal_id"(default: "-")
-                        }
-                }
-        }
-
         "deal_banner.shown"(description: "Search deal banner shown", acceptMultiVariantSession: true) {
                 startWith {
                         condition {
@@ -73,23 +53,6 @@ metrics {
                 }
         }
 
-        "deal_banner_click"(description: "Search deal banner clicked", deprecation_date:"2019/12/10") {
-                startWith {
-                        experiment("deal_banner_shown")
-                }
-
-                countsOn() {
-                        condition {
-                                path("/search")
-
-                                and(
-                                        equals("event_data.filters.deal", property("campaign_id")),
-                                        equals("event_data.click_banner.exhibitors_id", property("exhibitors_id"))
-                                )
-                        }
-                }
-        }
-
         "deal_banner.click"(description: "Search deal banner clicked") {
                 startWith {
                         experiment("deal_banner_shown")
@@ -103,25 +66,6 @@ metrics {
                                         equals("event_data.filters.deal", property("campaign_id")),
                                         equals("event_data.click_banner.exhibitors_id", property("exhibitors_id"))
                                 )
-                        }
-                }
-        }
-
-        "deal_banner_after_click"(description: "After click search deal banner", acceptMultiVariantSession: true, deprecation_date:"2019/12/10") {
-                startWith {
-                        condition {
-                                path("/search")
-
-                                and(
-                                        empty("event_data.filters.deal", false),
-                                        empty("event_data.click_banner.exhibitors_id", false)
-                                )
-                        }
-
-                        set_property("campaign_id", "event_data.filters.deal")
-
-                        openBy {
-                                "event_data.filters.deal"(default: "-")
                         }
                 }
         }
