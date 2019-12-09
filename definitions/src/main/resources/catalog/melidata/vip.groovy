94b84780dd8461576345449510c01173973489ae
Lucas Farias <lucas.farias@mercadolibre.com>
2019-12-09 15:04:49 -0300

    Add category path

--- definitions/src/main/resources/catalog/melidata/vip.groovy
+++ definitions/src/main/resources/catalog/melidata/vip.groovy
@@ -821,6 +821,7 @@ tracks {
     "/vip/back_to_listing"(platform: "/", type: TrackType.Event, parentPropertiesInherited: false) {
         item_id(required: true, type: PropertyType.String, description: "Item ID")
         category_id(required: true, type: PropertyType.String, description: "Item's category id")
+        category_path(required: false, type: PropertyType.ArrayList , description:  "Category path of the the item")
         domain_id(required: false, type: PropertyType.String, description: "Item's domain id")
         vertical(required: true, type: PropertyType.String,
                 values: ["core", "motors", "realEstate", "services"], description: "Vertical of the item")
