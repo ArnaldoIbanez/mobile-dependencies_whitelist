f06715272c030caca8bea082a195a278eab2551d
Jonatan Urquiza <jurquiza94@gmail.com>
2019-12-11 17:41:53 -0300

    shipping tracking

---
+++ definitions/src/main/resources/catalog/melidata/cards_hybrid.groovy
@@ -0,0 +1,36 @@
+import com.ml.melidata.catalog.PropertyType
+import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
+import com.ml.melidata.TrackType
+
+
+tracks {
+
+    //-----------------
+    // HYBRID
+    //-----------------
+
+    //Abstract Path
+    "/hybrid"(platform: "/", isAbstract: true) { }
+
+    // SHIPPING
+    // --------
+    "/hybrid/shipping"(platform: "/", isAbstract: true) { }
+
+    //Shipping: Tracking
+    "/hybrid/shipping/tracking"(platform: "/", type: TrackType.View) {
+        banner_is_present (required:false, type: PropertyType.Boolean, description: "Banner is present in Screen")
+        contact_is_present (required:false, type: PropertyType.Boolean, description: "Contact is present in Screen")
+    }
+    "/hybrid/shipping/tracking/tap"(platform:"/", type: TrackType.Event) {
+        action (required:true, type: PropertyType.String, description: "Action tapped")
+    }
+    "/hybrid/shipping/tracking/show"(platform:"/", type: TrackType.Event) {
+        component_id (required:true, type: PropertyType.String, description: "Component shown")
+    }
+
+    //Shipping: Delayed
+    "/hybrid/shipping/delayed"(platform: "/", type: TrackType.View) {}
+    "/hybrid/shipping/delayed/tap"(platform:"/", type: TrackType.Event) {
+        action (required:true, type: PropertyType.String, description: "Action tapped")
+    }
+}
---
+++ definitions/src/test/resources/melidata/cards_hybrid.groovy
@@ -0,0 +1,37 @@
+package src.test.resources.melidata
+
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+import com.ml.melidata.TrackType;
+
+trackTests {
+
+    //-----------------
+    // HYBRID
+    //-----------------
+
+
+    // SHIPPING
+    // --------
+
+    //Shipping: Tracking
+    test("cards hybrid shipping tracking") {
+        "/hybrid/shipping/tracking"(platform: "/", type: TrackType.View) {
+            banner_is_present = false
+            contact_is_present = true
+        }
+        "/hybrid/shipping/tracking/tap"(platform:"/", type: TrackType.Event) {
+            action = "contact"
+        }
+        "/hybrid/shipping/tracking/show"(platform:"/", type: TrackType.Event) {
+            action = "banner_unlock"
+        }
+
+    }
+    //Shipping: Delayed
+    test("cards hybrid shipping delayed") {
+        "/hybrid/shipping/delayed"(platform: "/", type: TrackType.View) {}
+        "/hybrid/shipping/delayed/tap"(platform:"/", type: TrackType.Event) {
+            action = "reissue"
+        }
+    }
+}
