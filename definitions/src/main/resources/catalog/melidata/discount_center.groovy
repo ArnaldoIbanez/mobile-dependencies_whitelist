5c4334394a7ad2d44525e007e70e5e04016e8e96
JuanchiFraga <juan.fraga@mercadolibre.com>
2019-12-12 11:50:54 -0300

    update required

--- definitions/src/main/resources/catalog/melidata/discount_center.groovy
+++ definitions/src/main/resources/catalog/melidata/discount_center.groovy
@@ -46,7 +46,7 @@ tracks {
     // DETAIL
 
     "/discount_center/payers/detail" (platform: "/mobile", type: TrackType.View) {
-        name(required: false, type: PropertyType.String, description: "The brand name")
+        name(required: true, type: PropertyType.String, description: "The brand name")
         enabled(required: true, type: PropertyType.Boolean, description: "If the discount can be consumed or not")
         availability(required: false, type: PropertyType.String, values: ['full', 'fewleft', 'soldout', 'soldout_today'], description: "Availability status of the discount")
         mcc(required: false, type: PropertyType.String, description: "The mcc of the category of the discount")
