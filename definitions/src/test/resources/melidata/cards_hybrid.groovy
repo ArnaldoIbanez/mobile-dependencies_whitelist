ca3c6bfbc9c7e45926cbecb4139a1f1e6dcd378e
Jonatan Urquiza <jurquiza94@gmail.com>
2019-12-12 12:23:51 -0300

    comments

--- definitions/src/test/resources/melidata/cards_hybrid.groovy
+++ definitions/src/test/resources/melidata/cards_hybrid.groovy
@@ -6,9 +6,41 @@ import com.ml.melidata.TrackType;
 trackTests {
 
     //-----------------
-    // HYBRID
+    // HYBRID MP
     //-----------------
 
+    defaultBusiness = "mercadopago"
+
+    // SHIPPING
+    // --------
+
+    //Shipping: Tracking
+    test("cards hybrid shipping tracking") {
+        "/hybrid/shipping/tracking"(platform: "/", type: TrackType.View) {
+            banner_is_present = false
+            contact_is_present = true
+        }
+        "/hybrid/shipping/tracking/tap"(platform:"/", type: TrackType.Event) {
+            action = "contact"
+        }
+        "/hybrid/shipping/tracking/show"(platform:"/", type: TrackType.Event) {
+            component_id = "banner_unlock"
+        }
+
+    }
+    //Shipping: Delayed
+    test("cards hybrid shipping delayed") {
+        "/hybrid/shipping/delayed"(platform: "/", type: TrackType.View) {}
+        "/hybrid/shipping/delayed/tap"(platform:"/", type: TrackType.Event) {
+            action = "reissue"
+        }
+    }
+
+    //-----------------
+    // HYBRID ML
+    //-----------------
+
+    defaultBusiness = "mercadolibre"
 
     // SHIPPING
     // --------
