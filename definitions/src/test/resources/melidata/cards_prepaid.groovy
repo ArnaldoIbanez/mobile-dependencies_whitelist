3daca8b32cdd250d02b1632c3e7d4034976a1acd
sddonne <sebastian.delle@mercadolibre.com>
2019-12-13 15:31:00 -0300

    fix test

--- definitions/src/test/resources/melidata/cards_prepaid.groovy
+++ definitions/src/test/resources/melidata/cards_prepaid.groovy
@@ -190,6 +190,7 @@ trackTests {
 
     test("Landing App Download button") {
         "/prepaid/acquisition/sellers_landing/store_button" (platform: "/", type: TrackType.Event) { }
+    }
 
     test("Prepaid cards MLM") {
         
