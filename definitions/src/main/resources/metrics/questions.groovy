11fe316dfd86cb9776d298f4ee6c00d13b8984e2
Martin Ciruzzi <martin.ciruzzi@mercadolibre.com>
2019-12-18 11:18:21 -0300

    add deprecation date

--- definitions/src/main/resources/metrics/questions.groovy
+++ definitions/src/main/resources/metrics/questions.groovy
@@ -11,7 +11,7 @@ metrics {
         }
     }
 
-    "pdp_questions_qadb"(description: "Track PDP questions of users in QADB experiment") {
+    "pdp_questions_qadb"(description: "Track PDP questions of users in QADB experiment", deprecation_date:"2019/12/18") {
         startWith {
             experiment("qadb/qadb-on")
         }
@@ -30,7 +30,22 @@ metrics {
         }
     }
 
-    "pdp_questions"(description: "Track PDP questions") {
+    "pdp_questions"(description: "Track PDP questions", deprecation_date:"2019/12/18") {
+        countsOn {
+            condition {
+                path("/questions/ask/post")
+                and(
+                        or(
+                                equals("event_data.context", "/pdp"),
+                                equals("event_data.context", "/qadb"),
+                                equals("event_data.context", "/questions/qadb")
+                        )
+                )
+            }
+        }
+    }
+
+    "questions.pdp"(description: "Track PDP questions") {
         countsOn {
             condition {
                 path("/questions/ask/post")
