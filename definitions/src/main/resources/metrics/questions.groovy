5e90bf365818e553d32634843c34a1a9c93f76ae
Martin <mciruzzi@users.noreply.github.com>
2019-12-19 22:28:17 -0300

    PDP Questions parcial fix (#3326)
    * Update questions.groovy
    
    * Update questions.groovy
    
    * retrigger

--- definitions/src/main/resources/metrics/questions.groovy
+++ definitions/src/main/resources/metrics/questions.groovy
@@ -1,6 +1,5 @@
 import static com.ml.melidata.metrics.parsers.dsl.MetricsDsl.metrics
 
-
 metrics {
 
     "questions"(description: "questions count") {
@@ -13,7 +12,7 @@ metrics {
 
     "pdp_questions_qadb"(description: "Track PDP questions of users in QADB experiment", deprecation_date:"2019/12/18") {
         startWith {
-            experiment("qadb/qadb-on")
+		experiment(regex("qadb/(qadb-on-vip|qadb-on-viewport-vip)"))
         }
 
         countsOn {
@@ -32,17 +31,24 @@ metrics {
 
     "pdp_questions"(description: "Track PDP questions", deprecation_date:"2019/12/18") {
         countsOn {
-            condition {
-                path("/questions/ask/post")
-                and(
-                        or(
-                                equals("event_data.context", "/pdp"),
-                                equals("event_data.context", "/qadb"),
-                                equals("event_data.context", "/questions/qadb")
-                        )
-                )
-            }
-        }
+			condition {
+				path("/questions/ask/post")
+				and(
+					or(
+						equals("event_data.context", "/pdp"),
+						and(
+							equals("event_data.context", "/qadb"),
+							empty("event_data.catalog_product_id", false)
+						)
+					),
+					or (
+						equals("event_data.failed", false) ,
+						isNull("event_data.failed" )
+
+					)
+				)
+			}
+		}
     }
 
     "questions.pdp"(description: "Track PDP questions") {
@@ -59,7 +65,5 @@ metrics {
             }
         }
     }
-
-
 
 }
