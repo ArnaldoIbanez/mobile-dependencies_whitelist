6ffc9ae44770c44495dda2855fa6aed5f9a445b6
Gonzalo (Junior) Saenz <gonzalosaenz@hotmail.com.ar>
2020-01-08 14:38:41 -0300

    fix

--- definitions/src/main/resources/catalog/melidata/search.groovy
+++ definitions/src/main/resources/catalog/melidata/search.groovy
@@ -35,6 +35,11 @@ tracks {
         selected_id(required:true, PropertyType.String)
     }
 
+    def promise_item_definition = objectSchemaDefinitions{
+        sameday(type: PropertyType.ArrayList(PropertyType.String), required: false)
+        nextday(type: PropertyType.ArrayList(PropertyType.String), required: false)
+    }
+
     //SEARCH FLOW
     
     "/search"(platform: "/") {
@@ -49,7 +54,7 @@ tracks {
         autoselected_filters(required: false, description: "filters not applied by the user (category from canonical or adults)", PropertyType.ArrayList)
         view_mode(required: true, description: "MOSAIC, LIST or GALLERY on WM and apps and STACK or GRID on desktop", values:["STACK","GRID","LIST","MOSAIC","GALLERY"])
         results(required: true, description: "item ids from search result", PropertyType.ArrayList)
-        promise_items(required: false, description:  "items with shipping promise", PropertyType.Map(PropertyType.ArrayList(PropertyType.String)))
+        promise_items(required: false, description:  "items with shipping promise", type: PropertyType.Map(promise_item_definition))
 
         billboards(required: false, description: "items ids from billboard results", PropertyType.ArrayList)
         pads(required: false, description: "item_id from the pads returned for listings")
