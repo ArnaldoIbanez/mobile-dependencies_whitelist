3b84a6366b994b762f378c977829256cbf724aac
Ezequiel Perez <ezequiel.perez@mercadolibre.com>
2020-01-14 10:24:38 -0300

    Se cambian a obligatorios los campos

--- definitions/src/main/resources/catalog/melidata/seller_central.groovy
+++ definitions/src/main/resources/catalog/melidata/seller_central.groovy
@@ -378,9 +378,9 @@ tracks {
 
 
     "/seller_central/bulk/offline/cbt/download/confirm"(platform: "/", type: TrackType.Event) {
-        items(required: false, type: PropertyType.Numeric, description: "Amount of downloaded items")
-        filters(required: false, type: PropertyType.ArrayList(PropertyType.String), description: "List of applied filters")
-        categories(required: false, type: PropertyType.ArrayList(PropertyType.String), description: "List of applied categories")
+        items(required: true, type: PropertyType.Numeric, description: "Amount of downloaded items")
+        filters(required: true, type: PropertyType.ArrayList(PropertyType.String), description: "List of applied filters")
+        categories(required: true, type: PropertyType.ArrayList(PropertyType.String), description: "List of applied categories")
     }
 
     "/seller_central/bulk/offline/cbt/download/congrats"(platform: "/", type: TrackType.View) {
