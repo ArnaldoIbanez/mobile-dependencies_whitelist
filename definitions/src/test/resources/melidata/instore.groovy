5eefe1f4269a354746ea3aa7b70cbec783cfdb53
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-01-15 12:30:38 -0300

    fix tests

--- definitions/src/test/resources/melidata/instore.groovy
+++ definitions/src/test/resources/melidata/instore.groovy
@@ -1176,6 +1176,7 @@ trackTests {
         "/instore/buyer_qr/landing/account_money_only"(platform: "/mobile", type: TrackType.View) {
             session_id = "8778E611-A48A-4CAF-9E7B-3700D1AC2692"
         }
+
         "/instore/buyer_qr/landing/insufficient_money_change_payment_method"(platform: "/mobile", type: TrackType.View) {
             session_id = "8778E611-A48A-4CAF-9E7B-3700D1AC2692"
             status = "failure"
@@ -1190,7 +1191,35 @@ trackTests {
             collector_id = "388741077"
         }
 
+        "/instore/buyer_qr/landing/insufficient_money_change_payment_method"(platform: "/mobile", business: "mercadopago", type: TrackType.View) {
+            session_id = "8778E611-A48A-4CAF-9E7B-3700D1AC2692"
+            status = "failure"
+            amount_paid = "160"
+            item_amount = "200"
+            payment_method_name = "Dinero en Mercado Pago"
+            payment_method_id = ""
+            currency_id =  "ARS"
+            payment_date = "12/07/07:23.55.32hs"
+            payment_id = "1879867544"
+            collector_name = "Oxxo"
+            collector_id = "388741077"
+        }
+
         "/instore/buyer_qr/landing/insufficient_money_add_card"(platform: "/mobile", type: TrackType.View) {
+            session_id = "8778E611-A48A-4CAF-9E7B-3700D1AC2692"
+            status = "failure"
+            amount_paid = "160"
+            item_amount = "200"
+            payment_method_name = "Dinero en Mercado Pago"
+            payment_method_id = ""
+            currency_id =  "ARS"
+            payment_date = "12/07/07:23.55.32hs"
+            payment_id = "1879867544"
+            collector_name = "Oxxo"
+            collector_id = "388741077"
+        }
+
+        "/instore/buyer_qr/landing/insufficient_money_add_card"(platform: "/mobile", business: "mercadopago", type: TrackType.View) {
             session_id = "8778E611-A48A-4CAF-9E7B-3700D1AC2692"
             status = "failure"
             amount_paid = "160"
