dbba6a1b1578976248e6e4ec9d51208937ba6277
Victor R <victor.rocha@mercadolivre.com>
2020-01-16 13:46:07 -0300

    added manual scan case tracking

--- definitions/src/main/resources/catalog/melidata/wms.groovy
+++ definitions/src/main/resources/catalog/melidata/wms.groovy
@@ -375,6 +375,7 @@ tracks {
     "/wms/problem_solver/menu"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_inbound_shipment"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/manual_input_inbound_shipment"(platform: "/mobile/android", type: TrackType.View) {}
+    "/wms/problem_solver/manual_input_case"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_destination"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_case"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_return_address"(platform: "/mobile/android", type: TrackType.View) {}
