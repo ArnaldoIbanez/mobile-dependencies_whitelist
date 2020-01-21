1a1ab630ad54002816134b158437c7e4173b07d0
Martin <mciruzzi@users.noreply.github.com>
2020-01-21 14:59:54 -0300

    delete metric not used

--- definitions/src/main/resources/metrics/questions.groovy
+++ definitions/src/main/resources/metrics/questions.groovy
@@ -10,25 +10,6 @@ metrics {
         }
     }
 
-    "pdp_questions_qadb"(description: "Track PDP questions of users in QADB experiment", deprecation_date:"2019/12/18") {
-        startWith {
-		experiment(regex("qadb/(qadb-on-vip|qadb-on-viewport-vip)"))
-        }
-
-        countsOn {
-            condition {
-                path("/questions/ask/post")
-                and(
-                        or(
-                                equals("event_data.context", "/pdp"),
-                                equals("event_data.context", "/qadb"),
-                                equals("event_data.context", "/questions/qadb")
-                        )
-                )
-            }
-        }
-    }
-
 
     "questions.pdp"(description: "Track PDP questions") {
       	countsOn {
