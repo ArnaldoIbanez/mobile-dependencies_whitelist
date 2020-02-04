4c34b4dfbcbcca09cddbac3f0370682b510d55cd
Noel Podesta <noel.podesta@mercadolibre.com>
2020-02-04 16:51:30 -0300

    fixing test

--- definitions/src/main/resources/catalog/melidata/seller_central.groovy
+++ definitions/src/main/resources/catalog/melidata/seller_central.groovy
@@ -803,7 +803,7 @@ tracks {
     "/seller_central/sales/list/onboarding/massive/confirm"(platform: "/", type: TrackType.Event) {}
     "/seller_central/sales/list/onboarding/massive/close"(platform: "/", type: TrackType.Event) {}
 
-    "/seller_central/sales/list/nfe_onboarding"(platform: "/", isAbstract: true) {}
+    "/seller_central/sales/list/nfe_onboarding"(platform: "/", isAbstract: true, parentPropertiesInherited:false) {}
     "/seller_central/sales/list/nfe_onboarding/action"(platform: "/", type: TrackType.Event) {
         id(required: true, type: PropertyType.String, description: "Id of the action", values: ["close", "dismiss", "start"])
         page(required: false, type: PropertyType.Numeric, description: "Page number")
