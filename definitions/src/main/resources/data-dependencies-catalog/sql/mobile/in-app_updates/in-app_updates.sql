de91f0ad8b894e5db6fe1b54bdc55c2814a55815
eferrin <eferrin_93@hotmail.com>
2020-02-12 11:43:37 -0300

    use jest and delete union

--- definitions/src/main/resources/data-dependencies-catalog/sql/mobile/in-app_updates/in-app_updates.sql
+++ definitions/src/main/resources/data-dependencies-catalog/sql/mobile/in-app_updates/in-app_updates.sql
@@ -1,15 +1,12 @@
-  (SELECT application.business,
+  SELECT application.business,
           application.site_id AS site,
           CASE
               WHEN path = '/in_app_updates/updatable/showed' THEN 'showed'
               WHEN path = '/in_app_updates/updatable/accepted' THEN 'accepted'
               ELSE 'rejected'
           END AS Action,
-          CASE
-              WHEN event_data LIKE '%flexible%' THEN 'Google Flexible'
-              WHEN event_data LIKE '%immediate%' THEN 'Google Immediate'
-              ELSE 'Bump'
-          END AS Flow_Type,
+          jest
+  (event_data, 'type') AS flow_type,
           COUNT
      (1) AS How_Many
    FROM tracks
@@ -21,34 +18,9 @@
         OR path = '/in_app_updates/updatable/postponed')
      AND device.platform = '/mobile/android'
    GROUP BY application.business,
-            event_data,
+            jest
+  (event_data, 'type'),
             path,
-            application.site_id)
-UNION
-  (SELECT application.business,
-          'ALL' AS site,
-          CASE
-              WHEN path = '/in_app_updates/updatable/showed' THEN 'showed'
-              WHEN path = '/in_app_updates/updatable/accepted' THEN 'accepted'
-              ELSE 'rejected'
-          END AS Action,
-          CASE
-              WHEN event_data LIKE '%flexible%' THEN 'Google Flexible'
-              WHEN event_data LIKE '%immediate%' THEN 'Google Immediate'
-              ELSE 'Bump'
-          END AS Flow_Type,
-          COUNT
-     (1) AS How_Many
-   FROM tracks tw
-   WHERE ds >= '@param01'
-     AND ds < '@param02'
-     AND
-       (path = '/in_app_updates/updatable/showed'
-        OR path = '/in_app_updates/updatable/accepted'
-        OR path = '/in_app_updates/updatable/postponed')
-     AND device.platform = '/mobile/android'
-   GROUP BY application.business,
-            path,
-            event_data)
+            application.site_id
 ORDER BY Action DESC,
          How_Many DESC
