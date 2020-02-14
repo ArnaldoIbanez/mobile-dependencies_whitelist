46600635d46812aee8b4b5c26ec1966076f25626
IvanRufino2471 <ivan.rufino@mercadolibre.com>
2020-02-14 09:32:52 -0300

    init API cache

--- definitions/src/main/groovy/com/melidata/definitions/manager/CatalogManager.groovy
+++ definitions/src/main/groovy/com/melidata/definitions/manager/CatalogManager.groovy
@@ -2,6 +2,8 @@ package com.melidata.definitions.manager
 
 import com.ml.melidata.catalog.Catalog
 import com.ml.melidata.Track
+import com.ml.melidata.catalog.initiatives.InitiativeAPI
+
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
@@ -34,6 +36,7 @@ class CatalogManager implements Runnable {
 	}
 
 	void init() {
+		InitiativeAPI.getInstance().init()
 		catalogHandlers.each{ _, catalogHandler -> catalogHandler.reload()}
 		Executors.newScheduledThreadPool(1).scheduleAtFixedRate(this, 0, 5, TimeUnit.MINUTES)
 	}
