7dd4b9acbb32fb2a1e3bba67fa91658b5bae058f
Martin Crespi <martin.crespi@mercadolibre.com>
2020-02-19 11:03:42 -0300

    Update discount_sellers.groovy

--- definitions/src/main/resources/catalog/melidata/discount_sellers.groovy
+++ definitions/src/main/resources/catalog/melidata/discount_sellers.groovy
@@ -26,7 +26,10 @@ tracks {
         tooltip(required: false, type: PropertyType.String, description: "Tooltip description")
     }
 
-    "/discount_sellers/form/redirect" (platform: "/mobile", type: TrackType.Event) {}
+    "/discount_sellers/form/redirect" (platform: "/mobile", type: TrackType.Event) {
+        melicampaign_id(required: false, type: PropertyType.String, description: "Melicampaign identifier")
+	redirection_to(required: false, type: PropertyType.String, description: "profile_check, landing, disabled_feature, etc")
+    }
 
     "/discount_sellers/form/section" (platform: "/mobile", isAbstract: true) {}
 
