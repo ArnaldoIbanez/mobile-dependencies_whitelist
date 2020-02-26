aec832cace6995ed0ad820185e66059e4dab09c4
IvanRufino2471 <ivan.rufino@mercadolibre.com>
2020-02-26 16:44:59 -0300

    just one gradle task

--- commands/test_integration.sh
+++ commands/test_integration.sh
@@ -4,17 +4,7 @@ apt-get install jq -y
 if [ $(echo "${BUILD_CONTEXT}" | jq -r '.git_base_branch') == "master" ] && [ $(echo "${BUILD_CONTEXT}" | jq -r '.git_is_merge') = true ]
 then
     ./gradlew testAll
-    echo "Starting upload of all catalogs"
-    ./gradlew uploadAllCatalog
-
-    echo "Starting upload metrics"
-    ./gradlew uploadMetrics
-
-    echo "Starting upload json for batch queries"
-    ./gradlew uploadBatchQueries
-
-    echo "Starting upload json for whitelisted paths"
-    ./gradlew uploadWhitelist
+    ./gradlew uploadAll
 else
     ./gradlew testAll
 fi
