0a5291980b11a80ba745f6b2652ef1e03e6cec0d
IvanRufino2471 <ivan.rufino@mercadolibre.com>
2020-02-26 11:53:13 -0300

    install jq

--- commands/test_integration.sh
+++ commands/test_integration.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-printenv
+sudo apt-get install jq
 ./gradlew testAll
 
 if [ $(echo "${BUILD_CONTEXT}" | jq -r '.git_base_branch') == "master" ] && [ $(echo "${BUILD_CONTEXT}" | jq -r '.git_is_merge') = true ]
