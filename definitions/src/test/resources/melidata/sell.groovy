28094e5cab115d5277f382b3eff35c1619e08304
ar venz <cuelloandres.js@gmail.com>
2020-02-26 16:48:50 -0300

    remove duplicated code

--- definitions/src/test/resources/melidata/sell.groovy
+++ definitions/src/test/resources/melidata/sell.groovy
@@ -1916,7 +1916,16 @@ trackTests {
     }
 
     test("SYI v4 List Similar variation selection page"){
-        "/sell/variation_selection"(platform: "/web", type: TrackType.View) {}
+        "/sell/variation_selection"(platform: "/web", type: TrackType.View) {
+            category_id = "MLA390784"
+            category_path = ["MLA1", "MLA2", "MLA3"]
+            seller_profile = "ADVANCED"
+            seller_segment = "professional_sellers"
+            session_id = "123-update-abc123"
+            seller_reputation = "green"
+            list_mode = "LIST"
+            vertical = "core"
+        }
     }
 
     test("SYI v4 List Similar variation selection, source variations, card show"){
