49d4a5b9ea9c5eb08f2a309bbf26f756790a2966
Santiago Marchioni <smarchioni87@gmail.com>
2020-03-03 17:40:58 -0300

    add puMa errors

---
+++ src/main/resources/data-dependencies-catalog/sql/sell/sell-bulk-errors.sql
@@ -0,0 +1,14 @@
+SELECT
+      tracks.application.site_id,
+      json_extract(listingsNotPublished,'$.categoryId') AS category,
+      json_extract(errors,'$.code') AS errorCode,
+      substr(ds,1,10) AS period,
+      sum(cast(json_extract(errors,'$.occurrences')AS INTEGER)) AS quantity
+FROM tracks
+CROSS JOIN UNNEST(CAST (jet(tracks.event_data,'listings_not_published') AS ARRAY<JSON>)) AS t (listingsNotPublished)
+CROSS JOIN UNNEST(CAST (json_extract(listingsNotPublished,'$.errors') AS ARRAY<JSON>)) AS t (errors)
+WHERE path = '/seller_central/bulk/publish/publish-callback'
+AND ds >= '@param01'
+AND ds < '@param02'
+GROUP BY 1,2,3,4
+ORDER BY quantity DESC
