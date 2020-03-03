06970225c4beff5baae0aecaa1a1b2eb7b828db3
Victor R <victor.rocha@mercadolivre.com>
2020-03-03 19:21:17 -0300

    removed some files not being used

--- src/main/resources/catalog/melidata/wms.groovy
+++ src/main/resources/catalog/melidata/wms.groovy
@@ -376,16 +376,13 @@ tracks {
     }
 
     /* Problem Solver - Flan */
-    "/wms/problem_solver/menu"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_inbound_shipment"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/manual_input_inbound_shipment"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_case"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/manual_input_case"(platform: "/mobile/android", type: TrackType.View) {}
-    "/wms/problem_solver/scan_inbound_destination"(platform: "/mobile/android", type: TrackType.View) {}
-    "/wms/problem_solver/scan_finish_transactions"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_transaction_destination"(platform: "/mobile/android", type: TrackType.View) {}
-    "/wms/problem_solver/scan_oriented_origin"(platform: "/mobile/android", type: TrackType.View) {}
     "/wms/problem_solver/scan_origin"(platform: "/mobile/android", type: TrackType.View) {}
+    "/wms/problem_solver/menu"(platform: "/mobile/android", type: TrackType.View) {}
 
     /* Stock audit*/
     "/wms/stock_audit/confirmation"(platform: "/mobile/android", type: TrackType.View) {}
