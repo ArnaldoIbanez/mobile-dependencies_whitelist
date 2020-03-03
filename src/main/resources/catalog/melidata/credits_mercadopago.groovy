990bc521269b82a47ed34250876d20e9665dc414
David Forneron <david.forneron@mercadolibre.com>
2020-03-03 09:58:39 -0300

    reason fixed

--- src/main/resources/catalog/melidata/credits_mercadopago.groovy
+++ src/main/resources/catalog/melidata/credits_mercadopago.groovy
@@ -437,7 +437,7 @@ tracks {
                 'loan_creation',
                 'feedback_creation',
                 'files_upload',
-                'rejected_by_regulation',
+                'rejected_loan_by_regulation',
                 'unknown-error',
                 'admin-is-restricted',
                 'default'
