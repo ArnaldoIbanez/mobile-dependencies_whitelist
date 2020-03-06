52c616b202a7aa412441c19781ed8dcfdb1457f2
Ariel Fernandez <ariel.fernandez@mercadolibre.com>
2020-03-06 11:14:09 -0300

    add initiative and experiment landings track

--- src/main/resources/catalog/melidata/front_core.groovy
+++ src/main/resources/catalog/melidata/front_core.groovy
@@ -425,4 +425,15 @@ tracks {
 
     "/wallet_home/cross_sell" (platform: "/mobile", type: TrackType.Event) {}
 
+    /**
+     * New Digital Wallet Tracks - Ukraine Lab
+     */
+    "/new_digital_wallet_landing"(platform: "/", type: TrackType.View, initiative: "1176") {
+        landing_id (required: true, type: PropertyType.String, description: "Indicate landing id")
+    }
+    "/new_digital_wallet_landing/click_download_app"(platform: "/", type: TrackType.Event) {
+        landing_id (required: true, type: PropertyType.String, description: "Indicate landing id")
+        section (required: true, type: PropertyType.String, values: ["hero", "fixed", "footer"], description: "Section from which the event comes")
+    }
+
 }
--- src/main/resources/catalog/melidata/mercadopago.groovy
+++ src/main/resources/catalog/melidata/mercadopago.groovy
@@ -190,7 +190,7 @@ tracks {
 
     // Services landings
     "/services"(platform: "/", isAbstract: true, initiative: "1159") {}
-    "/services/mkt_landing"(platform: "/web", type: TrackType.View) {
+    "/services/mkt_landing"(platform: "/web", type: TrackType.View, initiative: "1176") {
         campaign (require: true, type: PropertyType.String, description: "Indicates de campaign of landing")
     }
     "/services/mkt_landing/sms"(platform: "/web", type: TrackType.Event) {
@@ -207,7 +207,7 @@ tracks {
 
     // QR Payers Landings
     "/qr_payers"(platform: "/", isAbstract: true, initiative: "1159") {}
-    "/qr_payers/mkt_landing"(platform: "/web", type: TrackType.View) {
+    "/qr_payers/mkt_landing"(platform: "/web", type: TrackType.View, initiative: "1176") {
         campaign (require: true, type: PropertyType.String, description: "Indicates de campaign of landing")
     }
     "/qr_payers/mkt_landing/sms"(platform: "/web", type: TrackType.Event) {
