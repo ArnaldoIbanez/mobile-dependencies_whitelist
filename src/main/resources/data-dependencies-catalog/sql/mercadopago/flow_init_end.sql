254e3daa2a8bc4ee272311d27d283a8cd9b95812
Julio Cardenas <jcardenas@cl000000bjdyxt2.ml.com>
2020-03-10 17:56:05 -0300

    fix comentarios

--- src/main/resources/data-dependencies-catalog/sql/mercadopago/flow_init_end.sql
+++ src/main/resources/data-dependencies-catalog/sql/mercadopago/flow_init_end.sql
@@ -9,7 +9,7 @@ AND   ds < '@param02'
           AND type = 'event' 
           AND application.business = 'mercadopago' 
           AND device.platform = '/mobile/android'
-          AND application.version >= '2.109.0%'
+          AND application.version >= '2.109.%'
           AND path = '/flow/init'
           AND application.site_id IN ('MLA','MLB','MLC','MLM','MCO','MLU','MPE')
     GROUP BY substr(ds, 1,10), usr.user_id, device.platform, application.site_id, application.version, jest(event_data, 'flow_name') 
@@ -21,11 +21,12 @@ AND   ds < '@param02'
           AND type = 'event' 
           AND application.business = 'mercadopago'
           AND device.platform = '/mobile/android'
-          AND application.version  >= '2.109.0%'
+          AND application.version  >= '2.109.%'
           AND path = '/flow/end'
           AND application.site_id IN ('MLA','MLB','MLC','MLM','MCO','MLU','MPE')
     GROUP BY substr(ds, 1,10),usr.user_id, device.platform, application.site_id, application.version, jest(event_data, 'flow_name') 
 ) AS MP_FLOW_END
 ON MP_FLOW_INIT.user_id = MP_FLOW_END.user_id
   AND MP_FLOW_INIT.fecha = MP_FLOW_END.fecha
+  AND MP_FLOW_INIT.flow_name = MP_FLOW_END.flow_name
   GROUP BY MP_FLOW_INIT.fecha ,MP_FLOW_INIT.user_id, MP_FLOW_INIT.platform, MP_FLOW_INIT.site, MP_FLOW_INIT.version, MP_FLOW_INIT.flow_name
