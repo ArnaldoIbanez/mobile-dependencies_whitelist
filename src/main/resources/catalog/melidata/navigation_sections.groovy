6b79369378a9a8f412ec8bfb07130a79b5ef7030
Eden <eden.etorres@gmail.com>
2020-03-10 22:35:05 -0300

    Add navigation sections

---
+++ src/main/resources/catalog/melidata/navigation_sections.groovy
@@ -0,0 +1,30 @@
+package catalog.melidata
+
+import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
+import com.ml.melidata.TrackType
+import com.ml.melidata.catalog.PropertyType
+
+
+tracks {
+
+    // View
+
+    "/navigation_sections"(platform: "/mobile", isAbstract: true){}
+
+    "/navigation_sections/navigation_menu_mp"(platform: "/mobile", type: TrackType.View) {}
+    "/navigation_sections/your_profile"(platform: "/mobile", type: TrackType.View) {}
+    "/navigation_sections/your_business"(platform: "/mobile", type: TrackType.View) {}
+
+    // Events
+
+    "/navigation_sections/navigation_menu_mp/tap"(platform: "/mobile", type: TrackType.Event) {
+        id(required: true, type: PropertyType.String, description: "Section id tapped")
+    }
+    "/navigation_sections/your_profile/tap"(platform: "/mobile", type: TrackType.Event) {
+        id(required: true, type: PropertyType.String, description: "Section tapped")
+    }
+    "/navigation_sections/your_business/tap"(platform: "/mobile", type: TrackType.Event) {
+        id(required: true, type: PropertyType.String, description: "Section tapped")
+    }
+
+}
