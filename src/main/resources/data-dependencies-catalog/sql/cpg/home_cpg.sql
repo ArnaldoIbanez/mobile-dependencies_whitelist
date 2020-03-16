2232c1c24617a2c01d3c7fa9572a93d908018ed8
Leandro Faria <leandroffaria@gmail.com>
2020-03-16 17:27:26 -0300

    fix(regexp_extract): add check nullable

--- src/main/resources/data-dependencies-catalog/sql/cpg/home_cpg.sql
+++ src/main/resources/data-dependencies-catalog/sql/cpg/home_cpg.sql
@@ -6,7 +6,7 @@ FROM (
          WHEN jest(COALESCE(platform.fragment, others['fragment']), 'origin') RLIKE 'page_x' THEN 'page_x'
          WHEN jest(COALESCE(platform.fragment, others['fragment']), 'origin') RLIKE 'vip' THEN 'vip'
     END AS source,
-    CASE WHEN COALESCE(platform.fragment, others['fragment']) RLIKE 'EI:[0-9]+' THEN regexp_extract(others['fragment'], 'EI:([0-9]+)', 1)
+    CASE WHEN COALESCE(platform.fragment, others['fragment']) RLIKE 'EI:[0-9]+' THEN regexp_extract(COALESCE(platform.fragment, others['fragment']), 'EI:([0-9]+)', 1)
          WHEN jest(COALESCE(platform.fragment, others['fragment']), 'c_id') RLIKE '/home/exhibitors-carousel' THEN 'main_slider'
          WHEN jest(COALESCE(platform.fragment, others['fragment']), 'c_id') RLIKE '/home/discovery' THEN 'discovery_module'
          WHEN jest(COALESCE(platform.fragment, others['fragment']), 'c_id') RLIKE '/home/categories-carousel' THEN 'categories_carousel'
