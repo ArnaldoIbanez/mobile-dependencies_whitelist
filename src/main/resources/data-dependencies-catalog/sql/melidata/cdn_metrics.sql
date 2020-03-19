6a6d71676959ff0dafdc87c6843670602221a0e1
Krikor Krikorian <krikor.krikorian@todoconk.com>
2020-03-19 18:19:36 -0300

    order fields cdn metrics

--- src/main/resources/data-dependencies-catalog/sql/melidata/cdn_metrics.sql
+++ src/main/resources/data-dependencies-catalog/sql/melidata/cdn_metrics.sql
@@ -1,14 +1,14 @@
 SELECT
-jest(event_data, 'duration') as Duration,
-jest(event_data, 'connection') as ConnectionType,
-jest(event_data, 'filename') as Filename,
-jest(event_data, 'cache_status') as CacheStatus,
-jest(event_data, 'cdn_provider') as CDNProvider,
-jest(event_data, 'size') as FileSize,
-application.site_id as SiteId,
-device.platform as DevicePlatform,
-remote_ip as RemoteIP,
-substr(ds, 1, 10) as DS
+application.site_id as site_id,
+device.platform as device_platform,
+jest(event_data, 'cdn_provider') as cdn_provider,
+jest(event_data, 'connection') as connection_type,
+jest(event_data, 'filename') as file_name,
+jest(event_data, 'cache_status') as cache_status,
+jest(event_data, 'size') as file_size,
+jest(event_data, 'duration') as duration,
+remote_ip as remote_ip,
+substr(ds, 1, 10) as ds
 FROM tracks
 WHERE path = '/cdn/profiler'
 AND type = 'event'
