9a246ddd44e757c8fce3e14f3f5040e1cc255718
Javier Loucim <javier.loucim@mercadolibre.com>
2020-03-20 10:51:38 -0300

    refactor fields

--- src/main/resources/catalog/melidata/modals_engine.groovy
+++ src/main/resources/catalog/melidata/modals_engine.groovy
@@ -23,12 +23,10 @@ tracks {
     }
 
     "/modals_engine"(platform: "/mobile", type: TrackType.View) {
-        id(required: true, type: PropertyType.String, description: "modal content ID")
+        modalsEngineMinimumFields
         bu(required: true, type: PropertyType.String, description: "Numeric id of business: MP | ML | etc")
         bu_line(required: true, type: PropertyType.String, description: "Numeric id of line: Credits | wallet Point | etc")
         flow(required: true, type: PropertyType.String, description: "numeric id of app flow: Cellphone Recharge | Money transfer | Pago con QR | etc")
-        modal_type(required: true, type: PropertyType.String, description: "Layout type of the modal", values: ["full", "card"])
-        from(required: false, type: PropertyType.String, description: "Text to identify multiple sources to access the same content.")
     }
 
     "/modals_engine/dismiss"(platform: "/mobile", type: TrackType.Event) {} 
