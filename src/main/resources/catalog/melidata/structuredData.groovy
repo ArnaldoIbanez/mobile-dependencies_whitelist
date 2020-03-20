0f89efeff498753a577fbcd086aa241f68427a1f
Juan <jjrosalesuci@gmail.com>
2020-03-20 13:18:13 -0300

    Arreglo en tipo de dato

--- src/main/resources/catalog/melidata/structuredData.groovy
+++ src/main/resources/catalog/melidata/structuredData.groovy
@@ -260,7 +260,7 @@ tracks {
     }
 
     "/structure_data/product_merge/available_products"(platform: "/", type:TrackType.Event){
-        products_ids(required: true, description: "ProductsID collection", type: PropertyType.String)
+        products_ids(required: true, description: "ProductsID collection", type: PropertyType.Numeric)
     }
 
     // Catalog search tracks
