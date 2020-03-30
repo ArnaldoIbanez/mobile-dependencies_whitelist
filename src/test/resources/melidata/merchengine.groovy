99ac3d66f69422fc70a2d97373c2a1d5226cdb6d
Javier Loucim <javier.loucim@mercadolibre.com>
2020-03-30 12:11:04 -0300

    Remove tabs

--- src/test/resources/melidata/merchengine.groovy
+++ src/test/resources/melidata/merchengine.groovy
@@ -8,9 +8,9 @@ trackTests {
 
     ["mercadolibre", "mercadopago"].each { business ->
         defaultBusiness = business
-		//Modal Events
-		test("Merchengine Modals tracking / [${business}]") {
-			"/merchengine/modal"(platform: "/mobile", type: TrackType.View) {
+        //Modal Events
+        test("Merchengine Modals tracking / [${business}]") {
+            "/merchengine/modal"(platform: "/mobile", type: TrackType.View) {
                 component_id = "secondary_actions_payers_recharge_sube_mp"
                 content_id = "recharge_sube"
                 bu = "mp"
@@ -18,9 +18,9 @@ trackTests {
                 position = 0
                 logic = "user_journey"
                 flow = "default"
-			}
+            }
 
-			"/merchengine/modal/dismiss"(platform: "/mobile", type: TrackType.Event) {
+            "/merchengine/modal/dismiss"(platform: "/mobile", type: TrackType.Event) {
                 component_id = "secondary_actions_payers_recharge_sube_mp"
                 content_id = "recharge_sube"
                 bu = "mp"
@@ -28,9 +28,9 @@ trackTests {
                 position = 0
                 logic = "user_journey"
                 flow = "default"
-			}
+            }
 
-			"/merchengine/modal/cta"(platform: "/mobile", type: TrackType.Event) {
+            "/merchengine/modal/cta"(platform: "/mobile", type: TrackType.Event) {
                 component_id = "secondary_actions_payers_recharge_sube_mp"
                 content_id = "recharge_sube"
                 bu = "mp"
@@ -38,12 +38,12 @@ trackTests {
                 position = 0
                 logic = "user_journey"
                 flow = "default"
-				action_id = "cta1"
-				type = "primary"
-				link = "mercadopago://wallet"
-			}
+                action_id = "cta1"
+                type = "primary"
+                link = "mercadopago://wallet"
+            }
 
-			"/merchengine/modal/swipe"(platform: "/mobile", type: TrackType.Event) {
+            "/merchengine/modal/swipe"(platform: "/mobile", type: TrackType.Event) {
                 component_id = "secondary_actions_payers_recharge_sube_mp"
                 content_id = "recharge_sube"
                 bu = "mp"
@@ -51,7 +51,7 @@ trackTests {
                 position = 3
                 logic = "user_journey"
                 flow = "default"
-			}
-		}
-	}
+            }
+        }
+    }
 }
