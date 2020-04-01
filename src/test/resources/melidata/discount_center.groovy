105774ed3be348744976560e51ba28a1271aea28
Tomi De Lucca <tomas.delucca@mercadolibre.com>
2020-04-01 11:53:00 -0300

    Fixed px tracking

--- src/test/resources/melidata/discount_center.groovy
+++ src/test/resources/melidata/discount_center.groovy
@@ -170,12 +170,36 @@ trackTests {
     test("Mercado Pago discount center payers touchpoint px congrats tap") {
         "/discount_center/payers/touchpoint/px_congrats/tap" (platform: "/mobile", type: TrackType.Event) {
             tracking_id= "346859234"
+            flow= "/instore"
+            checkout_type= "one_tap"
+            security_enabled= false
+            experiments= "px nativo"
+            session_id= "65c7168b-cf25-4033-ad7a-e7874868a164"
+            session_time= 58
+            flow_detail= [
+                store_id: "30974197",
+                pos_id: "7414488"
+            ]     
         }
     }
 
     test("Mercado Pago discount center payers touchpoint px congrats show") {
         "/discount_center/payers/touchpoint/px_congrats/show" (platform: "/mobile", type: TrackType.Event) {
-            tracking_id= "346859234"
+            items= [ 
+                [ 
+                    tracking_id: "346859234" 
+                ]
+            ]
+            flow= "/instore"
+            checkout_type= "one_tap"
+            security_enabled= false
+            experiments= "px nativo"
+            session_id= "65c7168b-cf25-4033-ad7a-e7874868a164"
+            session_time= 58
+            flow_detail= [
+                store_id: "30974197",
+                pos_id: "7414488"
+            ]
         }
     }
 
