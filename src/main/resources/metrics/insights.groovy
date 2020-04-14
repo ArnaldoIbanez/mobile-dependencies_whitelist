d3da0d0b12cb33e73d0677da0d7d4dfffd02e7eb
Martin <mciruzzi@users.noreply.github.com>
2020-04-14 10:35:10 -0300

    Update insights.groovy

--- src/main/resources/metrics/insights.groovy
+++ src/main/resources/metrics/insights.groovy
@@ -16,46 +16,4 @@ metrics {
 		}
 	}
 
-	"mlinsights/search"(description: "mlinsights/search", acceptMultiVariantSession: true, parametricName: false) {
-		startWith {
-			condition {
-				path(regex("^/search.*"))
-			}
-
-			openBy {
-				"application.business"(default: "none")
-			}
-		}
-	}
-
-	"mlinsights/vip"(description: "mlinsights/vip", acceptMultiVariantSession: true, parametricName: false) {
-		startWith {
-			condition {
-				path(regex("^/vip.*"))
-
-			}
-
-			set_property("item_id", "event_data.item_id")
-
-			openBy {
-				"application.business"(default: "none")
-			}
-		}
-	}
-
-	"mlinsights/checkout"(description: "mlinsights/checkout", acceptMultiVariantSession: true, parametricName: false) {
-		startWith {
-			condition {
-				path(regex("^/checkout.*"))
-
-			}
-
-			set_property("order_id", "event_data.order_id")
-
-			openBy {
-				"application.business"(default: "none")
-			}
-		}
-	}
-
 }
