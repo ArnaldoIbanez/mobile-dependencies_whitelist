0423ebbb1617516b46d60489cf89867cd8a2dc2c
Matias Fraga <matias.fraga@mercadolibre.com>
2020-04-15 13:38:22 -0300

    Add gps tracks

--- src/test/resources/melidata/instore.groovy
+++ src/test/resources/melidata/instore.groovy
@@ -1135,6 +1135,12 @@ trackTests {
 
         "/instore/geofence/permission_already_granted"(platform: "/mobile", type: TrackType.Event) {}
 
+        "/instore/geofence/gps_request"(platform: "/mobile", type: TrackType.Event) {}
+
+        "/instore/geofence/gps_response"(platform: "/mobile", type: TrackType.Event) {
+            type = "granted"
+        }
+
         "/instore/geofence/updated"(platform: "/mobile", type: TrackType.Event) {}
 
         "/instore/geofence/enter"(platform: "/mobile", type: TrackType.Event) {
