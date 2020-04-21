d01796c64c01751b5e8fb5c6cebd2e6e61cca55e
Gianluca Tesio <gianluca.tesio@mercadolibre.com>
2020-04-21 15:09:36 -0300

    Agregado de rutas con el orden correcto en el modulo de insurtech. Agregado de tests correspondientes.

--- src/test/resources/melidata/insurtech.groovy
+++ src/test/resources/melidata/insurtech.groovy
@@ -32,6 +32,23 @@ trackTests {
 
         }
         "/garex/checkout/not_selected_garex"(platform:"/web", type: TrackType.Event) {}
+
+        //Mobile
+        "/garex/checkout"(platform:"/mobile", type: TrackType.View) {}
+        "/garex/checkout/more_info"(platform:"/mobile", type: TrackType.Event) {}
+        "/garex/checkout/selected_garex"(platform:"/mobile", type: TrackType.Event) {
+            "id": "MLA390289_GAR16001",
+            "period": 24,
+            "cost": 1234,
+            "revenue_share_fee": 70,
+            "revenue": 863.80,
+            "currency_id": "ARS"
+        }
+        "/garex/checkout/not_selected_garex"(platform:"/web", type: TrackType.Event) {}
+        "/garex/checkout/delete"(platform:"/mobile", type: TrackType.Event) {
+            "id": "MLA390289_GAR16001",
+            "cost": 1234,
+            "currency_id": "ARS"
+        }
     }
 }
-
