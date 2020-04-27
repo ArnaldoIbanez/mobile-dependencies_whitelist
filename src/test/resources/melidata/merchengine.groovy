8d5267512f3be7da7e4f0a58d99c4ff3a09e0c4a
Javier Loucim <javier.loucim@mercadolibre.com>
2020-04-27 18:44:59 -0300

    added optional audiences field for merchengine

--- src/test/resources/melidata/merchengine.groovy
+++ src/test/resources/melidata/merchengine.groovy
@@ -52,6 +52,52 @@ trackTests {
                 logic = "user_journey"
                 flow = "default"
             }
-        }
+            "/merchengine/modal"(platform: "/mobile", type: TrackType.View) {
+                component_id = "secondary_actions_payers_recharge_sube_mp"
+                content_id = "recharge_sube"
+                bu = "mp"
+                bu_line = "default"
+                position = 0
+                logic = "user_journey"
+				audience = "all"
+                flow = "default"
+            }
+
+            "/merchengine/modal/dismiss"(platform: "/mobile", type: TrackType.Event) {
+                component_id = "secondary_actions_payers_recharge_sube_mp"
+                content_id = "recharge_sube"
+                bu = "mp"
+                bu_line = "default"
+                position = 0
+                logic = "user_journey"
+				audience = "all"
+                flow = "default"
+            }
+
+            "/merchengine/modal/cta"(platform: "/mobile", type: TrackType.Event) {
+                component_id = "secondary_actions_payers_recharge_sube_mp"
+                content_id = "recharge_sube"
+                bu = "mp"
+                bu_line = "default"
+                position = 0
+                logic = "user_journey"
+                flow = "default"
+                action_id = "cta1"
+                type = "primary"
+				audience = "all"
+                link = "mercadopago://wallet"
+            }
+
+            "/merchengine/modal/swipe"(platform: "/mobile", type: TrackType.Event) {
+                component_id = "secondary_actions_payers_recharge_sube_mp"
+                content_id = "recharge_sube"
+                bu = "mp"
+                bu_line = "default"
+                position = 3
+ 				audience = "all"
+                logic = "user_journey"
+                flow = "default"
+            }        
+		}
     }
 }
