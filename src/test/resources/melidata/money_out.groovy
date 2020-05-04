e945af30132bdc4b4acbfadeedf22b7215c27d82
evaldezml <emanuel.valdez@mercadolibre.com>
2020-05-04 12:20:34 -0300

    add tracks for hub

--- src/test/resources/melidata/money_out.groovy
+++ src/test/resources/melidata/money_out.groovy
@@ -196,5 +196,10 @@ trackTests {
       "/money_out/transfers/home/scheduled_warning_modal/continue_button_clicked"(platform: "/", type: TrackType.Event) {}
       "/money_out/transfers/home/scheduled_warning_modal/cancel_button_clicked"(platform: "/", type: TrackType.Event) {}
 
+      //Transfer Hub - TED Campaign Modal
+      "/money_out/transfers/home/ted_campaign_modal"(platform:"/", type: TrackType.View){}
+      "/money_out/transfers/home/ted_campaign_modal/ted_button_clicked"(platform: "/", type: TrackType.Event) {}
+      "/money_out/transfers/home/ted_campaign_modal/withdraw_legacy_button_clicked"(platform: "/", type: TrackType.Event) {}
+
     }
 }
