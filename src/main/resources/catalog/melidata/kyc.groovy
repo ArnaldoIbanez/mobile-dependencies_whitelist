31b0c72cde2e6b9b85c73f3a0f5cc7639a0e755d
Juan Pablo de Blas <juanpablo.deblas@mercadolibre.com>
2020-05-05 19:07:22 -0300

    requested change

--- src/main/resources/catalog/melidata/kyc.groovy
+++ src/main/resources/catalog/melidata/kyc.groovy
@@ -16,7 +16,7 @@ tracks {
         initiative (required: true, type: PropertyType.String, description: "The initiative")
         configuration_token (required: false, type: PropertyType.String, description: "The configuration token")
         callback (required: false, type: PropertyType.String, description: "The callback deeplink that is executed when the flow ends")
-        kyc_flow_id (required: false, type: PropertyType.String, description: "The kyc flow identifier")
+        kyc_flow_id (required: true, type: PropertyType.String, description: "The kyc flow identifier")
     }
 
     // challenge life cyclev v2
