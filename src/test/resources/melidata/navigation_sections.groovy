dbae2e14f5bd95663694bd15da5d7a28acc5ee25
SantiagoLazzari <slazzari@fi.uba.ar>
2020-05-07 16:32:38 -0300

    ML Tests

--- src/test/resources/melidata/navigation_sections.groovy
+++ src/test/resources/melidata/navigation_sections.groovy
@@ -9,14 +9,33 @@ trackTests {
     test("Navigation Sections - Mercadolibre") {
 
         "/navigation_sections/navigation_menu_ml" (platform: "/mobile", type: TrackType.View) {}
-        "/navigation_sections/navigation_menu_mp/tap" (platform: "/mobile", type: TrackType.Event) {
+        "/navigation_sections/navigation_menu_ml/tap" (platform: "/mobile", type: TrackType.Event) {
             id = "login"
+        }
+
+        "/navigation_sections/navigation_menu_mp" (platform: "/mobile", type: TrackType.View) {}
+        "/navigation_sections/your_profile" (platform: "/mobile", type: TrackType.View) {}
+        "/navigation_sections/your_business" (platform: "/mobile", type: TrackType.View) {}
+
+        "/navigation_sections/navigation_menu_mp/tap" (platform: "/mobile", type: TrackType.Event) {
+            id = "your_business"
+        }
+        "/navigation_sections/your_profile/tap" (platform: "/mobile", type: TrackType.Event) {
+            id = "seguridad"
+        }
+        "/navigation_sections/your_business/tap" (platform: "/mobile", type: TrackType.Event) {
+            id = "point"
         }
     }
 
     defaultBusiness = "mercadopago"
 
     test("Navigation Sections - MercadoPago") {
+
+        "/navigation_sections/navigation_menu_ml" (platform: "/mobile", type: TrackType.View) {}
+        "/navigation_sections/navigation_menu_ml/tap" (platform: "/mobile", type: TrackType.Event) {
+            id = "login"
+        }
 
         "/navigation_sections/navigation_menu_mp" (platform: "/mobile", type: TrackType.View) {}
         "/navigation_sections/your_profile" (platform: "/mobile", type: TrackType.View) {}
