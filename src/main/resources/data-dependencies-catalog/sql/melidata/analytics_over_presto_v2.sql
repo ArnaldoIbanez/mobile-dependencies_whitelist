b3535f47e6f2459f6bffd7bd43320dbd574803ea
pdbenitez91 <patricio.benitez@mercadolibre.com>
2020-05-11 18:46:34 -0300

    fix query

--- src/main/resources/data-dependencies-catalog/sql/melidata/analytics_over_presto_v2.sql
+++ src/main/resources/data-dependencies-catalog/sql/melidata/analytics_over_presto_v2.sql
@@ -1,4 +1,7 @@
-select id, username, operation, type, user_agent, state, source, schema, table_read, cpu_time, lower(text) AS text, substr(ds, 1 , 10) AS ds
+select id, username, operation, type, user_agent, state, source, schema, table_read, cpu_time, replace(lower(text), chr(10), ' ') AS text, substr(ds, 1 , 10) AS ds
 from presto_queries
 lateral view explode(from_tables) tables as table_read
 WHERE ds >= '@param01' and ds< '@param02'
+
+
+
