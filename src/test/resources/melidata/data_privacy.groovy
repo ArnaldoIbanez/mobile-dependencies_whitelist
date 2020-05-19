ecbe3e96e0fddd2b9d117c9880f53f8af6f7f2aa
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-05-19 17:11:37 -0300

    Delete business expressed by default

--- src/test/resources/melidata/data_privacy.groovy
+++ src/test/resources/melidata/data_privacy.groovy
@@ -7,24 +7,24 @@ trackTests {
 
   test("data_privacy") {
     // Mercadolibre
-    "/landing_data_privacy"(platform: "/web", business: "mercadolibre") {}
-    "/landing_data_privacy/event/click"(platform: "/web", business: "mercadolibre") {
+    "/landing_data_privacy"(platform: "/web") {}
+    "/landing_data_privacy/event/click"(platform: "/web") {
       text = "Nuestros principios"
       component = "sidebar"
     }
-    "/landing_data_privacy/event/click"(platform: "/web", business: "mercadolibre") {
+    "/landing_data_privacy/event/click"(platform: "/web") {
       text = "Podés tener más detalles sobre el uso de nuestras plataformas en Términos y Condiciones"
       component = "link"
     }
-    "/landing_data_privacy/event/click"(platform: "/web", business: "mercadolibre") {
+    "/landing_data_privacy/event/click"(platform: "/web") {
       text = "Términos y condiciones de uso del sitio"
       component = "quick_access"
     }
-    "/landing_data_privacy/event/scroll"(platform: "/web", business: "mercadolibre") {
+    "/landing_data_privacy/event/scroll"(platform: "/web") {
       text = "Responsabilidad sobre los datos"
       component = "view"
     }
-    "/landing_data_privacy/event/hover"(platform: "/web", business: "mercadolibre") {
+    "/landing_data_privacy/event/hover"(platform: "/web") {
       text = "2-actualizar-corregir"
       component = "tooltip"
     }
