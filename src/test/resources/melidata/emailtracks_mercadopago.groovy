cb687ad04b63f4a5e0993daf57d12b886c48931d
einherjer-uy <andres.sica@gmail.com>
2020-05-21 18:53:56 -0300

    email optout mercadopago

--- src/test/resources/melidata/emailtracks_mercadopago.groovy
+++ src/test/resources/melidata/emailtracks_mercadopago.groovy
@@ -39,4 +39,29 @@ trackTests {
             email_template = "MCRDT_OPTOUT"
         }
     }
+
+    test("optout") {
+        def defaultTrackInformation = {
+            email_template = "EMKT"
+            event_type = "unsubscribe"
+        }
+
+        "/email/form-optout"(platform: "/web"){
+            defaultTrackInformation()
+        }
+
+        "/email/form-optout"(platform: "/web/mobile"){
+            defaultTrackInformation()
+        }
+
+        "/email/form-optout/unsubscribe"(platform: "/web"){
+            defaultTrackInformation()
+            selected_option = 1
+        }
+
+        "/email/form-optout/unsubscribe"(platform: "/web/mobile"){
+            defaultTrackInformation()
+            selected_option = 1
+        }
+    }
 }
