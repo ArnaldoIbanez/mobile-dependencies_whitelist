aef3caf7cc6c470d28e564a9587c7b6af6b9cc87
pdbenitez91 <patricio.benitez@mercadolibre.com>
2020-06-08 18:46:59 -0300

    better query

--- src/main/resources/data-dependencies-catalog/sql/melidata/d2id_sanity_check_jsversion.sql
+++ src/main/resources/data-dependencies-catalog/sql/melidata/d2id_sanity_check_jsversion.sql
@@ -3,12 +3,12 @@ application.sdk_version['js_version'] as js_version,
 device.platform,
 application.site_id,
 application.business,
-count(distinct jest(platform.http.cookies, '_d2id')) as qty_distinct_d2id,
-count(distinct CASE WHEN substr(jest(platform.http.cookies, '_d2id'), length(jest(platform.http.cookies, '_d2id')) - 1) = '-n' THEN jest(platform.http.cookies, '_d2id') END) as qty_distinct_d2id_with_n,
+count(distinct get_json_object(platform.http.cookies, '$._d2id')) as qty_distinct_d2id,
+count(distinct CASE WHEN substr(get_json_object(platform.http.cookies, '$._d2id'), length(get_json_object(platform.http.cookies, '$._d2id')) - 1) = '-n' THEN get_json_object(platform.http.cookies, '$._d2id') END) as qty_distinct_d2id_with_n,
 date_part(ds) as day
 from tracks
 where ds >= '@param01' and ds < '@param02'
-and jest(platform.http.cookies, '_d2id') is not null
+and get_json_object(platform.http.cookies, '$._d2id') is not null
 and device.platform in ('/web/mobile', '/web/desktop')
 group by application.sdk_version['js_version'],
 device.platform,
