a64a109373e2a44b87b28ebbe28c2c15f8569664
gmaspero <gaston.maspero@mercadolibre.com>
2020-06-19 17:32:47 -0300

    add unified_onboarding tests

---
+++ src/test/resources/melidata/unified_onboarding.groovy
@@ -0,0 +1,31 @@
+package src.test.resources.melidata
+
+import com.ml.melidata.TrackType
+
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+
+trackTests {
+
+  defaultBusiness = "mercadopago"
+  
+  test("Unified Onboarding Brand Name Challenge open") {
+        "/unified_onboarding/challenge/open" (platform: "/", type: TrackType.Event) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "brand_name"
+            challenge_type = "single_input"
+            from = "some initiative"
+        }
+  }
+  
+  test("Unified Onboarding Brand Name Challenge view") {
+        "/unified_onboarding/challenge" (platform: "/", type: TrackType.View) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "brand_name"
+            challenge_type = "single_input"
+            from = "some initiative"
+            progress = "{"current: 1, "total": 7}"
+        }
+  }
+}
