0413bd50c9d2717cbc0d764474b4a84e9abadc4c
gmaspero <gaston.maspero@mercadolibre.com>
2020-06-22 10:59:25 -0300

    fix abstract paths

--- src/main/resources/catalog/melidata/unified_onboarding.groovy
+++ src/main/resources/catalog/melidata/unified_onboarding.groovy
@@ -9,6 +9,10 @@ tracks {
 
     initiative="1218"
     
+    "/unified_onboarding" (platform: "/mobile", isAbstract: true)
+
+    "/unified_onboarding/challenge" (platform: "/mobile", isAbstract: true)
+
     "/unified_onboarding/challenge/open" (platform: "/mobile", type: TrackType.Event) {
         
     }
