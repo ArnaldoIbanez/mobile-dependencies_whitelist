fc88262706f2f54c67c72892c8d32a0e023976a5
gmaspero <gaston.maspero@mercadolibre.com>
2020-07-02 13:32:03 -0300

    Update unified_onboarding.groovy

--- src/test/resources/melidata/unified_onboarding.groovy
+++ src/test/resources/melidata/unified_onboarding.groovy
@@ -8,18 +8,11 @@ trackTests {
 
     defaultBusiness = "mercadopago"
   
-    test("Unified Onboarding Brand Name Challenge view") {
-        "/unified_onboarding/challenge" (platform: "/mobile", type: TrackType.View) {
-            session_id = "12345"
-            session_id_from = "1234"
-            challenge_id = "brand_name"
-            challenge_type = "single_input"
-            from = "some initiative"
-            progress = "{\"current\": 1, \"total\": 7}"
-        }
-    }
+    //TOOL FOR SELLERS
 
-    test("Tasas y Plazos Challenge view") {
+    //View
+
+    test("Tools for Sellers Challenge view with json value") {
         "/unified_onboarding/challenge" (platform: "/mobile", type: TrackType.View) {
             session_id = "12345"
             session_id_from = "1234"
@@ -29,6 +22,121 @@ trackTests {
             value = "{ some object }"
         }
     }
+
+    test("Tools for Sellers Challenge view with URL value") {
+        "/unified_onboarding/challenge" (platform: "/mobile", type: TrackType.View) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "ou_fees"
+            challenge_type = "typ_list"
+            from = "tools_for_sellers"
+            value = "some url"
+            progress = "{\"current\": 1, \"total\": 7}"
+        }
+    }
+
+    test("Tools for Sellers Challenge view without value") {
+        "/unified_onboarding/challenge" (platform: "/mobile", type: TrackType.View) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "ou_fees"
+            challenge_type = "typ_list"
+            from = "tools_for_sellers"
+        }
+    }
+
+    //Open
+
+    test("Tools for Sellers Challenge open with URL value") {
+        "/unified_onboarding/challenge/open" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "ou_fees"
+            challenge_type = "typ_list"
+            from = "tools_for_sellers"
+            value = "Some URL"
+        }
+    }
+
+    test("Tools for Sellers Challenge open without value") {
+        "/unified_onboarding/challenge/open" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "ou_fees"
+            challenge_type = "typ_list"
+            from = "tools_for_sellers"
+        }
+    }
+
+    //Error
+
+    test("Tools for Sellers Challenge Error") {
+        "/unified_onboarding/challenge/error" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "ou_fees"
+            challenge_type = "typ_list"
+            from = "tools_for_sellers"
+            value = "The operation couldn’t be completed. (com.mercadolibre.networking error 500."
+        }
+    }
+
+    //Back
+
+    test("Tools for Sellers Challenge Back") {
+        "/unified_onboarding/challenge/back" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "ou_fees"
+            challenge_type = "typ_list"
+            from = "tools_for_sellers"
+        }
+    }
+
+    //Component Tap
+
+    test("Tools for Sellers Component Tap with Json Value") {
+        "/unified_onboarding/challenge/component/tap" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "update_release_option_button"
+            challenge_type = "typ_release_option_selection"
+            from = "tools_for_sellers"
+            value = "Some Object"
+            component_id = "update_release_option_button"
+            component_type = "action"
+        }
+    }
+
+    test("Tools for Sellers Component Tap with URL Value") {
+        "/unified_onboarding/challenge/component/tap" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "typ"
+            challenge_type = "typ_release_option_selection"
+            from = "tools_for_sellers"
+            value = "Some URL"
+            component_id = "footer_cell"
+            component_type = "action"
+        }
+    }
+
+    //Onboarding Unificado
+
+    //View
+
+    test("Unified Onboarding Brand Name Challenge view") {
+        "/unified_onboarding/challenge" (platform: "/mobile", type: TrackType.View) {
+            session_id = "12345"
+            session_id_from = "1234"
+            challenge_id = "brand_name"
+            challenge_type = "single_input"
+            from = "some initiative"
+            progress = "{\"current\": 1, \"total\": 7}"
+        }
+    }    
+
+    //Open
 
     test("Unified Onboarding Brand Name Challenge open") {
         "/unified_onboarding/challenge/open" (platform: "/mobile", type: TrackType.Event) {
@@ -39,19 +147,9 @@ trackTests {
             from = "some initiative"
         }
     }
-
-    test("Tasas y Plazos Challenge open") {
-        "/unified_onboarding/challenge/open" (platform: "/mobile", type: TrackType.Event) {
-            session_id = "12345"
-            session_id_from = "1234"
-            challenge_id = "ou_fees"
-            challenge_type = "typ_list"
-            from = "tools_for_sellers"
-            value = "{ some object }"
-            progress = "{\"current\": 1, \"total\": 7}"
-        }
-    }
   
+    //Back
+
     test("Unified Onboarding Brand Name Challenge Back") {
         "/unified_onboarding/challenge/back" (platform: "/mobile", type: TrackType.Event) {
             session_id = "12345"
@@ -62,16 +160,7 @@ trackTests {
         }
     }
 
-    test("Tasas y plazos Challenge Back") {
-        "/unified_onboarding/challenge/error" (platform: "/mobile", type: TrackType.Event) {
-            session_id = "12345"
-            session_id_from = "1234"
-            challenge_id = "brand_name"
-            challenge_type = "single_input"
-            from = "some initiative"
-            value = "The operation couldn’t be completed. (com.mercadolibre.networking error 500."
-        }
-    }
+    //Confirm
 
     test("Unified Onboarding Brand Name Challenge Confirm") {
         "/unified_onboarding/challenge/confirm" (platform: "/mobile", type: TrackType.Event) {
@@ -83,6 +172,8 @@ trackTests {
             is_last_challenge = true
         }
     }
+
+    //Component Tap
 
     test("Unified Onboarding Brand Name Component Tap") {
         "/unified_onboarding/challenge/component/tap" (platform: "/mobile", type: TrackType.Event) {
