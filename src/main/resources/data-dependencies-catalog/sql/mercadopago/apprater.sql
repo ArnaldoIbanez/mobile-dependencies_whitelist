13160c485184dbb93aa27604eceb2f3abd6a6dfa
Federico Ventre <federico.ventre@mercadolibre.com>
2020-07-02 10:29:46 -0300

    initial

---
+++ src/main/resources/data-dependencies-catalog/sql/mercadopago/apprater.sql
@@ -0,0 +1,10 @@
+SELECT  Jest(event_data, 'type_track') AS type_track,
+        application.site_id AS site,
+        device.platform AS platform,
+        application.version AS version,
+        Substr(ds, 1, 10)  AS fecha
+FROM tracks
+WHERE ds >= '@param01'
+AND ds < '@param02'
+AND path LIKE '%/apprater%'
+AND application.business='mercadopago'
