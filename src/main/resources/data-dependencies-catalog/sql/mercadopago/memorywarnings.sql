d9877371a27c2450c71e33d0537940d4d9650720
lcarboneti <lucio.carboneti@mercadolibre.com>
2020-07-13 20:00:07 -0300

    add new parameter to memory Warning

--- src/main/resources/data-dependencies-catalog/sql/mercadopago/memorywarnings.sql
+++ src/main/resources/data-dependencies-catalog/sql/mercadopago/memorywarnings.sql
@@ -1,24 +1,26 @@
-SELECT  device.platform                     AS platform,
-        application.site_id                 AS site,
-        application.version                 AS version,
-        application.business                AS business,
-        count(1)                            AS incidents,
-        get_json_object(event_data, '$.view_controller') AS warnings,
-        Substr(ds, 1, 10)                   AS fecha
-FROM tracks
-WHERE ds >= '@param01'
-AND ds < '@param02'
-AND path = '/memory_warning'
-AND (application.site_id = 'MLA'
-  OR application.site_id = 'MCO'
-  OR application.site_id = 'MLB'
-  OR application.site_id = 'MLU'
-  OR application.site_id = 'MLC'
-  OR application.site_id = 'MPE'
-  OR application.site_id = 'MLM' )
-GROUP BY  application.version,
-          application.business,
-          application.site_id,
-          device.platform,
-          get_json_object(event_data, '$.view_controller'),
-          ds
+SELECT DISTINCT(warnings),
+      state,
+      sum(incidents) as incidents,
+      platform,
+      version,
+      business,
+      fecha
+FROM (SELECT  DISTINCT(Jest(event_data, 'view_controller')) AS warnings,
+              Jest(event_data, 'application_state') AS state
+              count(Jest(event_data, 'view_controller')) AS incidents,
+              device.platform AS platform,
+              concat(split_part(application.version, '.',1),'.', split_part(application.version, '.',2)) AS version,
+              application.business AS business,
+              Substr(ds, 1, 10)  AS fecha
+        FROM tracks
+        WHERE ds >= '@param01'
+        AND ds < '@param02'
+        AND path = '/memory_warning'
+        GROUP BY  Jest(event_data, 'view_controller'),
+                  Jest(event_data, 'application_state'),
+                  device.platform,
+                  application.version,
+                  application.business,
+                  ds)
+GROUP BY warnings, state, platform,version,business,fecha
+ORDER BY version, state, warnings
