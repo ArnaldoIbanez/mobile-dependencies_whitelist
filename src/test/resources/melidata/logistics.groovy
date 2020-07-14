5b7c3bdecee15f831a849526fda1e82849fe415f
Silvina Cano Saracho <silvina.cano@mercadolibre.com>
2020-07-14 15:18:24 -0300

    fixing test

--- src/test/resources/melidata/logistics.groovy
+++ src/test/resources/melidata/logistics.groovy
@@ -477,13 +477,13 @@ trackTests {
             first_mile_logistic_type = "FF"
         }
         "/logistics/last_mile/scanner/driver_affinity_modal/continue"(platform: "/mobile", type: TrackType.Event){
-            driver_id = "123"
+            driver_id = 400
         }
         "/logistics/last_mile/scanner/driver_affinity_modal/cancel"(platform: "/mobile", type: TrackType.Event){
-            driver_id = "123"
+            driver_id = 400
         }
         "/logistics/last_mile/scanner/driver_affinity_modal"(platform: "/mobile", type: TrackType.View){
-            driver_id = "123"
+            driver_id = 400
         }
     }
 
