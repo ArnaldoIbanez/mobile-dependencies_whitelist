dea1ca81ddaa4f0792b75d18544190b2072756d2
lcarboneti <lucio.carboneti@mercadolibre.com>
2020-07-22 19:29:09 -0300

    refactor query

--- src/main/resources/data-dependencies-catalog/sql/mercadopago/version_for_path.sql
+++ src/main/resources/data-dependencies-catalog/sql/mercadopago/version_for_path.sql
@@ -1,49 +1,50 @@
-SELECT DISTINCT( version ),
-               path,
-               Sum(visited_count) AS visited_count,
-               platform,
-               fecha
-FROM  (SELECT DISTINCT( Concat(Split_part(application.version, '.', 1), '.', Split_part(application.version, '.', 2))) AS version,
-                       CASE
-                            WHEN path = '/instore/scan_qr'
-                                THEN 'Pagos QR'
-                            WHEN path = '/bill_payments/home'
-                                THEN 'Pago de servicios'
-                            WHEN path = '/money_in/payment_methods'
-                                THEN 'Ingresar Dinero'
-                            WHEN path = '/withdraw/main'
-                                OR path = '/money_out/transfers/home'
-                                THEN 'Retirar Dinero'
-                            WHEN path = '/money_detail/balance'
-                                THEN 'Balance'
-                            WHEN path = '/cellphone_recharge/recents'
-                                OR path = '/single_player/prepaid/companies'
-                                THEN 'Recarga de celulares'
-                            WHEN path = '/single_player/prepaid/start_flow'
-                                THEN 'DirecTv/Sky'
-                            WHEN path = '/point_payment/qr_show_code'
-                                THEN 'Cobros Qr'
-                            WHEN path = '/point_payment/device_selection'
-                                THEN 'Cobros Point'
-                            WHEN path = '/point_payment/link_share'
-                                THEN 'Cobro con Link'
-                            WHEN path = '/mplayer/send_money/contact_picker'
-                                THEN 'Enviar dinero'
-                            WHEN path = '/money_out/cashout/ticket'
-                                THEN 'Extracción de dinero'
-                            WHEN path = '/asset_management/investment_detail'
-                                THEN 'Invertir dinero'
-                            WHEN path = '/account_fund/cvu/main'
-                                THEN 'CVU'
-                            WHEN path = '/listing/activities'
-                                THEN 'Actividades'
-                            WHEN path = '/wallet_home/home'
-                                THEN 'Home Wallet'
-                       END AS path,
-                       Count(path) AS visited_count,
-                       Substring(device.platform, 9, 16) AS platform,
-                       Substr(ds, 1, 10) AS fecha
-        FROM   default.tracks_mp
+SELECT version ,
+       path,
+       Sum(visited_count) AS visited_count,
+       platform,
+       fecha
+FROM  (SELECT substring_index(application.version, '.',2) AS version,
+              CASE
+                  WHEN path = '/instore/scan_qr'
+                      THEN 'Pagos QR'
+                  WHEN path = '/bill_payments/home'
+                      THEN 'Pago de servicios'
+                  WHEN path = '/money_in/payment_methods'
+                      THEN 'Ingresar Dinero'
+                  WHEN path = '/withdraw/main'
+                      OR path = '/money_out/transfers/home'
+                      THEN 'Retirar Dinero'
+                  WHEN path = '/money_detail/balance'
+                      THEN 'Balance'
+                  WHEN path = '/cellphone_recharge/recents'
+                      OR path = '/single_player/prepaid/companies'
+                      THEN 'Recarga de celulares'
+                  WHEN path = '/single_player/prepaid/start_flow'
+                  WHEN path = '/single_player/prepaid/start_flow'
+                      THEN 'DirecTv/Sky'
+                  WHEN path = '/point_payment/qr_show_code'
+                      THEN 'Cobros Qr'
+                  WHEN path = '/point_payment/device_selection'
+                      THEN 'Cobros Point'
+                  WHEN path = '/point_payment/link_share'
+                      THEN 'Cobro con Link'
+                  WHEN path = '/mplayer/send_money/contact_picker'
+                      THEN 'Enviar dinero'
+                  WHEN path = '/money_out/cashout/ticket'
+                      THEN 'Extracción de dinero'
+                  WHEN path = '/asset_management/investment_detail'
+                      THEN 'Invertir dinero'
+                  WHEN path = '/account_fund/cvu/main'
+                      THEN 'CVU'
+                  WHEN path = '/listing/activities'
+                      THEN 'Actividades'
+                  WHEN path = '/wallet_home/home'
+                      THEN 'Home Wallet'
+              END AS path,
+              Count(path) AS visited_count,
+              Substring(device.platform, 9, 16) AS platform,
+              Substr(ds, 1, 10) AS fecha
+        FROM   melidata.tracks_mp
         WHERE  ds >= '@param01'
                AND ds < '@param02'
                AND bu = 'mercadopago'
@@ -71,7 +72,7 @@ FROM  (SELECT DISTINCT( Concat(Split_part(application.version, '.', 1), '.', Spl
         GROUP  BY application.version,
                   path,
                   device.platform,
-                  ds)
+                  ds) t1
 GROUP  BY version,
           path,
           fecha,
