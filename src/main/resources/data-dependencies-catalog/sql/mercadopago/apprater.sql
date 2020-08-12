7dd7b111be70b35196162e4d8c031e81b31e2838
Federico Ventre <federico.ventre@mercadolibre.com>
2020-08-12 15:37:31 -0300

    refactor

--- src/main/resources/data-dependencies-catalog/sql/mercadopago/apprater.sql
+++ src/main/resources/data-dependencies-catalog/sql/mercadopago/apprater.sql
@@ -1,9 +1,18 @@
-SELECT  Jest(event_data, 'type_track') AS type_track,
+SELECT  Get_json_object(event_data, '$.type_track') AS type_track,
+        count(Get_json_object(event_data, '$.type_track')) AS count_track,
         application.site_id AS site,
-        device.platform AS platform,
-        application.version AS version,
+        Substring(device.platform, 9, 16) AS platform,
+        substring_index(application.version, '.',2) AS version,
         Substr(ds, 1, 10)  AS fecha
-FROM tracks_mp
-WHERE ds >= '@param01'
-AND ds < '@param02'
+FROM default.tracks
+WHERE ds >= '2020-07-30'
+AND ds < '2020-07-31'
 AND path LIKE '%/apprater%'
+AND application.business = 'mercadopago'
+AND (device.platform = '/mobile/ios'
+  OR device.platform = '/mobile/android' )
+GROUP BY  Get_json_object(event_data, '$.type_track'),
+          application.site_id,
+          Substring(device.platform, 9, 16),
+          substring_index(application.version, '.',2),
+          Substr(ds, 1, 10)
