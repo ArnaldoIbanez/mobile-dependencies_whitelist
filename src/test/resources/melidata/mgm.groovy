f3124be6955b12f23ef505e8a6658941777b9216
fedegiardini-ml <federico.giardini@mercadolibre.com>
2020-09-23 10:08:24 -0300

    Modified MGM test Ids

--- src/test/resources/melidata/mgm.groovy
+++ src/test/resources/melidata/mgm.groovy
@@ -22,32 +22,32 @@ trackTests {
 
             // Invite Screen
             "/mgm/payers/invite"(platform: "/mobile", type: TrackType.View) {
-                promoter = "123456"
+                promoter = "promoter_id"
             }
 
             // Invite Screen Events
             "/mgm/payers/invite/invite"(platform: "/mobile", type: TrackType.Event) {
                 media = "whatsapp"
-                promoter = "123456"
+                promoter = "promoter_id"
             }
             "/mgm/payers/invite/back"(platform: "/mobile", type: TrackType.Event) {
-                promoter = "123456"
+                promoter = "promoter_id"
             }
             "/mgm/payers/invite/terms"(platform: "/mobile", type: TrackType.Event) {
-                promoter = "123456"
+                promoter = "promoter_id"
             }
 
             // Redeem Screen
             "/mgm/payers/redeem"(platform: "/mobile", type: TrackType.View) {
-                promoter = "123456"
+                promoter = "promoter_id"
             }
             "/mgm/payers/redeem/option"(platform: "/mobile", type: TrackType.Event) {
                 option = "cellphone_recharge"
                 type = "use_coupon"
-                promoter = "123456"
+                promoter = "promoter_id"
             }
             "/mgm/payers/redeem/terms"(platform: "/mobile", type: TrackType.Event) {
-                promoter = "123456"
+                promoter = "promoter_id"
             }
 
             // No Redeem Screen
@@ -62,10 +62,10 @@ trackTests {
 
             // WEB
             "/mgm/payers/stop_landing"(platform: "/web", type: TrackType.View) {
-                promoter = "123456"
+                promoter = "promoter_id"
             }
             "/mgm/payers/stop_landing/redirect"(platform: "/web", type: TrackType.Event) {
-                promoter = "123456"
+                promoter = "promoter_id"
             }
             "/mgm/payers/stop_landing"(platform: "/web", type: TrackType.View) {}
             "/mgm/payers/stop_landing/redirect"(platform: "/web", type: TrackType.Event) {}
@@ -90,84 +90,84 @@ trackTests {
             *************************/
 
             "/mgm/v3/promoter"(platform: "/mobile", type: TrackType.View) {
-                program_id = "123"
+                program_id = "program_id"
                 is_recurrent = true
-                floating_banner_id = "1234"
+                floating_banner_id = "floating_banner_id"
                 sections = [
                     [ 
                         order: 0,
-                        section_id: "12323" 
+                        section_id: "section_id" 
                     ],
                     [
                         order: 1,
-                        section_id: "12323" 
+                        section_id: "section_id" 
                     ]
                 ]
                 from = "modals_engine"
             }
 
             "/mgm/v3/promoter/share/tap"(platform: "/mobile", type: TrackType.Event) {
-                    program_id = "123"
+                    program_id = "program_id"
                     is_recurrent = true
-                    action_id = "123"
-                    shared_app_id = "123"
+                    action_id = "action_id"
+                    shared_app_id = "shared_app_id"
                     from = "modals_engine"
             }
 
             "/mgm/v3/promoter/banner/tap"(platform: "/mobile", type: TrackType.Event) {
-                    program_id = "123"
+                    program_id = "program_id"
                     is_recurrent = true
                     target_program = "mgm_point"
                     from = "modals_engine"
             }    
 
             "/mgm/v3/promoter/floating_banner/dismiss"(platform: "/mobile", type: TrackType.Event) {
-                    program_id = "123"
+                    program_id = "program_id"
                     is_recurrent = true
-                    banner_id = "123"
+                    banner_id = "banner_id"
                     from = "modals_engine"
             }
 
             "/mgm/v3/guest"(platform: "/mobile", type: TrackType.View) {
-                    program_id = "123"
+                    program_id = "program_id"
                     sections = [
                         [ 
                             order: 0,
-                            section_id: "12323" 
+                            section_id: "section_id" 
                         ],
                         [
                             order: 1,
-                            section_id: "12323" 
+                            section_id: "section_id" 
                         ]
                     ]
                     from = "modals_engine"
             }
 
             "/mgm/v3/guest/cross_selling/tap"(platform: "/mobile", type: TrackType.Event) {
-                    program_id = "123"
-                    action_id = "123"
+                    program_id = "program_id"
+                    action_id = "action_id"
                     from = "modals_engine"
             }
 
             "/mgm/v3/guest/main_action/tap"(platform: "/mobile", type: TrackType.Event) {
-                    program_id = "123"
-                    action_id = "123"
+                    program_id = "program_id"
+                    action_id = "action_id"
                     from = "modals_engine"
             }
 
             "/mgm/v3/error"(platform: "/mobile", type: TrackType.View) {
-                    program_id = "123"
+                    program_id = "program_id"
                     reason = "kyc_invitado"
             }
 
             "/mgm/v3/error/dismiss"(platform: "/mobile", type: TrackType.Event) {
-                    program_id = "123"
+                    program_id = "program_id"
                     reason = "kyc_invitado"
             }
 
             "/mgm/v3/error/tap"(platform: "/mobile", type: TrackType.Event) {
                     cta = "quiet"
-                    program_id = "123"
+                    program_id = "program_id"
                     link = "meli://coupon"
                     reason = "kyc_invitado"
             }
