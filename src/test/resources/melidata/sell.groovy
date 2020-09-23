3ea05baf0480b5ea919edb429a9e0ff824e5eb2c
YuryRiera <yusmery.rierar@mercadolibre.cl>
2020-09-23 09:32:17 -0300

    Add path sip_optional_landing mobile in Motors

--- src/test/resources/melidata/sell.groovy
+++ src/test/resources/melidata/sell.groovy
@@ -850,6 +850,13 @@ trackTests {
             seller_profile = "NEWBIE"
             vertical = "core"
         }
+        "/sell/list/sip_optional_landing"(platform: "/mobile") {
+            session_id = "214464778-list-d5e5a20b2935"
+            seller_reputation = "NO_REPUTATION"
+            seller_segment = ""
+            seller_profile = "NEWBIE"
+            vertical = "core"
+        }
         "/sell/list/title_landing"(platform: "/mobile") {
             session_id = "214464778-list-d5e5a20b2935"
             seller_reputation = "NO_REPUTATION"
@@ -1777,6 +1784,13 @@ trackTests {
             vertical = "core"
         }
         "/sell/list/draft/sip_landing"(platform: "/mobile", type: TrackType.View) {
+            session_id = "214464778-list-d5e5a20b2935"
+            seller_reputation = "NO_REPUTATION"
+            seller_segment = ""
+            seller_profile = "NEWBIE"
+            vertical = "core"
+        }
+        "/sell/list/draft/sip_optional_landing"(platform: "/mobile", type: TrackType.View) {
             session_id = "214464778-list-d5e5a20b2935"
             seller_reputation = "NO_REPUTATION"
             seller_segment = ""
