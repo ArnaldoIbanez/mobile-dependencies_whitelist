69768c68d62abfe444f6597b8e804b86da08cd87
MatiasEKrenz <matias.krenz@mercadolibre.com>
2020-09-25 10:15:17 -0300

    add deprecation date

--- src/main/resources/metrics/credits.groovy
+++ src/main/resources/metrics/credits.groovy
@@ -149,7 +149,7 @@ metrics {
         }
     }
 
-    "credits_consumer.personal_loan_access_order"(description: "Credits conversion under personal loans access order in admin flow") {
+    "credits_consumer.personal_loan_access_order"(description: "Credits conversion under personal loans access order in admin flow", deprecation_date:"2020/12/31") {
         startWith {
             experiment("credits/credits_consumer_admin_pl_access_order")
         }
