f23ad5649abbfb5f6f0cc5da017af97149e4ef59
Ezequiel Eñiguez <ezequiel.eniguez@mercadolibre.com>
2020-10-26 11:54:46 -0300

    Credits Optin Authorization

--- src/test/resources/melidata/credits.groovy
+++ src/test/resources/melidata/credits.groovy
@@ -153,7 +153,13 @@ trackTests {
         "/credits/consumer/opensea/remedy/authorization/recovery"(platform: "/", type: TrackType.View, business:"mercadopago") {}
         "/credits/consumer/opensea/remedy/authorization/quit_remedy"(platform: "/", type: TrackType.Event, business:"mercadolibre") {}
         "/credits/consumer/opensea/remedy/authorization/quit_remedy"(platform: "/", type: TrackType.Event, business:"mercadopago") {}
-
+        /* Remedy MLM WebView */
+        "/credits/consumer/opensea/remedy/authorization"(platform: "/mobile", type: TrackType.View, business:"mercadolibre") {}
+        "/credits/consumer/opensea/remedy/authorization"(platform: "/mobile", type: TrackType.View, business:"mercadopago") {}
+        "/credits/consumer/opensea/remedy/authorization/give_consent"(platform: "/mobile", type: TrackType.Event, business:"mercadolibre") {}
+        "/credits/consumer/opensea/remedy/authorization/give_consent"(platform: "/mobile", type: TrackType.Event, business:"mercadopago") {}
+        "/credits/consumer/opensea/remedy/authorization/deny_consent"(platform: "/mobile", type: TrackType.Event, business:"mercadolibre") {}
+        "/credits/consumer/opensea/remedy/authorization/deny_consent"(platform: "/mobile", type: TrackType.Event, business:"mercadopago") {}
         /******************************************
          *   End: Consumers Opensea Flow
          ******************************************/
