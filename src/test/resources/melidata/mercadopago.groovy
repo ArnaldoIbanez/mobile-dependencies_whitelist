bb9337a2d491b650f00996d22bc5e2be172f6a58
fdiassilva <fernando.dias@mercadolivre.com>
2020-11-10 15:13:11 -0300

    testes

--- src/test/resources/melidata/mercadopago.groovy
+++ src/test/resources/melidata/mercadopago.groovy
@@ -2638,6 +2638,10 @@ trackTests {
             mandatory()
         }
 
+        "/bill_payments/home/back"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
         "/bill_payments/home/info_hour"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
         }
@@ -2649,9 +2653,27 @@ trackTests {
         "/bill_payments/home/scan_barcode"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
         }
+
+        "/bill_payments/home/search_entities"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
+        "/bill_payments/home/activity"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
         "/bill_payments/home/pay"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
             barcode = "34190.34016 04770.560003 00000.000000 5 82030005288261"
+        }
+
+        "/bill_payments/home/pay"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
+        "/bill_payments/home/pay"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+            product_id = "10"
         }
 
         // Invoice
@@ -2663,9 +2685,36 @@ trackTests {
             mandatory()
         }
 
+        "/bill_payments/invoices/back"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
         "/bill_payments/invoices/pay"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
             barcode = "34190.34016 04770.560003 00000.000000 5 82030005288261"
+        }
+
+        "/bill_payments/invoices/pay"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
+        "/bill_payments/invoices/pay"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+            product_id = "10"
+        }
+
+        // Product picker
+        "/bill_payments/product_picker"(platform: "/mobile") {
+            mandatory()
+        }
+
+        // Input option
+        "/bill_payments/input_option"(platform: "/mobile") {
+            mandatory()
+        }
+
+        "/bill_payments/input_option/back"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
         }
 
         // Input amount
@@ -2718,9 +2767,10 @@ trackTests {
             mandatory()
         }
 
-        "/bill_payments/opt_in"(platform: "/mobile", type: TrackType.View) {
+        "/bill_payments/opt_in/back"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
         }
+
         "/bill_payments/opt_in/close"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
         }
@@ -2749,6 +2799,10 @@ trackTests {
             mandatory()
         }
 
+        "/bill_payments/barcode_manual_input/back"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
         "/bill_payments/barcode_manual_input/info_hour"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
         }
@@ -2764,6 +2818,11 @@ trackTests {
         "/bill_payments/receipt"(platform: "/mobile") {
             mandatory()
         }
+
+        "/bill_payments/receipt/back"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
         "/bill_payments/receipt/share"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
         }
@@ -2839,9 +2898,24 @@ trackTests {
             mandatory()
         }
 
+        "/bill_payments/scan/back"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
+        "/bill_payments/scan_error"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
+        "/bill_payments/scan_error/back"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
         "/bill_payments/read"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
             barcode = "34190.34016 04770.560003 00000.000000 5 82030005288261"
+            camera_flash = false
+            timestamp = 1043
+            barcode_format = "ITR40"
         }
 
         "/bill_payments/fixed_amount"(platform: "/mobile") {
@@ -2946,18 +3020,26 @@ trackTests {
         }
 
         //congrats
-        "/bill_payments/congrats"(platform: "/mobile") {
+        "/bill_payments/congrats/success"(platform: "/mobile") {
             mandatory()
         }
-        "/bill_payments/congrats/receipt"(platform: "/mobile", type: TrackType.Event) {
+        "/bill_payments/congrats/success/receipt"(platform: "/mobile", type: TrackType.Event) {
             mandatory()
         }
 
-        "/bill_payments/congrats/dda"(platform: "/mobile", type: TrackType.Event) {
+        "/bill_payments/congrats/success/dda"(platform: "/mobile", type: TrackType.Event) {
+            mandatory()
+        }
+
+        //search
+        "/bill_payments/search"(platform: "/mobile") {
+            mandatory()
+        }
+
+        "/bill_payments/search_error"(platform: "/mobile") {
             mandatory()
         }
     }
-
 
     test("Fund account") {
         "/fund_account/fund_amount"(platform: "/mobile") {
