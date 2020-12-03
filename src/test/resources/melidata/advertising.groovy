e82f64dc7a238510de4b374379395365d191df99
natalia-meli <natalia.varela@mercadolibre.cl>
2020-12-02 21:33:04 -0300

    add test

--- src/test/resources/melidata/advertising.groovy
+++ src/test/resources/melidata/advertising.groovy
@@ -480,6 +480,9 @@ trackTests {
 
     }
     est("Advertising Dynamic budget "){
+        "/advertising/pads2/manager/update/budget"(platform: "/", type: TrackType.Event) {
+
+        }
         "/advertising/pads2/manager/update/budget/tooltip"(platform: "/", type: TrackType.Event) {
             budget = 3990
             campaign_id = 235666
