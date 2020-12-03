267931474fd918ff46ced9092186881a70f63b13
natalia-meli <natalia.varela@mercadolibre.cl>
2020-12-03 15:41:49 -0300

    Revert "update budget test"
    This reverts commit f3a6e0b59b96c21ee7fbfccaab6ed3cd44993f5e.

--- src/test/resources/melidata/advertising.groovy
+++ src/test/resources/melidata/advertising.groovy
@@ -493,11 +493,8 @@ trackTests {
             select_budget_pct = ""
             selected_budget_type = ""
         }
-
-    }
-    test("Dynamic budget") {
         "/advertising/pads2/manager/budget"(platform: "/", type: TrackType.Event) {
-
+            
         }
         "/advertising/pads2/manager/budget/tooltip"(platform: "/", type: TrackType.Event) {
             budget = 3990
@@ -505,6 +502,7 @@ trackTests {
             budget_type = "D"
         }
     }
+
 
     test("Advertising buybox opportunity") {
         "/advertising/pads2/manager/card/buy_box_winner"(platform: "/web", type: TrackType.View) {
