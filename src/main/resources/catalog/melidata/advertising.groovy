12bf2f3a98282478bc834a85e4ca39266033f7d7
natalia-meli <natalia.varela@mercadolibre.cl>
2020-12-04 16:00:45 -0300

    update: description budget close

--- src/main/resources/catalog/melidata/advertising.groovy
+++ src/main/resources/catalog/melidata/advertising.groovy
@@ -855,10 +855,10 @@ tracks {
         multi(required: true, type: PropertyType.Boolean, description: "Indicates if it is a multicampaign dashboard")
         campaign_id(required: true, description: "Id related to the campaign")
         budget(required: true, description: "Budget related to the campaign")
-        budget_type(required: true, description: "budget type",values: ['D', 'F'])
-        selected_budget(required: true, description: "selected budget")
-        select_budget_pct(required: true, description: "select budget pct")
-        selected_budget_type(required: true, description: "selected budget type")
+        budget_type(required: true, description: "Budget type related to the Modal campaign",values: ['D', 'F'])
+        selected_budget(required: true, description: "selected budget campaign Modal campaign")
+        select_budget_pct(required: true, description: "select budget pct Modal campaign")
+        selected_budget_type(required: true, description: "selected budget type  F  or  D Modal campaign")
     }
 
     // Multicampaña - Sads New
