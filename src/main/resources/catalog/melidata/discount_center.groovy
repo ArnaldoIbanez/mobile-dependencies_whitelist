c8ffdd42ff5df768e6bc53b8c0824471ae237427
Tomi De Lucca <tomas.delucca@mercadolibre.com>
2020-12-08 14:43:45 -0300

    Added touchpoints to melidata catalog

--- src/main/resources/catalog/melidata/discount_center.groovy
+++ src/main/resources/catalog/melidata/discount_center.groovy
@@ -16,16 +16,10 @@ tracks {
     "/discount_center/payers" (platform: "/mobile", isAbstract: true) {}
 
     propertyDefinitions {
-        flow_detail(required: false, description: "External info")
-        flow(required: false, type: PropertyType.String, description: "External flow name")
         session_id(required: false, type: PropertyType.String, description: "Internal session id")
-        session_time(required: false, type: PropertyType.Numeric, description: "Session time")
-        checkout_type(required: false, type: PropertyType.String, description: "Checkout type")
         collector_id(required: false, description: "Collector external id")
-        security_enabled(required: false, type: PropertyType.Boolean, description: "If the user has biometric or passcode validation to make a payment")
         category(type: PropertyType.String, required: false, description: "The category id")
         mcc(type: PropertyType.String, required: true, description: "The mcc")
-        experiments(required: false, type: PropertyType.String, description: "Active experiments")
         store_id(type: PropertyType.Numeric, required: true, description: "The store id")
         brand_id(type: PropertyType.Numeric, required: false, description: "The brand id")
         name(type: PropertyType.String, required: false, description: "The name")
@@ -36,7 +30,6 @@ tracks {
     }
 
     propertyGroups {
-        externalData(flow, flow_detail, collector_id, session_id, session_time, checkout_type, security_enabled, category, experiments)
         storeGroup(store_id, collector_id, brand_id, name, distance, category, mcc, review, discounts, delivery, session_id)
     }
 
@@ -117,30 +110,6 @@ tracks {
 
     "/discount_center/payers/session/end" (platform: "/mobile", type: TrackType.Event) {
         session_id(required: true, type: PropertyType.String, description: "Unique code that identifies a user's session")
-    }
-
-    // TOUCH POINT
-
-    def touchpoint_item_definition = objectSchemaDefinitions {
-        tracking_id(type: PropertyType.String, required: true, description: "The id of the item we are showing")
-    }
-
-    "/discount_center/payers/touchpoint" (platform: "/mobile", isAbstract: true) {}
-    "/discount_center/payers/touchpoint/px_congrats" (platform: "/mobile", isAbstract: true) {}
-
-    "/discount_center/payers/touchpoint/px_congrats/tap" (platform: "/mobile", type: TrackType.Event) {
-        externalData
-        tracking_id(required: true, type: PropertyType.String, description: "The id campaign")
-    }
-
-    "/discount_center/payers/touchpoint/px_congrats/show" (platform: "/mobile", type: TrackType.Event) {
-        externalData
-        items(required: true, type: PropertyType.ArrayList(PropertyType.Map(touchpoint_item_definition)), description: "The items")
-    }
-
-    "/discount_center/payers/touchpoint/px_congrats/print" (platform: "/mobile", type: TrackType.Event) {
-        externalData
-        items(required: true, type: PropertyType.ArrayList(PropertyType.Map(touchpoint_item_definition)), description: "The items")
     }
 
     // MARKETPLACE
--- src/test/resources/melidata/discount_center.groovy
+++ src/test/resources/melidata/discount_center.groovy
@@ -130,67 +130,6 @@ trackTests {
         }
     }
 
-    // TOUCH POINT
-
-    test("Mercado Pago discount center payers touchpoint px congrats tap") {
-        "/discount_center/payers/touchpoint/px_congrats/tap" (platform: "/mobile", type: TrackType.Event) {
-            tracking_id= "346859234"
-            flow= "/instore"
-            checkout_type= "one_tap"
-            security_enabled= false
-            session_id= "65c7168b-cf25-4033-ad7a-e7874868a164"
-            session_time= 58
-            flow_detail= [
-                store_id: "30974197",
-                pos_id: "7414488"
-            ]
-            category="category_test"
-            experiments="experiment_test"
-        }
-    }
-
-    test("Mercado Pago discount center payers touchpoint px congrats show") {
-        "/discount_center/payers/touchpoint/px_congrats/show" (platform: "/mobile", type: TrackType.Event) {
-            items= [
-                [
-                    tracking_id: "346859234"
-                ]
-            ]
-            flow= "/instore"
-            checkout_type= "one_tap"
-            security_enabled= false
-            session_id= "65c7168b-cf25-4033-ad7a-e7874868a164"
-            session_time= 58
-            flow_detail= [
-                store_id: "30974197",
-                pos_id: "7414488"
-            ]
-            category="category_test"
-            experiments="experiment_test"
-        }
-    }
-
-    test("Mercado Pago discount center payers touchpoint px congrats print") {
-        "/discount_center/payers/touchpoint/px_congrats/print" (platform: "/mobile", type: TrackType.Event) {
-            items= [
-                    [
-                            tracking_id: "346859234"
-                    ]
-            ]
-            flow= "/instore"
-            checkout_type= "one_tap"
-            security_enabled= false
-            session_id= "65c7168b-cf25-4033-ad7a-e7874868a164"
-            session_time= 58
-            flow_detail= [
-                    store_id: "30974197",
-                    pos_id: "7414488"
-            ]
-            category="category_test"
-            experiments="experiment_test"
-        }
-    }
-
     // MARKETPLACE
 
     test("Mercado Pago discount center payers marketplace") {
