f1cc5444a9ec0eb811a2398a51c0f4e3f985b12d
natalia-meli <natalia.varela@mercadolibre.cl>
2020-12-17 15:38:01 -0300

    add: new track close

--- src/main/resources/catalog/melidata/advertising.groovy
+++ src/main/resources/catalog/melidata/advertising.groovy
@@ -353,6 +353,15 @@ tracks {
         budget_type_new(required: true, description: "selected budget type  F  or  D Modal campaign",values: ['D', 'F'])
     }
 
+    "/advertising/pads2/manager/update/budget/close"(platform: "/", type: TrackType.Event) {
+        budget(required: true, description: "current budget related to the campaign")
+        campaign_id(required: true, description: "Id related to the campaign")
+        budget_type(required: true, description: "Budget type related to the Modal campaign",values: ['D', 'F'])
+        budget_new(required: true, description: " new budget campaign Modal campaign")
+        budget_pct_new(required: true, description: "select budget pct Modal campaign")
+        budget_type_new(required: true, description: "selected budget type  F  or  D Modal campaign",values: ['D', 'F'])
+    }
+
     //Buybox
     "/advertising/pads2/manager/buybox_winners"(platform: "/", isAbstract: true) {}
     "/advertising/pads2/manager/buybox_winners/landing"(platform: "/", isAbstract: true) {}
