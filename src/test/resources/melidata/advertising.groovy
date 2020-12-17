33b1dddb858ffc5116cad3a78c8147abe1c5ce46
natalia-meli <natalia.varela@mercadolibre.cl>
2020-12-17 16:02:16 -0300

    Revert "add: new track close"
    This reverts commit f1cc5444a9ec0eb811a2398a51c0f4e3f985b12d.

--- src/test/resources/melidata/advertising.groovy
+++ src/test/resources/melidata/advertising.groovy
@@ -499,14 +499,6 @@ trackTests {
             budget_pct_new = "18.8"
             budget_type_new = "D"
         }
-        "/advertising/pads2/manager/update/budget/close"(platform: "/", type: TrackType.Event) {
-            campaign_id = "2222222"
-            budget = "4000"
-            budget_new = 5600
-            budget_type = "D"
-            budget_pct_new = "18.8"
-            budget_type_new = "D"
-        }
     }
     
     test("Advertising Dynamic budget ") {
