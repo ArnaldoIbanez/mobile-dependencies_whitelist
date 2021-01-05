77fba6acd9fb317f38857337e860d869bdfa0feb
Eric Pennachini <eric.pennachini@mercadolibre.com>
2021-01-05 11:15:05 -0300

    fix

--- src/main/resources/catalog/melidata/costs_section.groovy
+++ src/main/resources/catalog/melidata/costs_section.groovy
@@ -4,8 +4,6 @@ import com.ml.melidata.TrackType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.catalog.PropertyType
 
-import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
-
 tracks {
 
     initiative = "1148"
