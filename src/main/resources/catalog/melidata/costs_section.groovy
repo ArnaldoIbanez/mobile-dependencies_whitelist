3eabab5ddd0ee5d60ccd8c82f1e87231d7c23e57
Rodrigo Basualdo <rodrigo.basualdo@mercadolibre.com>
2021-01-05 11:12:04 -0300

    Remove duplicate import

--- src/main/resources/catalog/melidata/costs_section.groovy
+++ src/main/resources/catalog/melidata/costs_section.groovy
@@ -1,7 +1,6 @@
 package catalog.melidata
 
 import com.ml.melidata.TrackType
-import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.catalog.PropertyType
 
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
