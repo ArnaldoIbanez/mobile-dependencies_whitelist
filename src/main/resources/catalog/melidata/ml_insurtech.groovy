315b2d6b274d1ca9cda30c724d8cb0790299d9a1
Agustin Pieruccioni <agustin.pieruccioni@mercadolibre.com>
2021-01-19 11:49:28 -0300

    [feat] add tracking view insurtech hub-on

--- src/main/resources/catalog/melidata/ml_insurtech.groovy
+++ src/main/resources/catalog/melidata/ml_insurtech.groovy
@@ -32,6 +32,38 @@ tracks {
         category_path(required: true, type: PropertyType.ArrayList, description: "Category path of the item. For ex: ['MLA1051', 'MLA1055']")
     }
 
+     def gdr_option_price = objectSchemaDefinitions {
+        final_amount(required: false, type: PropertyType.Numeric, description: "final amount")
+        original_amount(required: false, type: PropertyType.Numeric, description: "amount original")
+        discount_rate(required: false, type: PropertyType.Numeric, description: "discount rate")
+        currency_id(required: false, type: PropertyType.String, description: "currency id")
+        monthly(required: false, type: PropertyType.Numeric, description: "Monthly amount")
+    }
+
+    def grd_option = objectSchemaDefinitions {
+        brand(required: false, type: PropertyType.String, description: "model")
+        coverage(required: false, type: PropertyType.String, description: "coverage")
+        check(required: false, type: PropertyType.String, description: "check")
+        deductible_rate(required: false, type: PropertyType.Numeric, description: "deductible rate")
+        deductible_amount(required: false, type: PropertyType.Numeric, description: "deductible amount")
+        model(required: false, type: PropertyType.String, description: "model")
+        size(required: false, type: PropertyType.String, description: "size")
+        manufacturer_warranty(required: false, type: PropertyType.Numeric, description: "factory warranty time")
+    }
+
+    def grd_protection_option = objectSchemaDefinitions {
+        product_id(required: true, type: PropertyType.String, description: "id of the warranty option")
+        price(required: true, type: PropertyType.Map(gdr_option_price), description: "price of the warranty option")
+        period(required: true, type: PropertyType.Numeric, description: "period of the warranty option")
+        option_data(required: false, type: PropertyType.Map(grd_option), description: "extra information of the warranty option")
+    }
+
+    def grd_protection_item = objectSchemaDefinitions {
+        id(required: true, type: PropertyType.String, description: "id of the item that is offered a protection")
+        domain_id(required: true, type: PropertyType.String, description: "domain of the item that is offered a protection")
+        price(required: true, type: PropertyType.Numeric, description: "price of the item that is offered a protection")
+    }
+
     // Web
     "/garex/checkout"(platform:"/web", type: TrackType.View) {
         item(required: true, type: PropertyType.Map(item_track_structure))
@@ -203,4 +235,37 @@ tracks {
         currency_id(required: true, type: PropertyType.String, description: "Currency id")
     }
 
+    //INSURTECH Hub-ON
+    "/insurtech/protections/marketplace"(platform:"/", type: TrackType.View, parentPropertiesInherited:false) {
+        item_info(required: true, type: PropertyType.Map(grd_protection_item), description: "information of the item to which protection is offered")
+        options_info(required: true, type: PropertyType.ArrayList(PropertyType.Map(grd_protection_option)), description: "information on the coverage options offered to the item")
+    }
+    
+    "/insurtech/protections/marketplace/help"(platform:"/", type: TrackType.Event, parentPropertiesInherited:false) {
+         item_info(required: true, type: PropertyType.Map(grd_protection_item), description: "information of the item to which protection is offered")
+         option_selected(required: true, type:PropertyType.Map(grd_protection_option), description: "selected coverage options")
+    }
+
+    "/insurtech/protections/marketplace/selectProtection"(platform:"/", type: TrackType.Event, parentPropertiesInherited:false) {
+         item_info(required: true, type: PropertyType.Map(grd_protection_item), description: "information of the item to which protection is offered")
+         option_selected(required: true, type:PropertyType.Map(grd_protection_option), description: "selected coverage options")
+    }
+
+    "/insurtech/protections/marketplace/cancelProtection"(platform:"/", type: TrackType.Event, parentPropertiesInherited:false) {
+        item_info(required: true, type: PropertyType.Map(grd_protection_item), description: "information of the item to which protection is offered")
+        options_info(required: true, type: PropertyType.ArrayList(PropertyType.Map(grd_protection_option)), description: "information on the coverage options offered to the item")
+    }
+
+    "/insurtech/protections/marketplace/confirmProtection"(platform:"/", type: TrackType.Event, parentPropertiesInherited:false) {
+        item_info(required: true, type: PropertyType.Map(grd_protection_item), description: "information of the item to which protection is offered")
+        options_info(required: true, type: PropertyType.ArrayList(PropertyType.Map(grd_protection_option)), description: "information on the coverage options offered to the item")
+        option_selected(required: true, type:PropertyType.Map(grd_protection_option), description: "selected coverage options")
+    }
+
+    "/insurtech/protections/marketplace/faq"(platform:"/", type: TrackType.View, parentPropertiesInherited:false) {
+        product_id(required: true, type: PropertyType.String, description: "product type id")
+        coverage(required: false, type: PropertyType.String, description: "protection coverage")
+        manufacturer_warranty(required: false, type: PropertyType.Numeric, description: "factory warranty time")
+        period(required: false, type: PropertyType.Numeric, description: "duration of warranty")
+    }
 }
