4ec46f0515755033fc231beccb129a549a39f288
gmir <gustavo.mir@mercadolibre.com>
2021-01-21 14:21:44 -0300

    Add tracks for A12 and PSJ

--- src/main/resources/catalog/melidata/costs_section.groovy
+++ src/main/resources/catalog/melidata/costs_section.groovy
@@ -44,6 +44,41 @@ tracks {
             type: PropertyType.String,
             description: "Indicates an ID of a tooltip"
         )
+        shield_id(
+            required: true,
+            type: PropertyType.String,
+            description: "Indicates an ID of Shield in OptIn A12 and PSJ"
+        )
+        link_id(
+            required: true,
+            type: PropertyType.String,
+            description: "Indicates an ID of Link in OptIn A12 and PSJ"
+        )
+        channels(
+            required: true,
+            type: PropertyType.ArrayList,
+            description: "Indicates the selected channels in OptIn A12"
+        )
+        schema(
+            required: true,
+            type: PropertyType.String,
+            description: "Indicates the selected schema in OptIn A12"
+        )
+        installments(
+            required: true,
+            type: PropertyType.Numeric,
+            description: "Indicates the selected schema installments in OptIn PSJ"
+        )
+        status(
+            required: true,
+            type: PropertyType.String,
+            description: "The status code of and error"
+        )
+        message(
+            required: false,
+            type: PropertyType.String,
+            description: "An error message"
+        )
     }
 
     propertyGroups {
@@ -58,6 +93,11 @@ tracks {
         row_message_dismiss_group(message_id, section_id, card_id, row_id)
         row_action_group(section_id, card_id, row_id, url)
         tooltip_event_group(section_id, card_id, row_id, tooltip_id)
+        optin_shield_group(shield_id)
+        optin_link_group(link_id)
+        optin_a12_save_group(channels, schema)
+        optin_psj_save_group(installments)
+        result_request_group(status, message)
     }
 
     // Abstract paths
@@ -76,73 +116,104 @@ tracks {
     "/pricing_section/section/card/row/message" (platform: "/", isAbstract: true) { }
     "/pricing_section/section/card/row/message/action" (platform: "/", isAbstract: true) { }
     "/pricing_section/section/card/row/tooltip" (platform: "/", isAbstract: true) { }
+    "/pricing-section/a12" (platform: "/", isAbstract: true) { }
+    "/pricing-section/psj" (platform: "/", isAbstract: true) { }
 
     // Tracks
 
     "/pricing_section/success" (platform: "/", type: TrackType.View) { }
-    "/pricing_section/error" (platform: "/", type: TrackType.View) { 
-        status(
-            required: true,
-            type: PropertyType.String,
-            description: "The status code of and error"
-        )
-        message(
-            required: true,
-            type: PropertyType.String,
-            description: "An error message"
-        )
+    "/pricing_section/error" (platform: "/", type: TrackType.View) {
+        result_request_group
     }
     "/pricing_section/close" (platform: "/mobile", type: TrackType.Event) { }
-    "/pricing_section/message/dismiss" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/message/dismiss" (platform: "/", type: TrackType.Event) {
         top_message_dismiss_group
     }
-    "/pricing_section/message/action/primary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/message/action/primary" (platform: "/", type: TrackType.Event) {
         top_message_action_group
     }
-    "/pricing_section/message/action/secondary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/message/action/secondary" (platform: "/", type: TrackType.Event) {
         top_message_action_group
     }
-    "/pricing_section/section/message/dismiss" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/message/dismiss" (platform: "/", type: TrackType.Event) {
         section_message_dismiss_group
     }
-    "/pricing_section/section/message/action/primary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/message/action/primary" (platform: "/", type: TrackType.Event) {
         section_message_action_group
     }
-    "/pricing_section/section/message/action/secondary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/message/action/secondary" (platform: "/", type: TrackType.Event) {
         section_message_action_group
     }
-    "/pricing_section/section/component/success" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/component/success" (platform: "/", type: TrackType.Event) {
         component_event_group
     }
     "/pricing_section/section/component/error" (platform: "/", type: TrackType.Event) {
         component_event_group
     }
-    "/pricing_section/section/card/message/dismiss" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/message/dismiss" (platform: "/", type: TrackType.Event) {
         card_message_dismiss_group
     }
-    "/pricing_section/section/card/message/action/primary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/message/action/primary" (platform: "/", type: TrackType.Event) {
         card_message_action_group
     }
-    "/pricing_section/section/card/message/action/secondary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/message/action/secondary" (platform: "/", type: TrackType.Event) {
         card_message_action_group
     }
-    "/pricing_section/section/card/row/message/dismiss" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/row/message/dismiss" (platform: "/", type: TrackType.Event) {
         row_message_dismiss_group
     }
-    "/pricing_section/section/card/row/message/action/primary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/row/message/action/primary" (platform: "/", type: TrackType.Event) {
         row_message_action_group
     }
-    "/pricing_section/section/card/row/message/action/secondary" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/row/message/action/secondary" (platform: "/", type: TrackType.Event) {
         row_message_action_group
     }
-    "/pricing_section/section/card/row/action" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/row/action" (platform: "/", type: TrackType.Event) {
         row_action_group
     }
-    "/pricing_section/section/card/row/tooltip/show" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/row/tooltip/show" (platform: "/", type: TrackType.Event) {
         tooltip_event_group
     }
-    "/pricing_section/section/card/row/tooltip/hide" (platform: "/", type: TrackType.Event) { 
+    "/pricing_section/section/card/row/tooltip/hide" (platform: "/", type: TrackType.Event) {
         tooltip_event_group
     }
 
+    // Tracks Ahora 12
+    "/pricing-section/a12/error" (platform: "/", type: TrackType.View) { }
+    "/pricing-section/a12/success" (platform: "/", type: TrackType.View) { }
+    "/pricing-section/a12/optin" (platform: "/", type: TrackType.View) { }
+    "/pricing-section/a12/shield" (platform: "/", type: TrackType.View) {
+        optin_shield_group
+    }
+    "/pricing-section/a12/cuit-verification" (platform: "/", type: TrackType.View) { }
+    "/pricing-section/a12/back" (platform: "/", type: TrackType.Event) { }
+    "/pricing-section/a12/navigate" (platform: "/", type: TrackType.Event) {
+        optin_link_group
+    }
+    "/pricing-section/a12/save/success" (platform: "/", type: TrackType.Event) {
+        optin_a12_save_group
+    }
+    "/pricing-section/a12/save/error" (platform: "/", type: TrackType.Event) {
+        result_request_group
+    }
+    "/pricing-section/a12/cuit-verification/accept" (platform: "/", type: TrackType.Event) { }
+
+    // Tracks PSJ
+    "/pricing-section/psj/error" (platform: "/", type: TrackType.View) { }
+    "/pricing-section/psj/success" (platform: "/", type: TrackType.View) { }
+    "/pricing-section/psj/optin" (platform: "/", type: TrackType.View) { }
+    "/pricing-section/psj/shield" (platform: "/", type: TrackType.View) {
+        optin_shield_group
+    }
+    "/pricing-section/psj/back" (platform: "/", type: TrackType.Event) { }
+    "/pricing-section/psj/navigate" (platform: "/", type: TrackType.Event) {
+        optin_link_group
+    }
+    "/pricing-section/psj/help" (platform: "/", type: TrackType.Event) { }
+    "/pricing-section/psj/save/success" (platform: "/", type: TrackType.Event) {
+        optin_psj_save_group
+    }
+    "/pricing-section/psj/save/error" (platform: "/", type: TrackType.Event) {
+        result_request_group
+    }
 }
--- src/test/resources/melidata/costs_section.groovy
+++ src/test/resources/melidata/costs_section.groovy
@@ -12,7 +12,7 @@ trackTests {
     }
 
     test("Costs section finish loading with an error") {
-        "/pricing_section/error" (platform: "/", type: TrackType.View) { 
+        "/pricing_section/error" (platform: "/", type: TrackType.View) {
             status = "400"
             message = "server not found"
         }
@@ -23,7 +23,7 @@ trackTests {
     }
 
     test("A message at the most top level is dismissed") {
-        "/pricing_section/message/dismiss" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/message/dismiss" (platform: "/", type: TrackType.Event) {
             message_id = "cost_section_message"
         }
     }
@@ -45,7 +45,7 @@ trackTests {
     // Section
 
     test("A message inside a section is dismissed") {
-        "/pricing_section/section/message/dismiss" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/message/dismiss" (platform: "/", type: TrackType.Event) {
             message_id = "financing_section_message"
             section_id = "financing_section"
         }
@@ -70,14 +70,14 @@ trackTests {
     // Component
 
     test("An external component finish loading successful") {
-        "/pricing_section/section/component/success" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/component/success" (platform: "/", type: TrackType.Event) {
             section_id = "fees_section"
             component_id = "fees_component"
         }
     }
 
     test("An external component finish loading with an error") {
-        "/pricing_section/section/component/error" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/component/error" (platform: "/", type: TrackType.Event) {
             section_id = "fees_section"
             component_id = "fees_component"
         }
@@ -86,7 +86,7 @@ trackTests {
     // Card
 
     test("A message inside a card is dismissed") {
-        "/pricing_section/section/card/message/dismiss" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/card/message/dismiss" (platform: "/", type: TrackType.Event) {
             message_id = "financing_section_message"
             section_id = "financing_section"
             card_id = "financing_psj"
@@ -114,7 +114,7 @@ trackTests {
     // Row
 
     test("A message inside a row is dismissed") {
-        "/pricing_section/section/card/row/message/dismiss" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/card/row/message/dismiss" (platform: "/", type: TrackType.Event) {
             message_id = "financing_section_message"
             section_id = "financing_section"
             card_id = "financing_a12"
@@ -143,7 +143,7 @@ trackTests {
     }
 
     test("A row was clicked") {
-        "/pricing_section/section/card/row/action" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/card/row/action" (platform: "/", type: TrackType.Event) {
             section_id = "financing_section"
             card_id = "financing_a12"
             row_id = "a12"
@@ -152,7 +152,7 @@ trackTests {
     }
 
     test("A tooltip (triggered by a click in a row) was opened") {
-        "/pricing_section/section/card/row/tooltip/show" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/card/row/tooltip/show" (platform: "/", type: TrackType.Event) {
             section_id = "financing_section"
             card_id = "financing_a12"
             row_id = "a12"
@@ -161,12 +161,117 @@ trackTests {
     }
 
     test("A tooltip (triggered by a click in a row) was closed") {
-        "/pricing_section/section/card/row/tooltip/hide" (platform: "/", type: TrackType.Event) { 
+        "/pricing_section/section/card/row/tooltip/hide" (platform: "/", type: TrackType.Event) {
             section_id = "financing_section"
             card_id = "financing_a12"
             row_id = "a12"
             tooltip_id = "a12_tooltip"
         }
     }
-    
+
+    // OptIn Ahora 12
+    test("Error trying to render Ahora 12 page") {
+        "/pricing-section/a12/error" (platform: "/", type: TrackType.View) { }
+    }
+    test("Renders Ahora 12 page") {
+        "/pricing-section/a12/success" (platform: "/", type: TrackType.View) { }
+    }
+    test("Renders Ahora 12 OptIn component") {
+        "/pricing-section/a12/optin" (platform: "/", type: TrackType.View) { }
+    }
+    test("Renders Ahora 12 Shield component") {
+        "/pricing-section/a12/shield" (platform: "/", type: TrackType.View) {
+            shield_id = "shield_id"
+        }
+    }
+    test("Renders Ahora 12 Cuit-Verification component") {
+        "/pricing-section/a12/cuit-verification" (platform: "/", type: TrackType.View) { }
+    }
+    test("Go back was clicked") {
+        "/pricing-section/a12/back" (platform: "/", type: TrackType.Event) { }
+    }
+    test("A link was clicked") {
+        "/pricing-section/a12/navigate" (platform: "/", type: TrackType.Event) {
+            link_id = "link_id"
+        }
+    }
+    test("The save button was clicked with a satisfactory result") {
+        "/pricing-section/a12/save/success" (platform: "/", type: TrackType.Event) {
+            channels = [
+                    [
+                        id : "point",
+                        description : "Point",
+                        active : false
+                    ],
+                    [
+                        id : "qr",
+                        description : "Código QR",
+                        active : true
+                    ],
+                    [
+                        id : "op",
+                        description : "Cobros online",
+                        active : true
+                    ],
+            ]
+            schema = "AHORAADMIN_12"
+        }
+    }
+    test("The save button was clicked with a error result") {
+        "/pricing-section/a12/save/error" (platform: "/", type: TrackType.Event) {
+            status = "500"
+        }
+    }
+    test("The save button was clicked with a error result") {
+        "/pricing-section/a12/save/error" (platform: "/", type: TrackType.Event) {
+            status = "500"
+            message = "Error"
+        }
+    }
+    test("The accept button was clicked in cuit-verification component") {
+        "/pricing-section/a12/cuit-verification/accept" (platform: "/", type: TrackType.Event) { }
+    }
+
+    // OptIn PSJ
+    test("Error trying to render PSJ page") {
+        "/pricing-section/psj/error" (platform: "/", type: TrackType.View) { }
+    }
+    test("Renders PSJ page") {
+        "/pricing-section/psj/success" (platform: "/", type: TrackType.View) { }
+    }
+    test("Renders PSJ OptIn component") {
+        "/pricing-section/psj/optin" (platform: "/", type: TrackType.View) { }
+    }
+    test("Renders PSJ Shield component") {
+        "/pricing-section/psj/shield" (platform: "/", type: TrackType.View) {
+            shield_id = "shield_id"
+        }
+    }
+    test("Go back was clicked") {
+        "/pricing-section/psj/back" (platform: "/", type: TrackType.Event) { }
+    }
+    test("A link was clicked") {
+        "/pricing-section/psj/navigate" (platform: "/", type: TrackType.Event) {
+            link_id = "link_id"
+        }
+    }
+    test("A help was open") {
+        "/pricing-section/psj/help" (platform: "/", type: TrackType.Event) { }
+    }
+    test("The save button was clicked with a satisfactory result") {
+        "/pricing-section/psj/save/success" (platform: "/", type: TrackType.Event) {
+            installments = 12
+        }
+    }
+    test("The save button was clicked with a error result") {
+        "/pricing-section/psj/save/error" (platform: "/", type: TrackType.Event) {
+            status = "500"
+        }
+    }
+    test("The save button was clicked with a error result") {
+        "/pricing-section/psj/save/error" (platform: "/", type: TrackType.Event) {
+            status = "500"
+            message = "Error"
+        }
+    }
 }
