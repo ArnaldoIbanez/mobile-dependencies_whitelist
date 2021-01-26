e37a0bcbe3ec60c86f240dc1a3f070d4b6862ede
Mateo Martin <mateonicolasmartin@gmail.com>
2021-01-26 19:07:30 -0300

    agrego propiedad para contenidos predecidos

--- src/main/resources/catalog/melidata/cx.groovy
+++ src/main/resources/catalog/melidata/cx.groovy
@@ -62,6 +62,8 @@ tracks {
             description: "Indicates the score of the prediction for the contact")
         portal_contact_predicted_problem_id(required: false, type: PropertyType.Numeric,
             description: "Indicates the problem with which the user should contact")
+        portal_predicted_contents(required: false, type: PropertyType.ArrayList(PropertyType.Numeric),
+            description: "Indicates the ids of the contents predicted to a user")
     }
 
     propertyGroups {
@@ -86,6 +88,7 @@ tracks {
         portal_contact_predicted_team(portal_contact_predicted_team)
         portal_contact_prediction_score(portal_contact_prediction_score)
         portal_contact_predicted_problem_id(portal_contact_predicted_problem_id)
+        portal_predicted_contents(portal_predicted_contents)
     }
 
     "/portal"(platform: "/", isAbstract:  true) {}
@@ -211,6 +214,7 @@ tracks {
         portal_show_cancel_card
         portal_prediction_id
         portal_has_one_click
+        portal_predicted_contents
     }
 
     "/portal/zrp"(platform: "/", type: TrackType.View) {
