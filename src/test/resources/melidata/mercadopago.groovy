8b0d5a3364e3ebae32924743b0c65aefc595f481
dkarasawa <daniela.karasawa@mercadolivre.com>
2021-01-27 16:56:48 -0300

    updated for webview

--- src/test/resources/melidata/mercadopago.groovy
+++ src/test/resources/melidata/mercadopago.groovy
@@ -4770,13 +4770,13 @@ trackTests {
 
     //MP personalFrontend
     test("Listado de Herramientas Creadas"){
-        "/tools/list"(platform: "/web", type: TrackType.View){}
-        "/tools/list/button_create"(platform: "/web"){}
+        "/tools/list"(platform: "/", type: TrackType.View){}
+        "/tools/list/button_create"(platform: "/"){}
     }
 
     test("Crear Herramienta de cobro"){
-        "/tools/create"(platform: "/web", type: TrackType.View){}
-        "/tools/confirm_create_edit"(platform: "/web"){}
+        "/tools/create"(platform: "/", type: TrackType.View){}
+        "/tools/confirm_create_edit"(platform: "/"){}
     }
 
     test("Configuraciones de Negocio"){
