3c706d25fc35a31a903c53dd60b652560f1782f1
Enzo Zoppi <enzo.zoppi@mercadolibre.com>
2021-02-03 15:25:27 -0300

    Add properties for help card tracks

--- src/main/resources/catalog/melidata/cx.groovy
+++ src/main/resources/catalog/melidata/cx.groovy
@@ -66,6 +66,10 @@ tracks {
             description: "Indicates the features used in the prediction")
         portal_predicted_contents(required: false, type: PropertyType.ArrayList(PropertyType.Numeric),
             description: "Indicates the ids of the contents predicted to a user")
+        helpcard_title(required: true, type: PropertyType.String,
+            description: "Indicates the title of the help card as viewed by the user")
+        helpcard_contents(required: true, type: PropertyType.ArrayList(PropertyType.Map),
+            description: "Indicates the contents viewed by the user in the help card")
     }
 
     propertyGroups {
@@ -92,6 +96,7 @@ tracks {
         portal_contact_predicted_problem_id(portal_contact_predicted_problem_id)
         portal_predicted_features(portal_predicted_features)
         portal_predicted_contents(portal_predicted_contents)
+        helpcard_data(portal_source_id, helpcard_title, helpcard_contents)
     }
 
     "/portal"(platform: "/", isAbstract:  true) {}
@@ -376,5 +381,11 @@ tracks {
         type(required: true, type: PropertyType.String, 
             values: ["new_information", "greetings", "recontact"],
             description: "Contact type")
+    }
+
+    // Daisy
+
+    "/support/helpcard/contents"(platform: "/", type; TrackType.View) {
+        helpcard_data
     }
 }
