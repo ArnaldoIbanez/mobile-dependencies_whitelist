37ef132e5313af636a318d4d97f352d17966510f
Caro <carolina.amarillo@mercadolibre.com>
2021-02-04 12:18:00 -0300

    chore: change tracks for splinter categories

--- src/main/resources/catalog/melidata/category_landing.groovy
+++ src/main/resources/catalog/melidata/category_landing.groovy
@@ -8,13 +8,13 @@ tracks {
 
     initiative = "1202"
 
-    "/category_landing"(platform: "/", type: TrackType.View) {
-        category_id(required: false,  description: "Home's category")
-        category_path(required: false, description: "Category path of this category home")
+    "/category"(platform: "/", type: TrackType.View) {
+        category_id(required: true,  description: "Home's category")
+        category_path(required: true, description: "Category path of this category home")
         last_modified(required: false, description: "Date of last modification")
     }
 
-    "/category_landing"(platform: "/",  type: TrackType.Event) {
+    "/category/tap"(platform: "/",  type: TrackType.Event) {
         category(required: false, type: PropertyType.String, description: "Landing category", values: ["landings"])
         action(required: false, type: PropertyType.String, description: "Name of the campaign")
         label(required: false, type: PropertyType.String, description: "Component related info")
