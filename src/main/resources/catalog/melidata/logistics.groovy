ecb3b0a1d18be43892e9d646d61cd082164bee23
durquiza <diego.urquiza@mercadolibre.com>
2021-02-08 13:26:45 -0300

    DEXP-49 - fix data track type

--- src/main/resources/catalog/melidata/logistics.groovy
+++ src/main/resources/catalog/melidata/logistics.groovy
@@ -375,7 +375,7 @@ tracks {
         driver_id(required: true, type: PropertyType.String, description: "Specifies the current driver id")
     }
     "/logistics/last_mile/places/disclaimer_handshake/confirm"(platform: "/mobile", type: TrackType.Event) {
-        packages_to_scan(required: true, type: PropertyType.String, description: "Specifies the list of shipments id to scan", inheritable: false)
+        packages_to_scan(required: true, type: PropertyType.ArrayList(PropertyType.String), description: "Specifies the list of shipments id to scan", inheritable: false)
     }
     "/logistics/last_mile/places/modal_handshake"(platform: "/mobile", type: TrackType.Event) {
         route_id(required: true, type: PropertyType.String, description: "Specifies the current route id")
