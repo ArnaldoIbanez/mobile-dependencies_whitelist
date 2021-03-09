f479c07194db221cc43e62bfd2a077893c7f3517
Juan Ignacio <juan.nadin@mercadolibre.com>
2021-03-09 09:28:45 -0300

    adding abstract

--- src/main/resources/catalog/melidata/registrations.groovy
+++ src/main/resources/catalog/melidata/registrations.groovy
@@ -244,6 +244,7 @@ tracks {
     "/register/v3/hub/kyc"(platform: "/", isAbstract: true){}
     "/register/v3/hub/pix"(platform: "/", isAbstract: true){}
     "/register/v3/hub/regulations"(platform: "/", isAbstract: true){}
+    "/register/v3/hub/tyc"(platform: "/", isAbstract: true){}
     "/register/v3/hub/congrats"(platform: "/", isAbstract: true){}
     "/register/v3/hub/email_validation/congrats"(platform: "/", type: TrackType.View){}
     "/register/v3/hub/phone_validation/congrats"(platform: "/", type: TrackType.View){}
