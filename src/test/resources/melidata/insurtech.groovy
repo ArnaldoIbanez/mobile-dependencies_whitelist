1da4b8e07139e5f7976ba31c30f0e61b4969b7b8
Mauro Walter <mauro.walter@mercadolibre.com>
2021-03-25 09:16:39 -0300

    Added insurtech webview tracks

--- src/test/resources/melidata/insurtech.groovy
+++ src/test/resources/melidata/insurtech.groovy
@@ -1213,6 +1213,17 @@ trackTests {
             view_time = 964578
         }
 
+        // INSURTECH Webview
+        "/insurtech/webview/generic_error"(platform:"/mobile", type: TrackType.Event) {
+            deeplink = "mercadopago://insurtech/webview?url=https%3A%2F%2Fmercadopago.com.br%2Fprotections%2Froda%2Fqpage&toolbar=true"
+            device = [
+                brand: "Samsung",
+                model: "J7",
+                model_code: "SM-J700M",
+                size: "64GB",
+            ]
+        }
+
         // INSURTECH RODA Payments
 
         "/insurtech/payments/loading"(platform:"/mobile", type: TrackType.View) {
