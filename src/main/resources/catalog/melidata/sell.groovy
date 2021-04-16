9ba00bbd9dca85083894b3c20742dec98231a098
Emanuel Arias <emanuel.arias@mercadolibre.com>
2021-04-16 16:21:40 -0300

    dding property for user's mshops status and item's sale_channel

--- src/main/resources/catalog/melidata/sell.groovy
+++ src/main/resources/catalog/melidata/sell.groovy
@@ -135,6 +135,8 @@ tracks {
         sellGroupMobile
         listingTypeFlowMobile
         seller_reputation(required: false, type: PropertyType.String, description: "Seller's reputation")
+        user_has_shops(required: false, type: PropertyType.String, description: "Mercado Shops status for de users: active / inactive / none")
+        sale_channel(required: false, type: PropertyType.String, description: "Item's sale channel: both / marketplace / mshops")
     }
     "/sell/list"(platform: "/", isAbstract: true){
         session_id(required: false, description: "Session id for a specific user flow", type: PropertyType.String)
