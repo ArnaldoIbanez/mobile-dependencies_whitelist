18b5679f596fe7b87db00c4e5120612b46968d65
Gonzalo Macedo <gonzalo.macedo@mercadolibre.com>
2021-05-12 10:17:21 -0300

    Update

--- src/main/resources/catalog/melidata/kyc.groovy
+++ src/main/resources/catalog/melidata/kyc.groovy
@@ -75,7 +75,6 @@ tracks {
     "/kyc/challenge_proof_of_address"(platform: "/", type: TrackType.View) {}
     "/kyc/challenge_death"(platform: "/", type: TrackType.View) {}
     "/kyc/challenge_under_age"(platform: "/", type: TrackType.View) {}
-
     "/kyc/identity_handler"(platform: "/", type: TrackType.Event) {}
 
     // WebView events
