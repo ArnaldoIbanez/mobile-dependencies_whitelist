102034f2ab8e31139fcd3b1c40fe1511835de3d3
BrunoDM2943 <bdm2943@gmail.com>
2021-05-26 11:34:41 -0300

    Fix test

--- src/test/resources/melidata/cx_clicktocall.groovy
+++ src/test/resources/melidata/cx_clicktocall.groovy
@@ -9,11 +9,11 @@ trackTests {
 
     test("Console errors") {
         "/cx/c2c/console/twilio/errors"(platform: "/web/desktop", type: TrackType.Event) {
-            error_code = "30006",
-            error_text = "Error message",
-            call_sid = "CA123123XPTO",
-            call_id = "1234",
-            case_id = "1234",
+            error_code = "30006"
+            error_text = "Error message"
+            call_sid = "CA123123XPTO"
+            call_id = "1234"
+            case_id = "1234"
         }
     }
 }
