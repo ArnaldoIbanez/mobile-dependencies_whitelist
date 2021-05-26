619cf5dc02a330596a7fb58ade68e34f1639b552
BrunoDM2943 <bdm2943@gmail.com>
2021-05-26 11:11:02 -0300

    Adding new track info  for console errors

---
+++ src/main/resources/catalog/melidata/cx_clicktocall.groovy
@@ -0,0 +1,19 @@
+package catalog.melidata
+
+import com.ml.melidata.catalog.PropertyType
+import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
+import com.ml.melidata.TrackType
+
+
+tracks {
+
+    initiative = "1155"
+
+    "/c2c/console/errors"(platform: "/web/desktop", type: TrackType.Event) {
+        error_code(type: PropertyType.String, description: "Twilio Error code")
+        error_text(type: PropertyType.String, description: "Twilio Error text")
+        call_sid(type: PropertyType.String, description: "Twilio Call SID")
+        call_id(type: PropertyType.String, description: "Internal Call ID")
+        case_id(type: PropertyType.String, description: "Case ID")
+    }
+}
---
+++ src/test/resources/melidata/cx_clicktocall.groovy
@@ -0,0 +1,19 @@
+package src.test.resources.melidata
+
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+import com.ml.melidata.TrackType
+
+trackTests {
+
+    defaultBusiness = "mercadolibre"
+
+    test("Console errors") {
+        "/c2c/console/errors"(platform: "/web/desktop", type: TrackType.Event) {
+            error_code = "30006",
+            error_text = "Error message",
+            call_sid = "CA123123XPTO",
+            call_id = "1234",
+            case_id = "1234",
+        }
+    }
+}
