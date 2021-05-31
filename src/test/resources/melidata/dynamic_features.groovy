89ad4bd16cb9c948df5b28b97e27f49542e4cf7f
Rodrigo Pintos <rodrigo.pintos@mercadolibre.com>
2021-05-31 17:02:30 -0300

    add buisness

--- src/test/resources/melidata/dynamic_features.groovy
+++ src/test/resources/melidata/dynamic_features.groovy
@@ -6,11 +6,39 @@ import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 
 trackTests {
     
+    defaultBusiness = "mercadolibre"
+
     //------------------------------------------------------------------------------------------------------------------------------------------------------
     // TRACKS Dynamic Features
     //------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    test("Dynamic features track") {
+    test("Dynamic features track - ML") {
+
+	    // Dynamic Features Events
+	    "/dynamyc_features/downloaded"(platform: "/mobile", type: TrackType.Event) {
+            module_name = "module_dynamic"
+            new_deeplink = "meli://new_deeplink"
+            old_deeplink = "meli://old_deeplink"
+		}
+
+		"/dynamyc_features/failed"(platform: "/mobile", type: TrackType.Event) {
+            module_name = "module_dynamic"
+            old_deeplink = "meli://old_deeplink"
+		}
+        "/dynamyc_features/launched"(platform: "/mobile", type: TrackType.Event) {
+            module_name = "module_dynamic"
+            new_deeplink = "meli://new_deeplink"
+            old_deeplink = "meli://old_deeplink"
+		}    
+        "/dynamyc_features/canceled"(platform: "/mobile", type: TrackType.Event) {
+            module_name = "module_dynamic"
+            old_deeplink = "meli://old_deeplink"
+		}
+    }
+
+        defaultBusiness = "mercadopago"
+
+test("Dynamic features track - MP") {
 
 	    // Dynamic Features Events
 	    "/dynamyc_features/downloaded"(platform: "/mobile", type: TrackType.Event) {
