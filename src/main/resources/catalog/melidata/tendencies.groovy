8c79f9e8a67839c4ec98143ba266a7e0dc132dfe
MartinKellyML <martin.kelly@mercadolibre.com>
2021-06-03 18:13:12 -0300

    ref: remove unnecesary atributes

--- src/main/resources/catalog/melidata/tendencies.groovy
+++ src/main/resources/catalog/melidata/tendencies.groovy
@@ -9,120 +9,22 @@ tracks {
 
     initiative = "1171"
 
-    def pageStructure = objectSchemaDefinitions {
-        limit(required: true, type: PropertyType.Numeric, description: "max number of items per page")
-        offset(required: true, type: PropertyType.Numeric, description: "item list offset")
-        total(required: true, type: PropertyType.String, description: "total number of list items")
-    }
-
-    def priceStructure = objectSchemaDefinitions {
-        currency(required: true, type: PropertyType.String)
-        price(required: true, type: PropertyType.String)
-        decimals(required: true, type: PropertyType.String)
-        original_price(required: false, type: PropertyType.String)
-        discount(required: false, type: PropertyType.Numeric)
-        has_loyalty_discount(required: true, type: PropertyType.Boolean)
-    }
-
-    def installmentsDefinition = objectSchemaDefinitions {
-        quantity(required: true, type: PropertyType.Numeric)
-        amount(required: true, type: PropertyType.Numeric)
-        no_interest(required: true, type: PropertyType.Boolean)
-        currency_id(required: true, type: PropertyType.String)
-        decimals(required: false, type: PropertyType.String)
-        price(required: false, type: PropertyType.String)
-    }
-
-    def lightningDealConfigurationDefinition = objectSchemaDefinitions {
-        end_date(required: true, type: PropertyType.String)
-        initial_stock(required: true, type: PropertyType.Numeric)
-        start_date(required: true, type: PropertyType.String)
-        remaining_stock(required: false, type: PropertyType.Numeric)
-    }
-
-    def dealOfTheDayConfigurationDefinition = objectSchemaDefinitions {
-        end_date(required: true, type: PropertyType.String)
-        start_date(required: true, type: PropertyType.String)
-    }
-
     def itemStructure = objectSchemaDefinitions {
         id(required: true, type: PropertyType.String, description: "Item's id")
-        title(required: true, type: PropertyType.String, description: "Item's title")
-        price(required: true, type: PropertyType.Map(priceStructure), description: "Item's price")
-        free_shipping(required: true, type: PropertyType.Boolean, description: "Item's free_shipping condition")
-        fulfillment(required: true, type: PropertyType.Boolean)
-        score_prior(required: false, type: PropertyType.Numeric,
-                description: "Item's score calculated by ML model")
-        position_prior(required: false, type: PropertyType.Numeric,
-                description: "Item's position calculated by prior_score")
-        position_posterior(required: false, type: PropertyType.Numeric,
-                description: "Item's position calculated by posterior_score")
-        benefit(required: false, type: PropertyType.Numeric)
-        boosted(required: false, type: PropertyType.Boolean,
-                description: "Indicates whether the item was manually positioned")
-        model_version(required: false, type: PropertyType.String)
         position(required: false, type: PropertyType.Numeric)
-        created_date(required: false, type: PropertyType.String)
-        one_p(required: false, type: PropertyType.Numeric,
-                description: "Attribute that the publication has or has not. If present, credibility is replaced with 1")
-        private_label(required: false, type: PropertyType.Numeric,
-                description: "Attribute that the publication has or has not. If present, credibility is replaced with 1")
-        promotion_type(required: false, type: PropertyType.String,
-                values:["TODAY_PROMOTION", "DEAL_OF_THE_DAY", "LIGHTNING_DEAL", "BUY_BOX_WINNER", "others"], description: "Type of promotion")
-        prime_discount(required: false, type: PropertyType.Boolean,
-                description: "Indicates whether the item has discounts by mercado puntos")
-        score(required: false, type: PropertyType.Numeric,
-                description: "Item's score calculated by highlights backend based on several variables")
-        posterior_score(required: false, type: PropertyType.Numeric,
-                description: "Item's score calculated with prior_score and learning data")
-        listing_type_id(required: false, type: PropertyType.String)
-        logistic_type(required: false, type: PropertyType.String)
-        sold_quantity(required: false, type: PropertyType.Numeric)
-        available_quantity(required: false, type: PropertyType.Numeric)
-        tags(required: false, type: PropertyType.ArrayList(PropertyType.String))
-        deal_ids(required: true, type: PropertyType.ArrayList(PropertyType.String))
-        warranty(required: false, type: PropertyType.String)
-        accepts_mercadopago(required: false, type: PropertyType.Boolean)
-        interest(required: false, type: PropertyType.Numeric)
-        item_clicks(required: false, type: PropertyType.Numeric)
-        conversion_rate(required: false, type: PropertyType.Numeric)
-        expected_clicks(required: false, type: PropertyType.Numeric)
-        rebate(required: false, type: PropertyType.Numeric,
-                description: "Attribute that the publication has or has not")
-        tendency_print_id(required: false, type: PropertyType.String, description: "Unique id per render")
-        installments(required: false, type: PropertyType.Map(installmentsDefinition), description: "Installments info for the item")
-        lightning_deal_configuration(required: false, type: PropertyType.Map(lightningDealConfigurationDefinition), description: "Configuration for the item if is lighting deal")
-        deal_of_the_day_configuration(required: false, type: PropertyType.Map(dealOfTheDayConfigurationDefinition), description: "Configuration for the item if is deal of the day")
-        dispatching_normally(required: false, type: PropertyType.Boolean, description: "Indicate whether the item support dispatching normally")
-    }
-
-    def shortcutsStructure = objectSchemaDefinitions {
-        filter(required: true, type: PropertyType.Map, description: "filter described")
-        position(required: true, type: PropertyType.Numeric, description: "position in array")
-    }
-
-    def itemsExtraStructure = objectSchemaDefinitions {
-        component_name(required: true, type: PropertyType.String, description: "The component name used to display the items extra")
-        type_content(required: false, type: PropertyType.Boolean, description: "The items content type to display")
-        row(required: false, type: PropertyType.Numeric, description: "Indicate where the items extra should be displayed")
-        items(required: true, type: PropertyType.ArrayList(PropertyType.Map(itemExtraStructure)), description: "Items extra to display")
     }
 
     propertyDefinitions {
         tendency_print_id(required: true, type: PropertyType.String, description: "Unique id per render")
-        items(required: false, type: PropertyType.ArrayList(PropertyType.Map(itemStructure)),
-                description: "List of page's items")
-        page(required: false, type: PropertyType.Map(pageStructure),
-                description: "Indicates pagination information like limit, offset and total")
         origin(required: false, type: PropertyType.String, description: "The page section where the user interact")
-        filter_applied(required: false, type: PropertyType.String, description: "The current applied filter name")
+        items(required: true, type: PropertyType.ArrayList(PropertyType.Map(itemStructure)),
+                description: "List of page's items")
+        tendency_type(required: false, type: PropertyType.String, description: "The current tendency type")
         selected_filters(required: false, type: PropertyType.Map, description: "The data of all applied filters")
-        displayed_shortcuts(required: false, type: PropertyType.ArrayList(PropertyType.Map(shortcutsStructure)), description: "The filters shortcuts shown to the user in each request")
-        items_extra(required: false, type: PropertyType.Map(itemsExtraStructure), description: "Items extra to display")
     }
 
     propertyGroups {
-        general_tendencies_info(tendency_print_id, items, page, origin, filter_applied, selected_filters, displayed_shortcuts, items_extra)
+        general_tendencies_info(tendency_print_id, origin, items, tendency_type, selected_filters)
     }
 
     //Tendencies Landing
--- src/test/resources/melidata/tendencies.groovy
+++ src/test/resources/melidata/tendencies.groovy
@@ -10,214 +10,23 @@ trackTests {
     test("Tendencies landing") {
         def general= {
             tendency_print_id = "fce4b1d0-cf45-11e9-8e9d-c5fad1dc1dc5"
+            origin = "vip"
+            tendency_type = "best_seller"
             items = [
                     [
                             id: "MLA611663121",
-                            title: "Some item title",
-                            free_shipping: true,
-                            fulfillment: false,
-                            price: [
-                                    currency: "\$",
-                                    price: "1.414",
-                                    decimals: "55",
-                                    original_price: "1.489",
-                                    discount: 5,
-                                    has_loyalty_discount: false
-                            ],
-                            free_shipping: false,
-                            official_store: null,
-                            listing_type_id: "gold_special",
-                            score: 0.89,
-                            score_prior: 0.7,
-                            position_prior: 1,
-                            posterior_score: 0.6,
-                            position_posterior: 1,
-                            benefit: 0.05,
-                            interest: 0.98,
-                            logistic_type: "xd_drop_off",
-                            sold_quantity: 15,
-                            available_quantity: 62,
-                            tags: [
-                                    "brand_verified",
-                                    "good_quality_picture",
-                                    "good_quality_thumbnail",
-                                    "loyalty_discount_eligible",
-                                    "immediate_payment",
-                                    "cart_eligible"
-                            ],
-                            deal_ids: [
-                                    "MLA1234",
-                                    "MLA1235",
-                                    "MLA1236"
-                            ],
-                            warranty: "",
-                            accepts_mercadopago: true,
-                            boosted: true,
-                            model_version: "0.0.1",
-                            position: 1,
-                            created_date: "2019-10-07T17:51:21Z",
-                            promotion_type: "TODAY_PROMOTION",
-                            lightning_deal_configuration: [
-                                    end_date: "2020-01-29T19:00:00-06:00",
-                                    initial_stock: 64,
-                                    remaining_stock: 2,
-                                    start_date: "2020-01-29T11:00:00-06:00"
-                            ],
-                            deal_of_the_day_configuration: null,
-                            dispatching_normally: true
-                    ],
-                    [
-                            id: "MLA611663122",
-                            title: "Some other title",
-                            free_shipping: true,
-                            fulfillment: false,
-                            price: [
-                                    currency: "\$",
-                                    price: "2.414",
-                                    decimals: "55",
-                                    discount: null,
-                                    has_loyalty_discount: false
-                            ],
-                            free_shipping: false,
-                            official_store: null,
-                            listing_type_id: "gold_special",
-                            score: 0.89,
-                            score_prior: 0.7,
-                            position_prior: 1,
-                            posterior_score: 0.6,
-                            position_posterior: 1,
-                            benefit: 0.05,
-                            interest: 0.98,
-                            logistic_type: "xd_drop_off",
-                            sold_quantity: 15,
-                            available_quantity: 62,
-                            tags: [
-                                    "brand_verified",
-                                    "good_quality_picture",
-                                    "good_quality_thumbnail",
-                                    "loyalty_discount_eligible",
-                                    "immediate_payment",
-                                    "cart_eligible"
-                            ],
-                            deal_ids: [
-                                    "MLA2234",
-                                    "MLA2235",
-                                    "MLA2236"
-                            ],
-                            accepts_mercadopago: true,
-                            rebate: 1,
-                            one_p: 0,
-                            private_label: 1,
-                            promotion_type: "DEAL_OF_THE_DAY",
-                            lightning_deal_configuration: null,
-                            deal_of_the_day_configuration: null
-                    ],
-                    [
-                            id: "MLA611663126",
-                            title: "Some other title",
-                            free_shipping: true,
-                            fulfillment: false,
-                            price: [
-                                    currency: "\$",
-                                    price: "2.414",
-                                    decimals: "55",
-                                    discount: null,
-                                    has_loyalty_discount: false
-                            ],
-                            free_shipping: false,
-                            official_store: null,
-                            listing_type_id: "gold_special",
-                            score: 0.89,
-                            score_prior: 0.7,
-                            position_prior: 1,
-                            posterior_score: 0.6,
-                            position_posterior: 1,
-                            benefit: 0.05,
-                            interest: 0.98,
-                            logistic_type: "xd_drop_off",
-                            sold_quantity: 15,
-                            available_quantity: 62,
-                            tags: [
-                                    "brand_verified",
-                                    "good_quality_picture",
-                                    "good_quality_thumbnail",
-                                    "loyalty_discount_eligible",
-                                    "immediate_payment",
-                                    "cart_eligible"
-                            ],
-                            deal_ids: [
-                                    "MLA3234",
-                                    "MLA3235",
-                                    "MLA3236"
-                            ],
-                            warranty: "",
-                            accepts_mercadopago: true,
-                            rebate: 1,
-                            one_p: 0,
-                            private_label: 1,
-                            promotion_type: "LIGHTNING_DEAL",
-                            lightning_deal_configuration: null,
-                            deal_of_the_day_configuration: null
-                    ]
-            ]
-            selected_filters = [category_id: "MLA1055"]
-            origin = "vip"
-            filter_applied = "category_id"
-            displayed_shortcuts =[
-                    [
-                            filter: [
-                                    category_id: "MLA1055"
-                            ],
                             position: 1
                     ],
                     [
-                            filter: [
-                                    category_id: "MLA1055",
-                                    attribute_value: "BRAND",
-                                    attribute_value_id: "1234"
-                            ],
+                            id: "MLA611663122",
                             position: 2
+                    ],
+                    [
+                            id: "MLA611663126",
+                            position: 3
                     ]
             ]
-            items_extra = [
-                    component_name: "offers_for_user",
-                    type_content: "domain_id",
-                    row: 4,
-                    items: [
-                            [
-                                    id: "MLA123",
-                                    title: "Notebook Hp Intel Core I3 4gb 1tb Pantalla 14 Hdmi Wifi Gtia Oficial",
-                                    image_id: "683598-MLA42907993112_072020",
-                                    image_src: "https://http2.mlstatic.com/D_683598-MLA42907993112_072020-T.jpg",
-                                    free_shipping: true,
-                                    fulfillment: false,
-                                    deal_ids: [
-                                            "MLA1234",
-                                            "MLA1235",
-                                            "MLA1236"
-                                    ],
-                                    link: [
-                                            url: "https://articulo.mercadolibre.com.ar/MLA-689581949-notebook-hp-intel-core-i3-4gb-1tb-pantalla"
-                                    ],
-                                    price: [
-                                            price: "68.999",
-                                            original_price: "106.649",
-                                            discount: 35,
-                                            currency: "\$",
-                                            decimals: "00",
-                                            has_loyalty_discount: false
-                                    ],
-                                    installments: [
-                                            quantity: 12,
-                                            amount: 9784.63,
-                                            no_interest: false,
-                                            currency_id: "ARS",
-                                            decimals: "63",
-                                            price: "9.784"
-                                    ]
-                            ]
-                    ]
-            ]
+            selected_filters = [category_id: "MLA1055"]
         }
 
         "/tendencies"(platform:"/web/desktop", type: TrackType.Event, {
