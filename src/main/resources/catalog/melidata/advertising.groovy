56e1a1964f85b8311f2879bd56c926ccd23b8dd1
Harold Sanchez Balaguera <harold.sanchez@mercadolibre.cl>
2021-06-03 09:53:07 -0400

    Add values of type

--- src/main/resources/catalog/melidata/advertising.groovy
+++ src/main/resources/catalog/melidata/advertising.groovy
@@ -25,7 +25,7 @@ tracks {
     "/advertising/communications"(platform: "/", isAbstract: true) {}
 
     "/advertising/communications"(platform: "/", type: TrackType.Event) {
-        type(required: true, type: PropertyType.String, description: "type of communication action")
+        type(required: true, type: PropertyType.String, description: "type of communication action", values: ['show', 'click'])
         placement(required: true, type: PropertyType.String, description: "communication placement")
         communications(required: true, type: PropertyType.ArrayList, description: "Array of communications available for placement")
     }
