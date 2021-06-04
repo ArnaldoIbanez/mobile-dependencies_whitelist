1b7cad53f99fde9815b1344efd132ae23059db9e
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-06-04 19:24:54 -0300

    Fixing address id property

--- src/test/resources/melidata/addresses.groovy
+++ src/test/resources/melidata/addresses.groovy
@@ -11,26 +11,26 @@ trackTests {
     test("Addresses - Test the views") {
         "/addresses/input_address"(platform: "/", type: TrackType.View) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map"(platform: "/", type: TrackType.View) {
             context = "MY_ML"
-            editing_address = null
+            address_id = null
         }
     }
 
     test("Addresses - Test the views actions") {
         "/addresses/input_address/back"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/submit"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map/back"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
     }
 
@@ -39,122 +39,122 @@ trackTests {
             label = "La cantidad de caracteres ingresados es inválida"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/zip_code"(platform:"/", type: TrackType.Event){
             label = "La cantidad de caracteres ingresados es inválida"
             value = "00"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/street_name"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/street_type"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/street_number"(platform:"/",  type: TrackType.Event){
             label = "Ingresar solo valores numéricos"
             value = "aaa"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/intersection"(platform:"/",  type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/city"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/state"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/colony"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/delivery"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/delivery_instructions"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/phone"(platform:"/", type: TrackType.Event){
             label = "Ingresar solo valores numéricos"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/additional_info"(platform:"/", type: TrackType.Event){
             label = "Revisa este dato"
             value = "!*"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map/error/map"(platform: "/", type: TrackType.Event) {
             label = "Mové el mapa y ubicá el pin en tu ubicación exacta"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
     }
 
     test("Addresses - Test the UX info") {
         "/addresses/input_address/unknown_zip_code"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/unknown_zip_code_submit"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/unknown_zip_code_back"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_street_name"(platform: "/", type: TrackType.Event) {
             written = "sug"
             final_text = "suggested street"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_phone"(platform: "/", type: TrackType.Event) {
             written = "123"
             final_text = "12345678"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/search_replaced_zip_code"(platform: "/", type: TrackType.Event) {
             count = 10.0
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_replaced_zip_code"(platform: "/", type: TrackType.Event) {
             count = 2.0
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_map_position"(platform: "/", type: TrackType.Event) {
             suggested = [
@@ -166,7 +166,7 @@ trackTests {
                 "longitude": "42.40"
             ]
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map/moved_to_my_location"(platform: "/", type: TrackType.Event) { }
         "/addresses/input_address/map/location_permission_granted"(platform: "/", type: TrackType.Event) { }
@@ -181,26 +181,26 @@ trackTests {
     test("Addresses - Test the views") {
         "/addresses/input_address"(platform: "/", type: TrackType.View) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map"(platform: "/", type: TrackType.View) {
             context = "MY_ML"
-            editing_address = null
+            address_id = null
         }
     }
 
     test("Addresses - Test the views actions") {
         "/addresses/input_address/back"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/submit"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map/back"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
     }
 
@@ -209,122 +209,122 @@ trackTests {
             label = "La cantidad de caracteres ingresados es inválida"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/zip_code"(platform:"/", type: TrackType.Event){
             label = "La cantidad de caracteres ingresados es inválida"
             value = "00"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/street_type"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/street_name"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/intersection"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/street_number"(platform:"/",  type: TrackType.Event){
             label = "Ingresar solo valores numéricos"
             value = "aaa"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/state"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/city"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/colony"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/delivery"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/delivery_instructions"(platform:"/", type: TrackType.Event){
             label = "Completa este dato"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/phone"(platform:"/", type: TrackType.Event){
             label = "Ingresar solo valores numéricos"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/error/additional_info"(platform:"/", type: TrackType.Event){
             label = "Revisa este dato"
             value = "!*"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map/error/map"(platform: "/", type: TrackType.Event) {
             label = "Mové el mapa y ubicá el pin en tu ubicación exacta"
             value = ""
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
     }
 
     test("Addresses - Test the UX info") {
         "/addresses/input_address/unknown_zip_code"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/unknown_zip_code_submit"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/unknown_zip_code_back"(platform: "/", type: TrackType.Event) {
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_street_name"(platform: "/", type: TrackType.Event) {
             written = "sug"
             final_text = "suggested street"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_phone"(platform: "/", type: TrackType.Event) {
             written = "123"
             final_text = "12345678"
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/search_replaced_zip_code"(platform: "/", type: TrackType.Event) {
             count = 10.0
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_replaced_zip_code"(platform: "/", type: TrackType.Event) {
             count = 2.0
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/select_map_position"(platform: "/", type: TrackType.Event) {
             suggested = [
@@ -336,7 +336,7 @@ trackTests {
                 "longitude": "42.40"
             ]
             context = "MY_ML"
-            editing_address = 12312312312
+            address_id = 12312312312
         }
         "/addresses/input_address/map/moved_to_my_location"(platform: "/", type: TrackType.Event) { }
         "/addresses/input_address/map/location_permission_granted"(platform: "/", type: TrackType.Event) { }
