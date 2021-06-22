974898e9b1fa666fe327581c6a51032e835cde8a
Martín Pérez <jose.martin@mercadolibre.com>
2021-06-22 14:30:01 -0300

    Add accessibility metrics

--- src/main/resources/catalog/melidata/mobile.groovy
+++ src/main/resources/catalog/melidata/mobile.groovy
@@ -129,6 +129,9 @@ tracks {
         app_data(required: false, type: PropertyType.Numeric, description: "Application data occupied storage in bytes")
         dark_mode_status(required: false, type: PropertyType.String, values: ["enabled", "battery_enabled", "disabled", "undefined"],
          description: "Dark Mode status")
+        font_scale(required: false, type: PropertyType.String, values: ["small", "medium", "large", "xlarge"], description: "Font scale")
+        voice_over(required: false, type: PropertyType.Boolean, description: "Voice over is enabled or not")
+        high_contrast(required: false, type: PropertyType.Boolean, description: "High contrast is enabled or not")
         battery_save_mode(required: false, type: PropertyType.String, values: ["enabled", "disabled"], description: "Battery Save mode")
         data_save_mode(required: false, type: PropertyType.String, values: ["enabled", "disabled", "whitelisted", "undefined"], description: "Data Save mode")
         do_not_disturb_mode(required: false, type: PropertyType.String, values: ["disabled", "important_interruptions", "no_interruptions", "alarms_only", "undefined"], description: "Do Not Disturb mode")
