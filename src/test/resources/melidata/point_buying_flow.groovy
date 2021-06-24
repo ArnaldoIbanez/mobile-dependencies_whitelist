e8dd08cb74d02f30d99442d533cd2879dc8f54e3
ailenzeta <ailen.zaccagnino@mercadolibre.com>
2021-06-24 17:09:36 -0300

    change path and add items - propertyDefinitions

--- src/test/resources/melidata/point_buying_flow.groovy
+++ src/test/resources/melidata/point_buying_flow.groovy
@@ -9,38 +9,56 @@ trackTests {
     test("Point Buying Flow") {
         "/point/buyingflow/start"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           is_guest = true
           has_coupon = true
           discount_code = "GHZM"
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/shipping_options"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/shipping/options"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/new_address"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/shipping/new_address"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_methods"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/payment/methods"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
@@ -48,71 +66,113 @@ trackTests {
           e2e_test = false
           has_account_money = true
           has_consumer_credits = true
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
         
-        "/point/buyingflow/payment_ticket_info"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/payment/ticket_info"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_installments"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/payment/installments"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_installments/installments"(platform: "/", type: TrackType.Event) {
+        "/point/buyingflow/payment/installments/installments"(platform: "/", type: TrackType.Event) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_new_card"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/payment/new_card"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_card_security_code"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/payment/card_security_code"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_bank_selector"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/payment/bank_selector"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_review"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/review"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           is_guest = true
@@ -121,11 +181,17 @@ trackTests {
           discount_code = "GHZM"
           installments = 6
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
         "/point/buyingflow/payment_rejected"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           is_guest = true
@@ -134,79 +200,141 @@ trackTests {
           discount_code = "GHZM"
           installments = 6
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
         
         "/point/buyingflow/error"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           is_guest = true
           discount_code = "GHZM"
           error_code = 6
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/payment_review/confirm_purchase"(platform: "/", type: TrackType.Event) {
+        "/point/buyingflow/review/confirm_purchase"(platform: "/", type: TrackType.Event) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/invalid_address"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/shipping/invalid_address"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/complete_phone"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/shipping/complete_phone"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           discount_code = "GHZM"
           is_guest = true
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/complete_card_data"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/payment/complete_card_data"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           is_guest = true
           discount_code = "GHZM"
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/sim_card"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/shipping/sim_card"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           is_guest = true
           discount_code = "GHZM"
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
 
-        "/point/buyingflow/xsell"(platform: "/", type: TrackType.View) {
+        "/point/buyingflow/shipping/xsell"(platform: "/", type: TrackType.View) {
           flow_id = "83ee2407-1a73-4eca-922d-b07c7904552c"
-          product = "11"
+          product_id = "11"
           currency = "ARS"
           price = 299
           is_guest = true
           discount_code = "GHZM"
           e2e_test = false
+          bu = null
+          ch = null
+          camp = null
+          strategy = null
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
         }
         
         "/point/buyingflow/regret"(platform: "/", type: TrackType.View) {}
+
+        "/point/buyingflow/congrats"(platform: "/", type: TrackType.View) {
+          payment_method = "pagofacil"
+          payment_id 15511951431
+          device_id = "11"
+          amount = 599
+          is_guest = false
+          e2e_test = false
+          discount_code = "POINT_ORG"
+          product = "Merdaco Pago Point Bluetooth"
+          coupon_type = "mgm"
+          product_id = "11"
+          paymentStatus = "rejected"
+        }
     }
 }
