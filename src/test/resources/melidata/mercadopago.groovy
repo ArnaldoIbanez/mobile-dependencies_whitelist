111fbd19e801559546a0ee0f3c7c1b16ddd0dd7f
Nicholas Pedroso <nicholas.pedroso@mercadolivre.com>
2021-06-27 14:04:03 -0300

    CORECH-235 Add Core Checkouts oneclick tracks

--- src/test/resources/melidata/mercadopago.groovy
+++ src/test/resources/melidata/mercadopago.groovy
@@ -4126,4 +4126,15 @@ trackTests {
     test("About screen") {
         "/about/rate_app"(platform:"/mobile", type:TrackType.Event) {}
     }
+
+    test("CHO API webpay onelick") {
+        "/checkout_api/payment/one_click_stop_landing"(platform:"/web/mobile", type:TrackType.View) {}
+        "/checkout_api/payment/one_click_stop_landing/one_click_redirect"(platform:"/web/mobile", type:TrackType.Event) {}
+        "/checkout_api/payment/one_click_stop_landing/seller_redirect"(platform:"/web/mobile", type:TrackType.Event) {}
+
+        "/checkout_api/payment/one_click_return"(platform:"/web/mobile", type:TrackType.View) {}
+        "/checkout_api/payment/one_click_return/token_creation_success"(platform:"/web/mobile", type:TrackType.Event) {}
+        "/checkout_api/payment/one_click_return/webpay_error"(platform:"/web/mobile", type:TrackType.Event) {}
+    }
+    
 }
