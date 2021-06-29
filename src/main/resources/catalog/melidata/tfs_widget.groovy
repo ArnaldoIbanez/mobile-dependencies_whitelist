c067e869c21113b628117d5dcea08395731df02b
Santiago Cobelli <santiago.cobelli@mercadolibre.com>
2021-06-29 11:15:21 -0300

    Modify platform to only accept android

--- src/main/resources/catalog/melidata/tfs_widget.groovy
+++ src/main/resources/catalog/melidata/tfs_widget.groovy
@@ -42,36 +42,36 @@ tracks {
 
     // Abstract paths
 
-    "/widget" (platform: "/mobile", isAbstract: true) { }
-    "/widget/collect" (platform: "/mobile", isAbstract: true) { }
-    "/widget/collect/config" (platform: "/mobile", isAbstract: true) { }
+    "/widget" (platform: "/mobile/android", isAbstract: true) { }
+    "/widget/collect" (platform: "/mobile/android", isAbstract: true) { }
+    "/widget/collect/config" (platform: "/mobile/android", isAbstract: true) { }
 
 
-    "/widget/collect/config/open" (platform: "/mobile", type: TrackType.View) {
+    "/widget/collect/config/open" (platform: "/mobile/android", type: TrackType.View) {
         config_group
     }
-    "/widget/collect/config/success" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/config/success" (platform: "/mobile/android", type: TrackType.Event) {
         config_successful_group
     }
-    "/widget/collect/config/error" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/config/error" (platform: "/mobile/android", type: TrackType.Event) {
         config_group
     }
-    "/widget/collect/config/back" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/config/back" (platform: "/mobile/android", type: TrackType.Event) {
         config_group
     }
-    "/widget/collect/config/save" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/config/save" (platform: "/mobile/android", type: TrackType.Event) {
         use_widget_group
     }
-    "/widget/collect/config/not_logged" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/config/not_logged" (platform: "/mobile/android", type: TrackType.Event) {
         widget_id_group
     }
-    "/widget/collect/tap" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/tap" (platform: "/mobile/android", type: TrackType.Event) {
         use_widget_group
     }
-    "/widget/collect/remove" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/remove" (platform: "/mobile/android", type: TrackType.Event) {
         use_widget_group
     }
-    "/widget/collect/not_logged" (platform: "/mobile", type: TrackType.Event) {
+    "/widget/collect/not_logged" (platform: "/mobile/android", type: TrackType.Event) {
         use_widget_not_logged_group
     }
 
--- src/test/resources/melidata/tfs_widget.groovy
+++ src/test/resources/melidata/tfs_widget.groovy
@@ -10,14 +10,14 @@ trackTests {
     defaultBusiness = "mercadopago"
 
     test("User tries to add a new widget") {
-        "/widget/collect/config/open" (platform: "/mobile", type: TrackType.View) {
+        "/widget/collect/config/open" (platform: "/mobile/android", type: TrackType.View) {
             widget_id = 1
             is_collaborator = false
         }
     }
 
     test("User adds widget to homescreen") {
-        "/widget/collect/config/success"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/config/success"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
             is_collaborator = true
             collect_count = 0
@@ -25,21 +25,21 @@ trackTests {
     }
 
     test("Error trying to add a widget") {
-        "/widget/collect/config/error"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/config/error"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
             is_collaborator = true
         }
     }
 
     test("User exist config page using back button") {
-        "/widget/collect/config/back"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/config/back"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
             is_collaborator = true
         }
     }
 
     test("User saves widget configuration") {
-        "/widget/collect/config/save"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/config/save"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
             collect_method = "point"
             is_collaborator = false
@@ -47,13 +47,13 @@ trackTests {
     }
 
     test("User tries tu add a new widget not being logged in") {
-        "/widget/collect/config/not_logged"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/config/not_logged"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
         }
     }
 
     test("User taps payment button") {
-        "/widget/collect/tap"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/tap"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
             collect_method = "point"
             is_collaborator = false
@@ -61,7 +61,7 @@ trackTests {
     }
 
     test("User removes widget from homescreen") {
-        "/widget/collect/remove"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/remove"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
             collect_method = "qr"
             is_collaborator = false
@@ -69,7 +69,7 @@ trackTests {
     }
 
     test("User taps widget not being logged in") {
-        "/widget/collect/not_logged"(platform: "/mobile", type: TrackType.Event) {
+        "/widget/collect/not_logged"(platform: "/mobile/android", type: TrackType.Event) {
             widget_id = 1
             collect_method = "point"
         }
