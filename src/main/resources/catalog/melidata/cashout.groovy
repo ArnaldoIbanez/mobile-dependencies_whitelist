3677984d64225eda629fa63f3cbd242af0148443
Julio Galeano <julio.galeano@mercadolibre.com.co>
2021-07-08 12:06:55 -0500

    Hub Modal Cashout tracks added

--- src/main/resources/catalog/melidata/cashout.groovy
+++ src/main/resources/catalog/melidata/cashout.groovy
@@ -29,6 +29,10 @@ tracks {
     "/cashout/hub/congrats/success/continue"(platform: "/", type: TrackType.Event) {}
     "/cashout/hub/congrats/success/close"(platform: "/", type: TrackType.Event) {}
 
+    "/cashout/modal"(platform: "/", type: TrackType.View) {}
+    "/cashout/modal/close"(platform: "/", type: TrackType.Event) {}
+    "/cashout/modal/withdraw"(platform: "/", type: TrackType.Event) {}
+
     "/cashout/extracash"(platform: "/", isAbstract: true) {}
     "/cashout/extracash/onboarding"(platform: "/", type: TrackType.View) {}
     "/cashout/extracash/onboarding/continue"(platform: "/", type: TrackType.Event) {}
--- src/test/resources/melidata/cashout.groovy
+++ src/test/resources/melidata/cashout.groovy
@@ -31,6 +31,10 @@ trackTests {
     "/cashout/hub/congrats/success/continue"(platform: "/", type: TrackType.Event) {}
     "/cashout/hub/congrats/success/close"(platform: "/", type: TrackType.Event) {}
 
+    "/cashout/modal"(platform: "/", type: TrackType.View) {}
+    "/cashout/modal/close"(platform: "/", type: TrackType.Event) {}
+    "/cashout/modal/withdraw"(platform: "/", type: TrackType.Event) {}
+
     "/cashout/extracash"(platform: "/") {}
     "/cashout/extracash/onboarding"(platform: "/", type: TrackType.View) {}
     "/cashout/extracash/onboarding/continue"(platform: "/", type: TrackType.Event) {}
