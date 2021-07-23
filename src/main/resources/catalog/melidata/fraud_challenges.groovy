414d33d9e94ff5d44ba4e99dfab52971dabb51bc
Bruno Strasser <bruno.strasser@mercadolibre.com>
2021-07-23 16:48:28 -0300

    Add initiative

--- src/main/resources/catalog/melidata/fraud_challenges.groovy
+++ src/main/resources/catalog/melidata/fraud_challenges.groovy
@@ -4,6 +4,8 @@ import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
 
 tracks {
+
+    initiative = '1100'
     
     "/auth/card"(platform: "/", isAbstract: true) {}
     "/auth/card/validation"(platform: "/", type: TrackType.Event) {}
