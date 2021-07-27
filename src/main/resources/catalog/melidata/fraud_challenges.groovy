659e67e4b46761b241d3b86b26100f450b70aca1
Bruno Strasser <bruno.strasser@mercadolibre.com>
2021-07-27 13:08:14 -0300

    Add event properties

--- src/main/resources/catalog/melidata/fraud_challenges.groovy
+++ src/main/resources/catalog/melidata/fraud_challenges.groovy
@@ -8,6 +8,13 @@ tracks {
     initiative = '1100'
     
     "/auth/card"(platform: "/", isAbstract: true) {}
-    "/auth/card/validation"(platform: "/", type: TrackType.Event) {}
-    "/auth/card/validation"(platform: "/", type: TrackType.View) {}
+    "/auth/card/validation/"(platform: "/", type: TrackType.View) {}
+    "/auth/card/validation/"(platform: "/", type: TrackType.Event) {
+        challenge(type: PropertyType.String, required: true, description: "The name of the challenge")
+        status(type: PropertyType.String, required: false, description: "The status of the challenge")
+        site(type: PropertyType.String, required: false, description: "The site of the user")
+        marketplace(type: PropertyType.String, required: false, description: "Marketplace on/off of the payment")
+        bank(type: PropertyType.String, required: false, description: "The bank of the card")
+        payment_method_id(type: PropertyType.String, required: false, description: "The payment method. i.e visa, master, etc")
+    }
 }
--- src/test/resources/melidata/fraud_challenges.groovy
+++ src/test/resources/melidata/fraud_challenges.groovy
@@ -9,15 +9,29 @@ trackTests {
     defaultBusiness = "mercadolibre"
 
     test("Test auth card validation ml") {
-        "/auth/card/validation"(platform: "/", type:TrackType.View) {}
-        "/auth/card/validation"(platform: "/", type:TrackType.Event) {}
+        "/auth/card/validation/"(platform: "/", type:TrackType.View) {}
+        "/auth/card/validation/"(platform: "/", type:TrackType.Event) {
+            challenge = "test"
+            status = "started"
+            site = "mlb"
+            marketplace = "on"
+            bank = "nubank"
+            payment_method_id = "visa"
+        }
     }
 
     defaultBusiness = "mercadopago"
 
     test("Test auth card validation mp") {
-        "/auth/card/validation"(platform: "/", type:TrackType.View) {}
-        "/auth/card/validation"(platform: "/", type:TrackType.Event) {}
+        "/auth/card/validation/"(platform: "/", type:TrackType.View) {}
+        "/auth/card/validation/"(platform: "/", type:TrackType.Event) {
+            challenge = "test"
+            status = "started"
+            site = "mlb"
+            marketplace = "on"
+            bank = "nubank"
+            payment_method_id = "visa"
+        }
     }
 
 }
