ec8607c57d4bca044c31011eb912747c2f08255d
GerardoCaceres <gerardo.caceres@mercadolibre.com>
2021-07-30 13:00:23 -0300

    fix: property

--- src/main/resources/catalog/melidata/payments_digital_accounts.groovy
+++ src/main/resources/catalog/melidata/payments_digital_accounts.groovy
@@ -114,7 +114,7 @@ tracks {
 
     // Balance Views
     "/banking/balance"(platform: "/", type: TrackType.View) { componentsViews }
-    "/banking/to_release"(platform: "/", type: TrackType.View) {}
+    "/banking/to_release"(platform: "/", type: TrackType.View) { componentsViews }
     "/banking/calendar"(platform: "/", type: TrackType.View) {}
     "/banking/activities"(platform: "/", type: TrackType.View) {}
     "/banking/cerc"(platform: "/", type: TrackType.View) {}
