2d59c90efc0a1b14d12022bba9c181fb6b5d2444
Ignacio Musacchio <musacchio.ignacio@gmail.com>
2021-08-05 12:46:54 -0300

    remove point buyingflow tracks

--- src/main/resources/catalog/melidata/mercadopago.groovy
+++ src/main/resources/catalog/melidata/mercadopago.groovy
@@ -20,21 +20,6 @@ import com.ml.melidata.TrackType
 /**/
 tracks {
 
-    propertyDefinitions {
-        flow_id (type: PropertyType.String, required: true, description: "Flow ID")
-        product (type: PropertyType.String, required: true, description: "Product identifier")
-        currency (type: PropertyType.String, required: true, description: "ISO Currency")
-        price (type: PropertyType.Numeric, required: true, description: "Price of device")
-        discount_code (type: PropertyType.String, required: true, description: "Discount code")
-        is_guest (type: PropertyType.Boolean, required: true, description: "User logged as guest")
-        user_id (type: PropertyType.Numeric, required: true, description: "User ID")
-        e2e_test (type: PropertyType.Boolean, required: true, description: "e2e Test")
-    }
-
-    propertyGroups {
-        groupCheckoutProperties(flow_id, product, currency, price, discount_code, is_guest, user_id, e2e_test)
-    }
-
     "/"(platform: "/", isAbstract: true) {
     }
 
@@ -116,33 +101,6 @@ tracks {
         type (required: true, type: PropertyType.String, description: "success")
         action (required: true, type: PropertyType.String, description: "open")
     }
-
-    // Point Flows
-    "/point/flows"(platform: "/", isAbstract: true) {}
-    "/point/flows/congrats"(platform:"/", type: TrackType.View) {
-        payment_id (required: true, type: PropertyType.Numeric, description: "ID of payment")
-        payment_method (required: true, type: PropertyType.String, description: "Method of payment")
-        device_id (required: true, type: PropertyType.String, description: "ID of Point device")
-        amount (required: true, type: PropertyType.Numeric, description: "Ticket amount")
-        is_guest (required: true, type: PropertyType.Boolean, description: "Guest user flag")
-        e2e_test (required: true, type: PropertyType.Boolean, description: "e2e Test")
-        discount_code (required: true, type: PropertyType.String, description: "Discount code")
-    }
-
-    "/point/flows/congrats/instructions"(platform:"/", type: TrackType.View) {}
-
-    // Point Flows Events
-    //Congrats ON
-    "/point/flows/congrats/print"(platform:"/", type: TrackType.Event) {}
-    "/point/flows/congrats/copy"(platform:"/", type: TrackType.Event) {}
-    "/point/flows/congrats/map"(platform:"/", type: TrackType.Event) {}
-    "/point/flows/congrats/continue"(platform:"/", type: TrackType.Event) {}
-
-    //congrats OFF
-    "/point/flows/congrats/instructions/continue"(platform:"/", type: TrackType.Event) {}
-    "/point/flows/congrats/instructions/print"(platform:"/", type: TrackType.Event) {}
-    "/point/flows/congrats/instructions/copy"(platform:"/", type: TrackType.Event) {}
-    "/point/flows/congrats/instructions/map"(platform:"/", type: TrackType.Event) {}
 
 
     //Point Devices
