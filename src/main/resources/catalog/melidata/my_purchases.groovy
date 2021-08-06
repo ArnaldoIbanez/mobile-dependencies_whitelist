d776ffe6f62c58e603777608f8cabd1155539db2
Federico Hertzulis <federico.hertzulis@mercadolibre.com>
2021-08-06 13:02:46 -0300

    Update my purchases events

--- src/main/resources/catalog/melidata/my_purchases.groovy
+++ src/main/resources/catalog/melidata/my_purchases.groovy
@@ -9,50 +9,11 @@ tracks {
     initiative = "1198"
 
     propertyDefinitions {
-
         items(required: true, type: PropertyType.ArrayList, description: "Items in this purchase")
-            // business
-            // page_vertical
-            // domain
-            // category_l1
-            // category_l2
-            // category_l3 (optional)
-            // category_l4 (optional)
-            // category_l5 (optional)
-            // listing_type
-            // item_id
-            // quantity
-            // variation_id (optional)
-            // condition
-            // product_id (optional)
-            // deals (optional)
-
         payments(required: false, type: PropertyType.ArrayList, description: "Payments in this purchase")
-            // payment_method_type
-            // payment_method_id
-            // payment_status
-            // payment_status_detail
-
         shipping(required: false, type: PropertyType.ArrayList, description: "Shipments in this purchase")
-        	// shipping_method
-        	// shipping_mode
-        	// logistic_type
-        	// shipping_status
-        	// shipping_sub_status
-
         seller(required: true, type:PropertyType.ArrayList, description: "Array of sellers with their data")
-		    // id
-		    // nickname
-		    // mercado_lider
-		    // reputation_level
-	        // messages_count
-
 	    buyer(required: true, type:PropertyType.ArrayList, description: "Array of buyers with their data")
-		    // id
-		    // nickname
-		    // loyalty_level
-	        // is_prime
-
     	purchase_status(required: true, type: PropertyType.String, description: "Purchase status")
     	purchases_flow(required: true, type: PropertyType.String, description: "Flow identification to know if it is the new or old flow")
     	checkout_flow(required: false, type: PropertyType.String, values: ["pack", "order"], description: "cart (pack) or direct (order) purchase")
@@ -70,6 +31,7 @@ tracks {
 
     propertyGroups {
         newPurchasesGroup(items, payments, shipping, seller, buyer, purchases_flow, purchase_status, checkout_flow, pack_ids, order_ids, garex, vertical_case_id, vertical_sub_case_id, x_mc_request_id, purchase_id, pack_id, order_id)
+        newPurchasesEventGroupFull(label, x_mc_request_id, vertical_case_id, vertical_sub_case_id, items, payments, shipping, seller, buyer, purchases_flow, purchase_status, checkout_flow, pack_ids, order_ids, garex, vertical_case_id, vertical_sub_case_id, x_mc_request_id, purchase_id, pack_id, order_id)
         newPurchasesEventGroup(label, x_mc_request_id, vertical_case_id, vertical_sub_case_id)
     }
 
@@ -87,31 +49,31 @@ tracks {
     }
 
     "/my_purchases/status/show_action"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
     "/my_purchases/status/show_help"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
     "/my_purchases/status/show_faq"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
     "/my_purchases/status/click_action"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
     "/my_purchases/status/click_help"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
     "/my_purchases/status/click_faq"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
     "/my_purchases/status/click_view_item"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
     "/my_purchases/status/click_view_messages"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
@@ -141,7 +103,7 @@ tracks {
     "/my_purchases/status/items"(platform:"/", type: TrackType.View) {}
 
 	"/my_purchases/status/items/click_view_item"(platform:"/", type: TrackType.Event, parentPropertiesInherited: false) {
-        newPurchasesEventGroup
+        newPurchasesEventGroupFull
     }
 
 
