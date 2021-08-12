8b5cb515cf9ed9c0eaeec8585b182e8dd0f5f620
costarilom <mariela.costa@mercadolibre.com>
2021-08-12 16:33:22 -0300

    rollback props

--- src/main/resources/catalog/melidata/notifications.groovy
+++ src/main/resources/catalog/melidata/notifications.groovy
@@ -1683,83 +1683,34 @@ tracks {
        * NOTIFICATIONS TRAY
        **/
       "/notification"(platform: "/") {
-        event_type(required: true,
-            type: PropertyType.String,
-            name: PropertyType.String,
-            values: ["sent", "resent", "arrived", "received", "dismiss", "discarded", "open", "auto_dismiss", "shown", "action_open", "control", "carousel","purged_token", "swipe"],
-            description: "Event type")
-
-        action_type(required: false,
-            type: PropertyType.String,
-            name: PropertyType.String,
-            values: ["deeplinking", "directions", "favorite", "reply", "ask", "postpone", "twitter_bar", "picture", "answer", "messages", "vop", "claims", "received", "tracking", "shipping_print_label", "feedback", "buy"],
-            description: "Action type")
-          
-        notification_type(required: false,
-            type: PropertyType.String,
-            name: PropertyType.String,
-            values: ["deep_linking", "directions", "favorite", "reply", "ask", "postpone", "twitter_bar", "picture", "answer", "messages", "vop", "claims", "received", "tracking", "shipping_print_label", "feedback", "buy"],
-            description: "Type of notification")
-            
-        deeplink(required: false, 
-            type: PropertyType.String,
-            name: PropertyType.String,
-            description: "The link were the notification should navigate to, if applies")
+          event_type(required: true,
+                  values: ["sent", "resent", "arrived", "received", "dismiss", "discarded", "open", "auto_dismiss", "shown", "action_open", "control", "carousel","purged_token", "swipe"],
+          description: "Type of notification event")
+          action_type(required: false,
+                  values: ["deeplinking", "directions", "favorite", "reply", "ask", "postpone", "twitter_bar", "picture", "answer", "messages", "vop", "claims", "received", "tracking", "shipping_print_label", "feedback", "buy"])
+          notification_type(required: false,
+              values: ["deep_linking", "directions", "favorite", "reply", "ask", "postpone", "twitter_bar", "picture", "answer", "messages", "vop", "claims", "received", "tracking", "shipping_print_label", "feedback", "buy"])
+          deeplink(required: false, description: "The link were the notification should navigate to, if applies")
 
           //For event_type:autodismiss, indicates why the notification was dismissed
-        source(required: false,
-            type: PropertyType.String,
-            name: PropertyType.String,
-            values: ["notification_center","logout","overwrite","dismiss_notification"],
-            description: "Source of notification")
+          source(required: false,
+                 values: ["notification_center","logout","overwrite","dismiss_notification"])
 
-        discard_reason(required: false, 
-            name: PropertyType.String,
-            description: "The discarded reason of the notification", values: ["invalid_payload","invalid_user", "settings_disabled"], 
-            type: PropertyType.String)
+          discard_reason(required: false, description: "The discarded reason of the notification", values: ["invalid_payload","invalid_user", "settings_disabled"], type: PropertyType.String)
 
-        notification_created_error(required: false, 
-            name: PropertyType.String,
-            description: "The notification created error", 
-            type: PropertyType.String)
+          notification_created_error(required: false, description: "The notification created error", type: PropertyType.String)
 
-        news_id(required: false, 
-            type: PropertyType.String,
-            name: PropertyType.String,
-            description: "Identifier of the notification generated")
+          news_id(required: false, description: "Identifier of the notification generated")
+          notification_style(required: false, description: "The notification style used when displaying the notification to the user.")
 
-        notification_style(required: false, 
-            type: PropertyType.String,
-            name: PropertyType.String,
-            description: "The notification style used when displaying the notification to the user.")
+          status(required: false, values: ["read", "unread"], deprecated: true, description: "*Deprecated*: Just for old NotifCenter.")
 
-        status(required: false, 
-            type: PropertyType.String,
-            name: PropertyType.String,
-            values: ["read", "unread"], 
-            deprecated: true, 
-            description: "*Deprecated*: Just for old NotifCenter.")
+          device_id(required: false, description: "The real device_id, may differ from device field")
+          device_status(required: false, values: ["active", "not_engaged"], description: "Device status at the moment")
 
-        device_id(required: false, 
-            type: PropertyType.String,
-            name: PropertyType.String,
-            description: "The real device_id, may differ from device field")
+          context(required: false, type: PropertyType.String)
 
-        device_status(required: false, 
-            type: PropertyType.String,
-            name: PropertyType.String,
-            values: ["active", "not_engaged"], 
-            description: "Device status at the moment")
-
-        context(required: false, 
-            name: PropertyType.String,
-            type: PropertyType.String,
-            description: "Context")
-
-        send_health_check(required: false, 
-            name: PropertyType.String,
-            type: PropertyType.Boolean, 
-            description: "Indicates if notification has associated a health check's sent")
+          send_health_check(required: false, type: PropertyType.Boolean, description: "Indicates if notification has associated a health check's sent")
       }
 
     //Anses
