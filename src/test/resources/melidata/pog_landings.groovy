70cd7de507158a8ba3c19fda4c9b80eedad975a6
María Eugenia Carlini <eugenia.carlini@mercadolibre.com>
2021-08-12 17:59:56 -0300

    Fix tests

--- src/test/resources/melidata/pog_landings.groovy
+++ src/test/resources/melidata/pog_landings.groovy
@@ -86,12 +86,10 @@ trackTests {
       // [Point Online Growth] Landing Multiproduct > Webview experiment
       "/pog/landings/multiproduct/go_taxes_section" (platform:"/", type: TrackType.Event) {
         product = "multiproduct MLB"
-        experiments = ["1234"]
       }
 
       "/pog/landings/multiproduct/device/more_information" (platform:"/", type: TrackType.Event) {
         product = "point-pro-2"
-        experiments = ["1234"]
       }
     }
 }
