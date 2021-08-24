e646e620f747daac073845e3230bd9aa5181edad
Germán E. Trouillet <german.trouillet@mercadolibre.com>
2021-08-24 15:16:09 -0300

    Test catalog definition

--- src/main/resources/catalog/mlo-mon-fda-test-app/tracking.groovy
+++ src/main/resources/catalog/mlo-mon-fda-test-app/tracking.groovy
@@ -5,7 +5,21 @@ import com.ml.melidata.catalog.PropertyType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
 tracks {
-
+    
     "/"(platform: "/") {}
+
+    "/input"(platform: "/api") { 
+	    record(type: PropertyType.Map(record_info), required:true)
+    }
+
+    def record_info = objectSchemaDefinitions {
+        data(type: PropertyType.Map(record_data), required:true)
+    }
+
+    def record_data = objectSchemaDefinitions {
+        user_id(type: PropertyType.Numeric, required: true)
+        is_payer(type: PropertyType.Boolean, required: false)
+        status(type: PropertyType.String, values: ["OK", "ERROR"], required: true)
+    }
 
 }
