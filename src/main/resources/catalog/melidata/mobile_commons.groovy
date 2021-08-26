3234338be2e96ebbb6201d2b6710bf19f3a9264f
abruno- <bruno.ardanaz@mercadolibre.com>
2021-08-26 15:56:52 -0300

    Apply suggestions from code review
    Co-authored-by: Martin <mciruzzi@users.noreply.github.com>

--- src/main/resources/catalog/melidata/mobile_commons.groovy
+++ src/main/resources/catalog/melidata/mobile_commons.groovy
@@ -57,8 +57,8 @@ tracks {
     }    
     "/in_app_updates/updatable/update_not_available"(platform: "/mobile", type: TrackType.Event){
         cause (required: true, type: PropertyType.String, description: "why we dont show the dialog of update")
-        type (required: false, type: PropertyType.String, description: "type of in-app-update FLEXIBLE | IMMEDIATE | LEGACY")
-        version (required: false, type: PropertyType.String, description: "v1 if bump legacy, v2 if in-app-update implemented")
+        type (required: trye, type: PropertyType.String, description: "type of in-app-update FLEXIBLE | IMMEDIATE | LEGACY", values:[ "FLEXIBLE" , "IMMEDIATE", "LEGACY"] )
+        version (required: true, type: PropertyType.String, description: "v1 if bump legacy, v2 if in-app-update implemented")
     }
     "/in_app_updates/updatable/installed"(platform: "/mobile", type: TrackType.Event){
         version (required: true, type: PropertyType.String, description: "v1 if bump legacy, v2 if in-app-update implemented")
