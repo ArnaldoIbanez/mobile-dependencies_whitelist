8dc181640b9b28af8e8f298395f445edc1f89df5
Julio Melo <julio.vmelo@mercadolivre.com>
2021-08-27 15:37:54 -0300

    feat: add new tracks and fix olders tracks

--- src/main/resources/catalog/melidata/point_loyalty.groovy
+++ src/main/resources/catalog/melidata/point_loyalty.groovy
@@ -7,7 +7,9 @@ tracks {
 
     "/point/loyalty/register/congrats"(platform:"/web", type: TrackType.View) { }
 
-    "/point/loyalty/register"(platform: "/web", type: TrackType.View) {}
+    "/point/loyalty/register/status"(platform: "/web", type: TrackType.View) {
+        status(required: true, type: PropertyType.Numeric, description: "Status of token user")
+    }
 
     "/point/loyalty/register/failure"(platform: "/web", type: TrackType.Event) {
         error_message(required: true, type: PropertyType.String, description: "Generic error message")
@@ -18,9 +20,42 @@ tracks {
         error_message(required: true, type: PropertyType.String, description: "error message")
     }
 
-    "/point/loyalty/register/faq"(platform: "/web", type: TrackType.Event) { 
+    "/point/loyalty/register/faq"(platform: "/web", type: TrackType.Event) { }
+
+    "/point/loyalty/register/tyc"(platform: "/web", type: TrackType.Event) { }
+
+    "/point/loyalty/phone_register"(platform: "/web", type: TrackType.View) { }
+    
+    "/point/loyalty/cashback_exchange"(platform: "/web", type: TrackType.View) { }
+
+    "/point/loyalty/feedback_sms"(platform: "/web", type: TrackType.Event) { }
+
+    "/point/loyalty/cashback_modal"(platform: "/web", type: TrackType.Event) { 
+        context(required: true, type: PropertyType.String, description: "Page, section or flow origin")
+
     }
 
-    "/point/loyalty/register/tyc"(platform: "/web", type: TrackType.Event) {     
+    "/point/loyalty/cashback_tooltip"(platform: "/web", type: TrackType.Event) {
+        context(required: true, type: PropertyType.String, description: "Page, section or flow origin")
+     }
+
+    "/point/loyalty/cashback_continue"(platform: "/web", type: TrackType.Event) {
+        context(required: true, type: PropertyType.String, description: "Page, section or flow origin")
+    }
+
+    "/point/loyalty/cashback_apply"(platform: "/web", type: TrackType.Event) {
+        context(required: true, type: PropertyType.String, description: "Page, section or flow origin")
+    }
+
+    "/point/loyalty/cashback_quit"(platform: "/web", type: TrackType.Event) {
+        context(required: true, type: PropertyType.String, description: "Page, section or flow origin")
+    }
+
+    "/point/loyalty/customer_change"(platform: "/web", type: TrackType.Event) {
+        context(required: true, type: PropertyType.String, description: "Page, section or flow origin")
+    }
+
+    "/point/loyalty/quit_customer"(platform: "/web", type: TrackType.Event) {
+        context(required: true, type: PropertyType.String, description: "Page, section or flow origin")
     }
 }
--- src/test/resources/melidata/point_loyalty.groovy
+++ src/test/resources/melidata/point_loyalty.groovy
@@ -10,8 +10,16 @@ trackTests {
         "/point/loyalty/register/congrats"(platform: "/web", type: TrackType.View) {}
     }
 
-     test("Point loyalty register view") {
-        "/point/loyalty/register"(platform: "/web", type: TrackType.View) {}
+    test("Point loyalty register view") {
+        "/point/loyalty/register/status"(platform: "/web", type: TrackType.View) {
+            status = 1
+        }
+    }
+
+    test("Point loyalty register view token expired") {   
+        "/point/loyalty/register/status"(platform: "/web", type: TrackType.View) {
+            status = 2
+        }
     }
 
     test("Point loyalty generic error") {
@@ -34,6 +42,63 @@ trackTests {
 
     test("Point loyalty tyc") {
         "/point/loyalty/register/tyc"(platform: "/web", type: TrackType.Event) {
+        }
+    }
+
+    test("Point loyalty phone register view") {   
+        "/point/loyalty/phone_register"(platform: "/web", type: TrackType.View) {
+        }
+    }
+
+    test("Point loyalty cashback exchange view") {   
+        "/point/loyalty/cashback_exchange"(platform: "/web", type: TrackType.View) {
+        }
+    }
+
+    test("Point loyalty feedback sms") {   
+        "/point/loyalty/feedback_sms"(platform: "/web", type: TrackType.Event) {
+        }
+    }
+
+    test("Point loyalty modal component in cashback exchange view") {   
+        "/point/loyalty/cashback_modal"(platform: "/web", type: TrackType.Event) {
+            context = "/cashback-exchange"
+        }
+    }
+
+    test("Point loyalty tooltip component in cashback exchange view") {   
+        "/point/loyalty/cashback_tooltip"(platform: "/web", type: TrackType.Event) {
+            context = "/cashback-exchange"
+        }
+    }
+
+    test("Point loyalty continue with payment whitout cashback in cashback exchange view") {   
+        "/point/loyalty/cashback_continue"(platform: "/web", type: TrackType.Event) {
+            context = "/cashback-exchange"
+        }
+    }
+
+    test("Point loyalty continue with payment with cashback in cashback exchange view") {   
+        "/point/loyalty/cashback_apply"(platform: "/web", type: TrackType.Event) {
+            context = "/cashback-exchange"
+        }
+    }
+
+    test("Point loyalty quit cashback in cashback exchange view") {   
+        "/point/loyalty/cashback_quit"(platform: "/web", type: TrackType.Event) {
+            context = "/cashback-exchange"
+        }
+    }
+
+    test("Point loyalty change customer") {   
+        "/point/loyalty/customer_change"(platform: "/web", type: TrackType.Event) {
+            context = "/cashback-exchange"
+        }
+    }
+
+    test("Point loyalty quit customer") {   
+        "/point/loyalty/quit_customer"(platform: "/web", type: TrackType.Event) {
+            context = "/cashback-exchange"
         }
     }
 }
