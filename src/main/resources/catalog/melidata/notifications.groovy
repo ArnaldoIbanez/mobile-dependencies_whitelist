3a03abb3588d45812957b347a957dd8e7aceb9d3
Agustina <agustina.amachalani@mercadolibre.com>
2021-08-31 10:39:42 -0300

    tracks for paygo pushes:  device number success and fail

--- src/main/resources/catalog/melidata/notifications.groovy
+++ src/main/resources/catalog/melidata/notifications.groovy
@@ -1560,6 +1560,14 @@ tracks {
         latest_news_type(required: true, type: PropertyType.String, description: "Corresponds to the type of the latest news of the newsgroup that is showing.")
         latest_news_id(required: true, type: PropertyType.String, description:"Corresponds to the id of the latest news of the newsgroup that is showing.")
     }
+    "/notification_center/single_player_payg_change_device_number_success_mp"(platform: "/", type: TrackType.Event) {
+        latest_news_type(required: true, type: PropertyType.String, description: "Corresponds to the type of the latest news of the newsgroup that is showing.")
+        latest_news_id(required: true, type: PropertyType.String, description:"Corresponds to the id of the latest news of the newsgroup that is showing.")
+    }
+    "/notification_center/single_player_payg_change_device_number_fail_mp"(platform: "/", type: TrackType.Event) {
+        latest_news_type(required: true, type: PropertyType.String, description: "Corresponds to the type of the latest news of the newsgroup that is showing.")
+        latest_news_id(required: true, type: PropertyType.String, description:"Corresponds to the id of the latest news of the newsgroup that is showing.")
+    }
     "/notification_center/single_player_scheduled_payment_utility"(platform: "/", type: TrackType.Event) {
         latest_news_type(required: true, type: PropertyType.String, description: "Corresponds to the type of the latest news of the newsgroup that is showing.")
         latest_news_id(required: true, type: PropertyType.String, description:"Corresponds to the id of the latest news of the newsgroup that is showing.")
@@ -3446,6 +3454,8 @@ tracks {
     "/notification/single_player_payg_create_device_fail_mp"(platform: "/") {}
     "/notification/single_player_payg_blocked_device_mp"(platform: "/mobile") {}
     "/notification/single_player_payg_cashback_mp"(platform: "/mobile") {}
+    "/notification/single_player_payg_change_device_number_success_mp"(platform: "/mobile") {}
+    "/notification/single_player_payg_change_device_number_fail_mp"(platform: "/mobile") {}
     "/notification/single_player_scheduled_payment_utility_grouped_rejected"(platform: "/mobile") {}
     "/notification/single_player_scheduled_payment_utility_grouped_reminder"(platform: "/mobile") {}
     "/notification/single_player_scheduled_payment_utility_grouped_success"(platform: "/mobile") {}
--- src/test/resources/melidata/notifications.groovy
+++ src/test/resources/melidata/notifications.groovy
@@ -2066,8 +2066,16 @@ trackTests {
             latest_news_id= "single_player_payg_blocked_device_mp-143816022-MLB1013046714"
         }
         "/notification_center/single_player_payg_cashback_mp"(platform: "/", type: TrackType.Event) {
-            latest_news_type= "single_player_payg_cashback_device_mp"
+            latest_news_type= "single_player_payg_cashback_mp"
             latest_news_id= "single_player_payg_cashback_mp-143816022-MLB1013046714"
+        }
+        "/notification_center/single_player_payg_change_device_number_success_mp"(platform: "/", type: TrackType.Event) {
+            latest_news_type= "single_player_payg_change_device_number_success_mp"
+            latest_news_id= "single_player_payg_change_device_number_success_mp-143816022-MLB1013046714"
+        }
+        "/notification_center/single_player_payg_change_device_number_fail_mp"(platform: "/", type: TrackType.Event) {
+            latest_news_type= "single_player_payg_change_device_number_fail_mp"
+            latest_news_id= "single_player_payg_change_device_number_fail_mp-143816022-MLB1013046714"
         }
         "/notification_center/single_player_scheduled_payment_utility"(platform: "/", type: TrackType.Event) {
             latest_news_type= "single_player_scheduled_payment_utility"
@@ -6352,6 +6360,14 @@ trackTests {
             news_id = "single_player_payg_cashback_mp_123"
             event_type = "open"
         }
+        "/notification/single_player_payg_change_device_number_success_mp"(platform: "/mobile") {
+            news_id = "single_player_payg_change_device_number_success_mp_123"
+            event_type = "open"
+        }
+        "/notification/single_player_payg_change_device_number_fail_mp"(platform: "/mobile") {
+            news_id = "single_player_payg_change_device_number_fail_mp_123"
+            event_type = "open"
+        }
         "/notification/single_player_scheduled_payment_utility_grouped_rejected"(platform: "/mobile") {
             news_id = "single_player_scheduled_payment_utility_grouped_rejected"
             event_type = "open"
@@ -8503,6 +8519,14 @@ trackTests {
             latest_news_type= "single_player_payg_cashback_mp"
             latest_news_id= "single_player_payg_cashback_mp-143816022-MLB1013046714"
         }
+        "/notification_center/single_player_payg_change_device_number_success_mp"(platform: "/", type: TrackType.Event) {
+            latest_news_type= "single_player_payg_change_device_number_success_mp"
+            latest_news_id= "single_player_payg_change_device_number_success_mp-143816022-MLB1013046714"
+        }
+        "/notification_center/single_player_payg_change_device_number_fail_mp"(platform: "/", type: TrackType.Event) {
+            latest_news_type= "single_player_payg_change_device_number_fail_mp"
+            latest_news_id= "single_player_payg_change_device_number_fail_mp-143816022-MLB1013046714"
+        }
         "/notification_center/single_player_scheduled_payment_utility"(platform: "/", type: TrackType.Event) {
             latest_news_type= "single_player_scheduled_payment_utility"
             latest_news_id= "single_player_scheduled_payment_utility-143816022-MLB1013046714"
@@ -10304,6 +10328,14 @@ trackTests {
         }
         "/notification/single_player_payg_cashback_mp"(platform: "/mobile") {
             news_id = "single_player_payg_cashback_mp_123"
+            event_type = "open"
+        }
+        "/notification/single_player_payg_change_device_number_success_mp"(platform: "/mobile") {
+            news_id = "single_player_payg_change_device_number_success_mp_123"
+            event_type = "open"
+        }
+        "/notification/single_player_payg_change_device_number_fail_mp"(platform: "/mobile") {
+            news_id = "single_player_payg_change_device_number_fail_mp_123"
             event_type = "open"
         }
         "/notification/single_player_scheduled_payment_utility_grouped_rejected"(platform: "/mobile") {
