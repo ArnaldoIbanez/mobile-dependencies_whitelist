bac61d44f1c356ad4b65c04ea0175cb06089ee66
gcondori2 <german.condori@mercadolibre.com>
2021-09-22 13:00:07 -0300

    change path name

--- src/main/resources/catalog/melidata/notifications.groovy
+++ src/main/resources/catalog/melidata/notifications.groovy
@@ -3725,6 +3725,6 @@ tracks {
         latest_news_type(required: true, type: PropertyType.String, description: "Corresponds to the type of the latest news of the newsgroup that is showing.")
         latest_news_id(required: true, type: PropertyType.String, description: "Corresponds to the id of the latest news of the newsgroup that is showing.")
     }
-    "/notification/moneyin_debin_approved"(platform: "/") {}
+    "/notification/moneyin_cca_approved"(platform: "/") {}
     "/notification/moneyin_cca_rejected"(platform: "/") {}
 }
