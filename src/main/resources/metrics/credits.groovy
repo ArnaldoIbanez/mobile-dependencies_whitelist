5e2354dbf5fcee0df44afe0526bee452e1789ae5
Gabriel Tassone <gabriel.tassone@mercadolibre.com>
2021-09-23 10:47:07 -0300

    Adds credits merchant enrollment, express money and open market metrics

--- src/main/resources/metrics/credits.groovy
+++ src/main/resources/metrics/credits.groovy
@@ -181,4 +181,193 @@ metrics {
             } 
         }
     }
+
+    "credits_merchant_enrollment_entries.hub"(description: "Credits merchant hub entries under enrollmment flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/merchant/enrollment/hub")
+            } 
+        }
+    }
+    
+    "credits_merchant_enrollment_entries.ftl"(description: "Credits merchant entries under enrollmment flow with fixed term loan") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                and(
+                    path("/credits/merchant/enrollment", "/credits/merchant/enrollment/onboarding", "/credits/merchant/enrollment/hub", "/credits/merchant/enrollment/simulator") 
+                    in("event_data.product_types", "fixed_term_loan")
+                )
+            } 
+        }
+    }
+
+    "credits_merchant_enrollment_entries.spl"(description: "Credits merchant entries under enrollmment flow with sales percentage loan") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                and (
+                    path("/credits/merchant/enrollment", "/credits/merchant/enrollment/onboarding", "/credits/merchant/enrollment/hub", "/credits/merchant/enrollment/simulator")
+                    in("event_data.product_types", "sales_percentage_loan")
+                )
+            } 
+        }
+    }
+
+    "credits_merchant_enrollment_conversions.ftl"(description: "Credits merchant fixed term loan conversions under enrollmment flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/merchant/enrollment/congrats")
+                and (
+                    equals("event_data.product_type", "fixed_term_loan")
+                )
+            } 
+        }
+    }
+
+    "credits_merchant_enrollment_conversions.spl"(description: "Credits merchant sales percentage loan conversions under enrollmment flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/merchant/enrollment/congrats")
+                and (
+                    equals("event_data.product_type", "sales_percentage_loan")
+                )
+            } 
+        }
+    }
+    
+
+    "credits_merchant_express_money_entries"(description: "Credits merchant entries under express money flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/express_money/amount_input", "/credits/express_money/onboarding")
+            } 
+        }
+    }
+
+    "credits_merchant_express_money_entries.with_onboarding"(description: "Credits merchant entries under express money flow with") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/express_money/onboarding")
+            } 
+        }
+    }
+
+    "credits_merchant_express_money_conversions"(description: "Credits merchant conversions under express money flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/express_money/congrats")
+            } 
+        }
+    }
+
+    "credits_merchant_open_market_entries.upsell_offer"(description: "Credits merchant entries under open market upsell offer flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/merchant/open_market")
+                and (
+                    equals("event_data.flow", "upsell_offer")
+                )
+            } 
+        }
+    }
+
+    "credits_merchant_open_market_entries.request_offer"(description: "Credits merchant entries under open market request offer flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                path("/credits/merchant/open_market")
+                and (
+                    equals("event_data.flow", "request_offer")
+                )
+            } 
+        }
+    }
+
+    "credits_merchant_open_market_conversions.upsell_offer#financial_files"(description: "Credits merchant financial files conversions under open market upsell offer flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                and (
+                    path("/credits/merchant/open_market/congrats")
+                    equals("event_data.flow", "upsell_offer")
+                    in("event_data.reason", "financial_files")
+                )
+            } 
+        }
+    }
+
+    "credits_merchant_open_market_conversions.upsell_offer#financial_scraping"(description: "Credits merchant financial scraping conversions under open market upsell offer flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                and (
+                    path("/credits/merchant/open_market/congrats")
+                    equals("event_data.flow", "upsell_offer")
+                    in("event_data.reason", "financial_scraping")
+                )
+            } 
+        }
+    }
+
+    "credits_merchant_open_market_conversions.request_offer#financial_files"(description: "Credits merchant financial files conversions under open market request offer flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                and (
+                    path("/credits/merchant/open_market/congrats")
+                    equals("event_data.flow", "request_offer")
+                    in("event_data.reason", "financial_files")   
+                )        
+            } 
+        }
+    }
+
+    "credits_merchant_open_market_conversions.request_offer#financial_scraping"(description: "Credits merchant financial scraping conversions under open market request offer flow") {
+        startWith {
+            experiment(regex("(wallet|credits)/.*"))
+        }
+        countsOn { 
+            condition { 
+                and (
+                    path("/credits/merchant/open_market/congrats")
+                    equals("event_data.flow", "request_offer")
+                    in("event_data.reason", "financial_scraping")   
+                )        
+            } 
+        }
+    }
 }
