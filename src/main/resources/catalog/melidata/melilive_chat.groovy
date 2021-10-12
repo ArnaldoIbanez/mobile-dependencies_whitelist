a7c727c3a0670e0e551227c9ac37749f05d6454e
Matías Paverini <matias.paverini@mercadolibre.com>
2021-10-12 18:13:12 -0300

    Agregados tracks para chat creator

--- src/main/resources/catalog/melidata/melilive_chat.groovy
+++ src/main/resources/catalog/melidata/melilive_chat.groovy
@@ -8,29 +8,36 @@ import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 tracks {
     //Definitions
 
-    def broadcast = objectSchemaDefinitions {
-        broadcast_id(required: true, PropertyType.String, description: "Broadcast ID")
+    //Broadcast response object format definition
+    def chat_stream_info_definition = objectSchemaDefinitions {
+        broadcast_id(required: true, type: PropertyType.String, description: "Broadcast ID")
     }
 
     //Tracks
 
-    "/melilive/stream/chat/first_message"(platform: "/", type: TrackType.Event, parentPropertiesInherited: false) {
-        broadcast(required: true, type: PropertyType.Map(broadcast), description: "Broadcast object")
+    "/melilive/stream/chat/first_message"(platform: "/", type: TrackType.Event) {
+        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
         room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
-        user_role(required: true, type: PropertyType.String, description: "User associated with the action")
     }
 
-    "/melilive/stream/chat/message_error"(platform: "/", type: TrackType.Event, parentPropertiesInherited: false) {
-        broadcast(required: true, type: PropertyType.Map(broadcast), description: "Broadcast object")
+    "/melilive/stream/chat/message_error"(platform: "/", type: TrackType.Event) {
+        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
         room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
-        user_role(required: true, type: PropertyType.String, description: "User associated with the action")
     }
 
-    "/melilive/stream/chat/chat_scroll"(platform: "/", type: TrackType.Event, parentPropertiesInherited: false) {
-        broadcast(required: true, type: PropertyType.Map(broadcast), description: "Broadcast object")
+    "/melilive/stream/chat/chat_scroll"(platform: "/", type: TrackType.Event) {
+        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
         room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
-        user_role(required: true, type: PropertyType.String, description: "User associated with the action")
     }
 
+    "/melilive/creator/chat/moderation"(platform: "/", type: TrackType.Event) {
+        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
+        room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
+    }
+
+    "/melilive/creator/chat/ban"(platform: "/", type: TrackType.Event) {
+        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
+        room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
+    }
 
 }
--- src/test/resources/melidata/melilive_chat.groovy
+++ src/test/resources/melidata/melilive_chat.groovy
@@ -6,28 +6,41 @@ trackTests {
     defaultBuisiness = "mercadolibre"
 
     test("Chat messages") {
-        "melilive/stream/chat/first_message"(platform: "/") {
-            broadcast = [
+        "/melilive/stream/chat/first_message"(platform: "/") {
+            stream = [
                     "broadcast_id": "ede1ad69-c277-4f60-90c8-b845ca804fbe"
             ]
-            user_role: ""
-            room_id: "room-v3-03"
+            room_id = "room-v3-03"
         }
 
-        "melilive/stream/chat/message_error"(platform: "/") {
-            broadcast = [
+        "/melilive/stream/chat/message_error"(platform: "/") {
+            stream = [
                     "broadcast_id": "ede1ad69-c277-4f60-90c8-b845ca804fbe"
             ]
-            user_role: ""
-            room_id: "room-v3-03"
+            room_id = "room-v3-03"
         }
 
-        "melilive/stream/chat/chat_scroll"(platform: "/") {
-            broadcast = [
+        "/melilive/stream/chat/chat_scroll"(platform: "/") {
+            stream = [
                     "broadcast_id": "ede1ad69-c277-4f60-90c8-b845ca804fbe"
             ]
-            user_role: ""
-            room_id: "room-v3-03"
+            room_id = "room-v3-03"
+        }
+    }
+
+    test("Creator / Moderator chat") {
+        "/melilive/creator/chat/ban"(platform: "/") {
+            stream = [
+                    "broadcast_id": "ede1ad69-c277-4f60-90c8-b845ca804fbe"
+            ]
+            room_id = "room-v3-03"
+        }
+
+        "/melilive/creator/chat/moderation"(platform: "/") {
+            stream = [
+                    "broadcast_id": "ede1ad69-c277-4f60-90c8-b845ca804fbe"
+            ]
+            room_id = "room-v3-03"
         }
     }
 }
