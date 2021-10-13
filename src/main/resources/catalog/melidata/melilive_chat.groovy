002920c7c8446741efc5f2a2d2443b21a0c2385c
Matías Paverini <matias.paverini@mercadolibre.com>
2021-10-13 12:20:26 -0300

    Modificación para herencia de paths por repetición de definiciones

--- src/main/resources/catalog/melidata/melilive_chat.groovy
+++ src/main/resources/catalog/melidata/melilive_chat.groovy
@@ -6,6 +6,8 @@ import com.ml.melidata.catalog.PropertyType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
 tracks {
+
+    initiative = 1267
     //Definitions
 
     //Broadcast response object format definition
@@ -15,29 +17,30 @@ tracks {
 
     //Tracks
 
-    "/melilive/stream/chat/first_message"(platform: "/", type: TrackType.Event) {
+    // ***********VIEWER***************
+    "/melilive/stream/chat"(platform: "/" , isAbstract:true ) {
         stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
         room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
     }
 
-    "/melilive/stream/chat/message_error"(platform: "/", type: TrackType.Event) {
+    "/melilive/stream/chat/first_message"(platform: "/", type: TrackType.Event) { }
+
+    "/melilive/stream/chat/message_error"(platform: "/", type: TrackType.Event) { }
+
+    "/melilive/stream/chat/chat_scroll"(platform: "/", type: TrackType.Event) { }
+
+    //*********************************
+
+    //***********CREATOR****************
+
+    "/melilive/creator/chat"(platform: "/", isAbstract: true) {
         stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
         room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
     }
 
-    "/melilive/stream/chat/chat_scroll"(platform: "/", type: TrackType.Event) {
-        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
-        room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
-    }
+    "/melilive/creator/chat/moderation"(platform: "/", type: TrackType.Event) { }
 
-    "/melilive/creator/chat/moderation"(platform: "/", type: TrackType.Event) {
-        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
-        room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
-    }
+    "/melilive/creator/chat/ban"(platform: "/", type: TrackType.Event) { }
 
-    "/melilive/creator/chat/ban"(platform: "/", type: TrackType.Event) {
-        stream(required: true, type: PropertyType.Map(chat_stream_info_definition), description: "Broadcast object")
-        room_id(required: true, type: PropertyType.String, description: "Room ID where chat belongs")
-    }
-
+    //*********************************
 }
