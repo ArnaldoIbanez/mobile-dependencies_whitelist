0db5e8d392cb79b4eab0ea03df14a1115e96b30f
Lautaro Delloni <lautaro.delloni@mercadolibre.com>
2021-10-15 17:12:57 -0300

    Fix

--- src/main/resources/catalog/melidata/mp_insurtech.groovy
+++ src/main/resources/catalog/melidata/mp_insurtech.groovy
@@ -780,8 +780,8 @@ tracks {
         flow_id(required: true, type: PropertyType.String, description: "Product id of insurtech")
     }
     //Congrats - Success View
-    "/insurtech/cards/congrats_success"(platform: "/", type: TrackType.View, parentPropertiesInherited: false){
-        quote_id(required: false, type: PropertyType.String, description: "the id of the quote generated")
+    "/insurtech/cards/congrats_success"(platform:"/", type: TrackType.View, parentPropertiesInherited: false){
+        quote_id(required: true, type: PropertyType.String, description: "the id of the quote generated")
         purchase_key(required: true, type: PropertyType.String, description: "the id of the purchase generated")
     }
     "/insurtech/cards/congrats_success/go_cards"(platform: "/", type: TrackType.Event, parentPropertiesInherited: true){
