adceceb2b09caaf07b7da73a1bf3cb84099568ab
gielRolon <gabriel.rolon@mercadolibre.com>
2021-10-27 17:57:13 -0300

    print test added

--- src/test/resources/melidata/discount_center.groovy
+++ src/test/resources/melidata/discount_center.groovy
@@ -671,7 +671,6 @@ trackTests {
                                 purchase_id: 12345678,
                                 purchase_state: "paid",
                                 action_target: "mercadopago://discount_center_payers/list?param1=test#fragment=mp/home",
-                                tracking_id: "order_tracking_id",
                                 component_version: 2,
                                 status_label: [
                                                 status_name: "en curso",
@@ -688,7 +687,7 @@ trackTests {
                                                    ]
                                          ]
                             ]
-                ],
+                ]
             ]
             product_type = "proximity"
             session_id= "27131d31-6910-4855-85fe-70ad2d97f7ed"
@@ -1011,6 +1010,26 @@ trackTests {
                                 action_target: "mercadopago://discount_center_payers/list?param1=test#fragment=mp/home",
                                 tracking_id: "purchase_tracking_id"
                     ]
+                ],
+                 order_status: [
+                    marketplace_type: "order_status",
+                    segment_id: "order_status",
+                    marketplace_index: 0,
+                    order_status: [
+                                index: 0,
+                                store_id: 30091709,
+                                collector_id: 1234567,
+                                name: "Mc Donalds",
+                                purchase_id: 12345678,
+                                purchase_state: "paid",
+                                action_target: "mercadopago://discount_center_payers/list?param1=test#fragment=mp/home",
+                                component_version: 2,
+                                status_label: [
+                                                status_name: "en curso",
+                                                status_icon: "icon_key",
+                                                status_color: "#FFFFFF"
+                                              ]
+                            ]
                 ]
             ]
             product_type = "delivery"
