f2ffc9a2a2423ba7ad69bba10dac2f8e245793dc
Irene <irene.garcia@mercadolibre.cl>
2021-10-28 16:52:32 -0300

    adding tests

--- src/test/resources/melidata/seller_central.groovy
+++ src/test/resources/melidata/seller_central.groovy
@@ -6493,4 +6493,19 @@ test("seller central catalog optin v2 item plus card show") {
       seller_reputation = "NEWBIE"
     }
   }
+
+  test("Price suggestion result") {
+    "/seller_central/price_suggestion/bands"(platform: "/", type: TrackType.Event) {
+      app_name = "update-desktop"
+      item_id = "MLA1106939454"
+      attributes = [
+        [attribute_id: "BRAND", attribute_value: "Chevrolet", attribute_name: "Marca"],
+        [attribute_id: "MODEL", attribute_value: "Onix", attribute_name: "Modelo"],
+        [attribute_id: "TRIM", attribute_value: "1.0 Turbo Premier Ii At", attribute_name: "Version"]
+      ]
+      prices_bands = [lower_band: "1500000", lower_limit: "1000000", upper_band: "3500000", upper_limit: "4000000",
+        estimated_price: "2500000"]
+      price = "2500000"
+    }
+  }
 }
