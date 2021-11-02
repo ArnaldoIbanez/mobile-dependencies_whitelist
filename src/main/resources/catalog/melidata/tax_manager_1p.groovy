a4b1febf946afa2a962d805d0f84e26ef229dada
federicodgonzalez <federico.fgonzalez@mercadolibre.com>
2021-11-02 16:23:14 -0300

    Change path to lowercase

--- src/main/resources/catalog/melidata/tax_manager_1p.groovy
+++ src/main/resources/catalog/melidata/tax_manager_1p.groovy
@@ -10,29 +10,29 @@ tracks {
     // TRACKS Vendor Central - TaxManager
     //------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    "/1P_TAX_MANAGER/PROCESS/SAP/RETURN/SUCCESS"(platform: "/web", type: TrackType.Event) {
+    "/1p_tax_manager/process/sap/return/success"(platform: "/web", type: TrackType.Event) {
         sku(required: true, type: PropertyType.String, description: "this property indicates the sku")
         site_id(required: true, type: PropertyType.String, description: "this property indicates the site_id")
         sap_user_id(required: true, type: PropertyType.String, description: "this property indicates the sap_user_id")
     }
-    "/1P_TAX_MANAGER/PROCESS/SAP/RETURN/FAIL"(platform: "/web", type: TrackType.Event) {
+    "/1p_tax_manager/process/sap/return/fail"(platform: "/web", type: TrackType.Event) {
         sku(required: true, type: PropertyType.String, description: "this property indicates the sku")
         site_id(required: true, type: PropertyType.String, description: "this property indicates the site_id")
         sap_user_id(required: true, type: PropertyType.String, description: "this property indicates the sap_user_id")
     }
-    "/1P_TAX_MANAGER/PRE_PROCESS/CONFLICT_CREATION"(platform: "/web", type: TrackType.Event) {
+    "/1p_tax_manager/pre_process/conflict_creation"(platform: "/web", type: TrackType.Event) {
         sku(required: true, type: PropertyType.String, description: "this property indicates the sku")
         site_id(required: true, type: PropertyType.String, description: "this property indicates the site_id")
         sap_user_id(required: true, type: PropertyType.String, description: "this property indicates the sap_user_id")
         site_conflict(required: true, type: PropertyType.Boolean, description: "this property indicates the site_conflict")
         combination_conflict(required: true, type: PropertyType.Boolean, description: "this property indicates the combination_conflict")
     }
-    "/1P_TAX_MANAGER/PROCESS/SAP/RETURN_SPREADSHEET/SUCCESS"(platform: "/web", type: TrackType.Event) {
+    "/1p_tax_manager/process/sap/return_spreadsheet/success"(platform: "/web", type: TrackType.Event) {
         sku(required: true, type: PropertyType.String, description: "this property indicates the sku")
         site_id(required: true, type: PropertyType.String, description: "this property indicates the site_id")
         sap_user_id(required: true, type: PropertyType.String, description: "this property indicates the sap_user_id")
     }
-    "/1P_TAX_MANAGER/PRE_PROCESS/TAX_COMBINATION"(platform: "/web", type: TrackType.Event) {
+    "/1p_tax_manager/pre_process/tax_combination"(platform: "/web", type: TrackType.Event) {
         sku(required: true, type: PropertyType.String, description: "this property indicates the sku")
         site_id(required: true, type: PropertyType.String, description: "this property indicates the site_id")
         sap_user_id(required: true, type: PropertyType.String, description: "this property indicates the sap_user_id")
--- src/test/resources/melidata/tax_manager_1p.groovy
+++ src/test/resources/melidata/tax_manager_1p.groovy
@@ -8,21 +8,21 @@ trackTests {
     defaultBusiness = "mercadolibre"
 
     test("1p TaxManager return success is OK"){
-        "/1P_TAX_MANAGER/PROCESS/SAP/RETURN/SUCCESS"(platform: "/web", type: TrackType.Event) {
+        "/1p_tax_manager/process/sap/return/success"(platform: "/web", type: TrackType.Event) {
             sku = "699227"
             site_id = "MLB"
             sap_user_id = ""
         }
     }
     test("1p TaxManager return fail is OK"){
-        "/1P_TAX_MANAGER/PROCESS/SAP/RETURN/FAIL"(platform: "/web", type: TrackType.Event) {
+        "/1p_tax_manager/process/sap/return/fail"(platform: "/web", type: TrackType.Event) {
             sku = "699227"
             site_id = "MLB"
             sap_user_id = "9500000326"
         }
     }
     test("1p TaxManager conflict creation success is OK"){
-        "/1P_TAX_MANAGER/PRE_PROCESS/CONFLICT_CREATION"(platform: "/web", type: TrackType.Event) {
+        "/1p_tax_manager/pre_process/conflict_creation"(platform: "/web", type: TrackType.Event) {
             sku = "699227"
             site_id = "MLB"
             sap_user_id = "9500000326"
@@ -31,14 +31,14 @@ trackTests {
         }
     }
     test("1p TaxManager return spreadsheet is OK"){
-        "/1P_TAX_MANAGER/PROCESS/SAP/RETURN_SPREADSHEET/SUCCESS"(platform: "/web", type: TrackType.Event) {
+        "/1p_tax_manager/process/sap/return_spreadsheet/success"(platform: "/web", type: TrackType.Event) {
             sku = "699227"
             site_id = "MLB"
             sap_user_id = "9500000326"
         }
     }
     test("1p TaxManager receive tax-combination is OK"){
-        "/1P_TAX_MANAGER/PRE_PROCESS/TAX_COMBINATION"(platform: "/web", type: TrackType.Event) {
+        "/1p_tax_manager/pre_process/tax_combination"(platform: "/web", type: TrackType.Event) {
             sku = "699227"
             site_id = "MLB"
             sap_user_id = "9500000326"
