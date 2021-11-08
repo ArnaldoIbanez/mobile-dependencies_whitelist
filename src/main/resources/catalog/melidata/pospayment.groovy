0e525d8b67dab58c14a27618012c7414b2e7c437
imelgrat <imelgrat@gmail.com>
2021-11-08 15:55:08 -0300

    Removed root track

--- src/main/resources/catalog/melidata/pospayment.groovy
+++ src/main/resources/catalog/melidata/pospayment.groovy
@@ -7,13 +7,6 @@ import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 tracks {
     initiative = "1171"
     // Pospayment Refunds
-    '/' (platform: '/', type: TrackType.View) {
-      user_profile(
-        required: false,
-        type: PropertyType.String,
-        values: ['guest', 'no_offer', 'seller', 'payer', 'newbie', 'collector', 'sellers', 'payers', 'collectors', 'undefined'],
-        description: 'Tracks the user profile')
-    }
 
     '/pospayment' (platform: '/', type: TrackType.View) {
       user_profile(
