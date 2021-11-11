247c9c84607e37e965031617d13478ebed4425fe
granadero <jimenezpablo_github@outlook.com>
2021-11-11 13:23:40 -0300

    linter issue

--- src/test/resources/melidata/investment_cdb.groovy
+++ src/test/resources/melidata/investment_cdb.groovy
@@ -9,18 +9,18 @@ trackTests {
     test("Investment CDB") {
 
     
-        "/investment-cdb/onboarding" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/home" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/offers" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/ryc" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/congrats" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/blocker" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/detail" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/feedback" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/list" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/not_found" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/error" (platform: "/mobile", type: TrackType.View) {}
-        "/investment-cdb/pre_suitability" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/onboarding" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/home" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/offers" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/ryc" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/congrats" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/blocker" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/detail" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/feedback" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/list" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/not_found" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/error" (platform: "/mobile", type: TrackType.View) {}
+        "/investment_cdb/pre_suitability" (platform: "/mobile", type: TrackType.View) {}
 
     }
 
@@ -29,5 +29,5 @@ trackTests {
         "/investment_suitability/investor_profile" (platform: "/mobile", type: TrackType.View) {}
 
     }
-    
+
 }
