233513f9fa454c3767ccb7946b67b7d1e7173447
granadero <jimenezpablo_github@outlook.com>
2021-11-12 12:55:43 -0300

    sections added to tracks

--- src/main/resources/catalog/melidata/investment.groovy
+++ src/main/resources/catalog/melidata/investment.groovy
@@ -13,26 +13,26 @@ tracks
 
     //CDB tracks
 
-    "/cdb"(platform: "/", isAbstract: true) {} 
+    "/asset_management/investment/cdb"(platform: "/", isAbstract: true) {} 
 
-    "/onboarding" (platform: "/mobile", type: TrackType.View) {}
-    "/home" (platform: "/mobile", type: TrackType.View) {}
-    "/offers" (platform: "/mobile", type: TrackType.View) {}
-    "/ryc" (platform: "/mobile", type: TrackType.View) {}
-    "/congrats" (platform: "/mobile", type: TrackType.View) {}
-    "/blocker" (platform: "/mobile", type: TrackType.View) {}
-    "/detail" (platform: "/mobile", type: TrackType.View) {}
-    "/feedback" (platform: "/mobile", type: TrackType.View) {}
-    "/list" (platform: "/mobile", type: TrackType.View) {}
-    "/not_found" (platform: "/mobile", type: TrackType.View) {}
-    "/error" (platform: "/mobile", type: TrackType.View) {}
-    "/pre_suitability" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/onboarding" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/home" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/offers" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/ryc" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/congrats" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/blocker" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/detail" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/feedback" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/list" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/not_found" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/error" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/cdb/pre_suitability" (platform: "/mobile", type: TrackType.View) {}
 
     //Suitability tracks
 
-    "/suitability"(platform: "/", isAbstract: true) {}
+    "/asset_management/investment/suitability"(platform: "/", isAbstract: true) {}
 
-    "/questions" (platform: "/mobile", type: TrackType.View) {}
-    "/investor_profile" (platform: "/mobile", type: TrackType.View) {}  
+    "/asset_management/investment/suitability/questions" (platform: "/mobile", type: TrackType.View) {}
+    "/asset_management/investment/suitability/investor_profile" (platform: "/mobile", type: TrackType.View) {}  
 
 }
--- src/test/resources/melidata/investment.groovy
+++ src/test/resources/melidata/investment.groovy
@@ -8,24 +8,24 @@ trackTests {
 
     test("CDB") {
     
-        "/onboarding" (platform: "/mobile", type: TrackType.View) {}
-        "/home" (platform: "/mobile", type: TrackType.View) {}
-        "/offers" (platform: "/mobile", type: TrackType.View) {}
-        "/ryc" (platform: "/mobile", type: TrackType.View) {}
-        "/congrats" (platform: "/mobile", type: TrackType.View) {}
-        "/blocker" (platform: "/mobile", type: TrackType.View) {}
-        "/detail" (platform: "/mobile", type: TrackType.View) {}
-        "/feedback" (platform: "/mobile", type: TrackType.View) {}
-        "/list" (platform: "/mobile", type: TrackType.View) {}
-        "/not_found" (platform: "/mobile", type: TrackType.View) {}
-        "/error" (platform: "/mobile", type: TrackType.View) {}
-        "/pre_suitability" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/onboarding" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/home" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/offers" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/ryc" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/congrats" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/blocker" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/detail" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/feedback" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/list" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/not_found" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/error" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/cdb/pre_suitability" (platform: "/mobile", type: TrackType.View) {}
 
     }
 
     test("Suitability") {
-        "/questions" (platform: "/mobile", type: TrackType.View) {}
-        "/investor_profile" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/suitability/questions" (platform: "/mobile", type: TrackType.View) {}
+        "/asset_management/investment/suitability/investor_profile" (platform: "/mobile", type: TrackType.View) {}  
 
     }
 
