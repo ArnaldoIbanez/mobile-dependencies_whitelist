b49af8460118bb4aa12a488f272eb1ced0d7901d
Pablo Carballo <pablo.carballo@mercadolibre.com>
2021-11-18 09:52:57 -0300

    update

--- src/main/resources/catalog/component_prints/front_core.groovy
+++ src/main/resources/catalog/component_prints/front_core.groovy
@@ -30,6 +30,6 @@ tracks {
     }
 
     "/wallet_home/print" (platform: "/mobile", type: TrackType.View) {
-        banking_v2balance(required: false, type: PropertyType.Map(banking_balance_item_definition), description: "The balance information")
+        "banking_v2balance"(required: false, type: PropertyType.Map(banking_balance_item_definition), description: "The balance information")
     }
 }
