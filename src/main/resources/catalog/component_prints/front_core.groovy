d87699cabd450ac6c08af98d6dcfc8eec734e0b8
Pablo Carballo <pablo.carballo@mercadolibre.com>
2021-11-17 22:47:28 -0300

    update

--- src/main/resources/catalog/component_prints/front_core.groovy
+++ src/main/resources/catalog/component_prints/front_core.groovy
@@ -15,14 +15,30 @@ import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
 tracks {
 
-    def banking_balance_definition = objectSchemaDefinitions {
+    def banking_action_item_definition = objectSchemaDefinitions {
+        component_id(type: PropertyType.String, required: true, description: "")
+        content_id(type: PropertyType.String, required: true, description: "")
+        position(type: PropertyType.Numeric, required: true, description: "")
+        enabled(type: PropertyType.Boolean, required: true, description: "")
+    }
+
+    def banking_balance_item_definition = objectSchemaDefinitions {
         component_id(required: true, type: PropertyType.String, description: "Balance id")
         balance_histogram(required: true, type: PropertyType.Numeric, description: "Balance segmentation")
         hidden(required: true, type: PropertyType.Boolean, description: "Balance hidden by user")
         pending_balance(required: true, type: PropertyType.Boolean, description: "Balance pending")
     }
 
-    "/wallet_home/prints" (platform: "/mobile") {
-        banking_v2-balance(required: false, type: PropertyType.Map(banking_balance_definition), description: "The banking v2 section information")
+    def banking_balance_definition = objectSchemaDefinitions {
+        banking_v2-balance(required: false, type: PropertyType.ArrayList(PropertyType.Map(banking_balance_item_definition)), description: "")
+    }
+
+    def banking_actions_definition = objectSchemaDefinitions {
+        banking_v2-actions(required: false, type: PropertyType.ArrayList(PropertyType.Map(banking_action_item_definition)), description: "")
+    }
+
+    "/wallet_home/prints" (platform: "/mobile", type: TrackType.View) {
+        banking_v2-balance(required: false, type: PropertyType.Map(banking_balance_definition), description: "The balance information")
+        banking_v2-actions(required: false, type: PropertyType.Map(banking_actions_definition), description: "The actions information")
     }
 }
---
+++ src/test/resources/component_prints/front_core.groovy
@@ -0,0 +1,31 @@
+package src.test.resources.component_prints
+import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
+import com.ml.melidata.TrackType
+
+/**************************************************************
+ * CONTACT: In case of changes over this file, please send us
+ *  a message to our e-mail: front_native_devs@mercadolibre.com
+ **************************************************************/
+
+/**************************
+ * WALLET HOME TEST TRACKS *
+ **************************/
+
+trackTests {
+
+    defaultBusiness = "mercadopago"
+
+    test("Mercadopago Home print") {
+        "/wallet_home/print" (platform: "/mobile", type: TrackType.View) {
+            banking_v2-balance = [
+                    [
+                            component_id: "balance",
+                            balance_histogram: 6.0,
+                            hidden: false,
+                            pending_balance: false
+                    ]
+            ]
+        }
+    }
+
+}
