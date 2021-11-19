5c6cc3091fdfb47306b6ea13464973c2b22e5051
Pablo Carballo <pablo.carballo@mercadolibre.com>
2021-11-19 17:40:17 -0300

    update

--- src/main/resources/catalog/component_prints/front_core.groovy
+++ src/main/resources/catalog/component_prints/front_core.groovy
@@ -84,7 +84,7 @@ tracks {
     }
 
     "/wallet_home/print" (platform: "/mobile", type: TrackType.View) {
-        //banking_v2-balance(required: false, type: PropertyType.ArrayList(PropertyType.Map(banking_balance_item_definition), description: "The balance information")
+        banking_v2-balance(required: false, type: PropertyType.ArrayList(PropertyType.Map(banking_balance_item_definition), description: "The balance information")
         //banking_v2-actions(required: false, type: PropertyType.ArrayList(PropertyType.Map(banking_action_item_definition), description: "The balance information")
         shortcuts(required: false, type: PropertyType.ArrayList(PropertyType.Map(shortcuts_item_definition)), description: "Shortcuts")
         discount_center(required: false, type: PropertyType.ArrayList(PropertyType.Map(discount_center_item_definition)), description: "Shortcuts")
