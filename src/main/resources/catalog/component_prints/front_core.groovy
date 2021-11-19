c5c7e609cfb31f5411d29d57d1bb20ad6b6aa9cb
Pablo Carballo <pablo.carballo@mercadolibre.com>
2021-11-19 12:09:46 -0300

    update

--- src/main/resources/catalog/component_prints/front_core.groovy
+++ src/main/resources/catalog/component_prints/front_core.groovy
@@ -47,10 +47,51 @@ tracks {
         component_id(required: true, type: PropertyType.String, description: "Component id")
     }
 
+    def cross_selling_item_definition = objectSchemaDefinitions {
+        component_id(required: true, type: PropertyType.String, description: "Component id")
+        audience(required: true, type: PropertyType.String, description: "Audience")
+        bu(required: true, type: PropertyType.String, description: "Bu")
+        content_id(required: true, type: PropertyType.String, description: "Content id")
+        action_id(required: true, type: PropertyType.String, description: "Action id")
+        bu_line(required: true, type: PropertyType.String, description: "Bu line")
+        logic(required: true, type: PropertyType.String, description: "Logic")
+        position(required: true, type: PropertyType.Numeric, description: "Position")
+        flow(required: true, type: PropertyType.String, description: "Flow")
+    }
+
+    def benefits_item_definition = objectSchemaDefinitions {
+        component_id(required: true, type: PropertyType.String, description: "Component id")
+        audience(required: true, type: PropertyType.String, description: "Audience")
+        bu(required: true, type: PropertyType.String, description: "Bu")
+        content_id(required: true, type: PropertyType.String, description: "Content id")
+        action_id(required: true, type: PropertyType.String, description: "Action id")
+        bu_line(required: true, type: PropertyType.String, description: "Bu line")
+        logic(required: true, type: PropertyType.String, description: "Logic")
+        position(required: true, type: PropertyType.Numeric, description: "Position")
+        flow(required: true, type: PropertyType.String, description: "Flow")    }
+
+    def activities_item_definition = objectSchemaDefinitions {
+        component_id(required: true, type: PropertyType.String, description: "Component id")
+        contains_rta(required: true, type: PropertyType.Boolean, description: "Contains RTA")
+    }
+
+    def repentance_button_item_definition = objectSchemaDefinitions {
+        component_id(required: true, type: PropertyType.String, description: "Component id")
+    }
+
+    def bcra_regulation_item_definition = objectSchemaDefinitions {
+        component_id(required: true, type: PropertyType.String, description: "Component id")
+    }
+
     "/wallet_home/print" (platform: "/mobile", type: TrackType.View) {
         //banking_v2-balance(required: false, type: PropertyType.Map(banking_balance_item_definition), description: "The balance information")
         //banking_v2-actions(required: false, type: PropertyType.Map(banking_action_item_definition), description: "The balance information")
         shortcuts(required: false, type: PropertyType.Map(shortcut_item_definition), description: "Shortcuts")
         discount_center(required: false, type: PropertyType.Map(discount_center_item_definition), description: "Shortcuts")
+        //cross_selling(required: false, type: PropertyType.Map(cross_selling_item_definition), description: "Cross selling")
+        //benefits(required: false, type: PropertyType.Map(benefits_item_definition), description: "Benefits")
+        //activities(required: false, type: PropertyType.Map(activities_item_definition), description: "Activities")
+        //repentance_button(required: false, type: PropertyType.Map(repentance_button_item_definition), description: "Repentance button")
+        //bcra_regulation(required: false, type: PropertyType.Map(bcra_regulation_item_definition), description: "BCRA regulation")
     }
 }
