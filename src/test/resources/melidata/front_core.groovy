495481e384c3b656207f6264b73aed505b603e47
Pablo Carballo <pablo.carballo@mercadolibre.com>
2021-11-20 09:13:44 -0300

    update home-tracks

--- src/test/resources/melidata/front_core.groovy
+++ src/test/resources/melidata/front_core.groovy
@@ -1126,13 +1126,16 @@ trackTests {
 
     test("Mercadopago Home Tap - carousel") {
         "/wallet_home/section/tap/carousel" (platform: "/mobile", type: TrackType.Event) {
-            link = "mercadopago://instore/scan_qr"
+            link = "mercadopago://instore/scan_qr",
+            section_id="carousel"
+
         }
     }
 
     test("Mercadopago Home Tap - Ad carousel") {
         "/wallet_home/section/tap/ad_carousel" (platform: "/mobile", type: TrackType.Event) {
-            link = "mercadopago://instore/scan_qr"
+            link = "mercadopago://instore/scan_qr",
+            section_id="ad carousel"
         }
     }
 
