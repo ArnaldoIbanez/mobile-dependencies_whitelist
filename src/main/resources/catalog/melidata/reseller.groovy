e8ceaf9d378161f901cda8bb537d41775bc5c073
kagudelo90 <kevin.alonso@mercadolibre.com.co>
2021-11-22 16:24:18 -0500

    Implemented tracks in reseller catalogs

--- src/main/resources/catalog/melidata/reseller.groovy
+++ src/main/resources/catalog/melidata/reseller.groovy
@@ -21,4 +21,32 @@ tracks {
         campaign_zone_id(required: true, type: PropertyType.String, inheritable: false, description: "The campaign zone id")
         show_store_in_map(required: true, type: PropertyType.String, inheritable: false, description: "The flag indicates if store is shown on map")
     }
+    "/reseller/scanner/back" (platform: "/mobile", type: TrackType.Event) {}
+    "/reseller/scanner/torch" (platform: "/mobile", type: TrackType.Event) {
+        torch_enabled(required: true, type: PropertyType.Boolean, inheritable: true, description: "The flag indicate if torch is enabled")
+    }
+    "/reseller/scanner/scanned" (platform: "/mobile", type: TrackType.Event) {
+        torch_enabled(required: true, type: PropertyType.Boolean, inheritable: true, description: "The flag indicate if torch is enabled")
+        code(required: true, type: PropertyType.String, inheritable: false, description: "The scanned code")
+    }
+    "/reseller/scanner/camera_permission" (platform: "/mobile", type: TrackType.View) {}
+    "/reseller/scanner/camera_permission/granted" (platform: "/mobile", type: TrackType.Event) {}
+    "/reseller/scanner/camera_permission/rejected" (platform: "/mobile", type: TrackType.Event) {}
+    "/reseller/scanner/camera_permission/configuration" (platform: "/mobile", type: TrackType.Event) {}
+
+    //Congrats
+    "/reseller/congrats" (platform: "/mobile", type: TrackType.Event) {
+        status(required: true, type: PropertyType.String, inheritable: false, description: "The status of transaction")
+    }
+    "/reseller/congrats/close" (platform: "/mobile", type: TrackType.Event) {}
+    "/reseller/congrats/continue" (platform: "/mobile", type: TrackType.Event) {
+        action_id(required: true, type: PropertyType.String, inheritable: false, description: "The action that the user did in the congrats view")
+    }
+
+    //Error
+    "/reseller/scanner/error" (platform: "/mobile", type: TrackType.View) {
+        type(required: true, type: PropertyType.String, inheritable: false, description: "The error type")
+        can_retry(required: true, type: PropertyType.Boolean, inheritable: false, description: "The flag indicate if the user can retry")
+    }
+    "/reseller/scanner/error/retry" (platform: "/mobile", type: TrackType.View) {}
 }
--- src/test/resources/melidata/reseller.groovy
+++ src/test/resources/melidata/reseller.groovy
@@ -1,6 +1,7 @@
 package src.test.resources.melidata
 
 import com.ml.melidata.TrackType
+import com.ml.melidata.catalog.PropertyType
 
 import static com.melidata.definitions.parsers.dsl.TrackTestDsl.trackTests
 
@@ -16,6 +17,61 @@ trackTests {
             store_id = "43678"
             campaign_zone_id = "1"
             show_store_in_map = "true"
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/scanner/back" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/scanner/torch" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+            torch_enabled = true
+        }
+
+        "/reseller/scanner/scanned" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+            torch_enabled = true
+            code = "https://mpago.la/pos/9962477"
+        }
+
+        "/reseller/scanner/camera_permission" (platform: "/mobile", type: TrackType.View) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/scanner/camera_permission/granted" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/scanner/camera_permission/rejected" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/scanner/camera_permission/configuration" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/congrats" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+            status = "rejected"
+        }
+
+        "/reseller/congrats/close" (platform: "/mobile", type: TrackType.Event) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/congrats/continue" (platform: "/mobile", type: TrackType.Event) {
+            action_id = "go_to_root"
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+        }
+
+        "/reseller/scanner/error" (platform: "/mobile", type: TrackType.View) {
+            session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
+            type = "service_error"
+            can_retry = false
+        }
+
+        "/reseller/scanner/error/retry" (platform: "/mobile", type: TrackType.View) {
             session_id = "27131d31-6910-4855-85fe-70ad2d97f7ed"
         }
     }
