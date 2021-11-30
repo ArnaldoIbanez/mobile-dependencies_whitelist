dcc734924eeb7ffdf1d86a066587d859ea3f31ec
John Alexander Fonseca Tumay <johnafonseca@gmail.com>
2021-11-30 15:20:02 -0500

    feat: add properties print

--- src/main/resources/catalog/melidata/combos.groovy
+++ src/main/resources/catalog/melidata/combos.groovy
@@ -16,7 +16,15 @@ tracks {
 
 
    
-    "/combos/print" (platform: "/"){}
+    "/combos/print" (platform: "/"){
+        backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
+        client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
+        track_info(required: false, type: PropertyType.Map)
+        has_errors(required: false, type: PropertyType.Boolean, description: "The recommendation has to be hidden because it has errors.")
+        item(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
+        items(required: true, description: "Item ids that are being added", type: PropertyType.ArrayList)
+        quantity(required: false, type: PropertyType.Numeric, description: "Items selected at the moment")
+    }
 
 
     "/combos/add_to_cart"(platform: "/", type: TrackType.Event) {
