774f62c9e2a87c334349338b8f1259ec49e3529f
John Alexander Fonseca Tumay <johnafonseca@gmail.com>
2021-11-30 14:59:53 -0500

    feat: checge propertis

--- src/main/resources/catalog/melidata/combos.groovy
+++ src/main/resources/catalog/melidata/combos.groovy
@@ -4,6 +4,7 @@ import com.ml.melidata.catalog.PropertyType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 import com.ml.melidata.TrackType
 
+
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -14,7 +15,13 @@ tracks {
     initiative = "1024"
 
 
-    propertyDefinitions {
+
+    "/combos" (platform: "/"){}
+   
+    "/combos/print" (platform: "/"){}
+
+
+    "/combos/add_to_cart"(platform: "/", type: TrackType.Event) {
         backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
         client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
         track_info(required: false, type: PropertyType.Map)
@@ -26,25 +33,28 @@ tracks {
         experiments(required: false, type: PropertyType.ArrayList, description: "Experiment Info")
     }
 
-    propertyGroups {
-        combosGroup(backend_id, client, item_id, track_info, has_errors, item, items, action, quantity, experiments)
-    }
-
-    "/combos" (platform: "/"){}
-   
-    "/combos/print" (platform: "/"){}
-
-
-    "/combos/add_to_cart"(platform: "/", type: TrackType.Event) {
-      combosGroup
-    }
-
     "/combos/checked"(platform: "/", type: TrackType.Event) {
-       combosGroup
+        backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
+        client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
+        track_info(required: false, type: PropertyType.Map)
+        has_errors(required: false, type: PropertyType.Boolean, description: "The recommendation has to be hidden because it has errors.")
+        item(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
+        items(required: true, description: "Item ids that are being added", type: PropertyType.ArrayList)
+        action(required: false, type: PropertyType.String, description: "User action")
+        quantity(required: false, type: PropertyType.Numeric, description: "Items selected at the moment")
+        experiments(required: false, type: PropertyType.ArrayList, description: "Experiment Info")
     }
 
     "/combos/view"(platform: "/", type: TrackType.View) {
-        combosGroup
+        backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
+        client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
+        track_info(required: false, type: PropertyType.Map)
+        has_errors(required: false, type: PropertyType.Boolean, description: "The recommendation has to be hidden because it has errors.")
+        item(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
+        items(required: true, description: "Item ids that are being added", type: PropertyType.ArrayList)
+        action(required: false, type: PropertyType.String, description: "User action")
+        quantity(required: false, type: PropertyType.Numeric, description: "Items selected at the moment")
+        experiments(required: false, type: PropertyType.ArrayList, description: "Experiment Info")
     }
 
 }
