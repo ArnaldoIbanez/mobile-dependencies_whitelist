0d89089435f3d1c420377b2639b0a0a1f153b633
John Alexander Fonseca Tumay <johnafonseca@gmail.com>
2021-11-30 15:24:17 -0500

    feat: add properties print

--- src/main/resources/catalog/melidata/combos.groovy
+++ src/main/resources/catalog/melidata/combos.groovy
@@ -16,15 +16,7 @@ tracks {
 
 
    
-    "/combos/print" (platform: "/"){
-        backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
-        client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
-        track_info(required: false, type: PropertyType.Map)
-        has_errors(required: false, type: PropertyType.Boolean, description: "The recommendation has to be hidden because it has errors.")
-        item(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
-        items(required: true, description: "Item ids that are being added", type: PropertyType.ArrayList)
-        quantity(required: false, type: PropertyType.Numeric, description: "Items selected at the moment")
-    }
+    "/combos/print" (platform: "/"){}
 
 
     "/combos/add_to_cart"(platform: "/", type: TrackType.Event) {
--- src/test/resources/melidata/combos.groovy
+++ src/test/resources/melidata/combos.groovy
@@ -10,12 +10,6 @@ trackTests {
     test("Combos :: Show congrats details tracking") {
 
         "/combos/print"(platform: "/") {
-            backend_id= "machinalis-vip-combos"
-            client = "vip-combos"
-            track_info = null
-            has_errors = false
-            item = "MLB1001877141"
-            items = []
         }
 
         "/combos/add_to_cart"(platform: "/") {
