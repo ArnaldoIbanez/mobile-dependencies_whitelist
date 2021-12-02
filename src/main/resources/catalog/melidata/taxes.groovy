920ba3451c104f78be40639dde7be77637f4f03e
Daniel Tostes <daniel.tostes@mercadolivre.com>
2021-12-02 19:13:41 -0300

    Update initiative

--- src/main/resources/catalog/melidata/taxes.groovy
+++ src/main/resources/catalog/melidata/taxes.groovy
@@ -1,13 +1,10 @@
 package catalog.melidata
 
-import com.ml.melidata.catalog.PropertyType
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
-import com.ml.melidata.TrackType
-
 
 tracks {
 
-    initiative = "1339"
+    initiative = "1014"
 
     // Taxes
     "/taxes/collected-at-source"(platform: "/") {}
