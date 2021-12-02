bd45deb1450c0d511162712f9fbaa82c0d10cea7
John Alexander Fonseca Tumay <johnafonseca@gmail.com>
2021-12-02 16:24:25 -0500

    fix: remove track_info

--- src/main/resources/catalog/melidata/combos.groovy
+++ src/main/resources/catalog/melidata/combos.groovy
@@ -15,6 +15,7 @@ tracks {
     initiative = "1024"
 
 
+
     "/combos"(platform:"/", isAbstract: true) {
     }
     
@@ -24,7 +25,6 @@ tracks {
     "/combos/add_to_cart"(platform: "/", type: TrackType.Event) {
         backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
         client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
-        track_info(required: false, type: PropertyType.Map, description: "aditional track_info")
         has_errors(required: false, type: PropertyType.Boolean, description: "The recommendation has to be hidden because it has errors.")
         item(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
         items(required: true, description: "Item ids that are being added", type: PropertyType.ArrayList)
@@ -34,7 +34,6 @@ tracks {
     "/combos/checked"(platform: "/", type: TrackType.Event) {
         backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
         client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
-        track_info(required: false, type: PropertyType.Map, description: "aditional track_info")
         has_errors(required: false, type: PropertyType.Boolean, description: "The recommendation has to be hidden because it has errors.")
         item(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
         items(required: true, description: "Item ids that are being added", type: PropertyType.ArrayList)
@@ -44,7 +43,6 @@ tracks {
     "/combos/view"(platform: "/", type: TrackType.View) {
         backend_id(required: true, type: PropertyType.String, description: "Unique string that identifies the backend used to create the recommendation")
         client(required: true, type: PropertyType.String, description: "Unique string that identifies the spot")
-        track_info(required: false, type: PropertyType.Map, description: "aditional track_info")
         has_errors(required: false, type: PropertyType.Boolean, description: "The recommendation has to be hidden because it has errors.")
         item(required: true, type: PropertyType.String, description: "Unique string that identifies the recommendation from were the item comes from")
         items(required: true, description: "Item ids that are being added", type: PropertyType.ArrayList)
