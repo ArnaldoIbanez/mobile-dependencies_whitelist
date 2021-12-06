a692ba6734d690d4539aa8faa01348ca7295f4ed
Daniel Tostes <daniel.tostes@mercadolivre.com>
2021-12-06 19:07:58 -0300

    Trying to fix lint

--- src/main/resources/catalog/melidata/taxes.groovy
+++ src/main/resources/catalog/melidata/taxes.groovy
@@ -1,7 +1,5 @@
 package catalog.melidata
 
-import com.ml.melidata.TrackType
-
 import static com.ml.melidata.catalog.parsers.dsl.TrackDsl.tracks
 
 tracks {
@@ -9,6 +7,6 @@ tracks {
     initiative = "1014"
 
     // Taxes
-    "/taxes/collected_at_source"(platform: "/", type: TrackType.View) {}
-    "/taxes/user_tax_file"(platform: "/", type: TrackType.View) {}
+    "/taxes/collected_at_source"(platform: "/") {}
+    "/taxes/user_tax_file"(platform: "/") {}
 }
