d25643a22b5e8e61cb03783316a3ad4bd939f110
Fernando Robles <starla.frr@gmail.com>
2021-12-07 10:57:03 -0600

    feature(credits-marketplace): remove dashboard_status in track

--- src/main/resources/catalog/melidata/credits_marketplace.groovy
+++ src/main/resources/catalog/melidata/credits_marketplace.groovy
@@ -335,9 +335,7 @@ tracks {
     "/credits/consumer/administrator_v2/onboarding/how_to_pay_installments"(platform: "/mobile", type: TrackType.Event) {}
     "/credits/consumer/administrator_v2/onboarding/go_mc"(platform: "/mobile", type: TrackType.Event) {}
     "/credits/consumer/administrator_v2/onboarding/close"(platform: "/mobile", type: TrackType.Event) {}
-    "/credits/consumer/administrator_v2/dashboard/personal_loan_download"(platform: "/web/desktop", type: TrackType.Event) {
-         dashboard_status = 'on_time'
-    }
+    "/credits/consumer/administrator_v2/dashboard/personal_loan_download"(platform: "/web/desktop", type: TrackType.Event) {}
 
     /******************************************
      *       End: Consumers Administrator
