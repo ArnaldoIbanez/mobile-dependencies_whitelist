dc30c133ecf96317bd5ded09f4da336e737be8ff
Ezequiel Artigas <ezequiel.artigas@mercadolibre.com>
2021-12-21 16:29:19 -0300

    add modal tracks

--- src/main/resources/catalog/melidata/money_in.groovy
+++ src/main/resources/catalog/melidata/money_in.groovy
@@ -524,15 +524,16 @@ tracks {
     "/money_in/hub/web/select"(platform:"/", type: TrackType.Event){
         payment_method_id(required:false, type: PropertyType.String, description:"indicates the selected payment method")
     }
-    "/money_in/hub/web/widget/shown"(platform:"/", type: TrackType.View){
-        id(required:false, type: PropertyType.String, description:"indicates what widget we are sending to the client")
-    }
     "/money_in/hub/web/widget/select"(platform:"/", type: TrackType.Event){
         widget_id(required:false, type: PropertyType.String, description:"indicates what widget the user select")
     }
     "/money_in/hub/web/widget/action_selected"(platform:"/", type: TrackType.Event){
         action_id(required:false, type: PropertyType.String, description:"indicates what widget action is selected")
     }
+    "/money_in/hub/web/modal"(platform:"/", type: TrackType.View){}
+    "/money_in/hub/web/modal/close"(platform:"/", type: TrackType.Event){}
+    "/money_in/hub/web/modal/primary_button"(platform:"/", type: TrackType.Event){}
+    "/money_in/hub/web/modal/secondary_button"(platform:"/", type: TrackType.Event){}
 
     // -------------Open Finance - Money In-------------
     //Onboarding
--- src/test/resources/melidata/money_in.groovy
+++ src/test/resources/melidata/money_in.groovy
@@ -752,15 +752,16 @@ trackTests {
         "/money_in/hub/web/select"(platform:"/", type: TrackType.Event){
             payment_method_id="pix"
         }
-        "/money_in/hub/web/widget/shown"(platform:"/", type: TrackType.View){
-            id="pix"
-        }
         "/money_in/hub/web/widget/select"(platform:"/", type: TrackType.Event){
             widget_id="pix"
         }
         "/money_in/hub/web/widget/action_selected"(platform:"/", type: TrackType.Event){
             action_id="pix_qr"
         }
+        "/money_in/hub/web/modal"(platform:"/", type: TrackType.View){}
+        "/money_in/hub/web/modal/close"(platform:"/", type: TrackType.Event){}
+        "/money_in/hub/web/modal/primary_button"(platform:"/", type: TrackType.Event){}
+        "/money_in/hub/web/modal/secondary_button"(platform:"/", type: TrackType.Event){}
 
         // -------------Open Finance - Money In-------------
         //Onboarding
