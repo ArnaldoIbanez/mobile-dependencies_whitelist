6a810d4d5bab511ab0af164869672526019c2664
MLGonzaloVanni <gonzalo.vanni@mercadolibre.com>
2021-12-22 17:31:34 -0300

    definitions + test

--- src/main/resources/catalog/melidata/benefits_per_challnege.groovy
+++ src/main/resources/catalog/melidata/benefits_per_challnege.groovy
@@ -9,32 +9,51 @@ tracks {
 
   initiative="1322"
 
-  def payment_status_object = objectSchemaDefinitions {
-    challenge_status(required: true, type: PropertyType.String, values:['ongoing','completed','expired'], description: "Challenge status")
-    payments(required: true, type: PropertyType.Numeric, description: "Payments amount")
-    challenge_stages(required: true, type: PropertyType.Numeric, description: "Stages amount")
-  }
-
   "/instore/challenge" (platform: "/", isAbstract: true) {
-    challenge_id(required: true, type: PropertyType.String, description: "Challenge id")
     session_id(required: true, type: PropertyType.String, description: "Session identifier")
     traffic_from(required: false, type: PropertyType.String, description: "Traffic identifier")
   }
+
+    // ----- HUB page -----
   "/instore/challenge/summary" (platform: "/", type: TrackType.View) {
-    payment_status(required: true, type:PropertyType.Map(payment_status_object), description: "state of payments")
+    suscribed(required: true, type: PropertyType.Numeric, description: "Suscribed amount")
+    not_suscribed(required: true, type: PropertyType.Numeric, description: "Not suscribed amount")
+    total_challenges(required: true, type: PropertyType.Numeric, description: "Total challenge amount")
+  }
+
+  "/instore/challenge/summary/how_to_use" (platform: "/", type: TrackType.View) {
+    suscribed(required: true, type: PropertyType.Numeric, description: "Suscribed amount")
+    not_suscribed(required: true, type: PropertyType.Numeric, description: "Not suscribed amount")
+    total_challenges(required: true, type: PropertyType.Numeric, description: "Total challenge amount")
+  }
+
+  // ----- Detail page -----
+  "/instore/challenge/detail" (platform: "/", type: TrackType.View) {
+    challenge_id(required: true, type: PropertyType.String, description: "Challenge id")
+    user_payments(required: true, type: PropertyType.Numeric, description: "User payments amount")
+    challenge_stages(required: true, type: PropertyType.Numeric, description: "Challenge stages amount")
+    payments_left(required: true, type: PropertyType.Numeric, description: "Payments left amount")
+    days_left(required: true, type: PropertyType.Numeric, description: "Days left amount")
+    optin_status(required: true, type: PropertyType.String, values:['ACCEPTED','PENDING'], description: "Optin status")
   }
   
-  "/instore/challenge/summary/find_stores" (platform: "/", type: TrackType.Event) {}
-  "/instore/challenge/summary/use_benefit" (platform: "/", type: TrackType.Event) {}
-  "/instore/challenge/summary/other_discounts" (platform: "/", type: TrackType.Event) {}
-  "/instore/challenge/summary/terms_and_conditions" (platform: "/", type: TrackType.Event) {}
+  "/instore/challenge/detail/find_stores" (platform: "/", type: TrackType.Event) {}
+  
+  "/instore/challenge/detail/join/success" (platform: "/", type: TrackType.Event) {}
+
+  "/instore/challenge/detail/join/fail" (platform: "/", type: TrackType.Event) {}
+
+  "/instore/challenge/detail/other_discounts" (platform: "/", type: TrackType.Event) {}
+
+  "/instore/challenge/detail/terms_and_conditions" (platform: "/", type: TrackType.Event) {}
 
   
   // ----- FTU pages -----
-  "/instore/challenge/first_time_user" (platform: "/", type: TrackType.View, isAbstract: true) {}
+  "/instore/challenge/onboarding" (platform: "/", type: TrackType.View) {}
 
-  "/instore/challenge/first_time_user/how_to_use" (platform: "/", type: TrackType.View) {}
+  "/instore/challenge/onboarding/start" (platform: "/", type: TrackType.Event) {}
   
+  // ----- Error pages -----
   "/instore/challenge/error" (platform: "/", type: TrackType.View) {}
  
 }
