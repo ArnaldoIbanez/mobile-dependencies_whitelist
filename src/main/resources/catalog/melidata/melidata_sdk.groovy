8c4eeb4d39b51466fda29052739d11e0741fb939
ftarcaya <facundo.tarcaya@mercadolibre.com>
2021-12-29 12:04:17 -0300

    Added fields to abstract platform

--- src/main/resources/catalog/melidata/melidata_sdk.groovy
+++ src/main/resources/catalog/melidata/melidata_sdk.groovy
@@ -10,22 +10,23 @@ tracks {
 
     "/melidata"(platform: "/", isAbstract: true) {}
 
-    "/melidata/statistics"(platform: "/mobile", isAbstract: true) {}
+    "/melidata/statistics"(platform: "/mobile", isAbstract: true) {
+        total_pending_tracks(type: PropertyType.Numeric, required: true, description: "Total number of pending tracks")
+        send_counter(type: PropertyType.Numeric, required: true, description: "Number of shippings")
+        database_size(type: PropertyType.Numeric, required: true, description: "Database size")
+        last_statistics_timestamp(type: PropertyType.String, required: false, description: "Last time statistics was sent")
+    }
 
     "/melidata/statistics"(platform: "/mobile/android", type: TrackType.Event) {
         errors_counter(type: PropertyType.Map)
         last_send_timestamp(type: PropertyType.String, required: false, description: "Last time a track was sent")
-        total_pending_tracks(type: PropertyType.Numeric, required: true, description: "Total number of pending tracks")
         stream_pending_tracks(type: PropertyType.Numeric, required: false, description: "Number of pending tracks for Private Stream")
         melidata_pending_tracks(type: PropertyType.Numeric, required: false, description: "Number of pending tracks for Melidata")
-        send_counter(type: PropertyType.Numeric, required: true, description: "Number of shippings")
-        database_size(type: PropertyType.Numeric, required: true, description: "Database size")
         tracks_counter(type: PropertyType.Numeric, required: true, description: "Number of sent tracks")
         stream_tracks_counter(type: PropertyType.Numeric, required: false, description: "Number of sent tracks for Private Stream")
         melidata_tracks_counter(type: PropertyType.Numeric, required: false, description: "Number of sent tracks for Melidata")
         average_ok_time(type: PropertyType.Numeric, required: true, description: "Average time without errors")
         average_error_time()
-        last_statistics_timestamp(type: PropertyType.String, required: false, description: "Last time statistics was sent")
         melidata_complete_dispatch_counter(type: PropertyType.Numeric, required: false, description: "Number of dispatches with amount of tracks same as limit for Melidata")
         stream_complete_dispatch_counter(type: PropertyType.Numeric, required: false, description: "Number of dispatches with amount of tracks same as limit for Private Stream")
     }
@@ -34,10 +35,6 @@ tracks {
         error_counter(type: PropertyType.Numeric, required: true, description: "Tracks numbers of errors")
         errors_array(type: PropertyType.ArrayList(PropertyType.String), required: false, description: "Track errors array")
         total_tracks_generated(type: PropertyType.Numeric, required: true, description: "Number of tracks generated")
-        total_pending_tracks(type: PropertyType.Numeric, required: true, description: "Number of pending tracks")
-        send_counter(type: PropertyType.Numeric, required: true, description: "Number of tracks sent")
-        database_size(type: PropertyType.Numeric, required: true, description: "Database size")
-        last_statistics_timestamp(type: PropertyType.String, required: false, description: "Last time statistics was sent")
     }
 
     "/melidata/statistics/experiments"(platform: "/mobile", type: TrackType.Control, parentPropertiesInherited: false) {
