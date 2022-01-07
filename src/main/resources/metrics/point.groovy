942d8816554fb8d390cdd5a44477f2221f98fa13
Fabrizio Fernandez <fabrizio.fernandez@mercadolibre.com>
2022-01-07 16:53:47 -0300

    Point congrats metric

--- src/main/resources/metrics/point.groovy
+++ src/main/resources/metrics/point.groovy
@@ -6,28 +6,16 @@ import static com.ml.melidata.metrics.parsers.dsl.MetricsDsl.metrics
 
 metrics {
 
-    "point_congrats"(description: "Arrival to congrats page from Point landings", bu: BuType.MercadoLibre, extent: ExtentType.Product) {
-        experiment(regex("mpos/.*"))
+    "point_congrats"(description: "Arrival to Point congrats page.",
+            bu: BuType.MercadoPago,
+            extent: ExtentType.Product) {
 
         countsOn {
             condition {
-                path("/checkout_off/v1/checkout_confirmed")
-                or(
-                    equals("event_data.collector_id", "228415881"),
-                    equals("event_data.collector_id", "179504451"),
-                    equals("event_data.collector_id", "193054976")
-                )
+                path("/point/buyingflow/congrats")
             }
         }
     }
 
-    "point_buy_intention"(description: "Point Landings buy intention", bu: BuType.MercadoLibre, extent: ExtentType.Product) {
-        experiment(regex("mpos/.*"))
-
-        countsOn {
-            condition {
-                path("/point/landings/buy")
-            }
-        }
-    }
 }
+
